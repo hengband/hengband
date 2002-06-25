43061bc8c98d34a6af0aa7155d9c7fd6681be83d
mogami <mogami@0568b783-4c39-0410-ac80-bf13821ea2a2>
2002-06-25 06:58:33 +0000

    英語版のモンスターの思い出中の速度の表示にスペース不足。修正。

--- src/monster1.c
+++ src/monster1.c
@@ -602,12 +602,12 @@ path_build(buf, 1024, ANGBAND_DIR_DATA, "r_info_j.raw");
 			else if (speed < 120) hook_c_roff(TERM_L_UMBER, "やや");
 			hook_c_roff(TERM_L_RED, "素早く");
 #else
-			if (speed > 139) hook_c_roff(TERM_RED, "incredibly ");
-			else if (speed > 134) hook_c_roff(TERM_ORANGE, "extremely ");
-			else if (speed > 129) hook_c_roff(TERM_ORANGE, "very ");
-			else if (speed > 124) hook_c_roff(TERM_UMBER, "fairly ");
-			else if (speed < 120) hook_c_roff(TERM_L_UMBER, "somewhat ");
-			hook_c_roff(TERM_L_RED, "quickly");
+			if (speed > 139) hook_c_roff(TERM_RED, " incredibly");
+			else if (speed > 134) hook_c_roff(TERM_ORANGE, " extremely");
+			else if (speed > 129) hook_c_roff(TERM_ORANGE, " very");
+			else if (speed > 124) hook_c_roff(TERM_UMBER, " fairly");
+			else if (speed < 120) hook_c_roff(TERM_L_UMBER, " somewhat");
+			hook_c_roff(TERM_L_RED, " quickly");
 #endif
 
 		}
@@ -620,11 +620,11 @@ path_build(buf, 1024, ANGBAND_DIR_DATA, "r_info_j.raw");
 			else if (speed > 104) hook_c_roff(TERM_GREEN, "やや");
 			hook_c_roff(TERM_L_BLUE, "ゆっくりと");
 #else
-			if (speed < 90) hook_c_roff(TERM_L_GREEN, "incredibly ");
-			else if (speed < 95) hook_c_roff(TERM_BLUE, "very ");
-			else if (speed < 100) hook_c_roff(TERM_BLUE, "fairly ");
-			else if (speed > 104) hook_c_roff(TERM_GREEN, "somewhat ");
-			hook_c_roff(TERM_L_BLUE, "slowly");
+			if (speed < 90) hook_c_roff(TERM_L_GREEN, " incredibly");
+			else if (speed < 95) hook_c_roff(TERM_BLUE, " very");
+			else if (speed < 100) hook_c_roff(TERM_BLUE, " fairly");
+			else if (speed > 104) hook_c_roff(TERM_GREEN, " somewhat");
+			hook_c_roff(TERM_L_BLUE, " slowly");
 #endif
 
 		}
