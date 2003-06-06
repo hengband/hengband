8d42a680bf6892da3765aec1f5cf8757089a5d81
nothere <nothere@0568b783-4c39-0410-ac80-bf13821ea2a2>
2003-06-06 11:14:38 +0000

    武器匠の武器比較時に "ふさわしくない" "乗馬中に向かない" といったメッ セージが出るバグを修正.

--- src/bldg.c
+++ src/bldg.c
@@ -3275,6 +3275,7 @@ static bool compare_weapons(void)
 	object_type *i_ptr;
 	cptr q, s;
 	int row = 2;
+	bool old_character_xtra = character_xtra;
 
 	screen_save();
 	/* Clear the screen */
@@ -3338,8 +3339,13 @@ s = "比べるものがありません。";
 	if (o1_ptr != i_ptr)
 		object_copy(i_ptr, o1_ptr);
 
+	/* Hack -- prevent "icky" message */
+	character_xtra = TRUE;
+
 	/* Get the new values */
 	calc_bonuses();
+
+	character_xtra = old_character_xtra;
 
 	/* List the new values */
 	list_weapon(o1_ptr, row, 2);
@@ -3351,8 +3357,13 @@ s = "比べるものがありません。";
 	else
 		object_copy(i_ptr, &orig_weapon);
 
+	/* Hack -- prevent "icky" message */
+	character_xtra = TRUE;
+
 	/* Get the new values */
 	calc_bonuses();
+
+	character_xtra = old_character_xtra;
 
 	/* List the new values */
 	list_weapon(o2_ptr, row, 40);
