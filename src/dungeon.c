ec146478e9421e5d1bc0e5fcf478b61a843e1967
nothere <nothere@0568b783-4c39-0410-ac80-bf13821ea2a2>
2003-07-10 16:35:59 +0000

    自分自身が光源ではない光源持ちのモンスターが目を覚ました際に, その モンスターが見えずに物陰にいる場合に光源がupdateされないバグを修正.

--- src/dungeon.c
+++ src/dungeon.c
@@ -1268,6 +1268,8 @@ static void process_monsters_counters(void)
 						/* Reset sleep counter */
 						m_ptr->csleep = 0;
 
+						if (r_ptr->flags7 & RF7_HAS_LD_MASK) p_ptr->update |= (PU_MON_LITE);
+
 						/* Notice the "waking up" */
 						if (m_ptr->ml)
 						{
@@ -1286,9 +1288,6 @@ static void process_monsters_counters(void)
 							/* Redraw the health bar */
 							if (p_ptr->health_who == m_idx) p_ptr->redraw |= (PR_HEALTH);
 							if (p_ptr->riding == m_idx) p_ptr->redraw |= (PR_UHEALTH);
-
-							if (r_ptr->flags7 & RF7_HAS_LD_MASK)
-								p_ptr->update |= (PU_MON_LITE);
 
 							/* Hack -- Count the wakings */
 							if (r_ptr->r_wake < MAX_UCHAR)
