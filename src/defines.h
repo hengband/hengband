51fa02d90658a94e3536d8c08394a77b75dc863f
mogami <mogami@0568b783-4c39-0410-ac80-bf13821ea2a2>
2003-08-26 14:23:35 +0000

    バージョン番号を1.7.0へ。

--- src/defines.h
+++ src/defines.h
@@ -56,8 +56,8 @@
  */
 #define FAKE_VERSION   0
 #define FAKE_VER_MAJOR 11
-#define FAKE_VER_MINOR 5
-#define FAKE_VER_PATCH 4
+#define FAKE_VER_MINOR 7
+#define FAKE_VER_PATCH 0
 
 
 /*
@@ -74,9 +74,9 @@
  * Upper compatibility is always guaranteed.
  */
 #define H_VER_MAJOR 1
-#define H_VER_MINOR 5
-#define H_VER_PATCH 4
-#define H_VER_EXTRA 1
+#define H_VER_MINOR 6
+#define H_VER_PATCH 0
+#define H_VER_EXTRA 0
 
 
 #define ANGBAND_2_8_1
