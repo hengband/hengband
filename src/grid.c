7381245d0f269f89d1fc596c4ff7b8e650c4595f
nothere <nothere@0568b783-4c39-0410-ac80-bf13821ea2a2>
2004-05-31 11:39:35 +0000

    is_outer_grid()と書くべき部分がis_inner_grid()と書かれており, ダン ジョン生成時に孤立した部屋が生成されていたバグを修正.

--- src/grid.c
+++ src/grid.c
@@ -629,7 +629,7 @@ void build_tunnel(int row1, int col1, int row2, int col2)
 		if (permanent_wall(&f_info[c_ptr->feat]))
 		{
 			/* Avoid the edge of vaults */
-			if (is_inner_grid(c_ptr)) continue;
+			if (is_outer_grid(c_ptr)) continue;
 		}
 
 		/* Avoid "solid" walls */
