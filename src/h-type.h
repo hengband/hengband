82565983d582ea28268eaa16a9d90527e488c318
habu <habu@0568b783-4c39-0410-ac80-bf13821ea2a2>
2012-11-18 13:43:54 +0000

    autoconfから--enable-c99を削除し、stdint.hの有無で判定するよう変更

--- src/h-type.h
+++ src/h-type.h
@@ -1,9 +1,5 @@
 /* File: h-type.h */
 
-#ifdef C99
-#include <stdint.h>
-#endif
-
 #ifndef INCLUDED_H_TYPE_H
 #define INCLUDED_H_TYPE_H
 
@@ -43,6 +39,9 @@
  */
 
 
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#endif
 
 /*** Special 4 letter names for some standard types ***/
 
@@ -117,7 +116,7 @@ typedef unsigned long huge;
 
 
 /* Signed/Unsigned 16 bit value */
-#ifdef C99
+#ifdef HAVE_STDINT_H
 typedef int16_t s16b;
 typedef uint16_t u16b;
 #else
@@ -126,7 +125,7 @@ typedef unsigned short u16b;
 #endif
 
 /* Signed/Unsigned 32 bit value */
-#ifdef C99
+#ifdef HAVE_STDINT_H
 typedef int32_t s32b;
 typedef uint32_t u32b;
 #else
