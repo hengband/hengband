48bb46a26f46b1b4b06391db405d82243b9fbff5
deskull <deskull@0568b783-4c39-0410-ac80-bf13821ea2a2>
2013-12-22 12:01:16 +0000

    Add Doxygen comment to artifact.c. Add EXTRACT_STATIC option to doxyfile.

--- src/artifact.c
+++ src/artifact.c
@@ -36,9 +36,10 @@ static int artifact_bias;
 
 
 /*!
- * 対象のオブジェクト構造体にランダムな能力維持を一つ付加する。 / Choose one random sustain
- *  @param o_ptr 対象のオブジェクト構造体
- *  @return なし
+ * @brief 対象のオブジェクト構造体にランダムな能力維持を一つ付加する。/ Choose one random sustain
+ * @details 重複の抑止はない。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_sustain(object_type *o_ptr)
 {
@@ -54,8 +55,11 @@ void one_sustain(object_type *o_ptr)
 }
 
 
-/*
- * Choose one random high resistance
+/*!
+ * @brief 対象のオブジェクト構造体にランダムな上位耐性を一つ付加する。/ Choose one random high resistance
+ * @details 重複の抑止はない。候補は毒、閃光、暗黒、破片、盲目、混乱、地獄、因果混乱、カオス、劣化、恐怖のいずれか。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_high_resistance(object_type *o_ptr)
 {
@@ -76,9 +80,13 @@ void one_high_resistance(object_type *o_ptr)
 	}
 }
 
-
-/*
- * Choose one random high resistance ( except poison and disenchantment )
+/*!
+ * @brief 対象のオブジェクト構造体に王者の指輪向けの上位耐性を一つ付加する。/ Choose one random high resistance
+ * @details 候補は閃光、暗黒、破片、盲目、混乱、地獄、因果混乱、カオス、恐怖であり
+ * 王者の指輪にあらかじめついている耐性をone_high_resistance()から除外したものである。
+ * ランダム付加そのものに重複の抑止はない。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_lordly_high_resistance(object_type *o_ptr)
 {
@@ -97,9 +105,11 @@ void one_lordly_high_resistance(object_type *o_ptr)
 	}
 }
 
-
-/*
- * Choose one random element resistance
+/*!
+ * @brief 対象のオブジェクト構造体に元素耐性を一つ付加する。/ Choose one random element resistance
+ * @details 候補は火炎、冷気、電撃、酸のいずれかであり、重複の抑止はない。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_ele_resistance(object_type *o_ptr)
 {
@@ -112,9 +122,11 @@ void one_ele_resistance(object_type *o_ptr)
 	}
 }
 
-
-/*
- * Choose one random element or poison resistance
+/*!
+ * @brief 対象のオブジェクト構造体にドラゴン装備向け元素耐性を一つ付加する。/ Choose one random element or poison resistance
+ * @details 候補は1/7の確率で毒、6/7の確率で火炎、冷気、電撃、酸のいずれか(one_ele_resistance()のコール)であり、重複の抑止はない。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_dragon_ele_resistance(object_type *o_ptr)
 {
@@ -128,9 +140,12 @@ void one_dragon_ele_resistance(object_type *o_ptr)
 	}
 }
 
-
-/*
- * Choose one lower rank esp
+/*!
+ * @brief 対象のオブジェクト構造体に弱いESPを一つ付加する。/ Choose one lower rank esp
+ * @details 候補は動物、アンデッド、悪魔、オーク、トロル、巨人、
+ * ドラゴン、人間、善良、ユニークESPのいずれかであり、重複の抑止はない。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_low_esp(object_type *o_ptr)
 {
@@ -150,9 +165,12 @@ void one_low_esp(object_type *o_ptr)
 }
 
 
-
-/*
- * Choose one random resistance
+/*!
+ * @brief 対象のオブジェクト構造体に耐性を一つ付加する。/ Choose one random resistance
+ * @details 1/3で元素耐性(one_ele_resistance())、2/3で上位耐性(one_high_resistance)
+ * をコールする。重複の抑止はない。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_resistance(object_type *o_ptr)
 {
@@ -167,15 +185,19 @@ void one_resistance(object_type *o_ptr)
 }
 
 
-/*
- * Choose one random ability
+/*!
+ * @brief 対象のオブジェクト構造体に能力を一つ付加する。/ Choose one random ability
+ * @details 候補は浮遊、永久光源+1、透明視、警告、遅消化、急回復、麻痺知らず、生命力維持のいずれか。
+ * 重複の抑止はない。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_ability(object_type *o_ptr)
 {
 	switch (randint0(10))
 	{
-	case 0: add_flag(o_ptr->art_flags, TR_LEVITATION);     break;
-	case 1: add_flag(o_ptr->art_flags, TR_LITE_1);        break;
+	case 0: add_flag(o_ptr->art_flags, TR_LEVITATION);  break;
+	case 1: add_flag(o_ptr->art_flags, TR_LITE_1);      break;
 	case 2: add_flag(o_ptr->art_flags, TR_SEE_INVIS);   break;
 	case 3: add_flag(o_ptr->art_flags, TR_WARNING);     break;
 	case 4: add_flag(o_ptr->art_flags, TR_SLOW_DIGEST); break;
@@ -189,8 +211,12 @@ void one_ability(object_type *o_ptr)
 	}
 }
 
-/*
- * Choose one random activation
+/*!
+ * @brief 対象のオブジェクト構造体に発動を一つ付加する。/ Choose one random activation
+ * @details 候補多数。ランダムアーティファクトのバイアスには一切依存せず、
+ * whileループによる構造で能力的に強力なものほど確率を落としている。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
  */
 void one_activation(object_type *o_ptr)
 {
@@ -306,6 +332,15 @@ void one_activation(object_type *o_ptr)
 	o_ptr->timeout = 0;
 }
 
+/*!
+ * @brief ランダムアーティファクト生成中のオブジェクト構造体を呪いのアーティファクトにする経過処理。/ generation process of cursed artifact.
+ * @details pval、AC、命中、ダメージが正の場合、符号反転の上1d4だけ悪化させ、重い呪い、呪いフラグを必ず付加。
+ * 祝福を無効。確率に応じて、永遠の呪い、太古の怨念、経験値吸収、弱い呪いの継続的付加、強い呪いの継続的付加、HP吸収の呪い、
+ * MP吸収の呪い、乱テレポート、反テレポート、反魔法をつける。
+ * @attention プレイヤーの職業依存処理あり。
+ * @param o_ptr 対象のオブジェクト構造体
+ * @return なし
+ */
 static void curse_artifact(object_type * o_ptr)
 {
 	if (o_ptr->pval > 0) o_ptr->pval = 0 - (o_ptr->pval + randint1(4));
