9335cd6bdf8bb9c6772267378bcf922c691d023a
deskull <deskull@0568b783-4c39-0410-ac80-bf13821ea2a2>
2014-01-12 05:51:38 +0000

    Fix Doxygen warning in object2.c.

--- src/object2.c
+++ src/object2.c
@@ -1764,7 +1764,7 @@ int object_similar_part(object_type *o_ptr, object_type *j_ptr)
 }
 
 /*
- * @brief 両オブジェクトがスロットに重ねることができるかどうかを返す。
+ * @brief 両オブジェクトをスロットに重ねることができるかどうかを返す。
  * Determine if an item can absorb a second item.
  * @param o_ptr 検証したいオブジェクトの構造体参照ポインタ1
  * @param j_ptr 検証したいオブジェクトの構造体参照ポインタ2
@@ -1790,9 +1790,12 @@ bool object_similar(object_type *o_ptr, object_type *j_ptr)
 }
 
 
-
 /*
+ * @brief 両オブジェクトをスロットに重ね合わせる。
  * Allow one item to "absorb" another, assuming they are similar
+ * @param o_ptr 重ね合わせ先のオブジェクトの構造体参照ポインタ
+ * @param j_ptr 重ね合わせ元のオブジェクトの構造体参照ポインタ
+ * @return なし
  */
 void object_absorb(object_type *o_ptr, object_type *j_ptr)
 {
@@ -1843,7 +1846,11 @@ void object_absorb(object_type *o_ptr, object_type *j_ptr)
 
 
 /*
+ * @brief tvalとsvalに対応するベースアイテムのIDを返す。
  * Find the index of the object_kind with the given tval and sval
+ * @param tval 検索したいベースアイテムのtval
+ * @param sval 検索したいベースアイテムのsval
+ * @return なし
  */
 s16b lookup_kind(int tval, int sval)
 {
@@ -1894,7 +1901,10 @@ s16b lookup_kind(int tval, int sval)
 
 
 /*
+ * @brief オブジェクトを初期化する
  * Wipe an object clean.
+ * @param o_ptr 初期化したいオブジェクトの構造体参照ポインタ
+ * @return なし
  */
 void object_wipe(object_type *o_ptr)
 {
@@ -1904,7 +1914,11 @@ void object_wipe(object_type *o_ptr)
 
 
 /*
- * Prepare an object based on an existing object
+ * @brief オブジェクトを複製する
+ * Wipe an object clean.
+ * @param o_ptr 複製元のオブジェクトの構造体参照ポインタ
+ * @param j_ptr 複製先のオブジェクトの構造体参照ポインタ
+ * @return なし
  */
 void object_copy(object_type *o_ptr, object_type *j_ptr)
 {
@@ -1914,7 +1928,11 @@ void object_copy(object_type *o_ptr, object_type *j_ptr)
 
 
 /*
+ * @brief オブジェクト構造体にベースアイテムを作成する
  * Prepare an object based on an object kind.
+ * @param o_ptr 代入したいオブジェクトの構造体参照ポインタ
+ * @param k_idx 新たに作成したいベースアイテム情報のID
+ * @return なし
  */
 void object_prep(object_type *o_ptr, int k_idx)
 {
@@ -1966,43 +1984,47 @@ void object_prep(object_type *o_ptr, int k_idx)
 
 
 /*
+ * @brief 上質以上のオブジェクトに与えるための各種ボーナスを正規乱数も加えて算出する。
  * Help determine an "enchantment bonus" for an object.
- *
- * To avoid floating point but still provide a smooth distribution of bonuses,
- * we simply round the results of division in such a way as to "average" the
- * correct floating point value.
- *
- * This function has been changed.  It uses "randnor()" to choose values from
- * a normal distribution, whose mean moves from zero towards the max as the
- * level increases, and whose standard deviation is equal to 1/4 of the max,
- * and whose values are forced to lie between zero and the max, inclusive.
- *
- * Since the "level" rarely passes 100 before Morgoth is dead, it is very
- * rare to get the "full" enchantment on an object, even a deep levels.
- *
- * It is always possible (albeit unlikely) to get the "full" enchantment.
- *
- * A sample distribution of values from "m_bonus(10, N)" is shown below:
- *
- *   N       0     1     2     3     4     5     6     7     8     9    10
- * ---    ----  ----  ----  ----  ----  ----  ----  ----  ----  ----  ----
- *   0   66.37 13.01  9.73  5.47  2.89  1.31  0.72  0.26  0.12  0.09  0.03
- *   8   46.85 24.66 12.13  8.13  4.20  2.30  1.05  0.36  0.19  0.08  0.05
- *  16   30.12 27.62 18.52 10.52  6.34  3.52  1.95  0.90  0.31  0.15  0.05
- *  24   22.44 15.62 30.14 12.92  8.55  5.30  2.39  1.63  0.62  0.28  0.11
- *  32   16.23 11.43 23.01 22.31 11.19  7.18  4.46  2.13  1.20  0.45  0.41
- *  40   10.76  8.91 12.80 29.51 16.00  9.69  5.90  3.43  1.47  0.88  0.65
- *  48    7.28  6.81 10.51 18.27 27.57 11.76  7.85  4.99  2.80  1.22  0.94
- *  56    4.41  4.73  8.52 11.96 24.94 19.78 11.06  7.18  3.68  1.96  1.78
- *  64    2.81  3.07  5.65  9.17 13.01 31.57 13.70  9.30  6.04  3.04  2.64
- *  72    1.87  1.99  3.68  7.15 10.56 20.24 25.78 12.17  7.52  4.42  4.62
- *  80    1.02  1.23  2.78  4.75  8.37 12.04 27.61 18.07 10.28  6.52  7.33
- *  88    0.70  0.57  1.56  3.12  6.34 10.06 15.76 30.46 12.58  8.47 10.38
- *  96    0.27  0.60  1.25  2.28  4.30  7.60 10.77 22.52 22.51 11.37 16.53
- * 104    0.22  0.42  0.77  1.36  2.62  5.33  8.93 13.05 29.54 15.23 22.53
- * 112    0.15  0.20  0.56  0.87  2.00  3.83  6.86 10.06 17.89 27.31 30.27
- * 120    0.03  0.11  0.31  0.46  1.31  2.48  4.60  7.78 11.67 25.53 45.72
- * 128    0.02  0.01  0.13  0.33  0.83  1.41  3.24  6.17  9.57 14.22 64.07
+ * @param max ボーナス値の限度
+ * @param level ボーナス値に加味する基準生成階
+ * @return 算出されたボーナス値
+ * @details
+ * To avoid floating point but still provide a smooth distribution of bonuses,\n
+ * we simply round the results of division in such a way as to "average" the\n
+ * correct floating point value.\n
+ *\n
+ * This function has been changed.  It uses "randnor()" to choose values from\n
+ * a normal distribution, whose mean moves from zero towards the max as the\n
+ * level increases, and whose standard deviation is equal to 1/4 of the max,\n
+ * and whose values are forced to lie between zero and the max, inclusive.\n
+ *\n
+ * Since the "level" rarely passes 100 before Morgoth is dead, it is very\n
+ * rare to get the "full" enchantment on an object, even a deep levels.\n
+ *\n
+ * It is always possible (albeit unlikely) to get the "full" enchantment.\n
+ *\n
+ * A sample distribution of values from "m_bonus(10, N)" is shown below:\n
+ *\n
+ *   N       0     1     2     3     4     5     6     7     8     9    10\n
+ * ---    ----  ----  ----  ----  ----  ----  ----  ----  ----  ----  ----\n
+ *   0   66.37 13.01  9.73  5.47  2.89  1.31  0.72  0.26  0.12  0.09  0.03\n
+ *   8   46.85 24.66 12.13  8.13  4.20  2.30  1.05  0.36  0.19  0.08  0.05\n
+ *  16   30.12 27.62 18.52 10.52  6.34  3.52  1.95  0.90  0.31  0.15  0.05\n
+ *  24   22.44 15.62 30.14 12.92  8.55  5.30  2.39  1.63  0.62  0.28  0.11\n
+ *  32   16.23 11.43 23.01 22.31 11.19  7.18  4.46  2.13  1.20  0.45  0.41\n
+ *  40   10.76  8.91 12.80 29.51 16.00  9.69  5.90  3.43  1.47  0.88  0.65\n
+ *  48    7.28  6.81 10.51 18.27 27.57 11.76  7.85  4.99  2.80  1.22  0.94\n
+ *  56    4.41  4.73  8.52 11.96 24.94 19.78 11.06  7.18  3.68  1.96  1.78\n
+ *  64    2.81  3.07  5.65  9.17 13.01 31.57 13.70  9.30  6.04  3.04  2.64\n
+ *  72    1.87  1.99  3.68  7.15 10.56 20.24 25.78 12.17  7.52  4.42  4.62\n
+ *  80    1.02  1.23  2.78  4.75  8.37 12.04 27.61 18.07 10.28  6.52  7.33\n
+ *  88    0.70  0.57  1.56  3.12  6.34 10.06 15.76 30.46 12.58  8.47 10.38\n
+ *  96    0.27  0.60  1.25  2.28  4.30  7.60 10.77 22.52 22.51 11.37 16.53\n
+ * 104    0.22  0.42  0.77  1.36  2.62  5.33  8.93 13.05 29.54 15.23 22.53\n
+ * 112    0.15  0.20  0.56  0.87  2.00  3.83  6.86 10.06 17.89 27.31 30.27\n
+ * 120    0.03  0.11  0.31  0.46  1.31  2.48  4.60  7.78 11.67 25.53 45.72\n
+ * 128    0.02  0.01  0.13  0.33  0.83  1.41  3.24  6.17  9.57 14.22 64.07\n
  */
 s16b m_bonus(int max, int level)
 {
