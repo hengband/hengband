d9c488c764cd81ef341bd07d374fb4c636cc8769
Deskull <desull@users.sourceforge.jp>
2014-08-08 23:10:26 +0900

    Add Doxygen comments to readdib.h.

--- src/readdib.h
+++ src/readdib.h
@@ -1,13 +1,16 @@
-﻿/* File: readdib.h */
-
-/*
- * This file has been modified for use with "Angband 2.8.2"
- *
+﻿/*!
+ * @file readdib.h
+ * @brief ビットマップファイル読み取り処理のヘッダファイル
+ * This package provides a routine to read a DIB file and set up the device dependent version of the image.
+ * @date 2014/08/08
+ * @author
  * Copyright 1991 Microsoft Corporation. All rights reserved.
+ * @details
+ * This file has been modified for use with "Angband 2.8.2"
  */
 
-/*
- * Information about a bitmap
+/*!
+ * @struct ビットマップファイル情報構造体 / Information about a bitmap
  */
 typedef struct {
 	HANDLE hDIB;
