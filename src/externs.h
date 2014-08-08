7cf7dc8e976c58f0951d6a4d85b0cc540ee7738b
Deskull <desull@users.sourceforge.jp>
2014-08-08 23:16:11 +0900

    Add Doxygen comments to externs.h.

--- src/externs.h
+++ src/externs.h
@@ -1,16 +1,14 @@
-﻿/* File: externs.h */
-
-/*
+﻿/*!
+ * @file externs.h
+ * @brief Angband(変愚蛮怒)基本関数、グローバル変数ヘッダファイル / 
+ * extern declarations (variables and functions)
+ * @date 2014/08/08
+ * @author
  * Copyright (c) 1997 Ben Harrison
- *
+ * @details
  * This software may be copied and distributed for educational, research,
  * and not for profit purposes provided that this copyright and statement
  * are included in all such copies.
- */
-
-/* Purpose: extern declarations (variables and functions) */
-
-/*
  * Note that some files have their own header files
  * (z-virt.h, z-util.h, z-form.h, term.h, random.h)
  */
@@ -19,7 +17,6 @@
 /*
  * Automatically generated "variable" declarations
  */
-
 extern int max_macrotrigger;
 extern cptr macro_template;
 extern cptr macro_modifier_chr;
