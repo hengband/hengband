ec8d67c445fbbe6f290ef693fb0eb2c0118a5aaa
Deskull <desull@users.sourceforge.jp>
2014-08-16 20:18:01 +0900

    Add Doxygen comments to old EGO_XTRA definition in defines.h.

--- src/defines.h
+++ src/defines.h
@@ -3140,17 +3140,12 @@
 #define FEEL_MAX               9 /*!< 簡易鑑定の種別数 */
 
 /*
- * Hack -- special "xtra" object powers
+ * 変愚ver1.5.0以前に使われていたアイテムの追加特性フラグ / Hack -- special "xtra" object powers
  */
+#define EGO_XTRA_SUSTAIN        1 /*!< 旧版アイテムフラグ(非推奨): 追加維持能力 / Sustain one stat */
+#define EGO_XTRA_POWER          2 /*!< 旧版アイテムフラグ(非推奨): 追加上級耐性 / High resist */
+#define EGO_XTRA_ABILITY        3 /*!< 旧版アイテムフラグ(非推奨): 追加能力 / Special ability */
 
-/* Sustain one stat */
-#define EGO_XTRA_SUSTAIN        1
-
-/* High resist */
-#define EGO_XTRA_POWER          2
-
-/* Special ability */
-#define EGO_XTRA_ABILITY        3
 
 /*** Object flag values ***/
 
