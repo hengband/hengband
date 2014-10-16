c8376194689da92f12b5d71e0d053a8d7a57c6d7
Deskull <desull@users.sourceforge.jp>
2014-10-16 21:36:32 +0900

    RBE_* によるモンスターの攻撃効果に定義に Doxygen コメントを追加。 / Add Doxygen comments to RBE_* for effects of monster attack.

--- src/defines.h
+++ src/defines.h
@@ -3486,44 +3486,44 @@
 #define RBM_SHOOT       25 /*!< モンスターの攻撃種別:射撃(非打撃) */
 
 
-/*
- * New monster blow effects
+/*!
+ * @note モンスターの攻撃効果 / New monster blow effects
  */
-#define RBE_HURT         1
-#define RBE_POISON       2
-#define RBE_UN_BONUS     3
-#define RBE_UN_POWER     4
-#define RBE_EAT_GOLD     5
-#define RBE_EAT_ITEM     6
-#define RBE_EAT_FOOD     7
-#define RBE_EAT_LITE     8
-#define RBE_ACID         9
-#define RBE_ELEC        10
-#define RBE_FIRE        11
-#define RBE_COLD        12
-#define RBE_BLIND       13
-#define RBE_CONFUSE     14
-#define RBE_TERRIFY     15
-#define RBE_PARALYZE    16
-#define RBE_LOSE_STR    17
-#define RBE_LOSE_INT    18
-#define RBE_LOSE_WIS    19
-#define RBE_LOSE_DEX    20
-#define RBE_LOSE_CON    21
-#define RBE_LOSE_CHR    22
-#define RBE_LOSE_ALL    23
-#define RBE_SHATTER     24
-#define RBE_EXP_10      25
-#define RBE_EXP_20      26
-#define RBE_EXP_40      27
-#define RBE_EXP_80      28
-#define RBE_DISEASE     29
-#define RBE_TIME        30
-#define RBE_DR_LIFE     31
-#define RBE_DR_MANA     32
-#define RBE_SUPERHURT   33
-#define RBE_INERTIA     34
-#define RBE_STUN        35
+#define RBE_HURT         1 /*!< モンスターの攻撃効果: 攻撃する*/
+#define RBE_POISON       2 /*!< モンスターの攻撃効果: 毒をくらわす*/
+#define RBE_UN_BONUS     3 /*!< モンスターの攻撃効果: 劣化させる*/
+#define RBE_UN_POWER     4 /*!< モンスターの攻撃効果: 充填魔力を吸収する*/
+#define RBE_EAT_GOLD     5 /*!< モンスターの攻撃効果: 金を盗む*/
+#define RBE_EAT_ITEM     6 /*!< モンスターの攻撃効果: アイテムを盗む*/
+#define RBE_EAT_FOOD     7 /*!< モンスターの攻撃効果: あなたの食糧を食べる*/
+#define RBE_EAT_LITE     8 /*!< モンスターの攻撃効果: 明かりを吸収する*/
+#define RBE_ACID         9 /*!< モンスターの攻撃効果: 酸を飛ばす*/
+#define RBE_ELEC        10 /*!< モンスターの攻撃効果: 感電させる*/
+#define RBE_FIRE        11 /*!< モンスターの攻撃効果: 燃やす*/
+#define RBE_COLD        12 /*!< モンスターの攻撃効果: 凍らせる*/
+#define RBE_BLIND       13 /*!< モンスターの攻撃効果: 盲目にする*/
+#define RBE_CONFUSE     14 /*!< モンスターの攻撃効果: 混乱させる*/
+#define RBE_TERRIFY     15 /*!< モンスターの攻撃効果: 恐怖させる*/
+#define RBE_PARALYZE    16 /*!< モンスターの攻撃効果: 麻痺させる*/
+#define RBE_LOSE_STR    17 /*!< モンスターの攻撃効果: 腕力を減少させる*/
+#define RBE_LOSE_INT    18 /*!< モンスターの攻撃効果: 知能を減少させる*/
+#define RBE_LOSE_WIS    19 /*!< モンスターの攻撃効果: 賢さを減少させる*/
+#define RBE_LOSE_DEX    20 /*!< モンスターの攻撃効果: 器用さを減少させる*/
+#define RBE_LOSE_CON    21 /*!< モンスターの攻撃効果: 耐久力を減少させる*/
+#define RBE_LOSE_CHR    22 /*!< モンスターの攻撃効果: 魅力を減少させる*/
+#define RBE_LOSE_ALL    23 /*!< モンスターの攻撃効果: 全ステータスを減少させる*/
+#define RBE_SHATTER     24 /*!< モンスターの攻撃効果: 粉砕する*/
+#define RBE_EXP_10      25 /*!< モンスターの攻撃効果: 経験値を減少(10d6+)させる*/
+#define RBE_EXP_20      26 /*!< モンスターの攻撃効果: 経験値を減少(20d6+)させる*/
+#define RBE_EXP_40      27 /*!< モンスターの攻撃効果: 経験値を減少(40d6+)させる*/
+#define RBE_EXP_80      28 /*!< モンスターの攻撃効果: 経験値を減少(80d6+)させる*/
+#define RBE_DISEASE     29 /*!< モンスターの攻撃効果: 病気にする*/
+#define RBE_TIME        30 /*!< モンスターの攻撃効果: 時間を逆戻りさせる*/
+#define RBE_DR_LIFE     31 /*!< モンスターの攻撃効果: 生命力を吸収する*/
+#define RBE_DR_MANA     32 /*!< モンスターの攻撃効果: 魔力を奪う*/
+#define RBE_SUPERHURT   33 /*!< モンスターの攻撃効果: 強力に攻撃する*/
+#define RBE_INERTIA     34 /*!< モンスターの攻撃効果: 減速させる*/
+#define RBE_STUN        35 /*!< モンスターの攻撃効果: 朦朧とさせる*/
 
 
 /*** Monster flag values (hard-coded) ***/
