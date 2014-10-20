8fd77939306227c6be8735b9b287f5ed2174283d
Deskull <desull@users.sourceforge.jp>
2014-10-20 22:12:34 +0900

    モンスター特性 RF3_* に Doxygen コメントを追加。 / Add Doxygen comments to RF3_*, monster traits.

--- src/defines.h
+++ src/defines.h
@@ -3604,38 +3604,38 @@
 /*
  * New monster race bit flags
  */
-#define RF3_ORC             0x00000001  /* Orc */
-#define RF3_TROLL           0x00000002  /* Troll */
-#define RF3_GIANT           0x00000004  /* Giant */
-#define RF3_DRAGON          0x00000008  /* Dragon */
-#define RF3_DEMON           0x00000010  /* Demon */
-#define RF3_UNDEAD          0x00000020  /* Undead */
-#define RF3_EVIL            0x00000040  /* Evil */
-#define RF3_ANIMAL          0x00000080  /* Animal */
-#define RF3_AMBERITE        0x00000100  /* TY: Amberite */
-#define RF3_GOOD            0x00000200  /* Good */
-#define RF3_AURA_COLD       0x00000400  /* Freezes in melee */
-#define RF3_NONLIVING       0x00000800  /* TY: Non-Living (?) */
-#define RF3_HURT_LITE       0x00001000  /* Hurt by lite */
-#define RF3_HURT_ROCK       0x00002000  /* Hurt by rock remover */
-#define RF3_HURT_FIRE       0x00004000  /* Hurt badly by fire */
-#define RF3_HURT_COLD       0x00008000  /* Hurt badly by cold */
-#define RF3_XXX16           0x00010000
-#define RF3_XXX17           0x00020000
-#define RF3_XXX18           0x00040000
-#define RF3_XXX19           0x00080000
-#define RF3_XXX20           0x00100000
-#define RF3_XXX21           0x00200000
-#define RF3_XXX22           0x00400000
-#define RF3_XXX23           0x00800000
-#define RF3_XXX24           0x01000000
-#define RF3_XXX25           0x02000000
-#define RF3_XXX26           0x04000000
-#define RF3_XXX27           0x08000000
-#define RF3_NO_FEAR         0x10000000  /* Cannot be scared */
-#define RF3_NO_STUN         0x20000000  /* Cannot be stunned */
-#define RF3_NO_CONF         0x40000000  /* Cannot be confused and resist confusion */
-#define RF3_NO_SLEEP        0x80000000  /* Cannot be slept */
+#define RF3_ORC             0x00000001  /*!< モンスター特性: オーク / Orc */
+#define RF3_TROLL           0x00000002  /*!< モンスター特性: トロル / Troll */
+#define RF3_GIANT           0x00000004  /*!< モンスター特性: 巨人 / Giant */
+#define RF3_DRAGON          0x00000008  /*!< モンスター特性: ドラゴン / Dragon */
+#define RF3_DEMON           0x00000010  /*!< モンスター特性: 悪魔 / Demon */
+#define RF3_UNDEAD          0x00000020  /*!< モンスター特性: アンデッド / Undead */
+#define RF3_EVIL            0x00000040  /*!< モンスター特性: 邪悪 / Evil */
+#define RF3_ANIMAL          0x00000080  /*!< モンスター特性: 動物 / Animal */
+#define RF3_AMBERITE        0x00000100  /*!< モンスター特性: アンバーの血族 / TY: Amberite */
+#define RF3_GOOD            0x00000200  /*!< モンスター特性: 善良 / Good */
+#define RF3_AURA_COLD       0x00000400  /*!< モンスター特性: 冷気オーラ / Freezes in melee */
+#define RF3_NONLIVING       0x00000800  /*!< モンスター特性: 無生物 / TY: Non-Living (?) */
+#define RF3_HURT_LITE       0x00001000  /*!< モンスター特性: 通常の光(GF_WEAK_LITE)でダメージを受ける / Hurt by lite */
+#define RF3_HURT_ROCK       0x00002000  /*!< モンスター特性: 岩石溶解(GF_KILL_WALL)でダメージを受ける / Hurt by rock remover */
+#define RF3_HURT_FIRE       0x00004000  /*!< モンスター特性: 火炎が弱点 / Hurt badly by fire */
+#define RF3_HURT_COLD       0x00008000  /*!< モンスター特性: 冷気が弱点 / Hurt badly by cold */
+#define RF3_XXX16           0x00010000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX17           0x00020000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX18           0x00040000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX19           0x00080000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX20           0x00100000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX21           0x00200000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX22           0x00400000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX23           0x00800000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX24           0x01000000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX25           0x02000000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX26           0x04000000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_XXX27           0x08000000  /*!< モンスター特性: 未使用 / XXX */
+#define RF3_NO_FEAR         0x10000000  /*!< モンスター特性: 恐怖しない / Cannot be scared */
+#define RF3_NO_STUN         0x20000000  /*!< モンスター特性: 朦朧としない / Cannot be stunned */
+#define RF3_NO_CONF         0x40000000  /*!< モンスター特性: 混乱しない / Cannot be confused and resist confusion */
+#define RF3_NO_SLEEP        0x80000000  /*!< モンスター特性: 眠らない / Cannot be slept */
 
 /*
  * New monster race bit flags
