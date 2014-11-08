87e94f20aa344be4a593f596a02d1ddfc29fd36e
Deskull <desull@users.sourceforge.jp>
2014-11-09 07:18:46 +0900

    KAMAE_*, KATA_*, ACTION_* フラグにDoxygenコメントを追加。 / Add Doxygen comments to KAMAE_*, KATA_* and ACTION_* definitions.

--- src/defines.h
+++ src/defines.h
@@ -2870,23 +2870,23 @@
 #define NINJA_KAWARIMI  0x00002000 /*!< プレイヤーのステータス:変わり身 */
 #define NINJA_S_STEALTH 0x00004000 /*!< プレイヤーのステータス:超隠密 */
 
-#define MAX_KAMAE 4
-#define KAMAE_MASK (KAMAE_GENBU | KAMAE_BYAKKO | KAMAE_SEIRYU | KAMAE_SUZAKU)
+#define MAX_KAMAE 4 /*!< 修行僧の構え最大数 */
+#define KAMAE_MASK (KAMAE_GENBU | KAMAE_BYAKKO | KAMAE_SEIRYU | KAMAE_SUZAKU) /*!< 修行僧の構えビット配列 */
 
-#define MAX_KATA 4
-#define KATA_MASK (KATA_IAI | KATA_FUUJIN | KATA_KOUKIJIN | KATA_MUSOU)
+#define MAX_KATA 4 /*!< 修行僧の型最大数 */
+#define KATA_MASK (KATA_IAI | KATA_FUUJIN | KATA_KOUKIJIN | KATA_MUSOU) /*!< 修行僧の型ビット配列 */
 
 
-#define ACTION_NONE     0
-#define ACTION_SEARCH   1
-#define ACTION_REST     2
-#define ACTION_LEARN    3
-#define ACTION_FISH     4
-#define ACTION_KAMAE    5
-#define ACTION_KATA     6
-#define ACTION_SING     7
-#define ACTION_HAYAGAKE 8
-#define ACTION_SPELL    9
+#define ACTION_NONE     0 /*!< 持続行動: なし */
+#define ACTION_SEARCH   1 /*!< 持続行動: 探索 */
+#define ACTION_REST     2 /*!< 持続行動: 休憩 */
+#define ACTION_LEARN    3 /*!< 持続行動: 青魔法ラーニング */
+#define ACTION_FISH     4 /*!< 持続行動: 釣り */
+#define ACTION_KAMAE    5 /*!< 持続行動: 修行僧の構え */
+#define ACTION_KATA     6 /*!< 持続行動: 剣術家の型 */
+#define ACTION_SING     7 /*!< 持続行動: 歌 */
+#define ACTION_HAYAGAKE 8 /*!< 持続行動: 早駆け */
+#define ACTION_SPELL    9 /*!< 持続行動: 呪術 */
 
 
 /* Empty hand status */
