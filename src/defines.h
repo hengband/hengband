d3968d043b44a3b638846c6140ac4c360197f279
Deskull <desull@users.sourceforge.jp>
2015-04-03 23:59:40 +0900

    モンスターとアイテムの深層生成に関する定数にDoxygenコメントを追加。 / Add Doxygen comments to constant for deeper generation of monsters and items.

--- src/defines.h
+++ src/defines.h
@@ -571,21 +571,25 @@
 #define CAN_TWO_HANDS_WIELDING() (!p_ptr->riding || (p_ptr->pet_extra_flags & PF_RYOUTE))
 
 
-/*
- * There is a 1/20 (5%) chance of inflating the requested object_level
+/*!
+ * @brief ベースアイテム生成階層が加算される確率
+ * @details
+ * There is a 1/10 (10%) chance of inflating the requested object_level
  * during the creation of an object (see "get_obj_num()" in "object.c").
  * Lower values yield better objects more often.
  */
 #define GREAT_OBJ       10
 
-/*
+/*!
+ * @brief 深層モンスターが生成される基本確率(1/x)
+ * @details
  * There is a 1/25 (4%) chance of inflating the requested monster_level
  * during the creation of a monsters (see "get_mon_num()" in "monster.c").
  * Lower values yield harder monsters more often.
  */
 #define NASTY_MON_BASE     25
-#define NASTY_MON_MAX      3
-#define NASTY_MON_PLUS_MAX 25
+#define NASTY_MON_MAX      3	/*! 深層モンスターが1フロアに生成される最大数  */
+#define NASTY_MON_PLUS_MAX 25	/*! 深層モンスターの階層加算最大量 */
 
 #define PENETRATE_INVULNERABILITY 13 /*! 無敵化が破られる確率(1/x) / 1/x chance of hurting even if invulnerable! */
 
@@ -607,7 +611,6 @@
 #define MIN_M_ALLOC_TD          4 /*!< 街(昼間)の最低住人配置数 / The town starts out with 4 residents during the day */
 #define MIN_M_ALLOC_TN          8 /*!< 街(夜間)の最低住人配置数 / The town starts out with 8 residents during the night */
 
-
 /*!
  * @brief モンスター増殖の最大数
  * @details
@@ -617,7 +620,6 @@
  * reproducing monsters.  Messy, but necessary.
  */
 #define MAX_REPRO       100
-
 
 /*
  * Player constants
