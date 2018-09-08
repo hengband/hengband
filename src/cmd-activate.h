ff155023a46a7c5a7223ad6dd0ab97e55d1bc72e
Deskull <deskull@users.sourceforge.jp>
2018-09-08 21:38:13 +0900

    [Refactor] #37353　artifact/cmd-activate間整理。 / Refactor between artifact and cmd-activate.

--- src/cmd-activate.h
+++ src/cmd-activate.h
@@ -1,4 +1,6 @@
 extern void do_cmd_activate(void);
 extern void do_cmd_activate_aux(int item);
+extern bool activate_artifact(object_type * o_ptr);
+extern void get_bloody_moon_flags(object_type *o_ptr);
 
 
--- src/externs.h
+++ src/externs.h
@@ -1518,9 +1518,7 @@ extern void one_ability(object_type *o_ptr);
 extern void one_activation(object_type *o_ptr);
 extern bool create_artifact(object_type *o_ptr, bool a_scroll);
 extern int activation_index(object_type *o_ptr);
-extern bool activate_random_artifact(object_type * o_ptr);
 extern const activation_type* find_activation_info(object_type *o_ptr);
-extern void get_bloody_moon_flags(object_type *o_ptr);
 extern void random_artifact_resistance(object_type * o_ptr, artifact_type *a_ptr);
 extern bool create_named_art(int a_idx, int y, int x);
 
