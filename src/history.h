c8ae0d62233f216a0c986de009414cda96b85c65
Deskull <deskull@users.sourceforge.jp>
2018-09-23 20:07:38 +0900

    [Refactor] #37353　プレイヤーの初期生い立ち情報を birth.c から history.c/h へ分離。 Separate array of player's default history from birth.c to history.c/h.

---
+++ src/history.h
@@ -0,0 +1,21 @@
+
+/*!
+* 生い立ちメッセージテーブル / Forward declare
+*/
+typedef struct hist_type hist_type;
+
+/*!
+* 生い立ちメッセージテーブルの構造体定義 / Player background information
+*/
+struct hist_type
+{
+	cptr info;			    /*!> メッセージ本文 / Textual History */
+
+	byte roll;			    /*!> 確率の重み / Frequency of this entry */
+	byte chart;			    /*!> 生い立ちメッセージの流れを示すチャートID / Chart index */
+	byte next;			    /*!> 次のチャートID */
+	byte bonus;			    /*!> メッセージに伴う社会的地位の変化量(50が基準値) / Social Class Bonus + 50 */
+};
+
+struct hist_type;
+extern hist_type bg[];
