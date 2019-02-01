816f1b99f6acae5acb22cc0728f113f0a643d456
Deskull <deskull@users.sourceforge.jp>
2019-02-02 00:13:14 +0900

    [Refactor] #37353 spells1.c から spell-diceroll.c を分離。 / Separate spell-diceroll.c from spells1.c.

---
+++ src/spells-diceroll.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+extern bool_hack common_saving_throw_control(player_type *player_ptr, HIT_POINT pow, monster_type *m_ptr);
+extern bool_hack common_saving_throw_charm(player_type *player_ptr, HIT_POINT pow, monster_type *m_ptr);
+
