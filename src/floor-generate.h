d8fd24f80279620d109cbdd7e2bd2838f731fa15
deskull <deskull@users.sourceforge.jp>
2019-03-18 13:37:44 +0900

    [Fix] #37353 コミットし忘れ修正。 / Fix forgetting commit.

---
+++ src/floor-generate.h
@@ -0,0 +1,3 @@
+#pragma once
+extern bool build_tunnel(POSITION row1, POSITION col1, POSITION row2, POSITION col2);
+extern bool build_tunnel2(POSITION x1, POSITION y1, POSITION x2, POSITION y2, int type, int cutoff);
