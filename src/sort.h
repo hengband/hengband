3fc088dd6ecd8976a29578d88c2bf91e9f0515be
deskull <deskull@users.sourceforge.jp>
2019-03-19 22:40:47 +0900

    [Refactor] #37353 ang_sort_*() in xtra2.c to sort.c.

--- src/sort.h
+++ src/sort.h
@@ -1,3 +1,6 @@
 ﻿extern void ang_sort_aux(vptr u, vptr v, int p, int q);
 extern void ang_sort(vptr u, vptr v, int n);
 
+extern bool ang_sort_comp_distance(vptr u, vptr v, int a, int b);
+extern bool ang_sort_comp_importance(vptr u, vptr v, int a, int b);
+extern void ang_sort_swap_distance(vptr u, vptr v, int a, int b);
