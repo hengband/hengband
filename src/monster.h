bb883326a32e68042af7c32ff2dac58870c2ba95
deskull <deskull@users.sourceforge.jp>
2019-04-02 23:42:36 +0900

    [Refactor] #37353 未使用関数宣言 create_name() を削除。

--- src/monster.h
+++ src/monster.h
@@ -41,7 +41,6 @@ extern void roff_top(MONRACE_IDX r_idx);
 extern void screen_roff(MONRACE_IDX r_idx, BIT_FLAGS mode);
 extern void display_roff(MONRACE_IDX r_idx);
 extern void output_monster_spoiler(MONRACE_IDX r_idx, void(*roff_func)(TERM_COLOR attr, concptr str));
-extern void create_name(int type, char *name);
 extern concptr extract_note_dies(MONRACE_IDX r_idx);
 extern void monster_death(MONSTER_IDX m_idx, bool drop_item);
 extern monsterrace_hook_type get_monster_hook(void);
