521f61175251e616d60cffecc67eb20f4a25d597
deskull <deskull@users.sourceforge.jp>
2019-04-11 23:47:35 +0900

    [Refactor] #37353 徳定義を　avater.h に移動。

--- src/avatar.h
+++ src/avatar.h
@@ -1,4 +1,25 @@
 ﻿/* avatar.c */
+#define MAX_VIRTUE 18 /*!< 徳定義の最大数 */
+
+#define V_COMPASSION    1
+#define V_HONOUR	    2
+#define V_JUSTICE	    3
+#define V_SACRIFICE	    4
+#define V_KNOWLEDGE	    5
+#define V_FAITH 	    6
+#define V_ENLIGHTEN	    7
+#define V_ENCHANT	    8
+#define V_CHANCE	    9
+#define V_NATURE	   10
+#define V_HARMONY	   11
+#define V_VITALITY	   12
+#define V_UNLIFE	   13
+#define V_PATIENCE	   14
+#define V_TEMPERANCE	   15
+#define V_DILIGENCE	   16
+#define V_VALOUR	   17
+#define V_INDIVIDUALISM    18
+
 extern bool compare_virtue(int type, int num, int tekitou);
 extern int virtue_number(int type);
 extern concptr virtue[MAX_VIRTUE];
--- src/defines.h
+++ src/defines.h
@@ -3420,27 +3420,6 @@ extern int PlayerUID;
  */
 #define SOUND_MAX 67 /*!< 効果音定義の最大数 */
 
-#define MAX_VIRTUE 18 /*!< 徳定義の最大数 */
-
-#define V_COMPASSION    1
-#define V_HONOUR	    2
-#define V_JUSTICE	    3
-#define V_SACRIFICE	    4
-#define V_KNOWLEDGE	    5
-#define V_FAITH 	    6
-#define V_ENLIGHTEN	    7
-#define V_ENCHANT	    8
-#define V_CHANCE	    9
-#define V_NATURE	   10
-#define V_HARMONY	   11
-#define V_VITALITY	   12
-#define V_UNLIFE	   13
-#define V_PATIENCE	   14
-#define V_TEMPERANCE	   15
-#define V_DILIGENCE	   16
-#define V_VALOUR	   17
-#define V_INDIVIDUALISM    18
-
 
 /*** Hack ***/
 
