93f1425cdd1854da66439634b3f030f3195ae3aa
deskull <deskull@users.sourceforge.jp>
2019-04-19 08:31:01 +0900

    [fix] #37353 snipe.h コミットし忘れ修正。

---
+++ src/snipe.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+/* snipe.c */
+extern void reset_concentration(bool msg);
+extern void display_snipe_list(void);
+extern MULTIPLY tot_dam_aux_snipe(MULTIPLY mult, monster_type *m_ptr, SPELL_IDX snipe_type);
+extern void do_cmd_snipe(void);
+extern void do_cmd_snipe_browse(void);
+extern int boost_concentration_damage(int tdam);
