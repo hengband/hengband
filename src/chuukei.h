f6d36226d33989e3729f70002939f2c47ec49293
deskull <deskull@users.sourceforge.jp>
2019-04-19 19:52:03 +0900

    [Refactor] #37353　chuukei.h を追加して宣言を分離。

---
+++ src/chuukei.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#ifdef CHUUKEI
+/* chuukei.c */
+extern bool chuukei_server;
+extern bool chuukei_client;
+
+extern int connect_chuukei_server(char *server_string);
+extern void browse_chuukei(void);
+extern void flush_ringbuf(void);
+extern void prepare_chuukei_hooks(void);
+#endif
--- src/externs.h
+++ src/externs.h
@@ -1119,17 +1119,6 @@ extern int connect_server(int timeout, concptr host, int port);
 extern int disconnect_server(int sd);
 extern concptr soc_err(void);
 
-#ifdef CHUUKEI
-/* chuukei.c */
-extern bool chuukei_server;
-extern bool chuukei_client;
-
-extern int connect_chuukei_server(char *server_string);
-extern void browse_chuukei(void);
-extern void flush_ringbuf(void);
-extern void prepare_chuukei_hooks(void);
-#endif
-
 extern void prepare_movie_hooks(void);
 extern void prepare_browse_movie_aux(concptr filename);
 extern void prepare_browse_movie(concptr filename);
