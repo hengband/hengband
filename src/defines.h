33b901350212db81f9085821c8326253fd6a0b3d
deskull <deskull@users.sourceforge.jp>
2019-05-23 23:52:21 +0900

    [Refactor] #37353 PY_FOOD_* の重複定義を削除．

--- src/defines.h
+++ src/defines.h
@@ -331,16 +331,6 @@
 #define PY_MAX_LEVEL    50              /*!< プレイヤーレベルの最大値 / Maximum level */
 
 /*
- * Player "food" crucial values
- */
-#define PY_FOOD_MAX     15000   /*!< 食べ過ぎ～満腹の閾値 / Food value (Bloated) */
-#define PY_FOOD_FULL    10000   /*!< 満腹～平常の閾値 / Food value (Normal) */
-#define PY_FOOD_ALERT   2000    /*!< 平常～空腹の閾値 / Food value (Hungry) */
-#define PY_FOOD_WEAK    1000    /*!< 空腹～衰弱の閾値 / Food value (Weak) */
-#define PY_FOOD_FAINT   500     /*!< 衰弱～衰弱(赤表示/麻痺)の閾値 / Food value (Fainting) */
-#define PY_FOOD_STARVE  100     /*!< 衰弱(赤表示/麻痺)～飢餓ダメージの閾値 / Food value (Starving) */
-
-/*
  * Player regeneration constants
  */
 #define PY_REGEN_NORMAL         197     /* Regen factor*2^16 when full */
