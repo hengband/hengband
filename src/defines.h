75414f895a3bf5de4d2d8584de4d7748fa5e62b1
deskull <deskull@users.sourceforge.jp>
2019-05-24 00:14:38 +0900

    [Refactor] #37353 MAX_SAN_* 定義を player-status.h へ移動．

--- src/defines.h
+++ src/defines.h
@@ -147,12 +147,6 @@
 #define MAX_CLASS       28 /*!< 職業の最大定義数 Maximum number of player "class" types (see "table.c", etc) */
 #define MAX_SEIKAKU     13 /*!< 性格の最大定義数 */
 
-/* ELDRITCH_HORRORによるsanity blast処理に関するメッセージの最大数 / Number of entries in the sanity-blast descriptions */
-#define MAX_SAN_HORROR 20 /*!< 恐ろしい対象の形容数(正常時) */
-#define MAX_SAN_FUNNY 22  /*!< 恐ろしい対象の形容数(幻覚時) */
-#define MAX_SAN_COMMENT 5 /*!< 恐ろしい対象を見たときの絶叫メッセージ数(幻覚時) */
-
-
 /* カオスの変異 / Chaos mutations */
 
 
--- src/player-status.c
+++ src/player-status.c
@@ -62,6 +62,11 @@ const concptr stat_names_reduced[6] =
 
 };
 
+/* ELDRITCH_HORRORによるsanity blast処理に関するメッセージの最大数 / Number of entries in the sanity-blast descriptions */
+#define MAX_SAN_HORROR 20 /*!< 恐ろしい対象の形容数(正常時) */
+#define MAX_SAN_FUNNY 22  /*!< 恐ろしい対象の形容数(幻覚時) */
+#define MAX_SAN_COMMENT 5 /*!< 恐ろしい対象を見たときの絶叫メッセージ数(幻覚時) */
+
 /*!
  * @var horror_desc
  * @brief ELDRITCH HORROR効果時のモンスターの形容メッセージ（通常時）
