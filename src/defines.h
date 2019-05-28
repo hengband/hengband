a2eb43ed641fc0373c3837f5697608deb0f80969
deskull <deskull@users.sourceforge.jp>
2019-05-28 21:53:49 +0900

    [Refactor] #37353 OM_* 定義を object.h へ移動．

--- src/defines.h
+++ src/defines.h
@@ -1200,20 +1200,6 @@
 /*** Object flag values ***/
 
 
-
-/* 
- * How object is marked (flags in object_type.mark)
- * OM_FOUND --- original boolean flag
- * OM_NOMSG --- temporary flag to suppress messages which were
- *              already printed in autopick_pickup_items().
- */
-#define OM_FOUND        0x01    /*!< アイテムを一度でも視界に収めたことがあるか */
-#define OM_NOMSG        0x02    /* temporary flag to suppress messages */
-#define OM_NO_QUERY     0x04    /* Query for auto-pick was already answered as 'No' */
-#define OM_AUTODESTROY  0x08    /* Destroy later to avoid illegal inventry shift */
-#define OM_TOUCHED      0x10    /* Object was touched by player */
-
-
 /*
  * Object flags
  *
