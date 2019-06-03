38f1a5d974f5effe3d177f93085aca9ee5926f86
deskull <deskull@users.sourceforge.jp>
2019-06-03 23:48:43 +0900

    [Refactor] #37353 自動拾いに関するビット定義を autopick.h へ移動．

--- src/autopick.h
+++ src/autopick.h
@@ -1,5 +1,12 @@
 ﻿#pragma once
 
+#define DO_AUTOPICK       0x01
+#define DO_AUTODESTROY    0x02
+#define DO_DISPLAY        0x04
+#define DONT_AUTOPICK     0x08
+#define ITEM_DISPLAY      0x10
+#define DO_QUERY_AUTOPICK 0x20
+
 /*!
  * @struct autopick_type
  * @brief 自動拾い/破壊設定データの構造体 / A structure type for entry of auto-picker/destroyer
--- src/defines.h
+++ src/defines.h
@@ -304,13 +304,6 @@
 /* Maximum "Nazguls" number */
 #define MAX_NAZGUL_NUM 5
 
-#define DO_AUTOPICK       0x01
-#define DO_AUTODESTROY    0x02
-#define DO_DISPLAY        0x04
-#define DONT_AUTOPICK     0x08
-#define ITEM_DISPLAY      0x10
-#define DO_QUERY_AUTOPICK 0x20
-
 #define VIRTUE_LARGE 1
 #define VIRTUE_SMALL 2
 
