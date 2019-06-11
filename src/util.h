75ac7571b16104fbcf3d6f88d69006f1c978fa2a
deskull <deskull@users.sourceforge.jp>
2019-06-11 22:03:39 +0900

    [Refactor] #37353 ASCII マクロを util.h へ移動．

--- src/util.h
+++ src/util.h
@@ -1,6 +1,21 @@
 #pragma once
 
 /*
+ * Hack -- allow use of "ASCII" and "EBCDIC" for "indexes", "digits",
+ * and "Control-Characters".
+ *
+ * Note that all "index" values must be "lowercase letters", while
+ * all "digits" must be "digits".  Control characters can be made
+ * from any legal characters.
+ */
+#define A2I(X)	((X) - 'a')
+#define I2A(X)	((char)(X) + 'a')
+#define D2I(X)	((X) - '0')
+#define I2D(X)	((X) + '0')
+#define KTRL(X)	((X) & 0x1F)
+#define ESCAPE	'\033'
+
+/*
  * Hack -- Prepare to use the "Secure" routines
  */
 #if defined(SET_UID) && defined(SECURE)
