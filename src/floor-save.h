894cc0f247b3ebb78acdcaf43f271e8a4f6123ed
deskull <deskull@users.sourceforge.jp>
2019-06-12 22:15:14 +0900

    [Refactor] #37353 CFM_* 定義を　prepare_change_floor_mode() 付近に移動。

--- src/floor-save.h
+++ src/floor-save.h
@@ -24,6 +24,21 @@ extern void leave_floor(void);
 extern void clear_saved_floor_files(void);
 extern saved_floor_type *get_sf_ptr(FLOOR_IDX floor_id);
 extern FLOOR_IDX get_new_floor_id(void);
+
+/*
+ * Flags for change floor mode
+ */
+#define CFM_UP        	 0x0001  /* Move up */
+#define CFM_DOWN      	 0x0002  /* Move down */
+#define CFM_LONG_STAIRS  0x0004  /* Randomly occurred long stairs/shaft */
+#define CFM_XXX  	 0x0008  /* XXX */
+#define CFM_SHAFT     	 0x0010  /* Shaft */
+#define CFM_RAND_PLACE   0x0020  /* Arrive at random grid */
+#define CFM_RAND_CONNECT 0x0040  /* Connect with random stairs */
+#define CFM_SAVE_FLOORS  0x0080  /* Save floors */
+#define CFM_NO_RETURN    0x0100  /* Flee from random quest etc... */
+#define CFM_FIRST_FLOOR  0x0200  /* Create exit from the dungeon */
 extern void prepare_change_floor_mode(BIT_FLAGS mode);
+
 extern void precalc_cur_num_of_pet(void);
 extern FLOOR_IDX max_floor_id;
--- src/floor.h
+++ src/floor.h
@@ -188,20 +188,6 @@ typedef struct {
 #define DUNGEON_DARKNESS 19
 
 /*
- * Flags for change floor mode
- */
-#define CFM_UP        	 0x0001  /* Move up */
-#define CFM_DOWN      	 0x0002  /* Move down */
-#define CFM_LONG_STAIRS  0x0004  /* Randomly occurred long stairs/shaft */
-#define CFM_XXX  	 0x0008  /* XXX */
-#define CFM_SHAFT     	 0x0010  /* Shaft */
-#define CFM_RAND_PLACE   0x0020  /* Arrive at random grid */
-#define CFM_RAND_CONNECT 0x0040  /* Connect with random stairs */
-#define CFM_SAVE_FLOORS  0x0080  /* Save floors */
-#define CFM_NO_RETURN    0x0100  /* Flee from random quest etc... */
-#define CFM_FIRST_FLOOR  0x0200  /* Create exit from the dungeon */
-
-/*
  * Determines if a map location is fully inside the outer walls
  */
 #define in_bounds(Y,X) \
