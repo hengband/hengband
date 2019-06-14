b3cfbe29a94aa29d0c8830ba28e26408841c4db1
deskull <deskull@users.sourceforge.jp>
2019-06-14 21:36:07 +0900

    [Refactor] #37353 モンスター間の打撃処理を melee1.c / melee.h へ移動．

--- src/melee.h
+++ src/melee.h
@@ -157,5 +157,6 @@ extern HIT_POINT critical_norm(WEIGHT weight, int plus, HIT_POINT dam, s16b meic
 extern bool py_attack(POSITION y, POSITION x, COMBAT_OPTION_IDX mode);
 extern bool make_attack_normal(MONSTER_IDX m_idx);
 extern void mon_take_hit_mon(MONSTER_IDX m_idx, HIT_POINT dam, bool *dead, bool *fear, concptr note, MONSTER_IDX who);
+extern bool monst_attack_monst(MONSTER_IDX m_idx, MONSTER_IDX t_idx);
 
 extern const mbe_info_type mbe_info[];
