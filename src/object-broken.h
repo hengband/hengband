6dba7831291cab228694c018e39bbe3966db9a08
deskull <deskull@users.sourceforge.jp>
2019-07-08 22:35:21 +0900

    [Refactor] #37353 オブジェクトの破損判定関数を player-damage.c/h から　object-broken.c/h へ移動．

--- src/object-broken.h
+++ src/object-broken.h
@@ -1,2 +1,12 @@
 ﻿#pragma once
+
+extern bool hates_acid(object_type *o_ptr);
+extern bool hates_elec(object_type *o_ptr);
+extern bool hates_fire(object_type *o_ptr);
+extern bool hates_cold(object_type *o_ptr);
+extern int set_acid_destroy(object_type *o_ptr);
+extern int set_elec_destroy(object_type *o_ptr);
+extern int set_fire_destroy(object_type *o_ptr);
+extern int set_cold_destroy(object_type *o_ptr);
+
 extern bool potion_smash_effect(MONSTER_IDX who, POSITION y, POSITION x, KIND_OBJECT_IDX k_idx);
--- src/player-damage.h
+++ src/player-damage.h
@@ -16,14 +16,6 @@ extern int take_hit(int damage_type, HIT_POINT damage, concptr kb_str, int monsp
  */
 typedef int(*inven_func)(object_type *);
 
-extern bool hates_acid(object_type *o_ptr);
-extern bool hates_elec(object_type *o_ptr);
-extern bool hates_fire(object_type *o_ptr);
-extern bool hates_cold(object_type *o_ptr);
-extern int set_acid_destroy(object_type *o_ptr);
-extern int set_elec_destroy(object_type *o_ptr);
-extern int set_fire_destroy(object_type *o_ptr);
-extern int set_cold_destroy(object_type *o_ptr);
 extern int inven_damage(inven_func typ, int perc);
 extern HIT_POINT acid_dam(HIT_POINT dam, concptr kb_str, int monspell, bool aura);
 extern HIT_POINT elec_dam(HIT_POINT dam, concptr kb_str, int monspell, bool aura);
