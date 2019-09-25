547f0f5673803c193098bde01b46b0f6ed26abed
Eric Branlund <ebranlund@fastmail.com>
2019-09-24 17:59:51 -0700

    For English message about summoning nothing, changed past perfect tense to past tense; otherwise would need to change "have" to "has" for subject/verb agreement.

--- src/mspells3.c
+++ src/mspells3.c
@@ -1642,7 +1642,7 @@ static bool cast_learned_spell(player_type *caster_ptr, int spell, bool success)
 
 	if (no_trump)
     {
-        msg_print(_("何も現れなかった。", "No one have appeared."));
+        msg_print(_("何も現れなかった。", "No one appeared."));
 	}
 
 	return TRUE;
