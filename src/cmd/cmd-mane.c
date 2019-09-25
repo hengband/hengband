a74b8eb8044dae9bcf34e177184dd4f0b0abd06c
Eric Branlund <ebranlund@fastmail.com>
2019-09-24 17:49:25 -0700

    Corrected subject/verb agreement in English-message for player creating traps.

--- src/cmd/cmd-mane.c
+++ src/cmd/cmd-mane.c
@@ -732,7 +732,7 @@ static bool use_mane(player_type *caster_ptr, int spell)
 
 	case MS_MAKE_TRAP:
 		if (!target_set(caster_ptr, TARGET_KILL)) return FALSE;
-		msg_print(_("呪文を唱えて邪悪に微笑んだ。", "You cast a spell and cackles evilly."));
+		msg_print(_("呪文を唱えて邪悪に微笑んだ。", "You cast a spell and cackle evilly."));
 		trap_creation(caster_ptr, target_row, target_col);
 		break;
 	case MS_FORGET:
