e9093de1bd09091d5eee01669de46bc40245d88b
Eric Branlund <ebranlund@fastmail.com>
2019-09-26 13:46:00 -0700

    To be more idiomatic, dropped "out" in the English message for the onset of "War cry".

--- src/realm-hissatsu.c
+++ src/realm-hissatsu.c
@@ -627,7 +627,7 @@ concptr do_hissatsu_spell(player_type *caster_ptr, SPELL_IDX spell, BIT_FLAGS mo
 
 		if (cast)
 		{
-			msg_print(_("雄叫びをあげた！", "You roar out!"));
+			msg_print(_("雄叫びをあげた！", "You roar!"));
 			project_all_los(caster_ptr, GF_SOUND, randint1(plev * 3));
 			aggravate_monsters(caster_ptr, 0);
 		}
