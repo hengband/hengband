dafd7cc0671bafba5a1ceb7159f1994216303c3b
Eric Branlund <ebranlund@fastmail.com>
2019-09-26 14:40:35 -0700

    Reworded English description of light bolt/beam mirror spell.

--- src/mind.c
+++ src/mind.c
@@ -503,7 +503,7 @@ static concptr const mind_tips[5][MAX_MIND_POWERS] =
 {
 	"Detects visible monsters in your vicinity. Detects invisible monsters at level 15. Gives telepathy at level 25. Magic mapping at level 35. All of the effects need 4 more levels unless on a mirror.",
 	"Makes a mirror under you.",
-	"Fires bolt of flash light. Or fires beam of light on a mirror at level 10.",
+	"Fires bolt of light. At level ten or higher, the bolt will be a beam of light if you are on a mirror.",
 	"Teleports you a short distance.",
 	"Lights up nearby area and the inside of a room permanently.",
 	"Teleports you a long distance.",
