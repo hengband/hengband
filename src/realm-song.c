09e26afb8ebcdeec6e051183be3d4740a451f8c7
Eric Branlund <ebranlund@fastmail.com>
2019-09-26 09:41:27 -0700

    For onset of one song, changed English message to match verb tenses.

--- src/realm-song.c
+++ src/realm-song.c
@@ -869,7 +869,7 @@ concptr do_music_spell(player_type *caster_ptr, SPELL_IDX spell, BIT_FLAGS mode)
 			if (cast)
 			{
 				msg_print(_("暗黒の中に光と美をふりまいた。体が元の活力を取り戻した。",
-					"You strewed light and beauty in the dark as you sing. You feel refreshed."));
+					"You strew light and beauty in the dark as you sing. You feel refreshed."));
 				(void)restore_all_status(caster_ptr);
 				(void)restore_level(caster_ptr);
 			}
