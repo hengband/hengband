215df71c9ff6adfbca28fd93dd30490b7f5d7a6d
Eric Branlund <ebranlund@fastmail.com>
2019-09-26 15:01:21 -0700

    For clarity, reworded the English description of the force trainer's force aura ability.

--- src/mind.c
+++ src/mind.c
@@ -461,7 +461,7 @@ static concptr const mind_tips[5][MAX_MIND_POWERS] =
 	"Fires a short energy beam.",
 	"Gives magic resistance for a while.",
 	"Increases your spirit energy temporarily. More spirit energy will boost the effect or duration of your force abilities. Too much spirit energy can result in an explosion.",
-	"Gives aura which damages all monsters which attacked you for a while.",
+	"Envelops you with a temporary aura that damages any monster which hits you in melee.",
 	"Damages an adjacent monster, and blows it away.",
 	"Fires a large energy ball.",
 	"Dispels all magics which are effecting a monster.",
