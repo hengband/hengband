2ff02515dce7414123a7aec51fb47dad22ae77e7
Eric Branlund <ebranlund@fastmail.com>
2019-09-26 13:38:18 -0700

    Reworded English description for "Strike to Stun" to be more idiomatic.

--- src/realm-hissatsu.c
+++ src/realm-hissatsu.c
@@ -142,7 +142,7 @@ concptr do_hissatsu_spell(player_type *caster_ptr, SPELL_IDX spell, BIT_FLAGS mo
 
 	case 5:
 		if (name) return _("みね打ち", "Strike to Stun");
-		if (desc) return _("相手にダメージを与えないが、朦朧とさせる。", "Attempts to stun a monster in the adjacent.");
+		if (desc) return _("相手にダメージを与えないが、朦朧とさせる。", "Attempts to stun a monster next to you.");
 
 		if (cast)
 		{
