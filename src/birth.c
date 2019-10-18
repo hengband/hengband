d80ed28422315caaec3ec5b7063df04c9a1ed5f6
Eric Branlund <ebranlund@fastmail.com>
2019-10-18 08:58:17 -0700

    Reworded English description of "ChargeMan" personality type to be more idiomatic and be closer to what Google Translate gives for the Japanese description.

--- src/birth.c
+++ src/birth.c
@@ -422,7 +422,7 @@ static concptr seikaku_jouhou[MAX_SEIKAKU] =
 
 "\"Munchkin\" is a personality for beginners.  It raises all your stats and skills.  With this personality, you can win the game easily, but gain little honor in doing so.",
 
-"\"ChargeMan\" is crazy killer. It render you powerfull strength and constitution, but poor intelligence.you are not confused and seen the illusion because this you go mad from the beginning.",
+"\"ChargeMan\" is a crazy killer. You are strong and tough but have poor intelligence.  Since you're already insane, confusion and hallucinations do not affect you.",
 
 #endif
 };
