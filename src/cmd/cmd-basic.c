9666e271e8a264ad5d8ffb1da500ab4886f60820
Eric Branlund <ebranlund@fastmail.com>
2019-12-15 12:41:16 -0800

    In English messages for things that return when thrown, interface "come" before "back" in two places where it was missing.

--- src/cmd/cmd-basic.c
+++ src/cmd/cmd-basic.c
@@ -2749,7 +2749,7 @@ bool do_cmd_throw(player_type *creature_ptr, int mult, bool boomerang, OBJECT_ID
 			{
 				if (item >= 0)
 				{
-					msg_format(_("%sを受け損ねた！", "%s comes backs, but you can't catch!"), o2_name);
+					msg_format(_("%sを受け損ねた！", "%s comes back, but you can't catch!"), o2_name);
 				}
 				else
 				{
