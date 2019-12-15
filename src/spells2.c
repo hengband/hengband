e17a1836038b8b40382772ff1934c38aab1cd710
Eric Branlund <ebranlund@fastmail.com>
2019-12-15 14:12:17 -0800

    Reworded English message about pets affecting concentration to hopefully be more idiomatic.

--- src/spells2.c
+++ src/spells2.c
@@ -4446,7 +4446,7 @@ bool clear_mind(player_type *creature_ptr)
 {
 	if (total_friends)
 	{
-		msg_print(_("今はペットを操ることに集中していないと。", "You need concentration on the pets now."));
+		msg_print(_("今はペットを操ることに集中していないと。", "Your pets demand all of your attention."));
 		return FALSE;
 	}
 
