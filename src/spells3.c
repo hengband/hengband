8b2f8cbd3ff9c2a0bf86e0dfe7f1e480851f15b6
Eric Branlund <ebranlund@fastmail.com>
2020-02-05 17:24:28 -0800

    Replaced current_world_ptr->game_turn with turn in English messages from alchemy().

--- src/spells3.c
+++ src/spells3.c
@@ -1315,7 +1315,7 @@ bool alchemy(player_type *caster_ptr)
 	if (command_arg > 0) force = TRUE;
 
 	concptr q = _("どのアイテムを金に変えますか？", "Turn which item to gold? ");
-	concptr s = _("金に変えられる物がありません。", "You have nothing to current_world_ptr->game_turn to gold.");
+	concptr s = _("金に変えられる物がありません。", "You have nothing to turn to gold.");
 	OBJECT_IDX item;
 	object_type *o_ptr;
 	o_ptr = choose_object(caster_ptr, &item, q, s, (USE_INVEN | USE_FLOOR), 0);
@@ -1339,21 +1339,21 @@ bool alchemy(player_type *caster_ptr)
 		if (confirm_destroy || (object_value(o_ptr) > 0))
 		{
 			char out_val[MAX_NLEN + 40];
-			sprintf(out_val, _("本当に%sを金に変えますか？", "Really current_world_ptr->game_turn %s to gold? "), o_name);
+			sprintf(out_val, _("本当に%sを金に変えますか？", "Really turn %s to gold? "), o_name);
 			if (!get_check(out_val)) return FALSE;
 		}
 	}
 
 	if (!can_player_destroy_object(o_ptr))
 	{
-		msg_format(_("%sを金に変えることに失敗した。", "You fail to current_world_ptr->game_turn %s to gold!"), o_name);
+		msg_format(_("%sを金に変えることに失敗した。", "You fail to turn %s to gold!"), o_name);
 		return FALSE;
 	}
 
 	PRICE price = object_value_real(o_ptr);
 	if (price <= 0)
 	{
-		msg_format(_("%sをニセの金に変えた。", "You current_world_ptr->game_turn %s to fool's gold."), o_name);
+		msg_format(_("%sをニセの金に変えた。", "You turn %s to fool's gold."), o_name);
 		vary_item(caster_ptr, item, -amt);
 		return TRUE;
 	}
@@ -1363,7 +1363,7 @@ bool alchemy(player_type *caster_ptr)
 	if (amt > 1) price *= amt;
 
 	if (price > 30000) price = 30000;
-	msg_format(_("%sを＄%d の金に変えた。", "You current_world_ptr->game_turn %s to %ld coins worth of gold."), o_name, price);
+	msg_format(_("%sを＄%d の金に変えた。", "You turn %s to %ld coins worth of gold."), o_name, price);
 
 	caster_ptr->au += price;
 	caster_ptr->redraw |= PR_GOLD;
