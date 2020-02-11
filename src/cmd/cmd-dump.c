a10259898ae55c4e08acdc34e001985ea38faeda
deskull <deskull@users.sourceforge.jp>
2020-02-11 15:10:25 +0900

    [Refactor] #39912 cmd-dump.c　の不要インクルード削除． / Deleted unused include files in cmd-dump.c.

--- src/cmd/cmd-dump.c
+++ src/cmd/cmd-dump.c
@@ -40,20 +40,15 @@
  */
 
 #include "angband.h"
-#include "util.h"
 #include "cmd-dump.h"
 #include "term.h"
-#include "inet.h"
 #include "core.h"
 
 #include "autopick.h"
 
-#include "bldg.h"
+#include "inet.h"
 #include "birth.h"
-#include "cmd-pet.h"
-#include "cmd-spell.h"
 #include "dungeon.h"
-#include "feature.h"
 #include "world.h"
 #include "player-effects.h"
 #include "player-status.h"
@@ -69,16 +64,12 @@
 #include "object-hook.h"
 #include "monster.h"
 #include "monster-status.h"
-#include "floor.h"
 #include "view-mainwindow.h"
 #include "dungeon-file.h"
 #include "files.h"
-#include "player-class.h"
-#include "player-move.h"
 #include "spells.h"
 #include "objectkind.h"
 #include "floor-town.h"
-#include "realm.h"
 #include "view-mainwindow.h"
 
 #include "english.h"
