64436f6f406718ca17a7b79b11e4ae02136d29c7
Hourier <hourier@users.sourceforge.jp>
2020-02-15 20:43:13 +0900

    [Refactor] #37285 process_monster() からrunaway_monster() を分離 / Separated runaway_monster() from process_monster()

--- src/monster-process.c
+++ src/monster-process.c
@@ -46,6 +46,7 @@ void produce_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see
 void process_special(player_type *target_ptr, MONSTER_IDX m_idx);
 bool decide_monster_multiplication(player_type *target_ptr, MONSTER_IDX m_idx, POSITION oy, POSITION ox);
 bool decide_monster_movement_direction(player_type *target_ptr, DIRECTION *mm, MONSTER_IDX m_idx, bool aware);
+bool runaway_monster(player_type *player_ptr, MONSTER_IDX m_idx, bool is_riding_mon, bool see_m);
 
  /*!
   * @brief モンスターが敵に接近するための方向を決める /
@@ -1301,7 +1302,6 @@ void process_monster(player_type *target_ptr, MONSTER_IDX m_idx)
 		}
 
 		delete_monster_idx(target_ptr, m_idx);
-
 		return;
 	}
 
@@ -1313,60 +1313,7 @@ void process_monster(player_type *target_ptr, MONSTER_IDX m_idx)
 		if (dead) return;
 	}
 
-	if ((is_pet(m_ptr) || is_friendly(m_ptr)) && ((r_ptr->flags1 & RF1_UNIQUE) || (r_ptr->flags7 & RF7_NAZGUL)) && !target_ptr->phase_out)
-	{
-		static int riding_pinch = 0;
-
-		if (m_ptr->hp < m_ptr->maxhp / 3)
-		{
-			GAME_TEXT m_name[MAX_NLEN];
-			monster_desc(target_ptr, m_name, m_ptr, 0);
-
-			if (is_riding_mon && riding_pinch < 2)
-			{
-				msg_format(_("%sは傷の痛さの余りあなたの束縛から逃れようとしている。",
-					"%^s seems to be in so much pain and tries to escape from your restriction."), m_name);
-				riding_pinch++;
-				disturb(target_ptr, TRUE, TRUE);
-			}
-			else
-			{
-				if (is_riding_mon)
-				{
-					msg_format(_("%sはあなたの束縛から脱出した。", "%^s succeeded to escape from your restriction!"), m_name);
-					if (rakuba(target_ptr, -1, FALSE))
-					{
-						msg_print(_("地面に落とされた。", "You have fallen from the pet you were riding."));
-					}
-				}
-
-				if (see_m)
-				{
-					if ((r_ptr->flags2 & RF2_CAN_SPEAK) && (m_ptr->r_idx != MON_GRIP) && (m_ptr->r_idx != MON_WOLF) && (m_ptr->r_idx != MON_FANG) &&
-						player_has_los_bold(target_ptr, m_ptr->fy, m_ptr->fx) && projectable(target_ptr, m_ptr->fy, m_ptr->fx, target_ptr->y, target_ptr->x))
-					{
-						msg_format(_("%^s「ピンチだ！退却させてもらう！」", "%^s says 'It is the pinch! I will retreat'."), m_name);
-					}
-					msg_format(_("%^sがテレポート・レベルの巻物を読んだ。", "%^s reads a scroll of teleport level."), m_name);
-					msg_format(_("%^sが消え去った。", "%^s disappears."), m_name);
-				}
-
-				if (is_riding_mon && rakuba(target_ptr, -1, FALSE))
-				{
-					msg_print(_("地面に落とされた。", "You have fallen from the pet you were riding."));
-				}
-
-				check_quest_completion(target_ptr, m_ptr);
-				delete_monster_idx(target_ptr, m_idx);
-				return;
-			}
-		}
-		else
-		{
-			/* Reset the counter */
-			if (is_riding_mon) riding_pinch = 0;
-		}
-	}
+	if (runaway_monster(target_ptr, m_idx, is_riding_mon, see_m)) return;
 
 	/* Handle "sleep" */
 	if (MON_CSLEEP(m_ptr))
@@ -2452,6 +2399,76 @@ bool decide_monster_movement_direction(player_type *target_ptr, DIRECTION *mm, M
 
 	if (!get_moves(target_ptr, m_idx, mm)) return FALSE;
 
+	return TRUE;
+}
+
+
+/*!
+ * @brief ペットや友好的なモンスターがフロアから逃げる処理を行う
+ * @param target_ptr プレーヤーへの参照ポインタ
+ * @param m_idx モンスターID
+ * @param is_riding_mon 騎乗状態ならばTRUE
+ * @param see_m モンスターが視界内にいたらTRUE
+ * @return モンスターがフロアから消えたらTRUE
+ */
+bool runaway_monster(player_type *target_ptr, MONSTER_IDX m_idx, bool is_riding_mon, bool see_m)
+{
+	monster_type *m_ptr = &target_ptr->current_floor_ptr->m_list[m_idx];
+	monster_race *r_ptr = &r_info[m_ptr->r_idx];
+	bool can_runaway = is_pet(m_ptr) || is_friendly(m_ptr);
+	can_runaway &= ((r_ptr->flags1 & RF1_UNIQUE) != 0) || ((r_ptr->flags7 & RF7_NAZGUL) != 0);
+	can_runaway &= !target_ptr->phase_out;
+	if (!can_runaway) return FALSE;
+
+	static int riding_pinch = 0;
+
+	if (m_ptr->hp >= m_ptr->maxhp / 3)
+	{
+		/* Reset the counter */
+		if (is_riding_mon) riding_pinch = 0;
+		
+		return FALSE;
+	}
+
+	GAME_TEXT m_name[MAX_NLEN];
+	monster_desc(target_ptr, m_name, m_ptr, 0);
+	if (is_riding_mon && riding_pinch < 2)
+	{
+		msg_format(_("%sは傷の痛さの余りあなたの束縛から逃れようとしている。",
+			"%^s seems to be in so much pain and tries to escape from your restriction."), m_name);
+		riding_pinch++;
+		disturb(target_ptr, TRUE, TRUE);
+		return FALSE;
+	}
+
+	if (is_riding_mon)
+	{
+		msg_format(_("%sはあなたの束縛から脱出した。", "%^s succeeded to escape from your restriction!"), m_name);
+		if (rakuba(target_ptr, -1, FALSE))
+		{
+			msg_print(_("地面に落とされた。", "You have fallen from the pet you were riding."));
+		}
+	}
+
+	if (see_m)
+	{
+		if ((r_ptr->flags2 & RF2_CAN_SPEAK) && (m_ptr->r_idx != MON_GRIP) && (m_ptr->r_idx != MON_WOLF) && (m_ptr->r_idx != MON_FANG) &&
+			player_has_los_bold(target_ptr, m_ptr->fy, m_ptr->fx) && projectable(target_ptr, m_ptr->fy, m_ptr->fx, target_ptr->y, target_ptr->x))
+		{
+			msg_format(_("%^s「ピンチだ！退却させてもらう！」", "%^s says 'It is the pinch! I will retreat'."), m_name);
+		}
+
+		msg_format(_("%^sがテレポート・レベルの巻物を読んだ。", "%^s reads a scroll of teleport level."), m_name);
+		msg_format(_("%^sが消え去った。", "%^s disappears."), m_name);
+	}
+
+	if (is_riding_mon && rakuba(target_ptr, -1, FALSE))
+	{
+		msg_print(_("地面に落とされた。", "You have fallen from the pet you were riding."));
+	}
+
+	check_quest_completion(target_ptr, m_ptr);
+	delete_monster_idx(target_ptr, m_idx);
 	return TRUE;
 }
 
