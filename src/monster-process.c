5e58aeb0287f2ba4ddcdc3bbdc1ef5df9a82c9d6
Hourier <hourier@users.sourceforge.jp>
2020-02-15 13:28:50 +0900

    [Implement] #37285 量子的効果の関数をprocess_monster() から分離 / Separated quantum effects from process_monster()

--- src/monster-process.c
+++ src/monster-process.c
@@ -40,6 +40,9 @@
 #include "files.h"
 #include "view-mainwindow.h"
 
+bool process_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
+void vanish_nonunique(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
+void produce_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
 
  /*!
   * @brief モンスターが敵に接近するための方向を決める /
@@ -1300,63 +1303,7 @@ void process_monster(player_type *target_ptr, MONSTER_IDX m_idx)
 		return;
 	}
 
-	/* Quantum monsters are odd */
-	if (r_ptr->flags2 & (RF2_QUANTUM))
-	{
-		/* Sometimes skip move */
-		if (!randint0(2)) return;
-
-		/* Sometimes die */
-		if (!randint0((m_idx % 100) + 10))
-		{
-			if ((r_ptr->flags1 & RF1_UNIQUE) == 0)
-			{
-				if (!(r_ptr->flags1 & RF1_QUESTOR))
-				{
-					if (see_m)
-					{
-						GAME_TEXT m_name[MAX_NLEN];
-						monster_desc(target_ptr, m_name, m_ptr, 0);
-						msg_format(_("%sは消え去った！", "%^s disappears!"), m_name);
-					}
-
-					monster_death(target_ptr, m_idx, FALSE);
-					delete_monster_idx(target_ptr, m_idx);
-					if (is_pet(m_ptr) && !(m_ptr->ml))
-					{
-						msg_print(_("少しの間悲しい気分になった。", "You feel sad for a moment."));
-					}
-
-					return;
-				}
-			}
-			else
-			{
-				if (see_m)
-				{
-					GAME_TEXT m_name[MAX_NLEN];
-					monster_desc(target_ptr, m_name, m_ptr, 0);
-					msg_format(_("%sは量子的効果を起こした！", "%^s produced a quantum effect!"), m_name);
-				}
-				else
-				{
-					msg_print(_("量子的効果が起こった！", "A quantum effect was produced!"));
-				}
-
-				bool target = one_in_(2);
-				const int blink = 32 * 5 + 4;
-				if (target)
-				{
-					(void)monspell_to_monster(target_ptr, blink, m_ptr->fy, m_ptr->fx, m_idx, m_idx);
-
-				}
-				else
-				{
-					teleport_player_away(m_idx, target_ptr, 10, TRUE);
-				}
-			}
-		}
-	}
+	if (process_quantum_effect(target_ptr,m_idx, see_m)) return;
 
 	if (m_ptr->r_idx == MON_SHURYUUDAN)
 	{
@@ -2415,6 +2362,103 @@ void process_monster(player_type *target_ptr, MONSTER_IDX m_idx)
 
 	if (m_ptr->ml) chg_virtue(target_ptr, V_COMPASSION, -1);
 }
+
+
+/*!
+ * @brief 量子生物の量子的効果を実行する
+ * @param target_ptr プレーヤーへの参照ポインタ
+ * @param m_idx モンスターID
+ * @param see_m モンスターが視界内にいたらTRUE
+ * @return モンスターが量子的効果により消滅したらTRUE
+ */
+bool process_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m)
+{
+	monster_type *m_ptr = &target_ptr->current_floor_ptr->m_list[m_idx];
+	monster_race *r_ptr = &r_info[m_ptr->r_idx];
+	if (!r_ptr->flags2 & RF2_QUANTUM) return FALSE;
+	if (!randint0(2)) return FALSE;
+	if (randint0((m_idx % 100) + 10)) return FALSE;
+
+	bool can_disappear = (r_ptr->flags1 & RF1_UNIQUE) == 0;
+	can_disappear &= (r_ptr->flags1 & RF1_QUESTOR) == 0;
+	if (can_disappear)
+	{
+		vanish_nonunique(target_ptr, m_idx, see_m);
+		return TRUE;
+	}
+	
+	produce_quantum_effect(target_ptr, m_idx, see_m);
+	return FALSE;
+}
+
+
+/*!
+ * @brief ユニークでない量子生物を消滅させる
+ * @param target_ptr プレーヤーへの参照ポインタ
+ * @param m_idx モンスターID
+ * @param see_m モンスターが視界内にいたらTRUE
+ * @return なし
+ */
+void vanish_nonunique(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m)
+{
+	monster_type *m_ptr = &target_ptr->current_floor_ptr->m_list[m_idx];
+	if (see_m)
+	{
+		GAME_TEXT m_name[MAX_NLEN];
+		monster_desc(target_ptr, m_name, m_ptr, 0);
+		msg_format(_("%sは消え去った！", "%^s disappears!"), m_name);
+	}
+
+	monster_death(target_ptr, m_idx, FALSE);
+	delete_monster_idx(target_ptr, m_idx);
+	if (is_pet(m_ptr) && !(m_ptr->ml))
+	{
+		msg_print(_("少しの間悲しい気分になった。", "You feel sad for a moment."));
+	}
+}
+
+
+/*!
+ * @brief 量子生物ユニークの量子的効果 (ショート・テレポートまたは距離10のテレポート・アウェイ)を実行する
+ * @param target_ptr プレーヤーへの参照ポインタ
+ * @param m_idx モンスターID
+ * @param see_m モンスターが視界内にいたらTRUE
+ * @return なし
+ * @details
+ * プレーヤーが量子生物を観測しているか、量子生物がプレーヤーを観測している場合、互いの相対的な位置を確定させる
+ * 波動関数の収縮はテレポートではないので反テレポート無効
+ * todo パターンは収縮どころか拡散しているが、この際気にしてはいけない
+ */
+void produce_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m)
+{
+	monster_type *m_ptr = &target_ptr->current_floor_ptr->m_list[m_idx];
+	bool coherent = los(target_ptr, m_ptr->fy, m_ptr->fx, target_ptr->y, target_ptr->x);
+	if (!see_m && !coherent) return;
+
+	if (see_m)
+	{
+		GAME_TEXT m_name[MAX_NLEN];
+		monster_desc(target_ptr, m_name, m_ptr, 0);
+		msg_format(_("%sは量子的効果を起こした！", "%^s produced a decoherence!"), m_name);
+	}
+	else
+	{
+		msg_print(_("量子的効果が起こった！", "A decoherence was produced!"));
+	}
+
+	bool target = one_in_(2);
+	const int blink = 32 * 5 + 4;
+	if (target)
+	{
+		(void)monspell_to_monster(target_ptr, blink, m_ptr->fy, m_ptr->fx, m_idx, m_idx);
+
+	}
+	else
+	{
+		teleport_player_away(m_idx, target_ptr, 10, TRUE);
+	}
+}
+
 
 /*!
  * @brief 全モンスターのターン管理メインルーチン /
