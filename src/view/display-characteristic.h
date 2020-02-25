9ddbb27682b345f1eedddd5a610a25f06e03644e
Hourier <hourier@users.sourceforge.jp>
2020-02-25 23:39:28 +0900

    [Refacotr] #39962 display-player.c からdisplay-characteristic.c/h を分離 / Separated display-characteristic.c/h from display-player.c

---
+++ src/view/display-characteristic.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#include "angband.h"
+#include "files.h" // 消すかどうか検討中.
+
+void display_one_characteristic_info(player_type *creature_ptr, TERM_LEN row, TERM_LEN col, concptr header, int flag1, all_player_flags *f, u16b mode);
