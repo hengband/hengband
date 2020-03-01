574d7767e4e1d93073320d096c14848f4a8948d8
Hourier <hourier@users.sourceforge.jp>
2020-03-01 20:29:30 +0900

    [Refactor] #39962 process_world_aux_digestion() を整形し、Doxygen用コメントを追加 / Reshaped process_world_aux_digestion() and added comments for Doxygen

--- src/core.c
+++ src/core.c
@@ -1070,48 +1070,51 @@ static object_type *choose_cursed_obj_name(player_type *player_ptr, BIT_FLAGS fl
 }
 
 
+/*!
+ * @brief 10ゲームターンが進行するごとにプレイヤーの空腹状態を飢餓方向に向かわせる
+ * @param creature_ptr プレーヤーへの参照ポインタ
+ * @return なし
+ */
 static void process_world_aux_digestion(player_type *creature_ptr)
 {
-	if (!creature_ptr->phase_out)
+	if (creature_ptr->phase_out) return;
+
+	if (creature_ptr->food >= PY_FOOD_MAX)
 	{
-		if (creature_ptr->food >= PY_FOOD_MAX)
-		{
-			(void)set_food(creature_ptr, creature_ptr->food - 100);
-		}
-		else if (!(current_world_ptr->game_turn % (TURNS_PER_TICK * 5)))
-		{
-			int digestion = SPEED_TO_ENERGY(creature_ptr->pspeed);
-			if (creature_ptr->regenerate)
-				digestion += 20;
-			if (creature_ptr->special_defense & (KAMAE_MASK | KATA_MASK))
-				digestion += 20;
-			if (creature_ptr->cursed & TRC_FAST_DIGEST)
-				digestion += 30;
+		(void)set_food(creature_ptr, creature_ptr->food - 100);
+	}
+	else if (!(current_world_ptr->game_turn % (TURNS_PER_TICK * 5)))
+	{
+		int digestion = SPEED_TO_ENERGY(creature_ptr->pspeed);
+		if (creature_ptr->regenerate)
+			digestion += 20;
+		if (creature_ptr->special_defense & (KAMAE_MASK | KATA_MASK))
+			digestion += 20;
+		if (creature_ptr->cursed & TRC_FAST_DIGEST)
+			digestion += 30;
 
-			if (creature_ptr->slow_digest)
-				digestion -= 5;
+		if (creature_ptr->slow_digest)
+			digestion -= 5;
 
-			if (digestion < 1) digestion = 1;
-			if (digestion > 100) digestion = 100;
+		if (digestion < 1) digestion = 1;
+		if (digestion > 100) digestion = 100;
 
-			(void)set_food(creature_ptr, creature_ptr->food - digestion);
-		}
+		(void)set_food(creature_ptr, creature_ptr->food - digestion);
+	}
 
-		if ((creature_ptr->food < PY_FOOD_FAINT))
-		{
-			if (!creature_ptr->paralyzed && (randint0(100) < 10))
-			{
-				msg_print(_("あまりにも空腹で気絶してしまった。", "You faint from the lack of food."));
-				disturb(creature_ptr, TRUE, TRUE);
-				(void)set_paralyzed(creature_ptr, creature_ptr->paralyzed + 1 + randint0(5));
-			}
+	if ((creature_ptr->food >= PY_FOOD_FAINT)) return;
 
-			if (creature_ptr->food < PY_FOOD_STARVE)
-			{
-				HIT_POINT dam = (PY_FOOD_STARVE - creature_ptr->food) / 10;
-				if (!IS_INVULN(creature_ptr)) take_hit(creature_ptr, DAMAGE_LOSELIFE, dam, _("空腹", "starvation"), -1);
-			}
-		}
+	if (!creature_ptr->paralyzed && (randint0(100) < 10))
+	{
+		msg_print(_("あまりにも空腹で気絶してしまった。", "You faint from the lack of food."));
+		disturb(creature_ptr, TRUE, TRUE);
+		(void)set_paralyzed(creature_ptr, creature_ptr->paralyzed + 1 + randint0(5));
+	}
+
+	if (creature_ptr->food < PY_FOOD_STARVE)
+	{
+		HIT_POINT dam = (PY_FOOD_STARVE - creature_ptr->food) / 10;
+		if (!IS_INVULN(creature_ptr)) take_hit(creature_ptr, DAMAGE_LOSELIFE, dam, _("空腹", "starvation"), -1);
 	}
 }
 
