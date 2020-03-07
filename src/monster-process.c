65d60ab10aa8b3bbb7e7a2ec59650cebb2de4b25
Hourier <hourier@users.sourceforge.jp>
2020-03-07 14:56:28 +0900

    [Refactor] #40030 Separated quantum-effect.c/h from monster-process.c

--- src/monster-process.c
+++ src/monster-process.c
@@ -15,6 +15,7 @@
 
 #include "angband.h"
 #include "util.h"
+#include "monster/quantum-effect.h"
 
 #include "cmd-dump.h"
 #include "cmd-pet.h"
@@ -102,9 +103,6 @@ bool process_stealth(player_type *target_ptr, MONSTER_IDX m_idx);
 bool vanish_summoned_children(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
 void awake_monster(player_type *target_ptr, MONSTER_IDX m_idx);
 void process_angar(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
-bool process_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
-void vanish_nonunique(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
-void produce_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
 bool explode_monster(player_type *target_ptr, MONSTER_IDX m_idx);
 bool decide_monster_multiplication(player_type *target_ptr, MONSTER_IDX m_idx, POSITION oy, POSITION ox);
 bool decide_monster_movement_direction(player_type *target_ptr, DIRECTION *mm, MONSTER_IDX m_idx, bool aware);
@@ -1340,101 +1338,6 @@ void process_angar(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m)
 	}
 
 	set_hostile(target_ptr, m_ptr);
-}
-
-
-/*!
- * @brief 量子生物の量子的効果を実行する
- * @param target_ptr プレーヤーへの参照ポインタ
- * @param m_idx モンスターID
- * @param see_m モンスターが視界内にいたらTRUE
- * @return モンスターが量子的効果により消滅したらTRUE
- */
-bool process_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m)
-{
-	monster_type *m_ptr = &target_ptr->current_floor_ptr->m_list[m_idx];
-	monster_race *r_ptr = &r_info[m_ptr->r_idx];
-	if ((r_ptr->flags2 & RF2_QUANTUM) == 0) return FALSE;
-	if (!randint0(2)) return FALSE;
-	if (randint0((m_idx % 100) + 10)) return FALSE;
-
-	bool can_disappear = (r_ptr->flags1 & RF1_UNIQUE) == 0;
-	can_disappear &= (r_ptr->flags1 & RF1_QUESTOR) == 0;
-	if (can_disappear)
-	{
-		vanish_nonunique(target_ptr, m_idx, see_m);
-		return TRUE;
-	}
-
-	produce_quantum_effect(target_ptr, m_idx, see_m);
-	return FALSE;
-}
-
-
-/*!
- * @brief ユニークでない量子生物を消滅させる
- * @param target_ptr プレーヤーへの参照ポインタ
- * @param m_idx モンスターID
- * @param see_m モンスターが視界内にいたらTRUE
- * @return なし
- */
-void vanish_nonunique(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m)
-{
-	monster_type *m_ptr = &target_ptr->current_floor_ptr->m_list[m_idx];
-	if (see_m)
-	{
-		GAME_TEXT m_name[MAX_NLEN];
-		monster_desc(target_ptr, m_name, m_ptr, 0);
-		msg_format(_("%sは消え去った！", "%^s disappears!"), m_name);
-	}
-
-	monster_death(target_ptr, m_idx, FALSE);
-	delete_monster_idx(target_ptr, m_idx);
-	if (is_pet(m_ptr) && !(m_ptr->ml))
-	{
-		msg_print(_("少しの間悲しい気分になった。", "You feel sad for a moment."));
-	}
-}
-
-
-/*!
- * @brief 量子生物ユニークの量子的効果 (ショート・テレポートまたは距離10のテレポート・アウェイ)を実行する
- * @param target_ptr プレーヤーへの参照ポインタ
- * @param m_idx モンスターID
- * @param see_m モンスターが視界内にいたらTRUE
- * @return なし
- * @details
- * プレーヤーが量子生物を観測しているか、量子生物がプレーヤーを観測している場合、互いの相対的な位置を確定させる
- * 波動関数の収縮はテレポートではないので反テレポート無効
- * todo パターンは収縮どころか拡散しているが、この際気にしてはいけない
- */
-void produce_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m)
-{
-	monster_type *m_ptr = &target_ptr->current_floor_ptr->m_list[m_idx];
-	bool coherent = los(target_ptr, m_ptr->fy, m_ptr->fx, target_ptr->y, target_ptr->x);
-	if (!see_m && !coherent) return;
-
-	if (see_m)
-	{
-		GAME_TEXT m_name[MAX_NLEN];
-		monster_desc(target_ptr, m_name, m_ptr, 0);
-		msg_format(_("%sは量子的効果を起こした！", "%^s produced a decoherence!"), m_name);
-	}
-	else
-	{
-		msg_print(_("量子的効果が起こった！", "A decoherence was produced!"));
-	}
-
-	bool target = one_in_(2);
-	const int blink = 32 * 5 + 4;
-	if (target)
-	{
-		(void)monspell_to_monster(target_ptr, blink, m_ptr->fy, m_ptr->fx, m_idx, m_idx);
-	}
-	else
-	{
-		teleport_player_away(m_idx, target_ptr, 10, TRUE);
-	}
 }
 
 
---
+++ src/monster/quantum-effect.h
@@ -0,0 +1,3 @@
+﻿#pragma once
+
+bool process_quantum_effect(player_type *target_ptr, MONSTER_IDX m_idx, bool see_m);
