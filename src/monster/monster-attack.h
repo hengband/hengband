455b83d14526a957d18b4ee0b2637c65e8e56878
Hourier <hourier@users.sourceforge.jp>
2020-03-08 09:54:41 +0900

    [Refactor] #40030 Separated monster-attack.c/h from monster-process.c

---
+++ src/monster/monster-attack.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "angband.h"
+#include "monster/monster-util.h"
+
+void exe_monster_attack_to_player(player_type *target_ptr, turn_flags *turn_flags_ptr, MONSTER_IDX m_idx, POSITION ny, POSITION nx);
+bool process_monster_attack_to_monster(player_type *target_ptr, turn_flags *turn_flags_ptr, MONSTER_IDX m_idx, grid_type *g_ptr, bool can_cross);
