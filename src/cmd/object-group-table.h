5fd7ca932e99b5f4149dfd3e7d8632a2c1316d41
Hourier <hourier@users.sourceforge.jp>
2020-03-08 20:59:48 +0900

    [Refactor] Separated object-group-table.c/h from cmd-dump.c

---
+++ src/cmd/object-group-table.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#define MAX_OBJECT_GROUP_TEXT 43
+#define MAX_OBJECT_GROUP_TVAL 43
+
+extern concptr object_group_text[MAX_OBJECT_GROUP_TEXT];
+extern byte object_group_tval[MAX_OBJECT_GROUP_TVAL];
+
