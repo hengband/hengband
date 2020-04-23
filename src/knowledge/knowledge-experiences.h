b6734f4cb892ff6ee958af27bd401496ae9447d2
Hourier <hourier@users.sourceforge.jp>
2020-04-23 22:58:25 +0900

    [Refactor] #40236 Separated knowledge-experiences.c/h from cmd-dump.c

---
+++ src/knowledge/knowledge-experiences.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+void do_cmd_knowledge_weapon_exp(player_type *creature_ptr);
+void do_cmd_knowledge_spell_exp(player_type *creature_ptr);
+void do_cmd_knowledge_skill_exp(player_type *creature_ptr);
