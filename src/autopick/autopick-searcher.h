cfd736894c66426ecd832d94dc32976f0b1df646
Hourier <hourier@users.sourceforge.jp>
2020-04-26 00:58:04 +0900

    [Refactor] #39964 Separated autopick-searcher.c/h from autopick.c

---
+++ src/autopick/autopick-searcher.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+bool get_object_for_search(player_type *player_ptr, object_type **o_handle, concptr *search_strp);
+bool get_destroyed_object_for_search(player_type *player_ptr, object_type **o_handle, concptr *search_strp);
+byte get_string_for_search(player_type *player_ptr, object_type **o_handle, concptr *search_strp);
+void search_for_object(player_type *player_ptr, text_body_type *tb, object_type *o_ptr, bool forward);
+void search_for_string(text_body_type *tb, concptr search_str, bool forward);
