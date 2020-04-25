9f150008baff6c7fdef6bfcaed99b5bd6816c2c4
Hourier <hourier@users.sourceforge.jp>
2020-04-25 11:04:24 +0900

    [Refactor] #39964 Separated autopick-entry.c/h from autopick.c

---
+++ src/autopick/autopick-entry.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+bool autopick_new_entry(autopick_type *entry, concptr str, bool allow_default);
+void autopick_entry_from_object(player_type *player_ptr, autopick_type *entry, object_type *o_ptr);
