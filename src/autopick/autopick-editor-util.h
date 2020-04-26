7e5c63e55a2e75723810bd90dbbcff0ec145cc39
Hourier <hourier@users.sourceforge.jp>
2020-04-26 11:17:25 +0900

    [Refactor] #39964 Separated autopick-editor-util.c/h from autopick.c

---
+++ src/autopick/autopick-editor-util.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+void toggle_keyword(text_body_type *tb, BIT_FLAGS flg);
+void toggle_command_letter(text_body_type *tb, byte flg);
+void add_keyword(text_body_type *tb, BIT_FLAGS flg);
+bool add_empty_line(text_body_type *tb);
+void kill_yank_chain(text_body_type *tb);
+void add_str_to_yank(text_body_type *tb, concptr str);
+void copy_text_to_yank(text_body_type *tb);
