461dcb115ebfa9b79d405bb2ac0ccec8b82d17e8
Hourier <hourier@users.sourceforge.jp>
2020-05-01 13:06:10 +0900

    [Refactor] #39963 Separated effect-monster-psi.c/h from effect-monster-switcher.c

---
+++ src/effect/effect-monster-psi.h
@@ -0,0 +1,4 @@
+﻿#pragma once
+
+gf_switch_result effect_monster_psi(player_type *caster_ptr, effect_monster_type *em_ptr);
+gf_switch_result effect_monster_psi_drain(player_type *caster_ptr, effect_monster_type *em_ptr);
