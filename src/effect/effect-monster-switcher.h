fe575b11c6a7e8b330f49fe2b745b5268a84208b
Hourier <hourier@users.sourceforge.jp>
2020-05-01 11:03:03 +0900

    [Refactor] #39963 Separated effect-monster-resist-hurt.c/h from effect-monster-switcher.c

--- src/effect/effect-monster-switcher.h
+++ src/effect/effect-monster-switcher.h
@@ -1,10 +1,3 @@
 ﻿#pragma once
 
-typedef enum gf_switch_result
-{
-	GF_SWITCH_FALSE = 0,
-	GF_SWITCH_TRUE = 1,
-	GF_SWITCH_CONTINUE = 2,
-} gf_switch_result;
-
 gf_switch_result switch_effects_monster(player_type *caster_ptr, effect_monster_type *em_ptr);
