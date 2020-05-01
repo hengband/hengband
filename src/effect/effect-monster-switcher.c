c0dee030036dcd306cf46a2d3c01c6f96e1619cf
Hourier <hourier@users.sourceforge.jp>
2020-05-01 13:18:37 +0900

    [Refactor] #39963 Separated effect_monster_domination_corrupted() from effect_monster_domination()

--- src/effect/effect-monster-switcher.c
+++ src/effect/effect-monster-switcher.c
@@ -43,6 +43,48 @@ gf_switch_result effect_monster_telekinesis(player_type *caster_ptr, effect_mons
 }
 
 
+// Powerful demons & undead can turn a mindcrafter's attacks back on them.
+static void effect_monster_domination_corrupted(player_type *caster_ptr, effect_monster_type *em_ptr)
+{
+	bool is_corrupted = ((em_ptr->r_ptr->flags3 & (RF3_UNDEAD | RF3_DEMON)) != 0) &&
+		(em_ptr->r_ptr->level > caster_ptr->lev / 2) &&
+		(one_in_(2));
+	if (!is_corrupted)
+	{
+		em_ptr->note = _("には効果がなかった。", " is unaffected.");
+		em_ptr->obvious = FALSE;
+		return;
+	}
+
+	em_ptr->note = NULL;
+	msg_format(_("%^sの堕落した精神は攻撃を跳ね返した！",
+		(em_ptr->seen ? "%^s's corrupted mind backlashes your attack!" :
+			"%^ss corrupted mind backlashes your attack!")), em_ptr->m_name);
+	if (randint0(100 + em_ptr->r_ptr->level / 2) < caster_ptr->skill_sav)
+	{
+		msg_print(_("しかし効力を跳ね返した！", "You resist the effects!"));
+		return;
+	}
+
+	switch (randint1(4))
+	{
+	case 1:
+		set_stun(caster_ptr, caster_ptr->stun + em_ptr->dam / 2);
+		break;
+	case 2:
+		set_confused(caster_ptr, caster_ptr->confused + em_ptr->dam / 2);
+		break;
+	default:
+	{
+		if (em_ptr->r_ptr->flags3 & RF3_NO_FEAR)
+			em_ptr->note = _("には効果がなかった。", " is unaffected.");
+		else
+			set_afraid(caster_ptr, caster_ptr->afraid + em_ptr->dam);
+	}
+	}
+}
+
+
 gf_switch_result effect_monster_domination(player_type *caster_ptr, effect_monster_type *em_ptr)
 {
 	if (!is_hostile(em_ptr->m_ptr)) return GF_SWITCH_CONTINUE;
@@ -57,52 +99,7 @@ gf_switch_result effect_monster_domination(player_type *caster_ptr, effect_monst
 			em_ptr->r_ptr->r_flags3 |= (RF3_NO_CONF);
 
 		em_ptr->do_conf = 0;
-
-		/*
-		 * Powerful demons & undead can turn a mindcrafter's
-		 * attacks back on them
-		 */
-		if ((em_ptr->r_ptr->flags3 & (RF3_UNDEAD | RF3_DEMON)) &&
-			(em_ptr->r_ptr->level > caster_ptr->lev / 2) &&
-			(one_in_(2)))
-		{
-			em_ptr->note = NULL;
-			msg_format(_("%^sの堕落した精神は攻撃を跳ね返した！",
-				(em_ptr->seen ? "%^s's corrupted mind backlashes your attack!" :
-					"%^ss corrupted mind backlashes your attack!")), em_ptr->m_name);
-
-			/* Saving throw */
-			if (randint0(100 + em_ptr->r_ptr->level / 2) < caster_ptr->skill_sav)
-			{
-				msg_print(_("しかし効力を跳ね返した！", "You resist the effects!"));
-			}
-			else
-			{
-				/* Confuse, stun, terrify */
-				switch (randint1(4))
-				{
-				case 1:
-					set_stun(caster_ptr, caster_ptr->stun + em_ptr->dam / 2);
-					break;
-				case 2:
-					set_confused(caster_ptr, caster_ptr->confused + em_ptr->dam / 2);
-					break;
-				default:
-				{
-					if (em_ptr->r_ptr->flags3 & RF3_NO_FEAR)
-						em_ptr->note = _("には効果がなかった。", " is unaffected.");
-					else
-						set_afraid(caster_ptr, caster_ptr->afraid + em_ptr->dam);
-				}
-				}
-			}
-		}
-		else
-		{
-			em_ptr->note = _("には効果がなかった。", " is unaffected.");
-			em_ptr->obvious = FALSE;
-		}
-
+		effect_monster_domination_corrupted(caster_ptr, em_ptr);		
 		em_ptr->dam = 0;
 		return GF_SWITCH_CONTINUE;
 	}
