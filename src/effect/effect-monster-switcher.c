8a3a1fc62fbd8e3c31eb596f89d4dceb6be0d16a
Hourier <hourier@users.sourceforge.jp>
2020-05-02 20:35:58 +0900

    [Refactor] #39963 Separated effect_monster_stasis() from switch_effects_monster()

--- src/effect/effect-monster-switcher.c
+++ src/effect/effect-monster-switcher.c
@@ -80,6 +80,49 @@ gf_switch_result effect_monster_death_ray(player_type *caster_ptr, effect_monste
 }
 
 
+gf_switch_result effect_monster_stasis_evil(player_type *caster_ptr, effect_monster_type *em_ptr)
+{
+	if (em_ptr->seen) em_ptr->obvious = TRUE;
+
+	if ((em_ptr->r_ptr->flags1 & RF1_UNIQUE) ||
+		!(em_ptr->r_ptr->flags3 & RF3_EVIL) ||
+		(em_ptr->r_ptr->level > randint1((em_ptr->dam - 10) < 1 ? 1 : (em_ptr->dam - 10)) + 10))
+	{
+		em_ptr->note = _("には効果がなかった。", " is unaffected.");
+		em_ptr->obvious = FALSE;
+	}
+	else
+	{
+		em_ptr->note = _("は動けなくなった！", " is suspended!");
+		em_ptr->do_sleep = 500;
+	}
+
+	em_ptr->dam = 0;
+	return GF_SWITCH_CONTINUE;
+}
+
+
+gf_switch_result effect_monster_stasis(player_type *caster_ptr, effect_monster_type *em_ptr)
+{
+	if (em_ptr->seen) em_ptr->obvious = TRUE;
+
+	if ((em_ptr->r_ptr->flags1 & RF1_UNIQUE) ||
+		(em_ptr->r_ptr->level > randint1((em_ptr->dam - 10) < 1 ? 1 : (em_ptr->dam - 10)) + 10))
+	{
+		em_ptr->note = _("には効果がなかった。", " is unaffected.");
+		em_ptr->obvious = FALSE;
+	}
+	else
+	{
+		em_ptr->note = _("は動けなくなった！", " is suspended!");
+		em_ptr->do_sleep = 500;
+	}
+
+	em_ptr->dam = 0;
+	return GF_SWITCH_CONTINUE;
+}
+
+
 /*!
  * @brief 魔法の効果によって様々なメッセーを出力したり与えるダメージの増減を行ったりする
  * @param em_ptr モンスター効果構造体への参照ポインタ
@@ -176,44 +219,9 @@ gf_switch_result switch_effects_monster(player_type *caster_ptr, effect_monster_
 	case GF_OLD_SLEEP:
 		return effect_monster_old_sleep(caster_ptr, em_ptr);
 	case GF_STASIS_EVIL:
-	{
-		if (em_ptr->seen) em_ptr->obvious = TRUE;
-
-		if ((em_ptr->r_ptr->flags1 & RF1_UNIQUE) ||
-			!(em_ptr->r_ptr->flags3 & RF3_EVIL) ||
-			(em_ptr->r_ptr->level > randint1((em_ptr->dam - 10) < 1 ? 1 : (em_ptr->dam - 10)) + 10))
-		{
-			em_ptr->note = _("には効果がなかった。", " is unaffected.");
-			em_ptr->obvious = FALSE;
-		}
-		else
-		{
-			em_ptr->note = _("は動けなくなった！", " is suspended!");
-			em_ptr->do_sleep = 500;
-		}
-
-		em_ptr->dam = 0;
-		break;
-	}
+		return effect_monster_stasis_evil(caster_ptr, em_ptr);
 	case GF_STASIS:
-	{
-		if (em_ptr->seen) em_ptr->obvious = TRUE;
-
-		if ((em_ptr->r_ptr->flags1 & RF1_UNIQUE) ||
-			(em_ptr->r_ptr->level > randint1((em_ptr->dam - 10) < 1 ? 1 : (em_ptr->dam - 10)) + 10))
-		{
-			em_ptr->note = _("には効果がなかった。", " is unaffected.");
-			em_ptr->obvious = FALSE;
-		}
-		else
-		{
-			em_ptr->note = _("は動けなくなった！", " is suspended!");
-			em_ptr->do_sleep = 500;
-		}
-
-		em_ptr->dam = 0;
-		break;
-	}
+		return effect_monster_stasis(caster_ptr, em_ptr);
 	case GF_CHARM:
 	{
 		int vir = virtue_number(caster_ptr, V_HARMONY);
