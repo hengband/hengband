2dc2b209be290f511fe8212518574266cfecd8d3
Hourier <hourier@users.sourceforge.jp>
2020-05-04 22:36:56 +0900

    [WIP] [Refactor] #39963 Separated process-effect./h from spells.h/spells1.c

--- src/spells.h
+++ src/spells.h
@@ -5,7 +5,6 @@
 extern bool in_disintegration_range(floor_type *floor_ptr, POSITION y1, POSITION x1, POSITION y2, POSITION x2);
 extern void breath_shape(player_type *caster_ptr, u16b *path_g, int dist, int *pgrids, POSITION *gx, POSITION *gy, POSITION *gm, POSITION *pgm_rad, POSITION rad, POSITION y1, POSITION x1, POSITION y2, POSITION x2, EFFECT_ID typ);
 extern POSITION dist_to_line(POSITION y, POSITION x, POSITION y1, POSITION x1, POSITION y2, POSITION x2);
-extern bool project(player_type *caster_ptr, MONSTER_IDX who, POSITION rad, POSITION y, POSITION x, HIT_POINT dam, EFFECT_ID typ, BIT_FLAGS flg, int monspell);
 extern bool binding_field(player_type *caster_ptr, HIT_POINT dam);
 extern void seal_of_mirror(player_type *caster_ptr, HIT_POINT dam);
 extern concptr spell_category_name(OBJECT_TYPE_VALUE tval);
