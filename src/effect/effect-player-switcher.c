2bf57b806aa63548e6424d065a98738bee6d0bb3
Hourier <hourier@users.sourceforge.jp>
2020-05-04 21:53:49 +0900

    [Refactor] #39963 Separated effect-player-curse.c/h from effect-player-switcher.c

--- src/effect/effect-player-switcher.c
+++ src/effect/effect-player-switcher.c
@@ -1,5 +1,6 @@
 #include "angband.h"
 #include "effect-player-util.h"
+#include "effect/effect-player-curse.h"
 #include "effect/effect-player-switcher.h"
 #include "effect/effect-player-oldies.h"
 #include "effect/effect-player-resist-hurt.h"
@@ -240,63 +241,6 @@ void effect_player_brain_smash(player_type *target_ptr,
 
   if (!target_ptr->resist_chaos) {
     (void)set_image(target_ptr, target_ptr->image + randint0(250) + 150);
-  }
-}
-
-void effect_player_curse_1(player_type *target_ptr,
-                           effect_player_type *ep_ptr) {
-  if ((randint0(100 + ep_ptr->rlev / 2) < target_ptr->skill_sav) &&
-      !CHECK_MULTISHADOW(target_ptr)) {
-    msg_print(_("しかし効力を跳ね返した！", "You resist the effects!"));
-    learn_spell(target_ptr, ep_ptr->monspell);
-  } else {
-    if (!CHECK_MULTISHADOW(target_ptr))
-      curse_equipment(target_ptr, 15, 0);
-    ep_ptr->get_damage = take_hit(target_ptr, DAMAGE_ATTACK, ep_ptr->dam,
-                                  ep_ptr->killer, ep_ptr->monspell);
-  }
-}
-
-void effect_player_curse_2(player_type *target_ptr,
-                           effect_player_type *ep_ptr) {
-  if ((randint0(100 + ep_ptr->rlev / 2) < target_ptr->skill_sav) &&
-      !CHECK_MULTISHADOW(target_ptr)) {
-    msg_print(_("しかし効力を跳ね返した！", "You resist the effects!"));
-    learn_spell(target_ptr, ep_ptr->monspell);
-  } else {
-    if (!CHECK_MULTISHADOW(target_ptr))
-      curse_equipment(target_ptr, 25, MIN(ep_ptr->rlev / 2 - 15, 5));
-    ep_ptr->get_damage = take_hit(target_ptr, DAMAGE_ATTACK, ep_ptr->dam,
-                                  ep_ptr->killer, ep_ptr->monspell);
-  }
-}
-
-void effect_player_curse_3(player_type *target_ptr,
-                           effect_player_type *ep_ptr) {
-  if ((randint0(100 + ep_ptr->rlev / 2) < target_ptr->skill_sav) &&
-      !CHECK_MULTISHADOW(target_ptr)) {
-    msg_print(_("しかし効力を跳ね返した！", "You resist the effects!"));
-    learn_spell(target_ptr, ep_ptr->monspell);
-  } else {
-    if (!CHECK_MULTISHADOW(target_ptr))
-      curse_equipment(target_ptr, 33, MIN(ep_ptr->rlev / 2 - 15, 15));
-    ep_ptr->get_damage = take_hit(target_ptr, DAMAGE_ATTACK, ep_ptr->dam,
-                                  ep_ptr->killer, ep_ptr->monspell);
-  }
-}
-
-void effect_player_curse_4(player_type *target_ptr,
-                           effect_player_type *ep_ptr) {
-  if ((randint0(100 + ep_ptr->rlev / 2) < target_ptr->skill_sav) &&
-      !(ep_ptr->m_ptr->r_idx == MON_KENSHIROU) &&
-      !CHECK_MULTISHADOW(target_ptr)) {
-    msg_print(_("しかし秘孔を跳ね返した！", "You resist the effects!"));
-    learn_spell(target_ptr, ep_ptr->monspell);
-  } else {
-    ep_ptr->get_damage = take_hit(target_ptr, DAMAGE_ATTACK, ep_ptr->dam,
-                                  ep_ptr->killer, ep_ptr->monspell);
-    if (!CHECK_MULTISHADOW(target_ptr))
-      (void)set_cut(target_ptr, target_ptr->cut + damroll(10, 10));
   }
 }
 
