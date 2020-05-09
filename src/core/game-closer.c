a96a668a4dbd2dafe508b7461c350b42818dca92
Hourier <hourier@users.sourceforge.jp>
2020-05-09 14:45:49 +0900

    [Refactor] #39970 Inverted if-condition in close_game()

--- src/core/game-closer.c
+++ src/core/game-closer.c
@@ -47,43 +47,47 @@ void close_game(player_type* player_ptr)
     highscore_fd = fd_open(buf, O_RDWR);
     safe_setuid_drop();
 
-    if (player_ptr->is_dead) {
-        if (current_world_ptr->total_winner)
-            kingly(player_ptr);
-
-        if (!cheat_save || get_check(_("死んだデータをセーブしますか？ ", "Save death? "))) {
-            if (!save_player(player_ptr))
-                msg_print(_("セーブ失敗！", "death save failed!"));
-        } else
-            do_send = FALSE;
-
-        print_tomb(player_ptr);
-        flush();
-
-        show_info(player_ptr, handle_stuff, update_playtime, display_player, map_name);
-        Term_clear();
-
-        if (check_score(player_ptr)) {
-            if ((!send_world_score(player_ptr, do_send, update_playtime, display_player, map_name))) {
-                if (get_check_strict(_("後でスコアを登録するために待機しますか？", "Stand by for later score registration? "),
-                        (CHECK_NO_ESCAPE | CHECK_NO_HISTORY))) {
-                    player_ptr->wait_report_score = TRUE;
-                    player_ptr->is_dead = FALSE;
-                    if (!save_player(player_ptr))
-                        msg_print(_("セーブ失敗！", "death save failed!"));
-                }
-            }
-            if (!player_ptr->wait_report_score)
-                (void)top_twenty(player_ptr);
-        } else if (highscore_fd >= 0) {
-            display_scores_aux(0, 10, -1, NULL);
-        }
-    } else {
+    if (!player_ptr->is_dead) {
         do_cmd_save_game(player_ptr, FALSE);
         prt(_("リターンキーか ESC キーを押して下さい。", "Press Return (or Escape)."), 0, 40);
         play_music(TERM_XTRA_MUSIC_BASIC, MUSIC_BASIC_EXIT);
         if (inkey() != ESCAPE)
             predict_score(player_ptr);
+
+        clear_floor(player_ptr);
+        return;
+    }
+
+    if (current_world_ptr->total_winner)
+        kingly(player_ptr);
+
+    if (!cheat_save || get_check(_("死んだデータをセーブしますか？ ", "Save death? "))) {
+        if (!save_player(player_ptr))
+            msg_print(_("セーブ失敗！", "death save failed!"));
+    } else
+        do_send = FALSE;
+
+    print_tomb(player_ptr);
+    flush();
+
+    show_info(player_ptr, handle_stuff, update_playtime, display_player, map_name);
+    Term_clear();
+
+    if (check_score(player_ptr)) {
+        if ((!send_world_score(player_ptr, do_send, update_playtime, display_player, map_name))) {
+            if (get_check_strict(_("後でスコアを登録するために待機しますか？", "Stand by for later score registration? "),
+                    (CHECK_NO_ESCAPE | CHECK_NO_HISTORY))) {
+                player_ptr->wait_report_score = TRUE;
+                player_ptr->is_dead = FALSE;
+                if (!save_player(player_ptr))
+                    msg_print(_("セーブ失敗！", "death save failed!"));
+            }
+        }
+
+        if (!player_ptr->wait_report_score)
+            (void)top_twenty(player_ptr);
+    } else if (highscore_fd >= 0) {
+        display_scores_aux(0, 10, -1, NULL);
     }
 
     clear_floor(player_ptr);
