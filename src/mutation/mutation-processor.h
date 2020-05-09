f3369f127c0ae44d3dbc41e353efdc1cd1f32300
Hourier <hourier@users.sourceforge.jp>
2020-05-09 16:53:49 +0900

    [Refactor] #39970 Separated lite-processor.c/h and mutation-processor.c/h from core.c

---
+++ src/mutation/mutation-processor.h
@@ -0,0 +1,3 @@
+﻿#pragma once
+
+void process_world_aux_mutation(player_type* creature_ptr);
---
+++ src/object/lite-processor.c
@@ -0,0 +1,61 @@
+﻿#include "angband.h"
+#include "object/lite-processor.h"
+#include "object-ego.h"
+#include "world.h"
+#include "player-move.h"
+#include "object-hook.h"
+
+/*!
+ * @brief 10ゲームターンが進行する毎に光源の寿命を減らす処理
+ * / Handle burning fuel every 10 game turns
+ * @return なし
+ */
+void process_world_aux_light(player_type* creature_ptr)
+{
+    object_type* o_ptr = &creature_ptr->inventory_list[INVEN_LITE];
+    if (o_ptr->tval == TV_LITE) {
+        if (!(object_is_fixed_artifact(o_ptr) || o_ptr->sval == SV_LITE_FEANOR) && (o_ptr->xtra4 > 0)) {
+            if (o_ptr->name2 == EGO_LITE_LONG) {
+                if (current_world_ptr->game_turn % (TURNS_PER_TICK * 2))
+                    o_ptr->xtra4--;
+            } else
+                o_ptr->xtra4--;
+
+            notice_lite_change(creature_ptr, o_ptr);
+        }
+    }
+}
+
+/*!
+ * @brief 寿命つき光源の警告メッセージ処理
+ * @param creature_ptr プレーヤーへの参照ポインタ
+ * @param o_ptr 現在光源として使っているオブジェクトの構造体参照ポインタ
+ * @return なし
+ */
+void notice_lite_change(player_type* creature_ptr, object_type* o_ptr)
+{
+    if ((o_ptr->xtra4 < 100) || (!(o_ptr->xtra4 % 100))) {
+        creature_ptr->window |= (PW_EQUIP);
+    }
+
+    if (creature_ptr->blind) {
+        if (o_ptr->xtra4 == 0)
+            o_ptr->xtra4++;
+    } else if (o_ptr->xtra4 == 0) {
+        disturb(creature_ptr, FALSE, TRUE);
+        msg_print(_("明かりが消えてしまった！", "Your light has gone out!"));
+        creature_ptr->update |= (PU_TORCH);
+        creature_ptr->update |= (PU_BONUS);
+    } else if (o_ptr->name2 == EGO_LITE_LONG) {
+        if ((o_ptr->xtra4 < 50) && (!(o_ptr->xtra4 % 5))
+            && (current_world_ptr->game_turn % (TURNS_PER_TICK * 2))) {
+            if (disturb_minor)
+                disturb(creature_ptr, FALSE, TRUE);
+            msg_print(_("明かりが微かになってきている。", "Your light is growing faint."));
+        }
+    } else if ((o_ptr->xtra4 < 100) && (!(o_ptr->xtra4 % 10))) {
+        if (disturb_minor)
+            disturb(creature_ptr, FALSE, TRUE);
+        msg_print(_("明かりが微かになってきている。", "Your light is growing faint."));
+    }
+}
