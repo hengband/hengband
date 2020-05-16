305155ec72d4721ecb250a25daa0beb2a5b60938
Hourier <hourier@users.sourceforge.jp>
2020-05-16 17:57:56 +0900

    [Refactor] #40413 Separated cmd-menu-content-table.c/h from util.c

---
+++ src/cmd/cmd-menu-content-table.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+typedef struct menu_content {
+    concptr name;
+    byte cmd;
+    bool fin;
+} menu_content;
+
+#define MAX_COMMAND_PER_SCREEN 10
+#define MAX_COMMAND_MENU_NUM 10
+
+extern menu_content menu_info[MAX_COMMAND_MENU_NUM][MAX_COMMAND_PER_SCREEN];
