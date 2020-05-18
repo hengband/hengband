7059bfaecfdafb2ba9d2434e6c382d05f2748ce3
Hourier <hourier@users.sourceforge.jp>
2020-05-18 17:20:44 +0900

    [Refactor] #40392 Added Doxygen comments to get_max_stats()

--- src/birth/birth-stat.h
+++ src/birth/birth-stat.h
@@ -2,5 +2,6 @@
 
 int adjust_stat(int value, int amount);
 void get_stats(player_type* creature_ptr);
-void get_max_stats(player_type* creature_ptr);
 void get_extra(player_type* creature_ptr, bool roll_hitdie);
+
+void get_max_stats(player_type* creature_ptr);
