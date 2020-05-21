643be7eb7f3c54225a47b78f2bce2c2449bd520e
Hourier <hourier@users.sourceforge.jp>
2020-05-21 19:44:57 +0900

    [Refactor] #40416 Separated samurai-slaying.c/h from melee.c

---
+++ src/mind/samurai-slaying.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "combat/combat-options-type.h"
+
+MULTIPLY mult_hissatsu(player_type *attacker_ptr, MULTIPLY mult, BIT_FLAGS *flgs, monster_type *m_ptr, combat_options mode);
