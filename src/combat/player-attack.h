9b903f00cf04b447d82295869f9f6dcd6cc0e599
Hourier <hourier@users.sourceforge.jp>
2020-05-22 20:33:32 +0900

    [Refactor] #40416 Separated player-attack.c/h from melee.c

---
+++ src/combat/player-attack.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include "combat/combat-options-type.h"
+
+void exe_player_attack_to_monster(player_type *attacker_ptr, POSITION y, POSITION x, bool *fear, bool *mdeath, s16b hand, combat_options mode);
