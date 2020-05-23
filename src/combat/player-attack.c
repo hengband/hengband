dc01785cf18f48c386d2ecf8f2f3a2f55faee0d5
Hourier <hourier@users.sourceforge.jp>
2020-05-23 16:08:59 +0900

    [Refactor] #40416 Separated process_vorpal_attack() from exe_player_attack_to_monster()

--- src/combat/player-attack.c
+++ src/combat/player-attack.c
@@ -310,6 +310,38 @@ static void print_vorpal_message(player_attack_type *pa_ptr, const int magnifica
     }
 }
 
+static void process_vorpal_attack(player_type *attacker_ptr, player_attack_type *pa_ptr, const bool vorpal_cut, const int vorpal_chance)
+{
+    if (!vorpal_cut)
+        return;
+
+    object_type *o_ptr = &attacker_ptr->inventory_list[INVEN_RARM + pa_ptr->hand];
+    int vorpal_magnification = 2;
+    if ((o_ptr->name1 == ART_CHAINSWORD) && !one_in_(2)) {
+        char chainsword_noise[1024];
+        if (!get_rnd_line(_("chainswd_j.txt", "chainswd.txt"), 0, chainsword_noise)) {
+            msg_print(chainsword_noise);
+        }
+    }
+
+    if (o_ptr->name1 == ART_VORPAL_BLADE)
+        msg_print(_("目にも止まらぬヴォーパルブレード、手錬の早業！", "Your Vorpal Blade goes snicker-snack!"));
+    else
+        msg_format(_("%sをグッサリ切り裂いた！", "Your weapon cuts deep into %s!"), pa_ptr->m_name);
+
+    while (one_in_(vorpal_chance))
+        vorpal_magnification++;
+
+    pa_ptr->attack_damage *= (HIT_POINT)vorpal_magnification;
+    monster_race *r_ptr = &r_info[pa_ptr->m_ptr->r_idx];
+    if (((r_ptr->flagsr & RFR_RES_ALL) ? pa_ptr->attack_damage / 100 : pa_ptr->attack_damage) > pa_ptr->m_ptr->hp)
+        msg_format(_("%sを真っ二つにした！", "You cut %s in half!"), pa_ptr->m_name);
+    else
+        print_vorpal_message(pa_ptr, vorpal_magnification);
+
+    pa_ptr->drain_result = pa_ptr->drain_result * 3 / 2;
+}
+
 /*!
  * @brief プレイヤーの打撃処理サブルーチン /
  * Player attacks a (poor, defenseless) creature        -RAK-
@@ -390,37 +422,7 @@ void exe_player_attack_to_monster(player_type *attacker_ptr, POSITION y, POSITIO
                 pa_ptr->attack_damage = critical_norm(attacker_ptr, o_ptr->weight, o_ptr->to_h, pa_ptr->attack_damage, attacker_ptr->to_h[hand], mode);
 
             pa_ptr->drain_result = pa_ptr->attack_damage;
-
-            if (vorpal_cut) {
-                int vorpal_magnification = 2;
-
-                if ((o_ptr->name1 == ART_CHAINSWORD) && !one_in_(2)) {
-                    char chainsword_noise[1024];
-                    if (!get_rnd_line(_("chainswd_j.txt", "chainswd.txt"), 0, chainsword_noise)) {
-                        msg_print(chainsword_noise);
-                    }
-                }
-
-                if (o_ptr->name1 == ART_VORPAL_BLADE) {
-                    msg_print(_("目にも止まらぬヴォーパルブレード、手錬の早業！", "Your Vorpal Blade goes snicker-snack!"));
-                } else {
-                    msg_format(_("%sをグッサリ切り裂いた！", "Your weapon cuts deep into %s!"), pa_ptr->m_name);
-                }
-
-                /* Try to increase the damage */
-                while (one_in_(vorpal_chance)) {
-                    vorpal_magnification++;
-                }
-
-                pa_ptr->attack_damage *= (HIT_POINT)vorpal_magnification;
-                if (((r_ptr->flagsr & RFR_RES_ALL) ? pa_ptr->attack_damage / 100 : pa_ptr->attack_damage) > m_ptr->hp)
-                    msg_format(_("%sを真っ二つにした！", "You cut %s in half!"), pa_ptr->m_name);
-                else
-                    print_vorpal_message(pa_ptr, vorpal_magnification);
-                
-                pa_ptr->drain_result = pa_ptr->drain_result * 3 / 2;
-            }
-
+            process_vorpal_attack(attacker_ptr, pa_ptr, vorpal_cut, vorpal_chance);
             pa_ptr->attack_damage += o_ptr->to_d;
             pa_ptr->drain_result += o_ptr->to_d;
         }
