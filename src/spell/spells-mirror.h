16fa6083c2edd60738b598a950ab0ad000d2c4af
Hourier <hourier@users.sourceforge.jp>
2020-06-03 21:09:24 +0900

    [Refactor] #40414 Separated spells-mirror.c/h from spells1.c/h

---
+++ src/spell/spells-mirror.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include "system/angband.h"
+
+void seal_of_mirror(player_type *caster_ptr, HIT_POINT dam);
--- src/spell/spells1.c
+++ src/spell/spells1.c
@@ -458,33 +458,6 @@ bool binding_field(player_type *caster_ptr, HIT_POINT dam)
 
 
 /*!
- * @brief 鏡魔法「鏡の封印」の効果処理
- * @param dam ダメージ量
- * @return 効果があったらTRUEを返す
- */
-void seal_of_mirror(player_type *caster_ptr, HIT_POINT dam)
-{
-	for (POSITION x = 0; x < caster_ptr->current_floor_ptr->width; x++)
-	{
-		for (POSITION y = 0; y < caster_ptr->current_floor_ptr->height; y++)
-		{
-			if (!is_mirror_grid(&caster_ptr->current_floor_ptr->grid_array[y][x]))
-				continue;
-
-			if (!affect_monster(caster_ptr, 0, 0, y, x, dam, GF_GENOCIDE,
-				(PROJECT_GRID | PROJECT_ITEM | PROJECT_KILL | PROJECT_JUMP), TRUE))
-				continue;
-
-			if (!caster_ptr->current_floor_ptr->grid_array[y][x].m_idx)
-			{
-				remove_mirror(caster_ptr, y, x);
-			}
-		}
-	}
-}
-
-
-/*!
  * @brief 領域魔法に応じて技能の名称を返す。
  * @param tval 魔法書のtval
  * @return 領域魔法の技能名称を保管した文字列ポインタ
