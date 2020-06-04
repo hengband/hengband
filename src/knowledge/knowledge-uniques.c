a426db52bcf554b8ce2c961e17aeac426edaec5c
Hourier <hourier@users.sourceforge.jp>
2020-06-04 23:27:53 +0900

    [Implement] #40465 撃破したユニークモンスターリストを出力するようにした/ Added output the list of having defeated unique monsters

--- src/knowledge/knowledge-uniques.c
+++ src/knowledge/knowledge-uniques.c
@@ -10,21 +10,20 @@
 #include "knowledge-items.h"
 #include "system/angband.h"
 
-/*
- * Display known uniques
- * With "XTRA HACK UNIQHIST" (Originally from XAngband)
+/*!
+ * @param is_alive 生きているユニークのリストならばTRUE、撃破したユニークのリストならばFALSE
  */
-void do_cmd_knowledge_uniques(player_type *creature_ptr)
+void do_cmd_knowledge_uniques(player_type *creature_ptr, bool is_alive)
 {
     u16b why = 2;
     IDX *who;
-    int n_alive[10];
-    int n_alive_surface = 0;
-    int n_alive_over100 = 0;
-    int n_alive_total = 0;
+    int num_uniques[10];
+    int num_uniques_surface = 0;
+    int num_uniques_over100 = 0;
+    int num_uniques_total = 0;
     int max_lev = -1;
     for (IDX i = 0; i < 10; i++)
-        n_alive[i] = 0;
+        num_uniques[i] = 0;
 
     FILE *fff = NULL;
     GAME_TEXT file_name[FILE_NAME_SIZE];
@@ -43,44 +42,59 @@ void do_cmd_knowledge_uniques(player_type *creature_ptr)
             continue;
         if (!r_ptr->rarity || ((r_ptr->rarity > 100) && !(r_ptr->flags1 & RF1_QUESTOR)))
             continue;
-        if (r_ptr->max_num == 0)
-            continue;
+        if (is_alive) {
+            if (r_ptr->max_num == 0)
+                continue;
+        } else {
+            if (r_ptr->max_num > 0)
+                continue;
+        }
 
         if (r_ptr->level) {
             int lev = (r_ptr->level - 1) / 10;
             if (lev < 10) {
-                n_alive[lev]++;
+                num_uniques[lev]++;
                 if (max_lev < lev)
                     max_lev = lev;
             } else
-                n_alive_over100++;
+                num_uniques_over100++;
         } else
-            n_alive_surface++;
+            num_uniques_surface++;
 
         who[n++] = i;
     }
 
     ang_sort(who, &why, n, ang_sort_comp_hook, ang_sort_swap_hook);
-    if (n_alive_surface) {
-        fprintf(fff, _("     地上  生存: %3d体\n", "      Surface  alive: %3d\n"), n_alive_surface);
-        n_alive_total += n_alive_surface;
+    if (num_uniques_surface) {
+        concptr surface_desc
+            = is_alive ? _("     地上  生存: %3d体\n", "      Surface  alive: %3d\n") : _("     地上  死亡: %3d体\n", "      Surface  dead: %3d\n");
+        fprintf(fff, surface_desc, num_uniques_surface);
+        num_uniques_total += num_uniques_surface;
     }
 
     for (IDX i = 0; i <= max_lev; i++) {
-        fprintf(fff, _("%3d-%3d階  生存: %3d体\n", "Level %3d-%3d  alive: %3d\n"), 1 + i * 10, 10 + i * 10, n_alive[i]);
-        n_alive_total += n_alive[i];
+        concptr dungeon_desc
+            = is_alive ? _("%3d-%3d階  生存: %3d体\n", "Level %3d-%3d  alive: %3d\n") : _("%3d-%3d階  死亡: %3d体\n", "Level %3d-%3d  dead: %3d\n");
+        fprintf(fff, dungeon_desc, 1 + i * 10, 10 + i * 10, num_uniques[i]);
+        num_uniques_total += num_uniques[i];
     }
 
-    if (n_alive_over100) {
-        fprintf(fff, _("101-   階  生存: %3d体\n", "Level 101-     alive: %3d\n"), n_alive_over100);
-        n_alive_total += n_alive_over100;
+    if (num_uniques_over100) {
+        concptr deep_desc
+            = is_alive ? _("101-   階  生存: %3d体\n", "Level 101-     alive: %3d\n") : _("101-   階  死亡: %3d体\n", "Level 101-     dead: %3d\n");
+        fprintf(fff, deep_desc, num_uniques_over100);
+        num_uniques_total += num_uniques_over100;
     }
 
-    if (n_alive_total) {
+    if (num_uniques_total) {
         fputs(_("---------  -----------\n", "-------------  ----------\n"), fff);
-        fprintf(fff, _("     合計  生存: %3d体\n\n", "        Total  alive: %3d\n\n"), n_alive_total);
+        concptr total_desc
+            = is_alive ? _("     合計  生存: %3d体\n\n", "        Total  alive: %3d\n\n") : _("     合計  死亡: %3d体\n\n", "        Total  dead: %3d\n\n");
+        fprintf(fff, total_desc, num_uniques_total);
     } else {
-        fputs(_("現在は既知の生存ユニークはいません。\n", "No known uniques alive.\n"), fff);
+        concptr no_unique_desc = is_alive ? _("現在は既知の生存ユニークはいません。\n", "No known uniques alive.\n")
+                                          : _("現在は既知の撃破ユニークはいません。\n", "No known uniques dead.\n");
+        fputs(no_unique_desc, fff);
     }
 
     for (int k = 0; k < n; k++) {
@@ -90,6 +104,7 @@ void do_cmd_knowledge_uniques(player_type *creature_ptr)
 
     C_KILL(who, max_r_idx, s16b);
     my_fclose(fff);
-    (void)show_file(creature_ptr, TRUE, file_name, _("まだ生きているユニーク・モンスター", "Alive Uniques"), 0, 0);
+    concptr title_desc = is_alive ? _("まだ生きているユニーク・モンスター", "Alive Uniques") : _("もう撃破したユニーク・モンスター", "Dead Uniques");
+    (void)show_file(creature_ptr, TRUE, file_name, title_desc, 0, 0);
     fd_kill(file_name);
 }
