a8952520a28a2da05f9c4e4d3d7cdbe48fcb2c10
Hourier <hourier@users.sourceforge.jp>
2020-06-11 22:45:05 +0900

    [Refactor] #40414 Separated set_flags1() from process_monster_lore()

--- src/lore/monster-lore.c
+++ src/lore/monster-lore.c
@@ -187,6 +187,30 @@ static void set_msex_flags(lore_type *lore_ptr)
         lore_ptr->msex = MSEX_MALE;
 }
 
+static void set_flags1(lore_type *lore_ptr)
+{
+    if (lore_ptr->r_ptr->flags1 & RF1_UNIQUE)
+        lore_ptr->flags1 |= (RF1_UNIQUE);
+
+    if (lore_ptr->r_ptr->flags1 & RF1_QUESTOR)
+        lore_ptr->flags1 |= (RF1_QUESTOR);
+
+    if (lore_ptr->r_ptr->flags1 & RF1_MALE)
+        lore_ptr->flags1 |= (RF1_MALE);
+
+    if (lore_ptr->r_ptr->flags1 & RF1_FEMALE)
+        lore_ptr->flags1 |= (RF1_FEMALE);
+
+    if (lore_ptr->r_ptr->flags1 & RF1_FRIENDS)
+        lore_ptr->flags1 |= (RF1_FRIENDS);
+
+    if (lore_ptr->r_ptr->flags1 & RF1_ESCORT)
+        lore_ptr->flags1 |= (RF1_ESCORT);
+
+    if (lore_ptr->r_ptr->flags1 & RF1_ESCORTS)
+        lore_ptr->flags1 |= (RF1_ESCORTS);
+}
+
 static void set_race_flags(lore_type *lore_ptr)
 {
     if (!lore_ptr->r_ptr->r_tkills && !lore_ptr->know_everything)
@@ -260,27 +284,7 @@ void process_monster_lore(player_type *player_ptr, MONRACE_IDX r_idx, BIT_FLAGS 
 
     set_drop_flags(lore_ptr);
     set_msex_flags(lore_ptr);
-    if (lore_ptr->r_ptr->flags1 & RF1_UNIQUE)
-        lore_ptr->flags1 |= (RF1_UNIQUE);
-
-    if (lore_ptr->r_ptr->flags1 & RF1_QUESTOR)
-        lore_ptr->flags1 |= (RF1_QUESTOR);
-
-    if (lore_ptr->r_ptr->flags1 & RF1_MALE)
-        lore_ptr->flags1 |= (RF1_MALE);
-
-    if (lore_ptr->r_ptr->flags1 & RF1_FEMALE)
-        lore_ptr->flags1 |= (RF1_FEMALE);
-
-    if (lore_ptr->r_ptr->flags1 & RF1_FRIENDS)
-        lore_ptr->flags1 |= (RF1_FRIENDS);
-
-    if (lore_ptr->r_ptr->flags1 & RF1_ESCORT)
-        lore_ptr->flags1 |= (RF1_ESCORT);
-
-    if (lore_ptr->r_ptr->flags1 & RF1_ESCORTS)
-        lore_ptr->flags1 |= (RF1_ESCORTS);
-
+    set_flags1(lore_ptr);
     set_race_flags(lore_ptr);
     display_kill_numbers(lore_ptr);
     concptr tmp = r_text + lore_ptr->r_ptr->text;
