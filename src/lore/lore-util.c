655ace417d075d261616132c724b694c5c06b22b
Hourier <hourier@users.sourceforge.jp>
2020-06-11 21:21:08 +0900

    [Refactor] #40014 Made lore-util.c/h to prepare to divide monster-lore.c

---
+++ src/lore/lore-util.c
@@ -0,0 +1,20 @@
+﻿#include "lore/lore-util.h"
+
+lore_type *initialize_lore_type(lore_type *lore_ptr, MONRACE_IDX r_idx, BIT_FLAGS mode)
+{
+    lore_ptr->nightmare = ironman_nightmare && !(mode & 0x02);
+    lore_ptr->r_ptr = &r_info[r_idx];
+    lore_ptr->speed = lore_ptr->nightmare ? lore_ptr->r_ptr->speed + 5 : lore_ptr->r_ptr->speed;
+    lore_ptr->drop_gold = lore_ptr->r_ptr->r_drop_gold;
+    lore_ptr->drop_item = lore_ptr->r_ptr->r_drop_item;
+    lore_ptr->flags1 = (lore_ptr->r_ptr->flags1 & lore_ptr->r_ptr->r_flags1);
+    lore_ptr->flags2 = (lore_ptr->r_ptr->flags2 & lore_ptr->r_ptr->r_flags2);
+    lore_ptr->flags3 = (lore_ptr->r_ptr->flags3 & lore_ptr->r_ptr->r_flags3);
+    lore_ptr->flags4 = (lore_ptr->r_ptr->flags4 & lore_ptr->r_ptr->r_flags4);
+    lore_ptr->a_ability_flags1 = (lore_ptr->r_ptr->a_ability_flags1 & lore_ptr->r_ptr->r_flags5);
+    lore_ptr->a_ability_flags2 = (lore_ptr->r_ptr->a_ability_flags2 & lore_ptr->r_ptr->r_flags6);
+    lore_ptr->flags7 = (lore_ptr->r_ptr->flags7 & lore_ptr->r_ptr->flags7);
+    lore_ptr->flagsr = (lore_ptr->r_ptr->flagsr & lore_ptr->r_ptr->r_flagsr);
+    lore_ptr->reinforce = FALSE;
+    return lore_ptr;
+}
---
+++ src/lore/lore-util.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "system/angband.h"
+#include "monster-race/monster-race.h"
+
+typedef struct lore_type {
+#ifdef JP
+    char jverb_buf[64];
+#else
+    bool sin = FALSE;
+#endif
+    bool nightmare;
+    monster_race *r_ptr;
+    SPEED speed;
+    ITEM_NUMBER drop_gold;
+    ITEM_NUMBER drop_item;
+    BIT_FLAGS flags1;
+    BIT_FLAGS flags2;
+    BIT_FLAGS flags3;
+    BIT_FLAGS flags4;
+    BIT_FLAGS a_ability_flags1;
+    BIT_FLAGS a_ability_flags2;
+    BIT_FLAGS flags7;
+    BIT_FLAGS flagsr;
+    bool reinforce;
+} lore_type;
+
+lore_type *initialize_lore_type(lore_type *lore_ptr, MONRACE_IDX r_idx, BIT_FLAGS mode);
