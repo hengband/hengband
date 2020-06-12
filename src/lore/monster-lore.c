b9d1d061e625f1f1611b8ec43df30a4cbff03d55
Hourier <hourier@users.sourceforge.jp>
2020-06-12 21:27:44 +0900

    [Refactor] #40014 Separated display-lore-status.c/h from monster-lore.c

--- src/lore/monster-lore.c
+++ src/lore/monster-lore.c
@@ -25,6 +25,7 @@
 #include "term/term-color-types.h"
 #include "util/util.h"
 #include "view/display-lore-magics.h"
+#include "view/display-lore-status.h"
 #include "view/display-lore.h"
 
 /*!
@@ -197,68 +198,10 @@ void process_monster_lore(player_type *player_ptr, MONRACE_IDX r_idx, BIT_FLAGS 
     set_summon_types(lore_ptr);
     display_monster_magic_types(lore_ptr);
     display_mosnter_magic_possibility(lore_ptr);
-    if (lore_ptr->know_everything || know_armour(r_idx)) {
-        hooked_roff(format(_("%^sは AC%d の防御力と", "%^s has an armor rating of %d"), wd_he[lore_ptr->msex], lore_ptr->r_ptr->ac));
-
-        if ((lore_ptr->flags1 & RF1_FORCE_MAXHP) || (lore_ptr->r_ptr->hside == 1)) {
-            u32b hp = lore_ptr->r_ptr->hdice * (lore_ptr->nightmare ? 2 : 1) * lore_ptr->r_ptr->hside;
-            hooked_roff(format(_(" %d の体力がある。", " and a life rating of %d.  "), (s16b)MIN(30000, hp)));
-        } else {
-            hooked_roff(format(
-                _(" %dd%d の体力がある。", " and a life rating of %dd%d.  "), lore_ptr->r_ptr->hdice * (lore_ptr->nightmare ? 2 : 1), lore_ptr->r_ptr->hside));
-        }
-    }
+    display_monster_hp_ac(lore_ptr);
 
     lore_ptr->vn = 0;
-    if (lore_ptr->flags7 & (RF7_HAS_LITE_1 | RF7_HAS_LITE_2)) {
-        lore_ptr->vp[lore_ptr->vn] = _("ダンジョンを照らす", "illuminate the dungeon");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags7 & (RF7_HAS_DARK_1 | RF7_HAS_DARK_2)) {
-        lore_ptr->vp[lore_ptr->vn] = _("ダンジョンを暗くする", "darken the dungeon");
-        lore_ptr->color[lore_ptr->vn++] = TERM_L_DARK;
-    }
-    if (lore_ptr->flags2 & RF2_OPEN_DOOR) {
-        lore_ptr->vp[lore_ptr->vn] = _("ドアを開ける", "open doors");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags2 & RF2_BASH_DOOR) {
-        lore_ptr->vp[lore_ptr->vn] = _("ドアを打ち破る", "bash down doors");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags7 & RF7_CAN_FLY) {
-        lore_ptr->vp[lore_ptr->vn] = _("空を飛ぶ", "fly");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags7 & RF7_CAN_SWIM) {
-        lore_ptr->vp[lore_ptr->vn] = _("水を渡る", "swim");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags2 & RF2_PASS_WALL) {
-        lore_ptr->vp[lore_ptr->vn] = _("壁をすり抜ける", "pass through walls");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags2 & RF2_KILL_WALL) {
-        lore_ptr->vp[lore_ptr->vn] = _("壁を掘り進む", "bore through walls");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags2 & RF2_MOVE_BODY) {
-        lore_ptr->vp[lore_ptr->vn] = _("弱いモンスターを押しのける", "push past weaker monsters");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags2 & RF2_KILL_BODY) {
-        lore_ptr->vp[lore_ptr->vn] = _("弱いモンスターを倒す", "destroy weaker monsters");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags2 & RF2_TAKE_ITEM) {
-        lore_ptr->vp[lore_ptr->vn] = _("アイテムを拾う", "pick up objects");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-    if (lore_ptr->flags2 & RF2_KILL_ITEM) {
-        lore_ptr->vp[lore_ptr->vn] = _("アイテムを壊す", "destroy objects");
-        lore_ptr->color[lore_ptr->vn++] = TERM_WHITE;
-    }
-
+    display_monster_concrete_abilities(lore_ptr);
     if (lore_ptr->vn > 0) {
         hooked_roff(format(_("%^sは", "%^s"), wd_he[lore_ptr->msex]));
         for (int n = 0; n < lore_ptr->vn; n++) {
---
+++ src/view/display-lore-status.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "system/angband.h"
+#include "lore/lore-util.h"
+
+void display_monster_hp_ac(lore_type *lore_ptr);
+void display_monster_concrete_abilities(lore_type *lore_ptr);
