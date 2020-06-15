5d7b580112b2f54167707bc362f61dc1c1afe6e0
Eric Branlund <ebranlund@fastmail.com>
2020-06-14 22:53:53 -0700

    Put prototypes inside include guard.

--- src/main/init.h
+++ src/main/init.h
@@ -16,10 +16,10 @@
 #include "system/angband.h"
 #include "info-reader/info-reader-util.h"
 
-#endif /* INCLUDED_INIT_H */
-
 void init_angband(player_type *player_ptr, void(*process_autopick_file_command)(char*));
 concptr get_check_sum(void);
 void init_file_paths(char *path);
 errr init_v_info(void);
 errr init_buildings(void);
+
+#endif /* INCLUDED_INIT_H */
