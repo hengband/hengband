c10e993c8f3de2b567c5b14d6accec8a273918ed
Hourier <hourier@users.sourceforge.jp>
2020-06-27 14:47:02 +0900

    [Refactor] #40483 Separated lighting-colors-table.c/h from display-main-window.c

---
+++ src/grid/lighting-colors-table.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "system/angband.h"
+
+#define MAX_COLORS 16
+#define MAX_TIME_ZONES 2
+
+TERM_COLOR lighting_colours[MAX_COLORS][MAX_TIME_ZONES];
--- src/view/display-main-window.c
+++ src/view/display-main-window.c
@@ -147,64 +147,7 @@ void get_screen_size(TERM_LEN *wid_p, TERM_LEN *hgt_p)
         *wid_p /= 2;
 }
 
-/*!
- * 照明の表現を行うための色合いの関係を{暗闇時, 照明時} で定義する /
- * This array lists the effects of "brightness" on various "base" colours.\n
- *\n
- * This is used to do dynamic lighting effects in ascii :-)\n
- * At the moment, only the various "floor" tiles are affected.\n
- *\n
- * The layout of the array is [x][0] = light and [x][1] = dark.\n
- */
-static TERM_COLOR lighting_colours[16][2] = {
-    /* TERM_DARK */
-    { TERM_L_DARK, TERM_DARK },
-
-    /* TERM_WHITE */
-    { TERM_YELLOW, TERM_SLATE },
-
-    /* TERM_SLATE */
-    { TERM_WHITE, TERM_L_DARK },
-
-    /* TERM_ORANGE */
-    { TERM_L_UMBER, TERM_UMBER },
-
-    /* TERM_RED */
-    { TERM_RED, TERM_RED },
-
-    /* TERM_GREEN */
-    { TERM_L_GREEN, TERM_GREEN },
-
-    /* TERM_BLUE */
-    { TERM_BLUE, TERM_BLUE },
-
-    /* TERM_UMBER */
-    { TERM_L_UMBER, TERM_RED },
-
-    /* TERM_L_DARK */
-    { TERM_SLATE, TERM_L_DARK },
-
-    /* TERM_L_WHITE */
-    { TERM_WHITE, TERM_SLATE },
-
-    /* TERM_VIOLET */
-    { TERM_L_RED, TERM_BLUE },
-
-    /* TERM_YELLOW */
-    { TERM_YELLOW, TERM_ORANGE },
-
-    /* TERM_L_RED */
-    { TERM_L_RED, TERM_L_RED },
-
-    /* TERM_L_GREEN */
-    { TERM_L_GREEN, TERM_GREEN },
-
-    /* TERM_L_BLUE */
-    { TERM_L_BLUE, TERM_L_BLUE },
-
-    /* TERM_L_UMBER */
-    { TERM_L_UMBER, TERM_UMBER }
-};
+#include "grid/lighting-colors-table.h"
 
 /*!
  * @brief 調査中
