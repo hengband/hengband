2636894726e3a2a69e79736b201658e8919ca0f8
Hourier <hourier@users.sourceforge.jp>
2020-06-28 18:59:22 +0900

    [Fix] #37353 Resolved the issue that player couldn't select any item

--- src/object/item-tester-hooker.c
+++ src/object/item-tester-hooker.c
@@ -16,7 +16,7 @@ bool (*item_tester_hook)(player_type *, object_type *);
  * @brief アイテムがitem_tester_hookグローバル関数ポインタの条件を満たしているかを返す汎用関数
  * Check an item against the item tester info
  * @param o_ptr 判定を行いたいオブジェクト構造体参照ポインタ
- * @return item_tester_hookの参照先、その他いくつかの例外に応じてTRUE/FALSEを返す。
+ * @return item_tester_hookの参照先が特にないならTRUE、その他いくつかの例外に応じてTRUE/FALSEを返す。
  */
 bool item_tester_okay(player_type *player_ptr, object_type *o_ptr, tval_type tval)
 {
@@ -36,5 +36,8 @@ bool item_tester_okay(player_type *player_ptr, object_type *o_ptr, tval_type tva
             return FALSE;
     }
 
-    return (item_tester_hook != NULL) && (*item_tester_hook)(player_ptr, o_ptr);
+    if ((item_tester_hook == NULL))
+        return TRUE;
+
+    return (*item_tester_hook)(player_ptr, o_ptr);
 }
