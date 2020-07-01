8ad22bbfddc7d6fbac73efa5d9a7ccfa0a5072ce
Hourier <hourier@users.sourceforge.jp>
2020-07-01 21:52:04 +0900

    [Refactor] #40466 Separated mspell-dispel.c/h from player-effects.c/h and mspell-status.c/h

---
+++ src/mspell/mspell-dispel.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+
+#include "system/angband.h"
+
+void spell_RF4_DISPEL(MONSTER_IDX m_idx, player_type *target_ptr, MONSTER_IDX t_idx, int TARGET_TYPE);
--- src/player/player-effects.h
+++ src/player/player-effects.h
@@ -3,7 +3,6 @@
 #include "system/angband.h"
 
 void set_action(player_type *creature_ptr, ACTION_IDX typ);
-void dispel_player(player_type *creature_ptr);
 void do_poly_self(player_type *creature_ptr);
 void do_poly_wounds(player_type *creature_ptr);
 void change_race(player_type *creature_ptr, player_race_type new_race, concptr effect_msg);
