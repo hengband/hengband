d4f663dac9a92509df01307e5e33f1059e42557b
Hourier <hourier@users.sourceforge.jp>
2020-07-04 20:19:07 +0900

    [Refactor] #40467 Moved rd_monster_old() from load.c to load-v1-5-0.c/h

--- src/savedata/load-v1-5-0.h
+++ src/savedata/load-v1-5-0.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
 #include "system/angband.h"
+#include "system/monster-type-definition.h"
 
 void rd_item_old(player_type *player_ptr, object_type *o_ptr);
+void rd_monster_old(player_type *player_ptr, monster_type *m_ptr);
