eedd08bd2dd1fd2eaa2372b4f20453f0ed3be1bb
Hourier <hourier@users.sourceforge.jp>
2020-07-05 11:45:23 +0900

    [Refactor] #40467 Separated birth-loader.c/h from load.c

--- src/savedata/load.c
+++ src/savedata/load.c
@@ -74,6 +74,7 @@
 #include "player/race-info-table.h"
 #include "player/special-defense-types.h"
 #include "savedata/angband-version-comparer.h"
+#include "savedata/birth-loader.h"
 #include "savedata/item-loader.h"
 #include "savedata/load-util.h"
 #include "savedata/load-v1-5-0.h"
@@ -107,73 +108,6 @@ static const BIT_FLAGS CAVE_TRAP = 0x8000;
 static const int OLD_QUEST_WATER_CAVE = 18; // 湖の洞窟.
 static const int QUEST_OLD_CASTLE = 27; // 古い城.
 static const int QUEST_ROYAL_CRYPT = 28; // 王家の墓.
-
-/*!
- * @brief ダミー情報スキップ / Hack -- strip the "ghost" info
- * @return なし
- * @details
- * This is such a nasty hack it hurts.
- */
-static void rd_ghost(void)
-{
-    char buf[64];
-    rd_string(buf, sizeof(buf));
-    strip_bytes(60);
-}
-
-/*!
- * @brief クイックスタート情報を読み込む / Load quick start data
- * @return なし
- */
-static void load_quick_start(void)
-{
-    if (z_older_than(11, 0, 13)) {
-        previous_char.quick_ok = FALSE;
-        return;
-    }
-
-    rd_byte(&previous_char.psex);
-    byte tmp8u;
-    rd_byte(&tmp8u);
-    previous_char.prace = (player_race_type)tmp8u;
-    rd_byte(&tmp8u);
-    previous_char.pclass = (player_class_type)tmp8u;
-    rd_byte(&tmp8u);
-    previous_char.pseikaku = (player_personality_type)tmp8u;
-    rd_byte(&tmp8u);
-    previous_char.realm1 = (REALM_IDX)tmp8u;
-    rd_byte(&tmp8u);
-    previous_char.realm2 = (REALM_IDX)tmp8u;
-
-    rd_s16b(&previous_char.age);
-    rd_s16b(&previous_char.ht);
-    rd_s16b(&previous_char.wt);
-    rd_s16b(&previous_char.sc);
-    rd_s32b(&previous_char.au);
-
-    for (int i = 0; i < A_MAX; i++)
-        rd_s16b(&previous_char.stat_max[i]);
-    for (int i = 0; i < A_MAX; i++)
-        rd_s16b(&previous_char.stat_max_max[i]);
-
-    for (int i = 0; i < PY_MAX_LEVEL; i++) {
-        s16b tmp16s;
-        rd_s16b(&tmp16s);
-        previous_char.player_hp[i] = (HIT_POINT)tmp16s;
-    }
-
-    rd_s16b(&previous_char.chaos_patron);
-
-    for (int i = 0; i < 8; i++)
-        rd_s16b(&previous_char.vir_types[i]);
-
-    for (int i = 0; i < 4; i++)
-        rd_string(previous_char.history[i], sizeof(previous_char.history[i]));
-
-    rd_byte(&tmp8u);
-    rd_byte(&tmp8u);
-    previous_char.quick_ok = (bool)tmp8u;
-}
 
 /*!
  * @brief その他の情報を読み込む / Read the "extra" information
@@ -1433,6 +1367,19 @@ static void rd_randomizer(void)
     for (int i = 0; i < RAND_DEG; i++) {
         rd_u32b(&Rand_state[i]);
     }
+}
+
+/*!
+ * @brief ダミー情報スキップ / Strip the "ghost" info
+ * @return なし
+ * @details
+ * This is such a nasty hack it hurts.
+ */
+static void rd_ghost(void)
+{
+    char buf[64];
+    rd_string(buf, sizeof(buf));
+    strip_bytes(60);
 }
 
 /*!
