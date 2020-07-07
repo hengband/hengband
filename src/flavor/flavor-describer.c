41281a6b78fb72baefa668aa1d29c56aff3ec66a
Hourier <hourier@users.sourceforge.jp>
2020-07-07 21:43:51 +0900

    [Refactor] #40534 Separated describe_book_crusade() from describe_flavor()

--- src/flavor/flavor-describer.c
+++ src/flavor/flavor-describer.c
@@ -327,6 +327,18 @@ static void describe_book_demon(flavor_type *flavor_ptr)
 #endif
 }
 
+static void describe_book_crusade(flavor_type *flavor_ptr)
+{
+#ifdef JP
+    flavor_ptr->basenm = "破邪の魔法書%";
+#else
+    if (mp_ptr->spell_book == TV_LIFE_BOOK)
+        flavor_ptr->basenm = "& Book~ of Crusade Magic %";
+    else
+        flavor_ptr->basenm = "& Crusade Spellbook~ %";
+#endif
+}
+
 /*!
  * @brief オブジェクトの各表記を返すメイン関数 / Creates a description of the item "o_ptr", and stores it in "out_val".
  * @param player_ptr プレーヤーへの参照ポインタ
@@ -438,18 +450,9 @@ void describe_flavor(player_type *player_ptr, char *buf, object_type *o_ptr, BIT
     case TV_DEMON_BOOK:
         describe_book_demon(flavor_ptr);
         break;
-    case TV_CRUSADE_BOOK: {
-#ifdef JP
-        flavor_ptr->basenm = "破邪の魔法書%";
-#else
-        if (mp_ptr->spell_book == TV_LIFE_BOOK)
-            flavor_ptr->basenm = "& Book~ of Crusade Magic %";
-        else
-            flavor_ptr->basenm = "& Crusade Spellbook~ %";
-#endif
-
+    case TV_CRUSADE_BOOK:
+        describe_book_crusade(flavor_ptr);
         break;
-    }
     case TV_MUSIC_BOOK: {
         flavor_ptr->basenm = _("歌集%", "& Song Book~ %");
         break;
