b1741c926d40fe6be88124a43b9002e88cf58ca9
Hourier <hourier@users.sourceforge.jp>
2020-07-10 09:14:09 +0900

    [Refactor] #40534 Separated display-sub-window-items.c/h from flavor-describer.c

--- src/flavor/flavor-describer.c
+++ src/flavor/flavor-describer.c
@@ -27,8 +27,8 @@
 #include "perception/object-perception.h"
 #include "sv-definition/sv-lite-types.h"
 #include "util/bit-flags-calculator.h"
-#include "util/quarks.h"
 #include "util/string-processor.h"
+#include "view/display-sub-window-items.h"
 
 static void describe_chest_trap(flavor_type *flavor_ptr)
 {
@@ -434,27 +434,6 @@ static void describe_remaining(flavor_type *flavor_ptr)
         flavor_ptr->t = object_desc_str(flavor_ptr->t, _("(充填中)", " (charging)"));
 }
 
-void display_short_flavors(player_type *player_ptr, flavor_type *flavor_ptr)
-{
-    flavor_ptr->tmp_val2[0] = '\0';
-    if ((abbrev_extra || abbrev_all) && object_is_fully_known(flavor_ptr->o_ptr)) {
-        if (!flavor_ptr->o_ptr->inscription || !angband_strchr(quark_str(flavor_ptr->o_ptr->inscription), '%')) {
-            bool kanji = _(TRUE, FALSE);
-            get_ability_abbreviation(player_ptr, flavor_ptr->tmp_val2, flavor_ptr->o_ptr, kanji, abbrev_all);
-        }
-    }
-
-    if (flavor_ptr->o_ptr->inscription == 0)
-        return;
-
-    char buff[1024];
-    if (flavor_ptr->tmp_val2[0])
-        strcat(flavor_ptr->tmp_val2, ", ");
-
-    get_inscription(player_ptr, buff, flavor_ptr->o_ptr);
-    angband_strcat(flavor_ptr->tmp_val2, buff, sizeof(flavor_ptr->tmp_val2));
-}
-
 static void decide_item_feeling(flavor_type *flavor_ptr)
 {
     flavor_ptr->fake_insc_buf[0] = '\0';
@@ -482,46 +461,6 @@ static void decide_item_feeling(flavor_type *flavor_ptr)
     
     if (!flavor_ptr->aware && object_is_tried(flavor_ptr->o_ptr))
         strcpy(flavor_ptr->fake_insc_buf, _("未判明", "tried"));
-}
-
-void display_item_discount(flavor_type *flavor_ptr)
-{
-    if ((flavor_ptr->o_ptr->discount == 0) || (flavor_ptr->tmp_val2[0] && ((flavor_ptr->o_ptr->ident & IDENT_STORE) == 0)))
-        return;
-
-    char discount_num_buf[4];
-    if (flavor_ptr->fake_insc_buf[0])
-        strcat(flavor_ptr->fake_insc_buf, ", ");
-
-    (void)object_desc_num(discount_num_buf, flavor_ptr->o_ptr->discount);
-    strcat(flavor_ptr->fake_insc_buf, discount_num_buf);
-    strcat(flavor_ptr->fake_insc_buf, _("%引き", "% off"));
-}
-
-/*!
- * @brief 呪いの有無や割引率等を表示する
- * @param flavor_ptr アイテム表記への参照ポインタ
- * @return なし
- */
-void display_item_fake_inscription(flavor_type *flavor_ptr)
-{
-    if ((flavor_ptr->fake_insc_buf[0] == '\0') && (flavor_ptr->tmp_val2[0] == '\0'))
-        return;
-
-    flavor_ptr->t = object_desc_chr(flavor_ptr->t, ' ');
-    flavor_ptr->t = object_desc_chr(flavor_ptr->t, flavor_ptr->c1);
-    if (flavor_ptr->fake_insc_buf[0])
-        flavor_ptr->t = object_desc_str(flavor_ptr->t, flavor_ptr->fake_insc_buf);
-
-    if ((flavor_ptr->fake_insc_buf[0] != '\0') && (flavor_ptr->tmp_val2[0] != '\0')) {
-        flavor_ptr->t = object_desc_chr(flavor_ptr->t, ',');
-        flavor_ptr->t = object_desc_chr(flavor_ptr->t, ' ');
-    }
-
-    if (flavor_ptr->tmp_val2[0])
-        flavor_ptr->t = object_desc_str(flavor_ptr->t, flavor_ptr->tmp_val2);
-
-    flavor_ptr->t = object_desc_chr(flavor_ptr->t, flavor_ptr->c2);
 }
 
 /*!
---
+++ src/view/display-sub-window-items.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "system/angband.h"
+
+typedef struct flavor_type flavor_type;
+void display_short_flavors(player_type *player_ptr, flavor_type *flavor_ptr);
+void display_item_discount(flavor_type *flavor_ptr);
+void display_item_fake_inscription(flavor_type *flavor_ptr);
