fcba7fac26510e3699499b1086c38f8d42220eb4
Hourier <hourier@users.sourceforge.jp>
2020-07-14 19:19:19 +0900

    [Refactor] #40561 random-art-characteristics.c/h

---
+++ src/artifact/random-art-characteristics.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "system/angband.h"
+
+void curse_artifact(player_type *player_ptr, object_type *o_ptr);
+void get_random_name(object_type *o_ptr, char *return_name, bool armour, int power);
+bool has_extreme_damage_rate(player_type *player_ptr, object_type *o_ptr);
