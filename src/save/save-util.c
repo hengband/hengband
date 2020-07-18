27fb6218a4f1c3402945c2caa88ab28311214a1c
Hourier <hourier@users.sourceforge.jp>
2020-07-18 14:59:55 +0900

    [Refactor] #40532 Separated save-util.c/h from save.c

---
+++ src/save/save-util.c
@@ -0,0 +1,81 @@
+﻿#include "save/save-util.h"
+
+FILE *saving_savefile; /* Current save "file" */
+byte save_xor_byte; /* Simple encryption */
+u32b v_stamp = 0L; /* A simple "checksum" on the actual values */
+u32b x_stamp = 0L; /* A simple "checksum" on the encoded bytes */
+
+/*!
+ * @brief 1バイトをファイルに書き込む / These functions place information into a savefile a byte at a time
+ * @param v 書き込むバイト値
+ * @return なし
+ */
+static void sf_put(byte v)
+{
+    /* Encode the value, write a character */
+    save_xor_byte ^= v;
+    (void)putc((int)save_xor_byte, saving_savefile);
+
+    /* Maintain the checksum info */
+    v_stamp += v;
+    x_stamp += save_xor_byte;
+}
+
+/*!
+ * @brief 1バイトをファイルに書き込む(sf_put()の糖衣)
+ * @param v 書き込むバイト
+ * @return なし
+ */
+void wr_byte(byte v) { sf_put(v); }
+
+/*!
+ * @brief 符号なし16ビットをファイルに書き込む
+ * @param v 書き込む符号なし16bit値
+ * @return なし
+ */
+void wr_u16b(u16b v)
+{
+    wr_byte((byte)(v & 0xFF));
+    wr_byte((byte)((v >> 8) & 0xFF));
+}
+
+/*!
+ * @brief 符号あり16ビットをファイルに書き込む
+ * @param v 書き込む符号あり16bit値
+ * @return なし
+ */
+void wr_s16b(s16b v) { wr_u16b((u16b)v); }
+
+/*!
+ * @brief 符号なし32ビットをファイルに書き込む
+ * @param v 書き込む符号なし32bit値
+ * @return なし
+ */
+void wr_u32b(u32b v)
+{
+    wr_byte((byte)(v & 0xFF));
+    wr_byte((byte)((v >> 8) & 0xFF));
+    wr_byte((byte)((v >> 16) & 0xFF));
+    wr_byte((byte)((v >> 24) & 0xFF));
+}
+
+/*!
+ * @brief 符号あり32ビットをファイルに書き込む
+ * @param v 書き込む符号あり32bit値
+ * @return なし
+ */
+void wr_s32b(s32b v) { wr_u32b((u32b)v); }
+
+/*!
+ * @brief 文字列をファイルに書き込む
+ * @param str 書き込む文字列
+ * @return なし
+ */
+void wr_string(concptr str)
+{
+    while (*str) {
+        wr_byte(*str);
+        str++;
+    }
+    wr_byte(*str);
+}
--- src/save/save.c
+++ src/save/save.c
@@ -40,6 +40,7 @@
 #include "monster/monster-info.h"
 #include "monster/monster-status.h"
 #include "object/object-kind.h"
+#include "save/save-util.h"
 #include "store/store-util.h"
 #include "store/store.h"
 #include "system/angband-version.h"
@@ -50,90 +51,6 @@
 #include "util/sort.h"
 #include "view/display-messages.h"
 #include "world/world.h"
-
-/*
- * Some "local" parameters, used to help write savefiles
- */
-
-static FILE *saving_savefile; /* Current save "file" */
-static byte save_xor_byte; /* Simple encryption */
-static u32b v_stamp = 0L; /* A simple "checksum" on the actual values */
-static u32b x_stamp = 0L; /* A simple "checksum" on the encoded bytes */
-
-/*!
- * @brief 1バイトをファイルに書き込む / These functions place information into a savefile a byte at a time
- * @param v 書き込むバイト値
- * @return なし
- */
-static void sf_put(byte v)
-{
-    /* Encode the value, write a character */
-    save_xor_byte ^= v;
-    (void)putc((int)save_xor_byte, saving_savefile);
-
-    /* Maintain the checksum info */
-    v_stamp += v;
-    x_stamp += save_xor_byte;
-}
-
-/*!
- * @brief 1バイトをファイルに書き込む(sf_put()の糖衣)
- * @param v 書き込むバイト
- * @return なし
- */
-static void wr_byte(byte v) { sf_put(v); }
-
-/*!
- * @brief 符号なし16ビットをファイルに書き込む
- * @param v 書き込む符号なし16bit値
- * @return なし
- */
-static void wr_u16b(u16b v)
-{
-    sf_put((byte)(v & 0xFF));
-    sf_put((byte)((v >> 8) & 0xFF));
-}
-
-/*!
- * @brief 符号あり16ビットをファイルに書き込む
- * @param v 書き込む符号あり16bit値
- * @return なし
- */
-static void wr_s16b(s16b v) { wr_u16b((u16b)v); }
-
-/*!
- * @brief 符号なし32ビットをファイルに書き込む
- * @param v 書き込む符号なし32bit値
- * @return なし
- */
-static void wr_u32b(u32b v)
-{
-    sf_put((byte)(v & 0xFF));
-    sf_put((byte)((v >> 8) & 0xFF));
-    sf_put((byte)((v >> 16) & 0xFF));
-    sf_put((byte)((v >> 24) & 0xFF));
-}
-
-/*!
- * @brief 符号あり32ビットをファイルに書き込む
- * @param v 書き込む符号あり32bit値
- * @return なし
- */
-static void wr_s32b(s32b v) { wr_u32b((u32b)v); }
-
-/*!
- * @brief 文字列をファイルに書き込む
- * @param str 書き込む文字列
- * @return なし
- */
-static void wr_string(concptr str)
-{
-    while (*str) {
-        wr_byte(*str);
-        str++;
-    }
-    wr_byte(*str);
-}
 
 /*
  * These functions write info in larger logical records
