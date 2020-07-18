3fc6cf0e79968e3373f9912072dc68bc291b14d8
Hourier <hourier@users.sourceforge.jp>
2020-07-18 15:52:54 +0900

    [Refator] #40532 Separated info-writer.c/h from save.c

---
+++ src/save/info-writer.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+typedef struct store_type store_type;
+void wr_store(store_type *store_ptr);
+void wr_randomizer(void);
+void wr_options(void);
+void wr_ghost(void);
+void save_quick_start(void);
