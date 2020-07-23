efae3239261c2bfb39774ad843ad9d18ecfd2add
Hourier <hourier@users.sourceforge.jp>
2020-07-23 16:54:26 +0900

    [Refactor] #40479 Separated check_high_resistances() from remove_bad_spells()

--- src/mspell/improper-mspell-remover.c
+++ src/mspell/improper-mspell-remover.c
@@ -153,7 +153,7 @@ static void check_acid_resistance(msr_type *msr_ptr)
         msr_ptr->f5 &= ~(RF5_BO_ACID);
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_ACID) && (msr_ptr->smart & SM_RES_ACID)) {
         if (int_outof(msr_ptr->r_ptr, 80))
             msr_ptr->f4 &= ~(RF4_BR_ACID);
@@ -163,10 +163,10 @@ static void check_acid_resistance(msr_type *msr_ptr)
 
         if (int_outof(msr_ptr->r_ptr, 80))
             msr_ptr->f5 &= ~(RF5_BO_ACID);
-        
+
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_ACID) || (msr_ptr->smart & SM_RES_ACID)) {
         if (int_outof(msr_ptr->r_ptr, 30))
             msr_ptr->f4 &= ~(RF4_BR_ACID);
@@ -187,7 +187,7 @@ static void check_elec_resistance(msr_type *msr_ptr)
         msr_ptr->f5 &= ~(RF5_BO_ELEC);
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_ELEC) && (msr_ptr->smart & SM_RES_ELEC)) {
         if (int_outof(msr_ptr->r_ptr, 80))
             msr_ptr->f4 &= ~(RF4_BR_ELEC);
@@ -200,7 +200,7 @@ static void check_elec_resistance(msr_type *msr_ptr)
 
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_ELEC) || (msr_ptr->smart & SM_RES_ELEC)) {
         if (int_outof(msr_ptr->r_ptr, 30))
             msr_ptr->f4 &= ~(RF4_BR_ELEC);
@@ -221,7 +221,7 @@ static void check_fire_resistance(msr_type *msr_ptr)
         msr_ptr->f5 &= ~(RF5_BO_FIRE);
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_FIRE) && (msr_ptr->smart & SM_RES_FIRE)) {
         if (int_outof(msr_ptr->r_ptr, 80))
             msr_ptr->f4 &= ~(RF4_BR_FIRE);
@@ -234,7 +234,7 @@ static void check_fire_resistance(msr_type *msr_ptr)
 
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_FIRE) || (msr_ptr->smart & SM_RES_FIRE)) {
         if (int_outof(msr_ptr->r_ptr, 30))
             msr_ptr->f4 &= ~(RF4_BR_FIRE);
@@ -256,7 +256,7 @@ static void check_cold_resistance(msr_type *msr_ptr)
         msr_ptr->f5 &= ~(RF5_BO_ICEE);
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_COLD) && (msr_ptr->smart & SM_RES_COLD)) {
         if (int_outof(msr_ptr->r_ptr, 80))
             msr_ptr->f4 &= ~(RF4_BR_COLD);
@@ -272,7 +272,7 @@ static void check_cold_resistance(msr_type *msr_ptr)
 
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_COLD) || (msr_ptr->smart & SM_RES_COLD)) {
         if (int_outof(msr_ptr->r_ptr, 30))
             msr_ptr->f4 &= ~(RF4_BR_COLD);
@@ -305,7 +305,7 @@ static void check_pois_resistance(msr_type *msr_ptr)
 
         return;
     }
-    
+
     if ((msr_ptr->smart & SM_OPP_POIS) || (msr_ptr->smart & SM_RES_POIS)) {
         if (int_outof(msr_ptr->r_ptr, 30))
             msr_ptr->f4 &= ~(RF4_BR_POIS);
@@ -445,6 +445,39 @@ static void check_reflection(msr_type *msr_ptr)
         msr_ptr->f5 &= ~(RF5_MISSILE);
 }
 
+void check_high_resistances(player_type *target_ptr, msr_type *msr_ptr)
+{
+    check_nether_resistance(target_ptr, msr_ptr);
+    check_lite_resistance(msr_ptr);
+    check_dark_resistance(target_ptr, msr_ptr);
+    if (msr_ptr->smart & SM_RES_FEAR)
+        msr_ptr->f5 &= ~(RF5_SCARE);
+
+    check_conf_resistance(msr_ptr);
+    check_chaos_resistance(msr_ptr);
+    if (((msr_ptr->smart & SM_RES_DISEN) != 0) && int_outof(msr_ptr->r_ptr, 40))
+        msr_ptr->f4 &= ~(RF4_BR_DISE);
+
+    if (msr_ptr->smart & SM_RES_BLIND)
+        msr_ptr->f5 &= ~(RF5_BLIND);
+
+    check_nexus_resistance(msr_ptr);
+    if (((msr_ptr->smart & SM_RES_SOUND) != 0) && int_outof(msr_ptr->r_ptr, 50))
+        msr_ptr->f4 &= ~(RF4_BR_SOUN);
+
+    if (((msr_ptr->smart & SM_RES_SHARD) != 0) && int_outof(msr_ptr->r_ptr, 40))
+        msr_ptr->f4 &= ~(RF4_BR_SHAR);
+
+    check_reflection(msr_ptr);
+    if (msr_ptr->smart & SM_IMM_FREE) {
+        msr_ptr->f5 &= ~(RF5_HOLD);
+        msr_ptr->f5 &= ~(RF5_SLOW);
+    }
+
+    if (msr_ptr->smart & SM_IMM_MANA)
+        msr_ptr->f5 &= ~(RF5_DRAIN_MANA);
+}
+
 /*!
  * @brief モンスターの魔法一覧から戦術的に適さない魔法を除外する /
  * Remove the "bad" spells from a spell list
@@ -477,36 +510,7 @@ void remove_bad_spells(MONSTER_IDX m_idx, player_type *target_ptr, u32b *f4p, u3
         return;
 
     check_element_resistance(msr_ptr);
-    check_nether_resistance(target_ptr, msr_ptr);
-    check_lite_resistance(msr_ptr);
-    check_dark_resistance(target_ptr, msr_ptr);
-    if (msr_ptr->smart & SM_RES_FEAR)
-        msr_ptr->f5 &= ~(RF5_SCARE);
-
-    check_conf_resistance(msr_ptr);
-    check_chaos_resistance(msr_ptr);
-    if (((msr_ptr->smart & SM_RES_DISEN) != 0) && int_outof(msr_ptr->r_ptr, 40))
-        msr_ptr->f4 &= ~(RF4_BR_DISE);
-
-    if (msr_ptr->smart & SM_RES_BLIND)
-        msr_ptr->f5 &= ~(RF5_BLIND);
-
-    check_nexus_resistance(msr_ptr);
-    if (((msr_ptr->smart & SM_RES_SOUND) != 0) && int_outof(msr_ptr->r_ptr, 50))
-        msr_ptr->f4 &= ~(RF4_BR_SOUN);
-
-    if (((msr_ptr->smart & SM_RES_SHARD) != 0) && int_outof(msr_ptr->r_ptr, 40))
-        msr_ptr->f4 &= ~(RF4_BR_SHAR);
-
-    check_reflection(msr_ptr);
-    if (msr_ptr->smart & SM_IMM_FREE) {
-        msr_ptr->f5 &= ~(RF5_HOLD);
-        msr_ptr->f5 &= ~(RF5_SLOW);
-    }
-
-    if (msr_ptr->smart & SM_IMM_MANA)
-        msr_ptr->f5 &= ~(RF5_DRAIN_MANA);
-
+    check_high_resistances(target_ptr, msr_ptr);
     *f4p = msr_ptr->f4;
     *f5p = msr_ptr->f5;
     *f6p = msr_ptr->f6;
