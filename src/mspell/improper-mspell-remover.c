d69daf4ef0104d967c09a1d25f33729300bc7a7d
Hourier <hourier@users.sourceforge.jp>
2020-07-23 16:34:20 +0900

    [Refactor] #40479 Separated check_*_resistance() (elements) from remove_bad_spells()

--- src/mspell/improper-mspell-remover.c
+++ src/mspell/improper-mspell-remover.c
@@ -145,6 +145,176 @@ static void add_cheat_remove_flags(player_type *target_ptr, msr_type *msr_ptr)
     add_cheat_remove_flags_others(target_ptr, msr_ptr);
 }
 
+static void check_acid_resistance(msr_type *msr_ptr)
+{
+    if (msr_ptr->smart & SM_IMM_ACID) {
+        msr_ptr->f4 &= ~(RF4_BR_ACID);
+        msr_ptr->f5 &= ~(RF5_BA_ACID);
+        msr_ptr->f5 &= ~(RF5_BO_ACID);
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_ACID) && (msr_ptr->smart & SM_RES_ACID)) {
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f4 &= ~(RF4_BR_ACID);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BA_ACID);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BO_ACID);
+        
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_ACID) || (msr_ptr->smart & SM_RES_ACID)) {
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f4 &= ~(RF4_BR_ACID);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BA_ACID);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BO_ACID);
+    }
+}
+
+static void check_elec_resistance(msr_type *msr_ptr)
+{
+    if (msr_ptr->smart & SM_IMM_ELEC) {
+        msr_ptr->f4 &= ~(RF4_BR_ELEC);
+        msr_ptr->f5 &= ~(RF5_BA_ELEC);
+        msr_ptr->f5 &= ~(RF5_BO_ELEC);
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_ELEC) && (msr_ptr->smart & SM_RES_ELEC)) {
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f4 &= ~(RF4_BR_ELEC);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BA_ELEC);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BO_ELEC);
+
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_ELEC) || (msr_ptr->smart & SM_RES_ELEC)) {
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f4 &= ~(RF4_BR_ELEC);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BA_ELEC);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BO_ELEC);
+    }
+}
+
+static void check_fire_resistance(msr_type *msr_ptr)
+{
+    if (msr_ptr->smart & (SM_IMM_FIRE)) {
+        msr_ptr->f4 &= ~(RF4_BR_FIRE);
+        msr_ptr->f5 &= ~(RF5_BA_FIRE);
+        msr_ptr->f5 &= ~(RF5_BO_FIRE);
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_FIRE) && (msr_ptr->smart & SM_RES_FIRE)) {
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f4 &= ~(RF4_BR_FIRE);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BA_FIRE);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BO_FIRE);
+
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_FIRE) || (msr_ptr->smart & SM_RES_FIRE)) {
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f4 &= ~(RF4_BR_FIRE);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BA_FIRE);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BO_FIRE);
+    }
+}
+
+static void check_cold_resistance(msr_type *msr_ptr)
+{
+    if (msr_ptr->smart & (SM_IMM_COLD)) {
+        msr_ptr->f4 &= ~(RF4_BR_COLD);
+        msr_ptr->f5 &= ~(RF5_BA_COLD);
+        msr_ptr->f5 &= ~(RF5_BO_COLD);
+        msr_ptr->f5 &= ~(RF5_BO_ICEE);
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_COLD) && (msr_ptr->smart & SM_RES_COLD)) {
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f4 &= ~(RF4_BR_COLD);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BA_COLD);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BO_COLD);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BO_ICEE);
+
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_COLD) || (msr_ptr->smart & SM_RES_COLD)) {
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f4 &= ~(RF4_BR_COLD);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BA_COLD);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BO_COLD);
+
+        if (int_outof(msr_ptr->r_ptr, 20))
+            msr_ptr->f5 &= ~(RF5_BO_ICEE);
+    }
+}
+
+static void check_pois_resistance(msr_type *msr_ptr)
+{
+    if ((msr_ptr->smart & SM_OPP_POIS) && (msr_ptr->smart & SM_RES_POIS)) {
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f4 &= ~(RF4_BR_POIS);
+
+        if (int_outof(msr_ptr->r_ptr, 80))
+            msr_ptr->f5 &= ~(RF5_BA_POIS);
+
+        if (int_outof(msr_ptr->r_ptr, 60))
+            msr_ptr->f4 &= ~(RF4_BA_NUKE);
+
+        if (int_outof(msr_ptr->r_ptr, 60))
+            msr_ptr->f4 &= ~(RF4_BR_NUKE);
+
+        return;
+    }
+    
+    if ((msr_ptr->smart & SM_OPP_POIS) || (msr_ptr->smart & SM_RES_POIS)) {
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f4 &= ~(RF4_BR_POIS);
+
+        if (int_outof(msr_ptr->r_ptr, 30))
+            msr_ptr->f5 &= ~(RF5_BA_POIS);
+    }
+}
+
 /*!
  * @brief モンスターの魔法一覧から戦術的に適さない魔法を除外する /
  * Remove the "bad" spells from a spell list
@@ -176,129 +346,11 @@ void remove_bad_spells(MONSTER_IDX m_idx, player_type *target_ptr, u32b *f4p, u3
     if (!msr_ptr->smart)
         return;
 
-    if (msr_ptr->smart & SM_IMM_ACID) {
-        msr_ptr->f4 &= ~(RF4_BR_ACID);
-        msr_ptr->f5 &= ~(RF5_BA_ACID);
-        msr_ptr->f5 &= ~(RF5_BO_ACID);
-    } else if ((msr_ptr->smart & SM_OPP_ACID) && (msr_ptr->smart & SM_RES_ACID)) {
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f4 &= ~(RF4_BR_ACID);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BA_ACID);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BO_ACID);
-    } else if ((msr_ptr->smart & SM_OPP_ACID) || (msr_ptr->smart & SM_RES_ACID)) {
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f4 &= ~(RF4_BR_ACID);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BA_ACID);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BO_ACID);
-    }
-
-    if (msr_ptr->smart & SM_IMM_ELEC) {
-        msr_ptr->f4 &= ~(RF4_BR_ELEC);
-        msr_ptr->f5 &= ~(RF5_BA_ELEC);
-        msr_ptr->f5 &= ~(RF5_BO_ELEC);
-    } else if ((msr_ptr->smart & SM_OPP_ELEC) && (msr_ptr->smart & SM_RES_ELEC)) {
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f4 &= ~(RF4_BR_ELEC);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BA_ELEC);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BO_ELEC);
-    } else if ((msr_ptr->smart & SM_OPP_ELEC) || (msr_ptr->smart & SM_RES_ELEC)) {
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f4 &= ~(RF4_BR_ELEC);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BA_ELEC);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BO_ELEC);
-    }
-
-    if (msr_ptr->smart & (SM_IMM_FIRE)) {
-        msr_ptr->f4 &= ~(RF4_BR_FIRE);
-        msr_ptr->f5 &= ~(RF5_BA_FIRE);
-        msr_ptr->f5 &= ~(RF5_BO_FIRE);
-    } else if ((msr_ptr->smart & SM_OPP_FIRE) && (msr_ptr->smart & SM_RES_FIRE)) {
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f4 &= ~(RF4_BR_FIRE);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BA_FIRE);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BO_FIRE);
-    } else if ((msr_ptr->smart & SM_OPP_FIRE) || (msr_ptr->smart & SM_RES_FIRE)) {
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f4 &= ~(RF4_BR_FIRE);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BA_FIRE);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BO_FIRE);
-    }
-
-    if (msr_ptr->smart & (SM_IMM_COLD)) {
-        msr_ptr->f4 &= ~(RF4_BR_COLD);
-        msr_ptr->f5 &= ~(RF5_BA_COLD);
-        msr_ptr->f5 &= ~(RF5_BO_COLD);
-        msr_ptr->f5 &= ~(RF5_BO_ICEE);
-    } else if ((msr_ptr->smart & SM_OPP_COLD) && (msr_ptr->smart & SM_RES_COLD)) {
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f4 &= ~(RF4_BR_COLD);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BA_COLD);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BO_COLD);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BO_ICEE);
-    } else if ((msr_ptr->smart & SM_OPP_COLD) || (msr_ptr->smart & SM_RES_COLD)) {
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f4 &= ~(RF4_BR_COLD);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BA_COLD);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BO_COLD);
-
-        if (int_outof(msr_ptr->r_ptr, 20))
-            msr_ptr->f5 &= ~(RF5_BO_ICEE);
-    }
-
-    if ((msr_ptr->smart & SM_OPP_POIS) && (msr_ptr->smart & SM_RES_POIS)) {
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f4 &= ~(RF4_BR_POIS);
-
-        if (int_outof(msr_ptr->r_ptr, 80))
-            msr_ptr->f5 &= ~(RF5_BA_POIS);
-
-        if (int_outof(msr_ptr->r_ptr, 60))
-            msr_ptr->f4 &= ~(RF4_BA_NUKE);
-
-        if (int_outof(msr_ptr->r_ptr, 60))
-            msr_ptr->f4 &= ~(RF4_BR_NUKE);
-    } else if ((msr_ptr->smart & SM_OPP_POIS) || (msr_ptr->smart & SM_RES_POIS)) {
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f4 &= ~(RF4_BR_POIS);
-
-        if (int_outof(msr_ptr->r_ptr, 30))
-            msr_ptr->f5 &= ~(RF5_BA_POIS);
-    }
-
+    check_acid_resistance(msr_ptr);
+    check_elec_resistance(msr_ptr);
+    check_fire_resistance(msr_ptr);
+    check_cold_resistance(msr_ptr);
+    check_pois_resistance(msr_ptr);
     if (msr_ptr->smart & SM_RES_NETH) {
         if (is_specific_player_race(target_ptr, RACE_SPECTRE)) {
             msr_ptr->f4 &= ~(RF4_BR_NETH);
