67aa8ea0196768bd10463131dbcb58f7eeb02c24
Hourier <hourier@users.sourceforge.jp>
2020-07-24 11:44:53 +0900

    [Refactor] #40570 Separated room-generator.c/h from rooms.c/h

---
+++ src/room/room-generator.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "system/angband.h"
+
+void generate_room_floor(player_type *player_ptr, POSITION y1, POSITION x1, POSITION y2, POSITION x2, int light);
+void generate_fill_perm_bold(player_type *player_ptr, POSITION y1, POSITION x1, POSITION y2, POSITION x2);
+bool generate_rooms(player_type *player_ptr);
