813b216f8ae26cc88d3aa7f50bd85e3c051e3964
Hourier <hourier@users.sourceforge.jp>
2020-07-24 10:23:07 +0900

    [Refactor] #40570 Separated lake-types.h and room-types.h from rooms.c/h

---
+++ src/room/lake-types.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+/* 池型地形の生成ID / Room types for generate_lake() */
+typedef enum lake_type {
+	LAKE_T_LAVA = 1, /*!< 池型地形ID: 溶岩 */
+    LAKE_T_WATER = 2, /*!< 池型地形ID: 池 */
+    LAKE_T_CAVE = 3, /*!< 池型地形ID: 空洞 */
+    LAKE_T_EARTH_VAULT = 4, /*!< 池型地形ID: 地属性VAULT */
+    LAKE_T_AIR_VAULT = 5, /*!< 池型地形ID: 風属性VAULT */
+    LAKE_T_WATER_VAULT = 6, /*!< 池型地形ID: 水属性VAULT */
+    LAKE_T_FIRE_VAULT = 7, /*!< 池型地形ID: 火属性VAULT */
+} lake_type;
--- src/room/rooms.h
+++ src/room/rooms.h
@@ -1,37 +1,9 @@
 ﻿#pragma once
 
+#include "room/room-types.h"
 #include "system/angband.h"
 
 #define DUN_ROOMS_MAX 40 /*!< 部屋生成処理の最大試行数 / Number of rooms to attempt (was 50) */
-
-/* 池型地形の生成ID / Room types for generate_lake() */
-#define LAKE_T_LAVA 1 /*!< 池型地形ID: 溶岩 */
-#define LAKE_T_WATER 2 /*!< 池型地形ID: 池 */
-#define LAKE_T_CAVE 3 /*!< 池型地形ID: 空洞 */
-#define LAKE_T_EARTH_VAULT 4 /*!< 池型地形ID: 地属性VAULT */
-#define LAKE_T_AIR_VAULT 5 /*!< 池型地形ID: 風属性VAULT */
-#define LAKE_T_WATER_VAULT 6 /*!< 池型地形ID: 水属性VAULT */
-#define LAKE_T_FIRE_VAULT 7 /*!< 池型地形ID: 火属性VAULT */
-
-/* 部屋型ID / Room types for room_build() */
-#define ROOM_T_NORMAL 0 /*!<部屋型ID:基本長方形 / Simple (33x11) */
-#define ROOM_T_OVERLAP 1 /*!<部屋型ID:長方形二つ重ね / Overlapping (33x11) */
-#define ROOM_T_CROSS 2 /*!<部屋型ID:十字 / Crossed (33x11) */
-#define ROOM_T_INNER_FEAT 3 /*!<部屋型ID:二重壁 / Large (33x11) */
-#define ROOM_T_NEST 4 /*!<部屋型ID:モンスターNEST / Monster nest (33x11) */
-#define ROOM_T_PIT 5 /*!<部屋型ID:モンスターPIT / Monster pit (33x11) */
-#define ROOM_T_LESSER_VAULT 6 /*!<部屋型ID:小型VAULT / Lesser vault (33x22) */
-#define ROOM_T_GREATER_VAULT 7 /*!<部屋型ID:大型VAULT / Greater vault (66x44) */
-#define ROOM_T_FRACAVE 8 /*!<部屋型ID:フラクタル地形 / Fractal room (42x24) */
-#define ROOM_T_RANDOM_VAULT 9 /*!<部屋型ID:ランダムVAULT / Random vault (44x22) */
-#define ROOM_T_OVAL 10 /*!<部屋型ID:円形部屋 / Circular rooms (22x22) */
-#define ROOM_T_CRYPT 11 /*!<部屋型ID:聖堂 / Crypts (22x22) */
-#define ROOM_T_TRAP_PIT 12 /*!<部屋型ID:トラップつきモンスターPIT / Trapped monster pit */
-#define ROOM_T_TRAP 13 /*!<部屋型ID:トラップ部屋 / Piranha/Armageddon trap room */
-#define ROOM_T_GLASS 14 /*!<部屋型ID:ガラス部屋 / Glass room */
-#define ROOM_T_ARCADE 15 /*!<部屋型ID:商店 / Arcade */
-#define ROOM_T_FIXED 16 /*!<部屋型ID:固定部屋 / Fixed room */
-#define ROOM_T_MAX 17 /*!<部屋型ID最大数 */
 
 /* Maximum locked/jammed doors */
 #define MAX_LJ_DOORS 8
