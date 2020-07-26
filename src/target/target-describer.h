4e7c6c0f50d8357a4ebf6726781fe441c68fb5d1
Hourier <hourier@users.sourceforge.jp>
2020-07-26 13:03:22 +0900

    [Refactor] #40571 Separated describe_scan_result() from examine_grid()

--- src/target/target-describer.h
+++ src/target/target-describer.h
@@ -5,4 +5,4 @@
 extern bool show_gold_on_floor;
 
 typedef enum target_type target_type;
-char examine_grid(player_type *subject_ptr, POSITION y, POSITION x, target_type mode, concptr info);
+char examine_grid(player_type *subject_ptr, const POSITION y, const POSITION x, target_type mode, concptr info);
