3bf58df0e13943017d38c2ceaa05571bd24b98a3
Hourier <hourier@users.sourceforge.jp>
2020-07-29 19:41:42 +0900

    [Refactor] #40573 Separated mind-info.c/h from mind-switcher.c/h

--- src/mind/mind-switcher.h
+++ src/mind/mind-switcher.h
@@ -2,14 +2,7 @@
 
 #include "system/angband.h"
 
-#define MIND_MINDCRAFTER    0 /*!< 特殊能力: 超能力 */
-#define MIND_KI             1 /*!< 特殊能力: 練気 */
-#define MIND_BERSERKER      2 /*!< 特殊能力: 怒り */
-#define MIND_MIRROR_MASTER  3 /*!< 特殊能力: 鏡魔法 */
-#define MIND_NINJUTSU       4 /*!< 特殊能力: 忍術 */
-
-extern void mindcraft_info(player_type *caster_ptr, char *p, int use_mind, int power);
-extern void do_cmd_mind(player_type *caster_ptr);
-extern void do_cmd_mind_browse(player_type *caster_ptr);
-extern void do_cmd_mind_browse(player_type *caster_ptr);
+void do_cmd_mind(player_type *caster_ptr);
+void do_cmd_mind_browse(player_type *caster_ptr);
+void do_cmd_mind_browse(player_type *caster_ptr);
 
