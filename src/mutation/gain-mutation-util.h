dcf4bf0b0911dc4b629651b28de71a8cee61a8a8
Hourier <hourier@users.sourceforge.jp>
2020-07-31 20:57:11 +0900

    [Refactor] #40574 Separated gain-mutation-switcher.c/h from mutation.c

--- src/mutation/gain-mutation-util.h
+++ src/mutation/gain-mutation-util.h
@@ -8,6 +8,7 @@ typedef struct gm_type {
     int muta_which; // mutation_flag_type_1 とmutation_flag_type_2 の両対応とするため、敢えてint型で定義する
     concptr muta_desc;
     bool muta_chosen;
+    MUTATION_IDX choose_mut;
 } gm_type;
 
-gm_type *initialize_gm_type(gm_type *gm_ptr);
+gm_type *initialize_gm_type(gm_type *gm_ptr, MUTATION_IDX choose_mut);
