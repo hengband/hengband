ad589245abac1f920cc058e9bc3e72e7a792cb89
Hourier <hourier@users.sourceforge.jp>
2020-08-01 16:12:45 +0900

    [Refactor] #40512 Separated spoiler-util.c/h from wizard-spoiler.c

---
+++ src/wizard/spoiler-util.c
@@ -0,0 +1,9 @@
+﻿#include "wizard/spoiler-util.h"
+
+const char item_separator = ',';
+const char list_separator = _(',', ';');
+const int max_evolution_depth = 64;
+concptr spoiler_indent = "    ";
+
+/* The spoiler file being created */
+FILE *spoiler_file = NULL;
