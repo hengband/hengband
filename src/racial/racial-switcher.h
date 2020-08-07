d9aac1389369e91110a0d2972f5eee438ad03fb0
Hourier <hourier@users.sourceforge.jp>
2020-08-07 18:06:35 +0900

    [Refactor] #40575 Separated cmd-racial.c/h from racial-switcher.c/h

--- src/racial/racial-switcher.h
+++ src/racial/racial-switcher.h
@@ -2,4 +2,17 @@
 
 #include "system/angband.h"
 
-void do_cmd_racial_power(player_type *creature_ptr);
+/* Racial Power Info / レイシャル・パワー情報の構造体定義 */
+typedef struct rpi_type {
+    GAME_TEXT racial_name[MAX_NLEN];
+    PLAYER_LEVEL min_level; //!<体得レベル
+    int cost;
+    int stat;
+    PERCENTAGE fail;
+    int number;
+    int racial_cost;
+} rpi_type;
+
+PERCENTAGE racial_chance(player_type *creature_ptr, rpi_type *rpi_ptr);
+int racial_aux(player_type *creature_ptr, rpi_type *rpi_ptr);
+bool exe_racial_power(player_type *creature_ptr, s32b command);
