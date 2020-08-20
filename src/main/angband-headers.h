169d72505f0c5c2152bb0f42fd59fcd1eee12279
Hourier <hourier@users.sourceforge.jp>
2020-08-20 20:53:16 +0900

    [Refactor] #40635 Moved init_*_info() from init.c to info-initializer.c/h

---
+++ src/main/angband-headers.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "system/angband.h"
+
+typedef struct angband_header angband_header;
+extern angband_header f_head;
+extern angband_header v_head;
+extern angband_header k_head;
+extern angband_header a_head;
+extern angband_header e_head;
+extern angband_header r_head;
+extern angband_header d_head;
+extern angband_header s_head;
+extern angband_header m_head;
