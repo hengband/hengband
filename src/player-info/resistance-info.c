d785ff1411517ecd51a9bf31b8a4ea09e0741967
Hourier <hourier@users.sourceforge.jp>
2020-08-21 12:36:54 +0900

    [Refactor] #40647 Moved set_high_resistance_info() from self-info.c to resisntance-info.c/h

--- src/player-info/resistance-info.c
+++ src/player-info/resistance-info.c
@@ -51,3 +51,41 @@ void set_element_resistance_info(player_type* creature_ptr, self_info_type* si_p
         si_ptr->info[si_ptr->line++] = _("あなたは毒への耐性を持っている。", "You are resistant to poison.");
     }
 }
+
+void set_high_resistance_info(player_type *creature_ptr, self_info_type *si_ptr)
+{
+    if (creature_ptr->resist_lite)
+        si_ptr->info[si_ptr->line++] = _("あなたは閃光への耐性を持っている。", "You are resistant to bright light.");
+
+    if (is_specific_player_race(creature_ptr, RACE_VAMPIRE) || is_specific_player_race(creature_ptr, RACE_S_FAIRY)
+        || (creature_ptr->mimic_form == MIMIC_VAMPIRE))
+        si_ptr->info[si_ptr->line++] = _("あなたは閃光に弱い。", "You are susceptible to damage from bright light.");
+
+    if (is_specific_player_race(creature_ptr, RACE_VAMPIRE) || (creature_ptr->mimic_form == MIMIC_VAMPIRE) || creature_ptr->wraith_form)
+        si_ptr->info[si_ptr->line++] = _("あなたは暗黒に対する完全なる免疫を持っている。", "You are completely immune to darkness.");
+    else if (creature_ptr->resist_dark)
+        si_ptr->info[si_ptr->line++] = _("あなたは暗黒への耐性を持っている。", "You are resistant to darkness.");
+    
+    if (creature_ptr->resist_conf)
+        si_ptr->info[si_ptr->line++] = _("あなたは混乱への耐性を持っている。", "You are resistant to confusion.");
+    
+    if (creature_ptr->resist_sound)
+        si_ptr->info[si_ptr->line++] = _("あなたは音波の衝撃への耐性を持っている。", "You are resistant to sonic attacks.");
+    
+    if (creature_ptr->resist_disen)
+        si_ptr->info[si_ptr->line++] = _("あなたは劣化への耐性を持っている。", "You are resistant to disenchantment.");
+    
+    if (creature_ptr->resist_chaos)
+        si_ptr->info[si_ptr->line++] = _("あなたはカオスの力への耐性を持っている。", "You are resistant to chaos.");
+    
+    if (creature_ptr->resist_shard)
+        si_ptr->info[si_ptr->line++] = _("あなたは破片の攻撃への耐性を持っている。", "You are resistant to blasts of shards.");
+    
+    if (creature_ptr->resist_nexus)
+        si_ptr->info[si_ptr->line++] = _("あなたは因果混乱の攻撃への耐性を持っている。", "You are resistant to nexus attacks.");
+
+    if (is_specific_player_race(creature_ptr, RACE_SPECTRE))
+        si_ptr->info[si_ptr->line++] = _("あなたは地獄の力を吸収できる。", "You can drain nether forces.");
+    else if (creature_ptr->resist_neth)
+        si_ptr->info[si_ptr->line++] = _("あなたは地獄の力への耐性を持っている。", "You are resistant to nether forces.");
+}
