7ea1aaa4e7c5acaf9e4ab87385528114123d8826
Hourier <hourier@users.sourceforge.jp>
2020-08-21 15:29:39 +0900

    [Refactor] #40581 Separated monster-death-util.c/h from monster-death.c

---
+++ src/monster-floor/monster-death-util.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "system/angband.h"
+
+typedef struct monster_type monster_type;
+typedef struct monster_race monster_race;
+typedef struct monster_death_type {
+    monster_type *m_ptr;
+    monster_race *r_ptr;
+    bool do_gold;
+    bool do_item;
+    bool cloned;
+    int force_coin;
+    bool drop_chosen_item;
+    POSITION md_y;
+    POSITION md_x;
+} monster_death_type;
+
+monster_death_type *initialize_monster_death_type(player_type *player_ptr, monster_death_type *md_ptr, MONSTER_IDX m_idx, bool drop_item);
--- src/monster-floor/monster-death.c
+++ src/monster-floor/monster-death.c
@@ -22,6 +22,7 @@
 #include "main/music-definitions-table.h"
 #include "main/sound-of-music.h"
 #include "market/arena-info-table.h"
+#include "monster-floor/monster-death-util.h"
 #include "monster-floor/monster-object.h"
 #include "monster-floor/monster-summon.h"
 #include "monster-floor/place-monster-types.h"
@@ -56,59 +57,6 @@
 #include "system/system-variables.h"
 #include "view/display-messages.h"
 #include "world/world.h"
-
-typedef struct monster_death_type {
-    monster_type *m_ptr;
-    monster_race *r_ptr;
-    bool do_gold;
-    bool do_item;
-    bool cloned;
-    int force_coin;
-    bool drop_chosen_item;
-    POSITION md_y;
-    POSITION md_x;
-} monster_death_type;
-
-monster_death_type *initialize_monster_death_type(player_type *player_ptr, monster_death_type *md_ptr, MONSTER_IDX m_idx, bool drop_item)
-{
-    floor_type *floor_ptr = player_ptr->current_floor_ptr;
-    md_ptr->m_ptr = &floor_ptr->m_list[m_idx];
-    md_ptr->r_ptr = &r_info[md_ptr->m_ptr->r_idx];
-    md_ptr->do_gold = (!(md_ptr->r_ptr->flags1 & RF1_ONLY_ITEM));
-    md_ptr->do_item = (!(md_ptr->r_ptr->flags1 & RF1_ONLY_GOLD));
-    md_ptr->cloned = (md_ptr->m_ptr->smart & SM_CLONED) ? TRUE : FALSE;
-    md_ptr->force_coin = get_coin_type(md_ptr->m_ptr->r_idx);
-    md_ptr->drop_chosen_item = drop_item && !md_ptr->cloned && !floor_ptr->inside_arena && !player_ptr->phase_out && !is_pet(md_ptr->m_ptr);
-    return md_ptr;
-}
-
-/*!
- * @brief モンスターを倒した際の財宝svalを返す
- * @param r_idx 倒したモンスターの種族ID
- * @return 財宝のsval
- * @details
- * Hack -- Return the "automatic coin type" of a monster race
- * Used to allocate proper treasure when "Creeping coins" die
- * Note the use of actual "monster names"
- */
-static OBJECT_SUBTYPE_VALUE get_coin_type(MONRACE_IDX r_idx)
-{
-    switch (r_idx) {
-    case MON_COPPER_COINS:
-        return 2;
-    case MON_SILVER_COINS:
-        return 5;
-    case MON_GOLD_COINS:
-        return 10;
-    case MON_MITHRIL_COINS:
-    case MON_MITHRIL_GOLEM:
-        return 16;
-    case MON_ADAMANT_COINS:
-        return 17;
-    }
-
-    return 0;
-}
 
 /*!
  * @brief モンスターが死亡した時の処理 /
