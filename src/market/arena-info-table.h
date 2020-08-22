be47ed3cf60e4f366af9f2a069ee76c836d85b82
Eric Branlund <ebranlund@fastmail.com>
2020-08-21 17:28:30 -0700

    Included additional headers to avoid incomplete type errors with clang 11.0.3 for arena_type and its components.

--- src/market/arena-info-table.h
+++ src/market/arena-info-table.h
@@ -1,8 +1,9 @@
 ﻿#pragma once
 
+#include "object/tval-types.h"
 #include "system/angband.h"
+#include "system/building-type-definition.h"
 
 #define MAX_ARENA_MONS 41 /*<! 闘技場のイベント件数 -KMW- */
 
-typedef struct arena_type arena_type;
 extern const arena_type arena_info[MAX_ARENA_MONS + 2];
