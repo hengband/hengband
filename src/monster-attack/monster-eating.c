7e98e49be639a3800b9f68ca48e3663cd38f4e76
Hourier <hourier@users.sourceforge.jp>
2020-10-07 23:29:48 +0900

    [Implement] #37285 calc_blow_un_power() とprocess_un_power()において、魔道具使用能力向上フラグによるダメージ減衰処理を追加 / Added attenuating calculation for un-power into calc_blow_un_power() and process_un_power()

--- src/monster-attack/monster-eating.c
+++ src/monster-attack/monster-eating.c
@@ -20,6 +20,7 @@
 #include "object/object-mark-types.h"
 #include "player-info/avatar.h"
 #include "player/mimic-info-table.h"
+#include "player/player-status-flags.h"
 #include "player/player-status-table.h"
 #include "status/experience.h"
 #include "system/floor-type-definition.h"
@@ -196,29 +197,32 @@ void process_eat_lite(player_type *target_ptr, monap_type *monap_ptr)
  * @param target_ptr プレーヤーへの参照ポインタ
  * @monap_ptr モンスターからモンスターへの直接攻撃構造体への参照ポインタ
  * @return 吸収されたらTRUE、されなかったらFALSE
+ * @details 魔道具使用能力向上フラグがあれば、吸収量は全部ではなく半分で済む
  */
 bool process_un_power(player_type *target_ptr, monap_type *monap_ptr)
 {
     if (((monap_ptr->o_ptr->tval != TV_STAFF) && (monap_ptr->o_ptr->tval != TV_WAND)) || (monap_ptr->o_ptr->pval == 0))
         return FALSE;
 
-    int heal = monap_ptr->rlev * monap_ptr->o_ptr->pval;
+    bool is_magic_mastery = has_magic_mastery(target_ptr) != 0;
+    int drain_ratio = is_magic_mastery ? 2 : 1;
+    int heal = monap_ptr->rlev * monap_ptr->o_ptr->pval / drain_ratio;
     if (monap_ptr->o_ptr->tval == TV_STAFF)
         heal *= monap_ptr->o_ptr->number;
 
-    heal = MIN(heal, monap_ptr->m_ptr->maxhp - monap_ptr->m_ptr->hp);
+    heal = MIN(heal, (monap_ptr->m_ptr->maxhp - monap_ptr->m_ptr->hp) / drain_ratio);
     msg_print(_("ザックからエネルギーが吸い取られた！", "Energy drains from your pack!"));
     monap_ptr->obvious = TRUE;
     monap_ptr->m_ptr->hp += (HIT_POINT)heal;
     if (target_ptr->health_who == monap_ptr->m_idx)
-        target_ptr->redraw |= (PR_HEALTH);
+        target_ptr->redraw |= PR_HEALTH;
 
     if (target_ptr->riding == monap_ptr->m_idx)
-        target_ptr->redraw |= (PR_UHEALTH);
+        target_ptr->redraw |= PR_UHEALTH;
 
-    monap_ptr->o_ptr->pval = 0;
-    target_ptr->update |= (PU_COMBINE | PU_REORDER);
-    target_ptr->window |= (PW_INVEN);
+    monap_ptr->o_ptr->pval = !is_magic_mastery || (monap_ptr->o_ptr->pval == 1) ? 0 : (monap_ptr->o_ptr->pval + 1) / 2;
+    target_ptr->update |= PU_COMBINE | PU_REORDER;
+    target_ptr->window |= PW_INVEN;
     return TRUE;
 }
 
