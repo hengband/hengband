6d1f8197136feb9f02eded4323366061560eda55
Hourier <hourier@users.sourceforge.jp>
2021-01-17 17:09:11 +0900

    [Refactor] #41149 Reshaped process_fall_off_horse() a little

--- src/pet/pet-fall-off.c
+++ src/pet/pet-fall-off.c
@@ -89,15 +89,14 @@ static bool calc_fall_off_possibility(player_type *creature_ptr, const HIT_POINT
  */
 bool process_fall_off_horse(player_type *creature_ptr, HIT_POINT dam, bool force)
 {
-    POSITION sy = 0, sx = 0;
+    POSITION sy = 0;
+    POSITION sx = 0;
     int sn = 0;
     GAME_TEXT m_name[MAX_NLEN];
     monster_type *m_ptr = &creature_ptr->current_floor_ptr->m_list[creature_ptr->riding];
     monster_race *r_ptr = &r_info[m_ptr->r_idx];
 
-    if (!creature_ptr->riding)
-        return FALSE;
-    if (creature_ptr->wild_mode)
+    if (!creature_ptr->riding || creature_ptr->wild_mode)
         return FALSE;
 
     if (dam >= 0 || force) {
