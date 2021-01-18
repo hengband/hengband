03dcf7ef83ed69387ac260f79190bc1c4b77ad9c
Eric Branlund <ebranlund@fastmail.com>
2021-01-18 15:53:49 -0800

    Implement workaround to resolve https://osdn.net/projects/hengband/ticket/41161 and part of https://osdn.net/projects/hengbandforosx/ticket/41228 .  Move macro definitions from autopick-editor-table.h to autopick-menu-data-table.c so the preprocessing with nkf on Linux and Mac can change the encoding for Japanese.  Remove autopick-editor-table.h.  Change the macros in autopick-keys-table.h to be pointers to constant strings.  Initialize those strings in autopick-menu-data-table.c so the initializing strings can be shared with the initializers for menu_data and so the preprocessing with nkf can change the encoding for Japanese.

--- src/autopick/autopick-keys-table.h
+++ src/autopick/autopick-keys-table.h
@@ -1,57 +1,57 @@
 ﻿#pragma once
 
-/*!
- * todo GAME_TEXT[] かconcptrを使いたかった
- * しかしコンパイルエラーで通らなかったので暫定的にdefineにした
- * 上手いことコンパイルできる方法があれば求む
- * @brief アイテム種別及び各種形容詞
+#include "system/h-basic.h"
+
+/*
+ * Note these are defined in autopick-menu-data-table.c.  See the comments
+ * there for why the code is structured that way.
  */
-#define KEY_ALL _("すべての", "all")
-#define KEY_UNAWARE _("未判明の", "unaware")
-#define KEY_UNIDENTIFIED _("未鑑定の", "unidentified")
-#define KEY_IDENTIFIED _("鑑定済みの", "identified")
-#define KEY_STAR_IDENTIFIED _("*鑑定*済みの", "*identified*")
-#define KEY_COLLECTING _("収集中の", "collecting")
-#define KEY_ARTIFACT _("アーティファクト", "artifact")
-#define KEY_EGO _("エゴ", "ego")
-#define KEY_GOOD _("上質の", "good")
-#define KEY_NAMELESS _("無銘の", "nameless")
-#define KEY_AVERAGE _("並の", "average")
-#define KEY_WORTHLESS _("無価値の", "worthless")
-#define KEY_RARE _("レアな", "rare")
-#define KEY_COMMON _("ありふれた", "common")
-#define KEY_BOOSTED _("ダイス目の違う", "dice boosted")
-#define KEY_MORE_THAN _("ダイス目", "more than")
-#define KEY_DICE _("以上の", "dice")
-#define KEY_MORE_BONUS _("修正値", "more bonus than")
-#define KEY_MORE_BONUS2 _("以上の", "")
-#define KEY_WANTED _("賞金首の", "wanted")
-#define KEY_UNIQUE _("ユニーク・モンスターの", "unique monster's")
-#define KEY_HUMAN _("人間の", "human")
-#define KEY_UNREADABLE _("読めない", "unreadable")
-#define KEY_REALM1 _("第一領域の", "first realm's")
-#define KEY_REALM2 _("第二領域の", "second realm's")
-#define KEY_FIRST _("1冊目の", "first")
-#define KEY_SECOND _("2冊目の", "second")
-#define KEY_THIRD _("3冊目の", "third")
-#define KEY_FOURTH _("4冊目の", "fourth")
-#define KEY_ITEMS _("アイテム", "items")
-#define KEY_WEAPONS _("武器", "weapons")
-#define KEY_FAVORITE_WEAPONS _("得意武器", "favorite weapons")
-#define KEY_ARMORS _("防具", "armors")
-#define KEY_MISSILES _("矢", "missiles")
-#define KEY_DEVICES _("魔法アイテム", "magical devices")
-#define KEY_LIGHTS _("光源", "lights")
-#define KEY_JUNKS _("がらくた", "junks")
-#define KEY_CORPSES _("死体や骨", "corpses or skeletons")
-#define KEY_SPELLBOOKS _("魔法書", "spellbooks")
-#define KEY_HAFTED _("鈍器", "hafted weapons")
-#define KEY_SHIELDS _("盾", "shields")
-#define KEY_BOWS _("弓", "bows")
-#define KEY_RINGS _("指輪", "rings")
-#define KEY_AMULETS _("アミュレット", "amulets")
-#define KEY_SUITS _("鎧", "suits")
-#define KEY_CLOAKS _("クローク", "cloaks")
-#define KEY_HELMS _("兜", "helms")
-#define KEY_GLOVES _("籠手", "gloves")
-#define KEY_BOOTS _("靴", "boots")
+extern concptr KEY_ALL;
+extern concptr KEY_UNAWARE;
+extern concptr KEY_UNIDENTIFIED;
+extern concptr KEY_IDENTIFIED;
+extern concptr KEY_STAR_IDENTIFIED;
+extern concptr KEY_COLLECTING;
+extern concptr KEY_ARTIFACT;
+extern concptr KEY_EGO;
+extern concptr KEY_GOOD;
+extern concptr KEY_NAMELESS;
+extern concptr KEY_AVERAGE;
+extern concptr KEY_WORTHLESS;
+extern concptr KEY_RARE;
+extern concptr KEY_COMMON;
+extern concptr KEY_BOOSTED;
+extern concptr KEY_MORE_THAN;
+extern concptr KEY_DICE;
+extern concptr KEY_MORE_BONUS;
+extern concptr KEY_MORE_BONUS2;
+extern concptr KEY_WANTED;
+extern concptr KEY_UNIQUE;
+extern concptr KEY_HUMAN;
+extern concptr KEY_UNREADABLE;
+extern concptr KEY_REALM1;
+extern concptr KEY_REALM2;
+extern concptr KEY_FIRST;
+extern concptr KEY_SECOND;
+extern concptr KEY_THIRD;
+extern concptr KEY_FOURTH;
+extern concptr KEY_ITEMS;
+extern concptr KEY_WEAPONS;
+extern concptr KEY_FAVORITE_WEAPONS;
+extern concptr KEY_ARMORS;
+extern concptr KEY_MISSILES;
+extern concptr KEY_DEVICES;
+extern concptr KEY_LIGHTS;
+extern concptr KEY_JUNKS;
+extern concptr KEY_CORPSES;
+extern concptr KEY_SPELLBOOKS;
+extern concptr KEY_HAFTED;
+extern concptr KEY_SHIELDS;
+extern concptr KEY_BOWS;
+extern concptr KEY_RINGS;
+extern concptr KEY_AMULETS;
+extern concptr KEY_SUITS;
+extern concptr KEY_CLOAKS;
+extern concptr KEY_HELMS;
+extern concptr KEY_GLOVES;
+extern concptr KEY_BOOTS;
