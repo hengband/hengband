7f14c926c273f36c1f68a27a5dbe9b18a1d68e43
Eric Branlund <ebranlund@fastmail.com>
2021-01-20 15:51:39 -0800

    Change listed keys in the English version of knowledge menu so they match with the Japanese and correspond to what needs to be pressed to access the given menu item.

--- src/cmd-io/cmd-knowledge.c
+++ src/cmd-io/cmd-knowledge.c
@@ -34,25 +34,25 @@ void do_cmd_knowledge(player_type *creature_ptr)
             prt(_("(1) 既知の伝説のアイテム                 の一覧", "(1) Display known artifacts"), 6, 5);
             prt(_("(2) 既知のアイテム                       の一覧", "(2) Display known objects"), 7, 5);
             prt(_("(3) 既知の生きているユニーク・モンスター の一覧", "(3) Display remaining uniques"), 8, 5);
-            prt(_("(4) 既知の撃破したユニーク・モンスター   の一覧", "(3) Display having defeated uniques"), 9, 5);
-            prt(_("(5) 既知のモンスター                     の一覧", "(4) Display known monster"), 10, 5);
-            prt(_("(6) 倒した敵の数                         の一覧", "(5) Display kill count"), 11, 5);
+            prt(_("(4) 既知の撃破したユニーク・モンスター   の一覧", "(4) Display having defeated uniques"), 9, 5);
+            prt(_("(5) 既知のモンスター                     の一覧", "(5) Display known monster"), 10, 5);
+            prt(_("(6) 倒した敵の数                         の一覧", "(6) Display kill count"), 11, 5);
             if (!vanilla_town)
-                prt(_("(7) 賞金首                               の一覧", "(6) Display wanted monsters"), 12, 5);
-            prt(_("(8) 現在のペット                         の一覧", "(7) Display current pets"), 13, 5);
-            prt(_("(9) 我が家のアイテム                     の一覧", "(8) Display home inventory"), 14, 5);
-            prt(_("(0) *鑑定*済み装備の耐性                 の一覧", "(9) Display *identified* equip."), 15, 5);
+                prt(_("(7) 賞金首                               の一覧", "(7) Display wanted monsters"), 12, 5);
+            prt(_("(8) 現在のペット                         の一覧", "(8) Display current pets"), 13, 5);
+            prt(_("(9) 我が家のアイテム                     の一覧", "(9) Display home inventory"), 14, 5);
+            prt(_("(0) *鑑定*済み装備の耐性                 の一覧", "(0) Display *identified* equip."), 15, 5);
         } else {
-            prt(_("(a) 地形の表示文字/タイル                の一覧", "(0) Display terrain symbols"), 6, 5);
-            prt(_("(b) 自分に関する情報                     の一覧", "(a) Display about yourself"), 7, 5);
-            prt(_("(c) 突然変異                             の一覧", "(b) Display mutations"), 8, 5);
-            prt(_("(d) 武器の経験値                         の一覧", "(c) Display weapon proficiency"), 9, 5);
-            prt(_("(e) 魔法の経験値                         の一覧", "(d) Display spell proficiency"), 10, 5);
-            prt(_("(f) 技能の経験値                         の一覧", "(e) Display misc. proficiency"), 11, 5);
-            prt(_("(g) プレイヤーの徳                       の一覧", "(f) Display virtues"), 12, 5);
-            prt(_("(h) 入ったダンジョン                     の一覧", "(g) Display dungeons"), 13, 5);
-            prt(_("(i) 実行中のクエスト                     の一覧", "(h) Display current quests"), 14, 5);
-            prt(_("(k) 現在の自動拾い/破壊設定              の一覧", "(i) Display auto pick/destroy"), 15, 5);
+            prt(_("(a) 地形の表示文字/タイル                の一覧", "(a) Display terrain symbols"), 6, 5);
+            prt(_("(b) 自分に関する情報                     の一覧", "(b) Display about yourself"), 7, 5);
+            prt(_("(c) 突然変異                             の一覧", "(c) Display mutations"), 8, 5);
+            prt(_("(d) 武器の経験値                         の一覧", "(d) Display weapon proficiency"), 9, 5);
+            prt(_("(e) 魔法の経験値                         の一覧", "(e) Display spell proficiency"), 10, 5);
+            prt(_("(f) 技能の経験値                         の一覧", "(f) Display misc. proficiency"), 11, 5);
+            prt(_("(g) プレイヤーの徳                       の一覧", "(g) Display virtues"), 12, 5);
+            prt(_("(h) 入ったダンジョン                     の一覧", "(h) Display dungeons"), 13, 5);
+            prt(_("(i) 実行中のクエスト                     の一覧", "(i) Display current quests"), 14, 5);
+            prt(_("(k) 現在の自動拾い/破壊設定              の一覧", "(k) Display auto pick/destroy"), 15, 5);
         }
 
         prt(_("-続く-", "-more-"), 17, 8);
