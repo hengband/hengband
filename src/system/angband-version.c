ff68d783fb98f3a11c882515877b86ca5d9dcafc
Eric Branlund <ebranlund@fastmail.com>
2021-01-26 16:09:20 -0800

    Include English version of alpha version title for splash screen.
    (cherry picked from commit 898e9aaeda2073e039e3690c200e6384210d4261)

--- src/system/angband-version.c
+++ src/system/angband-version.c
@@ -4,7 +4,7 @@
 void put_version(char *buf)
 {
     if (IS_ALPHA_VERSION) {
-        sprintf(buf, "変愚蛮怒 %d.%d.%dAlpha%d", H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH, H_VER_EXTRA);
+        sprintf(buf, _("変愚蛮怒 %d.%d.%dAlpha%d", "Hengband %d.%d.%dAlpha%d"), H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH, H_VER_EXTRA);
 
     } else {
         char *mode = IS_STABLE_VERSION ? _("安定版", "Stable") : _("開発版", "Developing");
