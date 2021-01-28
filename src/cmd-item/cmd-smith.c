f2a43b96a4798041e3e176df2589b3ae0b588f6c
Eric Branlund <ebranlund@fastmail.com>
2021-01-28 14:53:12 -0800

    Reworded English tip for the smithing enhance mode: replaced "an equipment" with "an item" and removed leading "the" in the second sentence.

--- src/cmd-item/cmd-smith.c
+++ src/cmd-item/cmd-smith.c
@@ -189,7 +189,7 @@ static concptr const kaji_tips[5] = {
     "Extract essences from an item. The item become non magical.",
     "Remove added essences from an equipment which was improved before. The removed essence will be ruined.",
     "Add essences to an item. The improved items or artifacts cannot be reimprove.",
-    "Enchant an equipment or make an equiment element-proofed. The improved items and artifacts can be enchanted too.",
+    "Enchant an item or make an item element-proofed. Improved items and artifacts can be enchanted too.",
 #endif
 };
 
