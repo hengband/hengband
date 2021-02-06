3684d784041e8d33095082b0425e64b93a475aef
Habu <habu@users.osdn.me>
2021-02-06 18:55:38 +0900

    [fix] #41489 明るいはずの場所でも灯りがない判定になっている
    デバッグ用自動セーブをONにしている時に現象が発生。
    セーブする時に、ダンジョンの明かりと視界の情報フラグが
    一旦リセットされるのが原因。
    同時に情報更新用のフラグが立てられているので、
    自動セーブ後にupdate_creature()を呼んで改めて
    明かりと視界の情報を更新するようにする。

--- src/io/input-key-requester.c
+++ src/io/input-key-requester.c
@@ -203,8 +203,10 @@ void request_command(player_type *player_ptr, int shopping)
     use_menu = FALSE;
 
     while (TRUE) {
-        if (!macro_running() && !command_new && auto_debug_save)
+        if (!macro_running() && !command_new && auto_debug_save) {
             save_player(player_ptr, SAVE_TYPE_DEBUG);
+            update_creature(player_ptr);
+        }
 
         if (command_new) {
             msg_erase();
