ea48422a3126c395919f76989c9f1abd7ea271ab
Eric Branlund <ebranlund@fastmail.com>
2021-02-07 15:59:52 -0800

    Add English translation for first SV_POTION_WATER message.

--- src/object-use/quaff-execution.c
+++ src/object-use/quaff-execution.c
@@ -145,7 +145,7 @@ void exe_quaff_potion(player_type *creature_ptr, INVENTORY_IDX item)
         switch (q_ptr->sval) {
             /* 飲みごたえをオリジナルより細かく表現 */
         case SV_POTION_WATER:
-            msg_print(_("口の中がさっぱりした。", ""));
+            msg_print(_("口の中がさっぱりした。", "That was refreshing."));
             msg_print(_("のどの渇きが少しおさまった。", "You feel less thirsty."));
             ident = TRUE;
             break;
