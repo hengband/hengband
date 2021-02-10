02c83d7d69638158258565f922c2563c19745f68
Eric Branlund <ebranlund@fastmail.com>
2021-02-10 11:25:20 -0800

    In English message for toragoroshi activation, add "the" and replace "enchanted" with "fascinated".

--- src/specific-object/toragoroshi.c
+++ src/specific-object/toragoroshi.c
@@ -4,7 +4,7 @@
 
 bool activate_toragoroshi(player_type *user_ptr)
 {
-    msg_print(_("あなたは妖刀に魅入られた…", "You are enchanted by cursed blade..."));
+    msg_print(_("あなたは妖刀に魅入られた…", "You are fascinated by the cursed blade..."));
     msg_print(_("「狂ほしく 血のごとき 月はのぼれり 秘めおきし 魔剣 いずこぞや」", "'Behold the blade arts.'"));
     massacre(user_ptr);
     return TRUE;
