3ca11937157ce0a7ac1762dad9453d12b281f8cf
dis- <dis.rogue@gmail.com>
2021-02-15 00:06:34 +0900

    [Feature] mon-info.txtの出力情報を現在のプレイヤー情報に依存させない
    デバッグ情報の出力が現在のキャラクターの状態に依存していて不安定であった。
    将来的にコマンドラインからコールすることも考慮し、依存関係を排除する。

--- src/view/display-lore.h
+++ src/view/display-lore.h
@@ -6,7 +6,7 @@
 void roff_top(MONRACE_IDX r_idx);
 void screen_roff(player_type *player_ptr, MONRACE_IDX r_idx, monster_lore_mode mode);
 void display_roff(player_type *player_ptr);
-void output_monster_spoiler(player_type *player_ptr, MONRACE_IDX r_idx, void (*roff_func)(TERM_COLOR attr, concptr str));
+void output_monster_spoiler(MONRACE_IDX r_idx, void (*roff_func)(TERM_COLOR attr, concptr str));
 void display_kill_numbers(lore_type *lore_ptr);
 bool display_where_to_appear(lore_type *lore_ptr);
 void display_random_move(lore_type *lore_ptr);
