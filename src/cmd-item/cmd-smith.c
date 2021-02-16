fe180e9d1ce7c2fd9310eb675e79f544d6918da6
Eric Branlund <ebranlund@fastmail.com>
2021-02-16 12:46:10 -0800

    Reword English phrases which had the awkward "an equipment".

--- src/cmd-item/cmd-smith.c
+++ src/cmd-item/cmd-smith.c
@@ -187,7 +187,7 @@ static concptr const kaji_tips[5] = {
 #else
     "Display essences you have.",
     "Extract essences from an item. The item become non magical.",
-    "Remove added essences from an equipment which was improved before. The removed essence will be ruined.",
+    "Remove added essences from equipment which was improved before. The removed essence will be ruined.",
     "Add essences to an item. The improved items or artifacts cannot be reimprove.",
     "Enchant an item or make an item element-proofed. Improved items and artifacts can be enchanted too.",
 #endif
