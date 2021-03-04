038faf23a474d14d2e36438379fcb971a9cc658e
deskull <deskull@users.sourceforge.jp>
2021-03-04 20:07:47 +0900

    [Add] secreen_object()に「それは二刀流の利き腕でない方に用いると扱いやすい」　を追加 / Add message "It is suitable for your non-dominant hand weapon of the dual wielding." to screen_object().

--- src/perception/identification.c
+++ src/perception/identification.c
@@ -162,6 +162,10 @@ bool screen_object(player_type *player_ptr, object_type *o_ptr, BIT_FLAGS mode)
         }
     }
 
+    if ((o_ptr->tval == TV_SWORD) && ((o_ptr->sval == SV_MAIN_GAUCHE) || (o_ptr->sval == SV_WAKIZASHI))) {
+        info[i++] = _("それは二刀流の利き腕でない方に用いると扱いやすい。", "It is suitable for your non-dominant hand weapon of the dual wielding.");
+    }
+
     if (has_flag(flgs, TR_STR)) {
         info[i++] = _("それは腕力に影響を及ぼす。", "It affects your strength.");
     }
