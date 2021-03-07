64ddffa0539e92c40171409b24803ff3be180745
iks <iks3@users.noreply.github.com>
2021-03-07 21:43:06 +0900

    [Feature] デバグコマンドの整理とコマンド一覧表示
    プレイヤー設定及びゲーム設定を変えるデバグコマンドをサブコマンド化。
    デバグコマンド入力時に一覧を出す。

---
+++ src/wizard/wizard-game-modifier.cpp
@@ -0,0 +1,130 @@
+﻿/*!
+ * @brief ゲーム属性を変更するデバッグコマンド
+ * @date 2021/03/07
+ */
+
+#include "wizard/wizard-game-modifier.h"
+#include "core/asking-player.h"
+#include "dungeon/quest.h"
+#include "info-reader/fixed-map-parser.h"
+#include "io/input-key-requester.h"
+#include "market/arena.h"
+#include "player-info/self-info.h"
+#include "system/floor-type-definition.h"
+#include "term/screen-processor.h"
+#include "util/int-char-converter.h"
+#include "view/display-messages.h"
+#include "wizard/wizard-special-process.h"
+#include <string>
+#include <vector>
+#include <sstream>
+
+void wiz_enter_quest(player_type *creature_ptr);
+void wiz_complete_quest(player_type *creature_ptr);
+
+/*!
+ * @brief ゲーム設定コマンド一覧表
+ */
+std::vector<std::vector<std::string>> wizard_game_modifier_menu_table = {
+    { "t", _("プレイ時間変更", "Modify played time") },
+    { "q", _("現在のクエストを完了", "Complete current quest") },
+    { "Q", _("クエストに突入", "Enter quest") },
+    { "g", _("モンスター闘技場出場者更新", "Update gambling monster") },
+};
+
+/*!
+ * @brief ゲーム設定コマンドの一覧を表示する
+ * @return なし
+ */
+void display_wizard_game_modifier_menu()
+{
+    for (int y = 1; y < 6; y++)
+        term_erase(14, y, 64);
+
+    int r = 1;
+    int c = 15;
+    int sz = wizard_game_modifier_menu_table.size();
+    for (int i = 0; i < sz; i++) {
+        std::stringstream ss;
+        ss << wizard_game_modifier_menu_table[i][0] << ") " << wizard_game_modifier_menu_table[i][1];
+        put_str(ss.str().c_str(), r++, c);
+    }
+}
+
+/*!
+ * @brief ゲーム設定コマンドの入力を受け付ける
+ * @param creature_ptr プレイヤーの情報へのポインタ
+ * @return なし
+ */
+void wizard_game_modifier(player_type *creature_ptr)
+{
+    screen_save();
+    display_wizard_game_modifier_menu();
+
+    char cmd;
+    get_com("Player Command: ", &cmd, FALSE);
+    screen_load();
+
+    switch (cmd) {
+    case ESCAPE:
+    case ' ':
+    case '\n':
+    case '\r':
+        break;
+    case 'g':
+        update_gambling_monsters(creature_ptr);
+        break;
+    case 'q':
+        wiz_complete_quest(creature_ptr);
+        break;
+    case 'Q':
+        wiz_enter_quest(creature_ptr);
+        break;
+    case 't':
+        set_gametime();
+        break;
+    }
+}
+
+/*!
+ * @brief 指定したクエストに突入する
+ * @param プレイヤーの情報へのポインタ
+ * @return なし
+ */
+void wiz_enter_quest(player_type* creature_ptr)
+{
+    char ppp[30];
+    char tmp_val[5];
+    int tmp_int;
+    sprintf(ppp, "QuestID (0-%d):", max_q_idx - 1);
+    sprintf(tmp_val, "%d", 0);
+
+    if (!get_string(ppp, tmp_val, 3))
+        return;
+
+    tmp_int = atoi(tmp_val);
+    if ((tmp_int < 0) || (tmp_int >= max_q_idx))
+        return;
+
+    creature_ptr->current_floor_ptr->inside_quest = (QUEST_IDX)tmp_int;
+    parse_fixed_map(creature_ptr, "q_info.txt", 0, 0, 0, 0);
+    quest[tmp_int].status = QUEST_STATUS_TAKEN;
+    creature_ptr->current_floor_ptr->inside_quest = 0;
+}
+
+/*!
+ * @brief 指定したクエストを完了させる
+ * @param プレイヤーの情報へのポインタ
+ * @return なし
+ */
+void wiz_complete_quest(player_type *creature_ptr)
+{
+    if (!creature_ptr->current_floor_ptr->inside_quest) {
+        msg_print("No current quest");
+        msg_print(NULL);
+        return;
+    }
+
+    if (quest[creature_ptr->current_floor_ptr->inside_quest].status == QUEST_STATUS_TAKEN)
+        complete_quest(creature_ptr, creature_ptr->current_floor_ptr->inside_quest);
+}
---
+++ src/wizard/wizard-player-modifier.cpp
@@ -0,0 +1,97 @@
+﻿/*!
+ * @brief プレイヤー属性を変更するデバッグコマンド
+ * @date 2021/03/07
+ */
+
+#include "wizard/wizard-player-modifier.h"
+#include "core/asking-player.h"
+#include "io/input-key-requester.h"
+#include "mutation/mutation-investor-remover.h"
+#include "player-info/self-info.h"
+#include "spell/spells-status.h"
+#include "term/screen-processor.h"
+#include "util/int-char-converter.h"
+#include "view/display-messages.h"
+#include "wizard/wizard-special-process.h"
+#include <string>
+#include <vector>
+#include <sstream>
+
+/*!
+ * @brief プレイヤー設定コマンド一覧表
+ */
+std::vector<std::vector<std::string>> wizard_player_modifier_menu_table = {
+    { "r", _("種族変更", "Change race") },
+    { "c", _("職業変更", "Change class") },
+    { "e", _("能力変更", "Change status") },
+    { "k", _("自己分析", "Self knowledge") },
+    { "l", _("ライフレート変更", "Set new life rate") },
+    { "m", _("突然変異", "Get mutation") },
+    { "a", _("属性表示", "Print your alignment") },
+};
+
+/*!
+ * @brief プレイヤー設定コマンドの一覧を表示する
+ * @return なし
+ */
+void display_wizard_player_modifier_menu()
+{
+    for (int y = 1; y < 9; y++)
+        term_erase(14, y, 64);
+
+    int r = 1;
+    int c = 15;
+    int sz = wizard_player_modifier_menu_table.size();
+    for (int i = 0; i < sz; i++) {
+        std::stringstream ss;
+        ss << wizard_player_modifier_menu_table[i][0] << ") " << wizard_player_modifier_menu_table[i][1];
+        put_str(ss.str().c_str(), r++, c);
+    }
+}
+
+/*!
+ * @brief プレイヤー設定コマンドの入力を受け付ける
+ * @param creature_ptr プレイヤーの情報へのポインタ
+ * @return なし
+ */
+void wizard_player_modifier(player_type *creature_ptr)
+{
+    screen_save();
+    display_wizard_player_modifier_menu();
+
+    char cmd;
+    get_com("Player Command: ", &cmd, FALSE);
+    screen_load();
+
+    switch (cmd) {
+    case ESCAPE:
+    case ' ':
+    case '\n':
+    case '\r':
+        break;
+    case 'a':
+        msg_format("Your alignment is %d.", creature_ptr->align);
+        break;
+    case 'c':
+        wiz_reset_class(creature_ptr);
+        break;
+    case 'e':
+        wiz_change_status(creature_ptr);
+        break;
+    case 'k':
+        self_knowledge(creature_ptr);
+        break;
+    case 'm':
+        (void)gain_mutation(creature_ptr, command_arg);
+        break;
+    case 'n':
+        roll_hitdice(creature_ptr, static_cast<spell_operation>(SPOP_DISPLAY_MES | SPOP_DEBUG));
+        break;
+    case 'r':
+        wiz_reset_race(creature_ptr);
+        break;
+    default:
+        msg_print("That is not a valid debug command.");
+        break;
+    }
+}
