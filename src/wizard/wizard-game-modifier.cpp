c4cb36d7630e25354031e76fed719f9baa93e8c4
iks <iks3@users.noreply.github.com>
2021-03-08 16:55:05 +0900

    [Feature] ユニーク/ナズグルを復活させるデバグコマンド #41

--- src/wizard/wizard-game-modifier.cpp
+++ src/wizard/wizard-game-modifier.cpp
@@ -9,9 +9,14 @@
 #include "info-reader/fixed-map-parser.h"
 #include "io/input-key-requester.h"
 #include "market/arena.h"
+#include "monster-race/monster-race.h"
+#include "monster-race/race-flags1.h"
+#include "monster-race/race-flags7.h"
 #include "player-info/self-info.h"
 #include "system/floor-type-definition.h"
+#include "system/system-variables.h"
 #include "term/screen-processor.h"
+#include "util/bit-flags-calculator.h"
 #include "util/int-char-converter.h"
 #include "view/display-messages.h"
 #include "wizard/wizard-special-process.h"
@@ -21,6 +26,7 @@
 
 void wiz_enter_quest(player_type *creature_ptr);
 void wiz_complete_quest(player_type *creature_ptr);
+void wiz_restore_monster_max_num();
 
 /*!
  * @brief ゲーム設定コマンド一覧表
@@ -29,6 +35,7 @@ std::vector<std::vector<std::string>> wizard_game_modifier_menu_table = {
     { "t", _("プレイ時間変更", "Modify played time") },
     { "q", _("現在のクエストを完了", "Complete current quest") },
     { "Q", _("クエストに突入", "Enter quest") },
+    { "u", _("ユニーク/ナズグルの生存数を復元", "Restore living info of unique/nazgul") },
     { "g", _("モンスター闘技場出場者更新", "Update gambling monster") },
 };
 
@@ -80,6 +87,9 @@ void wizard_game_modifier(player_type *creature_ptr)
     case 'Q':
         wiz_enter_quest(creature_ptr);
         break;
+    case 'u':
+        wiz_restore_monster_max_num();
+        break;
     case 't':
         set_gametime();
         break;
@@ -127,4 +137,50 @@ void wiz_complete_quest(player_type *creature_ptr)
 
     if (quest[creature_ptr->current_floor_ptr->inside_quest].status == QUEST_STATUS_TAKEN)
         complete_quest(creature_ptr, creature_ptr->current_floor_ptr->inside_quest);
+}
+
+void wiz_restore_monster_max_num()
+{
+    MONRACE_IDX r_idx = command_arg;
+    if (r_idx <= 0) {
+        std::stringstream ss;
+        ss << "Monster race (1-" << max_r_idx << "): ";
+
+        char tmp_val[160] = "\0";
+        if (!get_string(ss.str().c_str(), tmp_val, 5))
+            return;
+
+        r_idx = (MONRACE_IDX)atoi(tmp_val);
+        if (r_idx <= 0 || r_idx >= max_r_idx)
+            return;
+    }
+
+    monster_race *r_ptr = &r_info[r_idx];
+    if (!r_ptr->name) {
+        msg_print("そのモンスターは存在しません。");
+        msg_print(NULL);
+        return;
+    }
+
+    MONSTER_NUMBER n = 0;
+    if (any_bits(r_ptr->flags1, RF1_UNIQUE))
+        n = 1;
+    else if (any_bits(r_ptr->flags7, RF7_NAZGUL))
+        n = MAX_NAZGUL_NUM;
+
+    if (n == 0) {
+        msg_print("出現数に制限がないモンスターです。");
+        msg_print(NULL);
+        return;
+    }
+
+    r_ptr->max_num = n;
+    r_ptr->r_pkills = 0;
+    r_ptr->r_akills = 0;
+
+    std::stringstream ss;
+    std::string name(r_name + r_ptr->name);
+    ss << name << _("の出現数を復元しました。", " can appear again now.");
+    msg_print(ss.str().c_str());
+    msg_print(NULL);
 }
