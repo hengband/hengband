1b703ca8827f7dd8ed319d11052346a81849ddcc
deskull <deskull@users.sourceforge.jp>
2021-03-14 17:55:53 +0900

    [Version] 3.0.0Alpha11

--- src/system/angband-version.h
+++ src/system/angband-version.h
@@ -19,7 +19,7 @@
 #define FAKE_VER_MAJOR 13 /*!< ゲームのバージョン番号定義(メジャー番号 + 10) */
 #define FAKE_VER_MINOR 0 /*!< ゲームのバージョン番号定義(マイナー番号) */
 #define FAKE_VER_PATCH 0 /*!< ゲームのバージョン番号定義(パッチ番号) */
-#define FAKE_VER_EXTRA 10 /*!< ゲームのバージョン番号定義(エクストラ番号) */
+#define FAKE_VER_EXTRA 11 /*!< ゲームのバージョン番号定義(エクストラ番号) */
 
 /*!
  * @brief バージョンが開発版が安定版かを返す
