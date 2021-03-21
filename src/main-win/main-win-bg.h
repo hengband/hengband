570fccbbb423274491dd641f63c5fbac0dc96596
shimitei <shimitei@gmail.com>
2021-03-21 17:05:38 +0900

    [Feature] 壁紙の対応画像形式追加 #596
    壁紙の選択ダイアログでBMPの他に、PNGとJPEGを読み込めるようにした。
    また、効果音、BGMの初期化フラグのスコープをローカル変数にした。
    
    画像の読み込みをGDI+で行うように変更した。
    Windows XP以降は標準でGDI+に対応している。

--- src/main-win/main-win-bg.h
+++ src/main-win/main-win-bg.h
@@ -1,9 +1,13 @@
 ﻿#pragma once
 
-#include "main-win/main-win-windows.h"
 #include "main-win/main-win-define.h"
 
+#include <windows.h>
+
 extern char bg_bitmap_file[MAIN_WIN_MAX_PATH]; //!< 現在の背景ビットマップファイル名。
+
+void load_bg_prefs(void);
+void finalize_bg();
 
 void delete_bg(void);
 BOOL init_bg(void);
