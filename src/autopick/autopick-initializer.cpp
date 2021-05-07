bc201cffaef1f7d7964f5253361773c69343fb2b
shimitei <shimitei@gmail.com>
2021-05-08 08:32:09 +0900

    [Refactor] clang-format適用

--- src/autopick/autopick-initializer.cpp
+++ src/autopick/autopick-initializer.cpp
@@ -8,21 +8,20 @@
  */
 void init_autopick(void)
 {
-	static const char easy_autopick_inscription[] = "(:=g";
-	autopick_type entry;
-	int i;
+    static const char easy_autopick_inscription[] = "(:=g";
+    autopick_type entry;
+    int i;
 
-	if (!autopick_list)
-	{
-		max_max_autopick = MAX_AUTOPICK_DEFAULT;
-		C_MAKE(autopick_list, max_max_autopick, autopick_type);
-		max_autopick = 0;
-	}
+    if (!autopick_list) {
+        max_max_autopick = MAX_AUTOPICK_DEFAULT;
+        C_MAKE(autopick_list, max_max_autopick, autopick_type);
+        max_autopick = 0;
+    }
 
-	for (i = 0; i < max_autopick; i++)
-		autopick_free_entry(&autopick_list[i]);
+    for (i = 0; i < max_autopick; i++)
+        autopick_free_entry(&autopick_list[i]);
 
-	max_autopick = 0;
-	autopick_new_entry(&entry, easy_autopick_inscription, TRUE);
-	autopick_list[max_autopick++] = entry;
+    max_autopick = 0;
+    autopick_new_entry(&entry, easy_autopick_inscription, TRUE);
+    autopick_list[max_autopick++] = entry;
 }
