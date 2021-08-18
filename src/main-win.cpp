3450925bcdaeaf11207a2b84c121716d972b6037
Hourier <grapefox.whitelucifer.0408@gmail.com>
2021-08-19 03:38:07 +0900

    [Refactor] #924 Renamed term_data_link() to link_data() and moved main-win.cpp to main-win-term.cpp/h; related methods also were moved

--- src/main-win.cpp
+++ src/main-win.cpp
@@ -163,22 +163,10 @@ bool movie_in_progress = false;
 bool initialized = false;
 
 /*
- * Saved instance handle
- */
-static HINSTANCE hInstance;
-
-/*
- * Yellow brush for the cursor
- */
-static HBRUSH hbrYellow;
-
-/*
  * An icon
  */
 static HICON hIcon;
 
-/* bg */
-bg_mode current_bg_mode = bg_mode::BG_NONE;
 #define DEFAULT_BG_FILENAME "bg.bmp"
 char wallpaper_file[MAIN_WIN_MAX_PATH] = ""; //!< 壁紙ファイル名。
 
@@ -193,19 +181,9 @@ static bool keep_subwindows = true;
 static concptr ini_file = NULL;
 
 /*
- * Name of application
- */
-static LPCWSTR AppName = L"ANGBAND";
-
-/*
  * Name of sub-window type
  */
 static LPCWSTR AngList = L"AngList";
-
-/*
- * The "complex" color values
- */
-static COLORREF win_clr[256];
 
 /*
  * Flag for macro trigger with dump ASCII
@@ -597,535 +575,6 @@ void term_inversed_area(HWND hWnd, int x, int y, int w, int h)
 }
 
 /*!
- * @brief Windows版ユーザ設定項目実装部(実装必須) /Interact with the User
- */
-static errr term_user_win(int n)
-{
-    (void)n;
-    return 0;
-}
-
-/*!
- * @brief カラーパレットの変更？
- */
-static void refresh_color_table()
-{
-    for (int i = 0; i < 256; i++) {
-        byte rv = angband_color_table[i][1];
-        byte gv = angband_color_table[i][2];
-        byte bv = angband_color_table[i][3];
-        win_clr[i] = PALETTERGB(rv, gv, bv);
-    }
-}
-
-/*!
- * @brief グラフィクスのモード変更
- */
-static void change_graphics_mode(graphics_mode mode)
-{
-    graphics_mode ret = graphic.change_graphics(mode);
-    if (ret != mode) {
-        plog(_("グラフィクスを初期化できません!", "Cannot initialize graphics!"));
-    }
-    arg_graphics = static_cast<byte>(ret);
-    use_graphics = (arg_graphics > 0);
-}
-
-/*!
- * @brief React to global changes
- */
-static errr term_xtra_win_react(player_type *player_ptr)
-{
-    refresh_color_table();
-
-    const byte current_mode = static_cast<byte>(graphic.get_mode());
-    if (current_mode != arg_graphics) {
-        change_graphics_mode(static_cast<graphics_mode>(arg_graphics));
-        reset_visuals(player_ptr);
-    }
-
-    for (int i = 0; i < MAX_TERM_DATA; i++) {
-        term_data *td = &data[i];
-        if ((td->cols != td->t.wid) || (td->rows != td->t.hgt)) {
-            td->rebuild();
-        }
-    }
-
-    return 0;
-}
-
-/*!
- * @brief Process at least one event
- */
-static errr term_xtra_win_event(int v)
-{
-    MSG msg;
-    if (v) {
-        if (GetMessage(&msg, NULL, 0, 0)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    } else {
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return 0;
-}
-
-/*!
- * @brief Process all pending events
- */
-static errr term_xtra_win_flush(void)
-{
-    MSG msg;
-    while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
-}
-
-/*!
- * @brief Hack -- clear the screen
- * @details
- * Make this more efficient
- */
-static errr term_xtra_win_clear(void)
-{
-    term_data *td = (term_data *)(Term->data);
-
-    RECT rc;
-    GetClientRect(td->w, &rc);
-
-    HDC hdc = td->get_hdc();
-    SetBkColor(hdc, RGB(0, 0, 0));
-    SelectObject(hdc, td->font_id);
-    ExtTextOut(hdc, 0, 0, ETO_OPAQUE, &rc, NULL, 0, NULL);
-
-    if (current_bg_mode != bg_mode::BG_NONE) {
-        rc.left = 0;
-        rc.top = 0;
-        draw_bg(hdc, &rc);
-    }
-
-    td->refresh();
-    return 0;
-}
-
-/*!
- * @brief Hack -- make a noise
- */
-static errr term_xtra_win_noise(void)
-{
-    MessageBeep(MB_ICONASTERISK);
-    return 0;
-}
-
-/*!
- * @brief Hack -- make a sound
- */
-static errr term_xtra_win_sound(int v)
-{
-    if (!use_sound)
-        return 1;
-    return play_sound(v);
-}
-
-/*!
- * @brief Hack -- play a music
- */
-static errr term_xtra_win_music(int n, int v)
-{
-    if (!use_music) {
-        return 1;
-    }
-
-    return main_win_music::play_music(n, v);
-}
-
-/*!
- * @brief Hack -- play a music matches a situation
- */
-static errr term_xtra_win_scene(int v)
-{
-    // TODO 場面に合った壁紙変更対応
-    if (!use_music) {
-        return 1;
-    }
-
-    return main_win_music::play_music_scene(v);
-}
-
-/*!
- * @brief Delay for "x" milliseconds
- */
-static int term_xtra_win_delay(int v)
-{
-    Sleep(v);
-    return 0;
-}
-
-/*!
- * @brief Do a "special thing"
- * @todo z-termに影響があるのでplayer_typeの追加は保留
- */
-static errr term_xtra_win(int n, int v)
-{
-    switch (n) {
-    case TERM_XTRA_NOISE: {
-        return (term_xtra_win_noise());
-    }
-    case TERM_XTRA_FRESH: {
-        term_data *td = (term_data *)(Term->data);
-        if (td->w)
-            UpdateWindow(td->w);
-        return 0;
-    }
-    case TERM_XTRA_MUSIC_BASIC:
-    case TERM_XTRA_MUSIC_DUNGEON:
-    case TERM_XTRA_MUSIC_QUEST:
-    case TERM_XTRA_MUSIC_TOWN:
-    case TERM_XTRA_MUSIC_MONSTER: {
-        return term_xtra_win_music(n, v);
-    }
-    case TERM_XTRA_MUSIC_MUTE: {
-        return main_win_music::stop_music();
-    }
-    case TERM_XTRA_SCENE: {
-        return term_xtra_win_scene(v);
-    }
-    case TERM_XTRA_SOUND: {
-        return (term_xtra_win_sound(v));
-    }
-    case TERM_XTRA_BORED: {
-        return (term_xtra_win_event(0));
-    }
-    case TERM_XTRA_EVENT: {
-        return (term_xtra_win_event(v));
-    }
-    case TERM_XTRA_FLUSH: {
-        return (term_xtra_win_flush());
-    }
-    case TERM_XTRA_CLEAR: {
-        return (term_xtra_win_clear());
-    }
-    case TERM_XTRA_REACT: {
-        return (term_xtra_win_react(p_ptr));
-    }
-    case TERM_XTRA_DELAY: {
-        return (term_xtra_win_delay(v));
-    }
-    }
-
-    return 1;
-}
-
-/*!
- * @brief Low level graphics (Assumes valid input).
- * @details
- * Draw a "cursor" at (x,y), using a "yellow box".
- */
-static errr term_curs_win(int x, int y)
-{
-    term_data *td = (term_data *)(Term->data);
-    int tile_wid, tile_hgt;
-    tile_wid = td->tile_wid;
-    tile_hgt = td->tile_hgt;
-
-    RECT rc;
-    rc.left = x * tile_wid + td->size_ow1;
-    rc.right = rc.left + tile_wid;
-    rc.top = y * tile_hgt + td->size_oh1;
-    rc.bottom = rc.top + tile_hgt;
-
-    HDC hdc = td->get_hdc();
-    FrameRect(hdc, &rc, hbrYellow);
-    td->refresh(&rc);
-    return 0;
-}
-
-/*!
- * @brief Low level graphics (Assumes valid input).
- * @details
- * Draw a "big cursor" at (x,y), using a "yellow box".
- */
-static errr term_bigcurs_win(int x, int y)
-{
-    term_data *td = (term_data *)(Term->data);
-    int tile_wid, tile_hgt;
-    tile_wid = td->tile_wid;
-    tile_hgt = td->tile_hgt;
-
-    RECT rc;
-    rc.left = x * tile_wid + td->size_ow1;
-    rc.right = rc.left + 2 * tile_wid;
-    rc.top = y * tile_hgt + td->size_oh1;
-    rc.bottom = rc.top + tile_hgt;
-
-    HDC hdc = td->get_hdc();
-    FrameRect(hdc, &rc, hbrYellow);
-    td->refresh(&rc);
-    return 0;
-}
-
-/*!
- * @brief Low level graphics (Assumes valid input).
- * @details
- * Erase a "block" of "n" characters starting at (x,y).
- */
-static errr term_wipe_win(int x, int y, int n)
-{
-    term_data *td = (term_data *)(Term->data);
-    RECT rc;
-    rc.left = x * td->tile_wid + td->size_ow1;
-    rc.right = rc.left + n * td->tile_wid;
-    rc.top = y * td->tile_hgt + td->size_oh1;
-    rc.bottom = rc.top + td->tile_hgt;
-
-    HDC hdc = td->get_hdc();
-    SetBkColor(hdc, RGB(0, 0, 0));
-    SelectObject(hdc, td->font_id);
-    if (current_bg_mode != bg_mode::BG_NONE)
-        draw_bg(hdc, &rc);
-    else
-        ExtTextOut(hdc, 0, 0, ETO_OPAQUE, &rc, NULL, 0, NULL);
-
-    td->refresh(&rc);
-    return 0;
-}
-
-/*!
- * @brief Low level graphics.  Assumes valid input.
- * @details
- * Draw several ("n") chars, with an attr, at a given location.
- *
- * All "graphic" data is handled by "term_pict_win()", below.
- *
- * One would think there is a more efficient method for telling a window
- * what color it should be using to draw with, but perhaps simply changing
- * it every time is not too inefficient.
- */
-static errr term_text_win(int x, int y, int n, TERM_COLOR a, concptr s)
-{
-    term_data *td = (term_data *)(Term->data);
-    static HBITMAP WALL;
-    static HBRUSH myBrush, oldBrush;
-    static HPEN oldPen;
-    static bool init_done = false;
-
-    if (!init_done) {
-        WALL = LoadBitmapW(hInstance, AppName);
-        myBrush = CreatePatternBrush(WALL);
-        init_done = true;
-    }
-
-    RECT rc{ static_cast<LONG>(x * td->tile_wid + td->size_ow1), static_cast<LONG>(y * td->tile_hgt + td->size_oh1),
-        static_cast<LONG>(rc.left + n * td->tile_wid), static_cast<LONG>(rc.top + td->tile_hgt) };
-    RECT rc_start = rc;
-
-    HDC hdc = td->get_hdc();
-    SetBkColor(hdc, RGB(0, 0, 0));
-    SetTextColor(hdc, win_clr[a]);
-
-    SelectObject(hdc, td->font_id);
-    if (current_bg_mode != bg_mode::BG_NONE)
-        SetBkMode(hdc, TRANSPARENT);
-
-    ExtTextOut(hdc, 0, 0, ETO_OPAQUE, &rc, NULL, 0, NULL);
-    if (current_bg_mode != bg_mode::BG_NONE)
-        draw_bg(hdc, &rc);
-
-    rc.left += ((td->tile_wid - td->font_wid) / 2);
-    rc.right = rc.left + td->font_wid;
-    rc.top += ((td->tile_hgt - td->font_hgt) / 2);
-    rc.bottom = rc.top + td->font_hgt;
-
-    for (int i = 0; i < n; i++) {
-#ifdef JP
-        if (use_bigtile && *(s + i) == "■"[0] && *(s + i + 1) == "■"[1]) {
-            rc.right += td->font_wid;
-            oldBrush = static_cast<HBRUSH>(SelectObject(hdc, myBrush));
-            oldPen = static_cast<HPEN>(SelectObject(hdc, GetStockObject(NULL_PEN)));
-            Rectangle(hdc, rc.left, rc.top, rc.right + 1, rc.bottom + 1);
-            SelectObject(hdc, oldBrush);
-            SelectObject(hdc, oldPen);
-            rc.right -= td->font_wid;
-            i++;
-            rc.left += 2 * td->tile_wid;
-            rc.right += 2 * td->tile_wid;
-        } else if (iskanji(*(s + i))) /* 2バイト文字 */
-        {
-            char tmp[] = { *(s + i), *(s + i + 1), '\0' };
-            to_wchar wc(tmp);
-            const auto *buf = wc.wc_str();
-            rc.right += td->font_wid;
-            if (buf == NULL)
-                ExtTextOutA(hdc, rc.left, rc.top, ETO_CLIPPED, &rc, s + i, 2, NULL);
-            else
-                ExtTextOutW(hdc, rc.left, rc.top, ETO_CLIPPED, &rc, buf, wcslen(buf), NULL);
-            rc.right -= td->font_wid;
-            i++;
-            rc.left += 2 * td->tile_wid;
-            rc.right += 2 * td->tile_wid;
-        } else if (*(s + i) == 127) {
-            oldBrush = static_cast<HBRUSH>(SelectObject(hdc, myBrush));
-            oldPen = static_cast<HPEN>(SelectObject(hdc, GetStockObject(NULL_PEN)));
-            Rectangle(hdc, rc.left, rc.top, rc.right + 1, rc.bottom + 1);
-            SelectObject(hdc, oldBrush);
-            SelectObject(hdc, oldPen);
-            rc.left += td->tile_wid;
-            rc.right += td->tile_wid;
-        } else {
-            ExtTextOutA(hdc, rc.left, rc.top, ETO_CLIPPED, &rc, s + i, 1, NULL);
-            rc.left += td->tile_wid;
-            rc.right += td->tile_wid;
-        }
-#else
-        if (*(s + i) == 127) {
-            oldBrush = static_cast<HBRUSH>(SelectObject(hdc, myBrush));
-            oldPen = static_cast<HPEN>(SelectObject(hdc, GetStockObject(NULL_PEN)));
-            Rectangle(hdc, rc.left, rc.top, rc.right + 1, rc.bottom + 1);
-            SelectObject(hdc, oldBrush);
-            SelectObject(hdc, oldPen);
-            rc.left += td->tile_wid;
-            rc.right += td->tile_wid;
-        } else {
-            ExtTextOutA(hdc, rc.left, rc.top, ETO_CLIPPED, &rc, s + i, 1, NULL);
-            rc.left += td->tile_wid;
-            rc.right += td->tile_wid;
-        }
-#endif
-    }
-
-    rc.left = rc_start.left;
-    rc.top = rc_start.top;
-    td->refresh(&rc);
-    return 0;
-}
-
-/*!
- * @brief Low level graphics.  Assumes valid input.
- * @details
- * Draw an array of "special" attr/char pairs at the given location.
- *
- * We use the "term_pict_win()" function for "graphic" data, which are
- * encoded by setting the "high-bits" of both the "attr" and the "char"
- * data.  We use the "attr" to represent the "row" of the main bitmap,
- * and the "char" to represent the "col" of the main bitmap.  The use
- * of this function is induced by the "higher_pict" flag.
- *
- * If "graphics" is not available, we simply "wipe" the given grids.
- */
-static errr term_pict_win(TERM_LEN x, TERM_LEN y, int n, const TERM_COLOR *ap, concptr cp, const TERM_COLOR *tap, concptr tcp)
-{
-    term_data *td = (term_data *)(Term->data);
-    int i;
-    HDC hdcMask = NULL;
-    if (!use_graphics) {
-        return (term_wipe_win(x, y, n));
-    }
-
-    const tile_info &infGraph = graphic.get_tile_info();
-    const bool has_mask = (infGraph.hBitmapMask != NULL);
-    TERM_LEN w1 = infGraph.CellWidth;
-    TERM_LEN h1 = infGraph.CellHeight;
-    TERM_LEN tw1 = infGraph.TileWidth;
-    TERM_LEN th1 = infGraph.TileHeight;
-    TERM_LEN w2, h2, tw2 = 0;
-    w2 = td->tile_wid;
-    h2 = td->tile_hgt;
-    tw2 = w2;
-    if (use_bigtile)
-        tw2 *= 2;
-
-    TERM_LEN x2 = x * w2 + td->size_ow1 + infGraph.OffsetX;
-    TERM_LEN y2 = y * h2 + td->size_oh1 + infGraph.OffsetY;
-    HDC hdc = td->get_hdc();
-    HDC hdcSrc = CreateCompatibleDC(hdc);
-    HBITMAP hbmSrcOld = static_cast<HBITMAP>(SelectObject(hdcSrc, infGraph.hBitmap));
-
-    if (has_mask) {
-        hdcMask = CreateCompatibleDC(hdc);
-        SelectObject(hdcMask, infGraph.hBitmapMask);
-    }
-
-    for (i = 0; i < n; i++, x2 += w2) {
-        TERM_COLOR a = ap[i];
-        char c = cp[i];
-        int row = (a & 0x7F);
-        int col = (c & 0x7F);
-        TERM_LEN x1 = col * w1;
-        TERM_LEN y1 = row * h1;
-
-        if (has_mask) {
-            TERM_LEN x3 = (tcp[i] & 0x7F) * w1;
-            TERM_LEN y3 = (tap[i] & 0x7F) * h1;
-            tw2 = tw2 * w1 / tw1;
-            h2 = h2 * h1 / th1;
-            if ((tw1 == tw2) && (th1 == h2)) {
-                BitBlt(hdc, x2, y2, tw2, h2, hdcSrc, x3, y3, SRCCOPY);
-                BitBlt(hdc, x2, y2, tw2, h2, hdcMask, x1, y1, SRCAND);
-                BitBlt(hdc, x2, y2, tw2, h2, hdcSrc, x1, y1, SRCPAINT);
-                continue;
-            }
-
-            SetStretchBltMode(hdc, COLORONCOLOR);
-            StretchBlt(hdc, x2, y2, tw2, h2, hdcMask, x3, y3, w1, h1, SRCAND);
-            StretchBlt(hdc, x2, y2, tw2, h2, hdcSrc, x3, y3, w1, h1, SRCPAINT);
-            if ((x1 != x3) || (y1 != y3)) {
-                StretchBlt(hdc, x2, y2, tw2, h2, hdcMask, x1, y1, w1, h1, SRCAND);
-                StretchBlt(hdc, x2, y2, tw2, h2, hdcSrc, x1, y1, w1, h1, SRCPAINT);
-            }
-
-            continue;
-        }
-
-        if ((w1 == tw2) && (h1 == h2)) {
-            BitBlt(hdc, x2, y2, tw2, h2, hdcSrc, x1, y1, SRCCOPY);
-            continue;
-        }
-
-        SetStretchBltMode(hdc, COLORONCOLOR);
-        StretchBlt(hdc, x2, y2, tw2, h2, hdcSrc, x1, y1, w1, h1, SRCCOPY);
-    }
-
-    SelectObject(hdcSrc, hbmSrcOld);
-    DeleteDC(hdcSrc);
-    if (has_mask) {
-        SelectObject(hdcMask, hbmSrcOld);
-        DeleteDC(hdcMask);
-    }
-
-    td->refresh();
-    return 0;
-}
-
-/*!
- * @brief Create and initialize a "term_data" given a title
- */
-static void term_data_link(term_data *td)
-{
-    term_type *t = &td->t;
-    term_init(t, td->cols, td->rows, FILE_READ_BUFF_SIZE);
-    t->soft_cursor = true;
-    t->higher_pict = true;
-    t->attr_blank = TERM_WHITE;
-    t->char_blank = ' ';
-    t->user_hook = term_user_win;
-    t->xtra_hook = term_xtra_win;
-    t->curs_hook = term_curs_win;
-    t->bigcurs_hook = term_bigcurs_win;
-    t->wipe_hook = term_wipe_win;
-    t->text_hook = term_text_win;
-    t->pict_hook = term_pict_win;
-    t->data = (vptr)(td);
-}
-
-/*!
  * @brief Create the windows
  * @details
  * First, instantiate the "default" values, then read the "ini_file"
@@ -1219,7 +668,7 @@ static void init_windows(void)
         }
         td->size_hack = false;
 
-        term_data_link(td);
+        td->link_data();
         angband_term[i] = &td->t;
 
         if (td->visible) {
@@ -1250,7 +699,7 @@ static void init_windows(void)
     td->resize_window();
     td->size_hack = false;
 
-    term_data_link(td);
+    td->link_data();
     angband_term[0] = &td->t;
     normsize.x = td->cols;
     normsize.y = td->rows;
@@ -1262,7 +711,7 @@ static void init_windows(void)
 
     SetWindowPos(td->w, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
     hbrYellow = CreateSolidBrush(win_clr[TERM_YELLOW]);
-    (void)term_xtra_win_flush();
+    (void)term_data::term_xtra_win_flush();
 }
 
 /*!
@@ -2492,9 +1941,9 @@ int WINAPI WinMain(
     }
     ReleaseDC(NULL, hdc);
 
-    refresh_color_table();
+    term_data::refresh_color_table();
     init_windows();
-    change_graphics_mode(static_cast<graphics_mode>(arg_graphics));
+    term_data::change_graphics_mode(static_cast<graphics_mode>(arg_graphics));
     change_bg_mode(current_bg_mode, true);
 
     // after term_data initialize
