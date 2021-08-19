d370eb5ac88f858d3fd389985ee2c58d04f2c11b
Hourier <grapefox.whitelucifer.0408@gmail.com>
2021-08-19 23:59:16 +0900

    [Refactor] #1370 Defined constructor in it_type

--- src/action/throw-util.cpp
+++ src/action/throw-util.cpp
@@ -5,6 +5,17 @@
 
 #include "action/throw-util.h"
 #include "system/object-type-definition.h"
+#include "system/player-type-definition.h"
+
+it_type::it_type(player_type *creature_ptr, object_type *q_ptr, const int delay_factor_val, const int mult, const bool boomerang, const OBJECT_IDX shuriken)
+    : q_ptr(q_ptr)
+    , mult(mult)
+    , msec(delay_factor_val * delay_factor_val * delay_factor_val)
+    , boomerang(boomerang)
+    , shuriken(shuriken)
+    , creature_ptr(creature_ptr)
+{
+}
 
 it_type *initialize_it_type(
     it_type *item_throw_ptr, object_type *q_ptr, const int delay_factor_val, const int mult, const bool boomerang, const OBJECT_IDX shuriken)
--- src/action/throw-util.h
+++ src/action/throw-util.h
@@ -8,46 +8,54 @@
 #include "system/system-variables.h"
 
 // Item Throw.
-typedef struct grid_type grid_type;
-typedef struct monster_type monster_type;
-typedef struct object_type object_type;
-typedef struct it_type {
+struct grid_type;
+struct monster_type;
+struct object_type;
+struct player_type;
+struct it_type {
+public:
+    it_type() = default;
+    it_type(player_type *creature_ptr, object_type *q_ptr, const int delay_factor_val, const int mult, const bool boomerang, const OBJECT_IDX shuriken);
+    virtual ~it_type() = default;
+    object_type *q_ptr;
     int mult;
+    int msec;
     bool boomerang;
     OBJECT_IDX shuriken;
-    OBJECT_IDX item;
-    POSITION y;
-    POSITION x;
-    POSITION ty;
-    POSITION tx;
-    POSITION prev_y;
-    POSITION prev_x;
-    POSITION ny[19];
-    POSITION nx[19];
-    int chance;
-    int tdam;
-    int tdis;
-    int cur_dis;
-    int visible;
-    PERCENTAGE corruption_possibility;
-    object_type *q_ptr;
-    object_type *o_ptr;
-    bool hit_body;
-    bool hit_wall;
-    bool equiped_item;
-    bool return_when_thrown;
-    GAME_TEXT o_name[MAX_NLEN];
-    int msec;
-    BIT_FLAGS obj_flags[TR_FLAG_SIZE];
-    bool come_back;
-    bool do_drop;
-    grid_type *g_ptr;
-    monster_type *m_ptr;
-    GAME_TEXT m_name[MAX_NLEN];
-    int back_chance;
-    char o2_name[MAX_NLEN];
-    bool super_boomerang;
-} it_type;
+    OBJECT_IDX item{};
+    POSITION y{};
+    POSITION x{};
+    POSITION ty{};
+    POSITION tx{};
+    POSITION prev_y{};
+    POSITION prev_x{};
+    POSITION ny[19]{};
+    POSITION nx[19]{};
+    int chance{};
+    int tdam{};
+    int tdis{};
+    int cur_dis{};
+    int visible{};
+    PERCENTAGE corruption_possibility{};
+    object_type *o_ptr{};
+    bool hit_body = false;
+    bool hit_wall = false;
+    bool equiped_item = false;
+    bool return_when_thrown = false;
+    GAME_TEXT o_name[MAX_NLEN]{};
+    BIT_FLAGS obj_flags[TR_FLAG_SIZE]{};
+    bool come_back = false;
+    bool do_drop = true;
+    grid_type *g_ptr{};
+    monster_type *m_ptr{};
+    GAME_TEXT m_name[MAX_NLEN]{};
+    int back_chance{};
+    char o2_name[MAX_NLEN]{};
+    bool super_boomerang{};
+
+private:
+    player_type *creature_ptr;
+};
 
 it_type *initialize_it_type(
     it_type *item_throw_ptr, object_type *q_ptr, const int delay_factor_val, const int mult, const bool boomerang, const OBJECT_IDX shuriken);
