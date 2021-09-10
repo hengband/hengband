ca02b54d844a96aac7bb14790014b7ececefaa1a
Hourier <grapefox.whitelucifer.0408@gmail.com>
2021-09-11 00:52:07 +0900

    [Refactor] #1479 Changed o_ptr to player_ptr

--- src/world/world-object.h
+++ src/world/world-object.h
@@ -5,4 +5,4 @@
 struct floor_type;
 struct player_type;
 OBJECT_IDX o_pop(floor_type *floor_ptr);
-OBJECT_IDX get_obj_num(player_type *o_ptr, DEPTH level, BIT_FLAGS mode);
+OBJECT_IDX get_obj_num(player_type *player_ptr, DEPTH level, BIT_FLAGS mode);
