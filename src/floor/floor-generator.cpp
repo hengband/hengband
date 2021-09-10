9a5a85b622fe68754c4194ab4637c3406a91625e
Hourier <grapefox.whitelucifer.0408@gmail.com>
2021-09-11 00:46:08 +0900

    [Refactor] #1479 Changed challenger_ptr to player_ptr

--- src/floor/floor-generator.cpp
+++ src/floor/floor-generator.cpp
@@ -110,18 +110,18 @@ static void build_arena(player_type *player_ptr, POSITION *start_y, POSITION *st
 /*!
  * @brief 挑戦時闘技場への入場処理 / Town logic flow for generation of on_defeat_arena_monster -KMW-
  */
-static void generate_challenge_arena(player_type *challanger_ptr)
+static void generate_challenge_arena(player_type *player_ptr)
 {
     POSITION qy = 0;
     POSITION qx = 0;
-    floor_type *floor_ptr = challanger_ptr->current_floor_ptr;
+    floor_type *floor_ptr = player_ptr->current_floor_ptr;
     floor_ptr->height = SCREEN_HGT;
     floor_ptr->width = SCREEN_WID;
 
     POSITION y, x;
     for (y = 0; y < MAX_HGT; y++)
         for (x = 0; x < MAX_WID; x++) {
-            place_bold(challanger_ptr, y, x, GB_SOLID_PERM);
+            place_bold(player_ptr, y, x, GB_SOLID_PERM);
             floor_ptr->grid_array[y][x].info |= (CAVE_GLOW | CAVE_MARK);
         }
 
@@ -129,13 +129,13 @@ static void generate_challenge_arena(player_type *challanger_ptr)
         for (x = qx + 1; x < qx + SCREEN_WID - 1; x++)
             floor_ptr->grid_array[y][x].feat = feat_floor;
 
-    build_arena(challanger_ptr, &y, &x);
-    player_place(challanger_ptr, y, x);
-    if (place_monster_aux(challanger_ptr, 0, challanger_ptr->y + 5, challanger_ptr->x, arena_info[challanger_ptr->arena_number].r_idx, PM_NO_KAGE | PM_NO_PET))
+    build_arena(player_ptr, &y, &x);
+    player_place(player_ptr, y, x);
+    if (place_monster_aux(player_ptr, 0, player_ptr->y + 5, player_ptr->x, arena_info[player_ptr->arena_number].r_idx, PM_NO_KAGE | PM_NO_PET))
         return;
 
-    challanger_ptr->exit_bldg = true;
-    challanger_ptr->arena_number++;
+    player_ptr->exit_bldg = true;
+    player_ptr->arena_number++;
     msg_print(_("相手は欠場した。あなたの不戦勝だ。", "The enemy is unable to appear. You won by default."));
 }
 
