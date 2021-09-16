123fa9b59ef8f9912c39d3613ba374b5260f9657
Hourier <66951241+Hourier@users.noreply.github.com>
2021-09-16 22:49:39 +0900

    [Refactor] #1498 Defined StunRank and get_rank()

--- src/timed-effect/player-stun.cpp
+++ src/timed-effect/player-stun.cpp
@@ -1,8 +1,30 @@
 ﻿#include "timed-effect/player-stun.h"
 
-short PlayerStun::current()
+short PlayerStun::current() const
 {
     return this->stun;
+}
+
+StunRank PlayerStun::get_rank() const
+{
+    return this->get_rank(this->stun);
+}
+
+StunRank PlayerStun::get_rank(short value) const
+{
+    if (value > 100) {
+        return StunRank::UNCONSCIOUS;
+    }
+
+    if (value > 50) {
+        return StunRank::HARD;
+    }
+
+    if (value > 0) {
+        return StunRank::NORMAL;
+    }
+
+    return StunRank::NONE;
 }
 
 void PlayerStun::set(short value)
--- src/timed-effect/player-stun.h
+++ src/timed-effect/player-stun.h
@@ -1,11 +1,20 @@
 ﻿#pragma once
 
+enum class StunRank {
+    NONE = 0,
+    NORMAL = 1,
+    HARD = 2,
+    UNCONSCIOUS = 3,
+};
+
 class PlayerStun {
 public:
     PlayerStun() = default;
     virtual ~PlayerStun() = default;
 
-    short current();
+    short current() const;
+    StunRank get_rank() const;
+    StunRank get_rank(short value) const;
     void set(short value);
 
 private:
