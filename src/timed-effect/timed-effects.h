b8ac51096df83d7d6667b1d5c118008547a34f52
Hourier <66951241+Hourier@users.noreply.github.com>
2021-09-21 08:08:02 +0900

    [Refactor] #1578 Made a class 'PlayerCut'

--- src/timed-effect/timed-effects.h
+++ src/timed-effect/timed-effects.h
@@ -2,14 +2,17 @@
 
 #include <memory>
 
+class PlayerCut;
 class PlayerStun;
 class TimedEffects {
 public:
     TimedEffects();
     virtual ~TimedEffects() = default;
 
+    std::shared_ptr<PlayerCut> cut() const;
     std::shared_ptr<PlayerStun> stun() const;
 
 private:
+    std::shared_ptr<PlayerCut> player_cut;
     std::shared_ptr<PlayerStun> player_stun;
 };
