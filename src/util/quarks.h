776d4560f85d3687c2b84e57cd68f40df005c3d5
Habu <habu1010+github@gmail.com>
2021-09-21 17:38:38 +0900

    [Refactor] quark関連のコード
    C_MAKE を避けるため、アイテムに刻んだ銘を扱う配列変数を
    std::vector<std::string> にする。
    quark関連の変数や定数はグローバルで見える必要が無いので、cppファイルに
    移動する。

--- src/util/quarks.h
+++ src/util/quarks.h
@@ -2,17 +2,6 @@
 
 #include "system/angband.h"
 
-/*!
- * @brief 銘情報の最大数 / Maximum number of "quarks" (see "io.c")
- * @note
- * Default: assume at most 512 different inscriptions are used<br>
- * Was 512... 256 quarks added for random artifacts<br>
- */
-#define QUARK_MAX 768
-
-extern STR_OFFSET quark__num;
-extern concptr *quark__str;
-
 concptr quark_str(STR_OFFSET num);
 void quark_init(void);
 uint16_t quark_add(concptr str);
