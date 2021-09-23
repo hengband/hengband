825f72f107ba9698c65744d460d83bc2ae0df2a1
Hourier <66951241+Hourier@users.noreply.github.com>
2021-09-23 18:54:48 +0900

    [Refactor] #1621 Separated wiz_select_tval() from wiz_create_itemtype()

--- src/wizard/wizard-special-process.cpp
+++ src/wizard/wizard-special-process.cpp
@@ -90,6 +90,8 @@
 #include "wizard/wizard-spells.h"
 #include "wizard/wizard-spoiler.h"
 #include "world/world.h"
+#include <tuple>
+
 #define NUM_O_SET 8
 #define NUM_O_BIT 32
 
@@ -104,6 +106,37 @@ void wiz_cure_all(player_type *player_ptr)
     (void)set_food(player_ptr, PY_FOOD_MAX - 1);
 }
 
+static std::tuple<bool, KIND_OBJECT_IDX> wiz_select_tval()
+{
+    KIND_OBJECT_IDX list;
+    char ch;
+    for (list = 0; (list < 80) && (tvals[list].tval > TV_NONE); list++) {
+        auto row = 2 + (list % 20);
+        auto col = _(32, 24) * (list / 20);
+        ch = listsym[list];
+        prt(format("[%c] %s", ch, tvals[list].desc), row, col);
+    }
+
+    auto max_num = list;
+    KIND_OBJECT_IDX dummy = 0;
+    if (!get_com(_("アイテム種別を選んで下さい", "Get what type of object? "), &ch, false)) {
+        return std::make_tuple(false, dummy);
+    }
+
+    KIND_OBJECT_IDX selection;
+    for (selection = 0; selection < max_num; selection++) {
+        if (listsym[selection] == ch) {
+            break;
+        }
+    }
+
+    if ((selection < 0) || (selection >= max_num)) {
+        return std::make_tuple(false, dummy);
+    }
+
+    return std::make_tuple(true, selection);
+}
+
 /*!
  * @brief ベースアイテムのウィザード生成のために大項目IDと小項目IDを取得する /
  * Specify tval and sval (type and subtype of object) originally
@@ -116,32 +149,18 @@ void wiz_cure_all(player_type *player_ptr)
 static KIND_OBJECT_IDX wiz_create_itemtype()
 {
     term_clear();
-    int num;
-    char ch;
-    for (num = 0; (num < 80) && (tvals[num].tval > TV_NONE); num++) {
-        auto row = 2 + (num % 20);
-        auto col = _(32, 24) * (num / 20);
-        ch = listsym[num];
-        prt(format("[%c] %s", ch, tvals[num].desc), row, col);
+    auto [result, selection] = wiz_select_tval();
+    if (!result) {
+        return 0;
     }
 
-    int max_num = num;
-    if (!get_com(_("アイテム種別を選んで下さい", "Get what type of object? "), &ch, false))
-        return 0;
-
-    for (num = 0; num < max_num; num++)
-        if (listsym[num] == ch)
-            break;
-
-    if ((num < 0) || (num >= max_num))
-        return 0;
-
-    tval_type tval = i2enum<tval_type>(tvals[num].tval);
-    concptr tval_desc = tvals[num].desc;
+    tval_type tval = i2enum<tval_type>(tvals[selection].tval);
+    concptr tval_desc = tvals[selection].desc;
     term_clear();
-    num = 0;
-    KIND_OBJECT_IDX choice[80];
-    char buf[160];
+    auto num = 0;
+    KIND_OBJECT_IDX choice[80]{};
+    char buf[160]{};
+    char ch;
     for (const auto& k_ref : k_info) {
         if (num >= 80) {
             break;
@@ -160,16 +179,20 @@ static KIND_OBJECT_IDX wiz_create_itemtype()
         choice[num++] = k_ref.idx;
     }
 
-    max_num = num;
-    if (!get_com(format(_("%s群の具体的なアイテムを選んで下さい", "What Kind of %s? "), tval_desc), &ch, false))
+    auto max_num = num;
+    if (!get_com(format(_("%s群の具体的なアイテムを選んで下さい", "What Kind of %s? "), tval_desc), &ch, false)) {
         return 0;
+    }
 
-    for (num = 0; num < max_num; num++)
-        if (listsym[num] == ch)
+    for (num = 0; num < max_num; num++) {
+        if (listsym[num] == ch) {
             break;
+        }
+    }
 
-    if ((num < 0) || (num >= max_num))
+    if ((num < 0) || (num >= max_num)) {
         return 0;
+    }
 
     return choice[num];
 }
