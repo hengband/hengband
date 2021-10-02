941db9133481e53818133f943ccb9287845d0741
Hourier <66951241+Hourier@users.noreply.github.com>
2021-10-02 20:19:34 +0900

    [Refactor] info-reader-util.h に定義されていたusing sviewはここでしか使っておらず省略の意義も薄いのでstd::string_viewに修正した

--- src/info-reader/info-reader-util.h
+++ src/info-reader/info-reader-util.h
@@ -15,8 +15,6 @@ extern int error_line; //!< エラーが発生した行
 enum class RandomArtActType : short;
 RandomArtActType grab_one_activation_flag(concptr what);
 
-using sview = std::string_view;
-
 /*!
  * @brief infoフラグ文字列をフラグビットに変換する
  * @param flags ビットフラグ変数
@@ -25,7 +23,7 @@ using sview = std::string_view;
  * @return 見つけたらtrue
  */
 template <typename T>
-bool info_grab_one_flag(uint32_t &flags, const std::unordered_map<sview, T> &names, sview what)
+bool info_grab_one_flag(uint32_t &flags, const std::unordered_map<std::string_view, T> &names, std::string_view what)
 {
     if (auto it = names.find(what); it != names.end()) {
         set_bits(flags, it->second);
