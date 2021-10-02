b56c06491d4d2f2a91098078d0704286c9859933
Hourier <66951241+Hourier@users.noreply.github.com>
2021-10-02 20:44:32 +0900

    [Refactor] #1714 Unified alloc-entry-definition.h into alloc-entries.h

--- src/system/alloc-entries.h
+++ src/system/alloc-entries.h
@@ -1,9 +1,31 @@
-﻿#pragma once
+﻿/*
+ * @brief
+ * @author
+ * Copyright (c) 1997 Ben Harrison, James E. Wilson, Robert A. Koeneke
+ * 2002/01/12 mogami
+ * 2021/10/02 Hourier
+ */
+
+#pragma once
 
 #include "system/angband.h"
-#include "system/alloc-entry-definition.h"
-
 #include <vector>
+
+/*
+ * An entry for the object/monster allocation functions
+ *
+ * Pass 1 is determined from allocation information
+ * Pass 2 is determined from allocation restriction
+ */
+struct alloc_entry {
+    KIND_OBJECT_IDX index; /* The actual index */
+
+    DEPTH level; /* Base dungeon level */
+    PROB prob1; /* Probability, pass 1 */
+    PROB prob2; /* Probability, pass 2 */
+
+    uint16_t total; /* Unused for now */
+};
 
 extern std::vector<alloc_entry> alloc_race_table;
 
