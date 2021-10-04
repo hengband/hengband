7195d14c15b3eb29a22c51561ef80ba165270ee1
Hourier <66951241+Hourier@users.noreply.github.com>
2021-10-04 20:45:40 +0900

    [Refactor] #1744 重複しているenum列挙子を削除した

--- src/save/item-writer.cpp
+++ src/save/item-writer.cpp
@@ -11,85 +11,85 @@
 static void write_item_flags(object_type *o_ptr, BIT_FLAGS *flags)
 {
     if (o_ptr->pval)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_PVAL);
+        set_bits(*flags, SaveDataItemFlagType::PVAL);
 
     if (o_ptr->discount)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_DISCOUNT);
+        set_bits(*flags, SaveDataItemFlagType::DISCOUNT);
 
     if (o_ptr->number != 1)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_NUMBER);
+        set_bits(*flags, SaveDataItemFlagType::NUMBER);
 
     if (o_ptr->name1)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_NAME1);
+        set_bits(*flags, SaveDataItemFlagType::NAME1);
 
     if (o_ptr->name2)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_NAME2);
+        set_bits(*flags, SaveDataItemFlagType::NAME2);
 
     if (o_ptr->timeout)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_TIMEOUT);
+        set_bits(*flags, SaveDataItemFlagType::TIMEOUT);
 
     if (o_ptr->to_h)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_TO_H);
+        set_bits(*flags, SaveDataItemFlagType::TO_H);
 
     if (o_ptr->to_d)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_TO_D);
+        set_bits(*flags, SaveDataItemFlagType::TO_D);
 
     if (o_ptr->to_a)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_TO_A);
+        set_bits(*flags, SaveDataItemFlagType::TO_A);
 
     if (o_ptr->ac)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_AC);
+        set_bits(*flags, SaveDataItemFlagType::AC);
 
     if (o_ptr->dd)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_DD);
+        set_bits(*flags, SaveDataItemFlagType::DD);
 
     if (o_ptr->ds)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_DS);
+        set_bits(*flags, SaveDataItemFlagType::DS);
 
     if (o_ptr->ident)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_IDENT);
+        set_bits(*flags, SaveDataItemFlagType::IDENT);
 
     if (o_ptr->marked)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_MARKED);
+        set_bits(*flags, SaveDataItemFlagType::MARKED);
 
     if (o_ptr->art_flags.any())
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_ART_FLAGS);
+        set_bits(*flags, SaveDataItemFlagType::ART_FLAGS);
 
     if (o_ptr->curse_flags.any())
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_CURSE_FLAGS);
+        set_bits(*flags, SaveDataItemFlagType::CURSE_FLAGS);
 
     if (o_ptr->held_m_idx)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_HELD_M_IDX);
+        set_bits(*flags, SaveDataItemFlagType::HELD_M_IDX);
 
     if (o_ptr->xtra1)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_XTRA1);
+        set_bits(*flags, SaveDataItemFlagType::XTRA1);
 
     if (o_ptr->activation_id > RandomArtActType::NONE)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_ACTIVATION_ID);
+        set_bits(*flags, SaveDataItemFlagType::ACTIVATION_ID);
 
     if (o_ptr->xtra3)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_XTRA3);
+        set_bits(*flags, SaveDataItemFlagType::XTRA3);
 
     if (o_ptr->xtra4)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_XTRA4);
+        set_bits(*flags, SaveDataItemFlagType::XTRA4);
 
     if (o_ptr->xtra5)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_XTRA5);
+        set_bits(*flags, SaveDataItemFlagType::XTRA5);
 
     if (o_ptr->feeling)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_FEELING);
+        set_bits(*flags, SaveDataItemFlagType::FEELING);
 
     if (o_ptr->inscription)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_INSCRIPTION);
+        set_bits(*flags, SaveDataItemFlagType::INSCRIPTION);
 
     if (o_ptr->art_name)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_ART_NAME);
+        set_bits(*flags, SaveDataItemFlagType::ART_NAME);
 
     if (o_ptr->stack_idx)
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_STACK_IDX);
+        set_bits(*flags, SaveDataItemFlagType::STACK_IDX);
 
     if (o_ptr->is_smith()) {
-        set_bits(*flags, SaveDataItemFlagType::SAVE_ITEM_SMITH);
+        set_bits(*flags, SaveDataItemFlagType::SMITH);
     }
 
     wr_u32b(*flags);
@@ -98,70 +98,70 @@ static void write_item_flags(object_type *o_ptr, BIT_FLAGS *flags)
 static void write_item_info(object_type *o_ptr, const BIT_FLAGS flags)
 {
     wr_s16b((int16_t)o_ptr->weight);
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_NAME1))
+    if (any_bits(flags, SaveDataItemFlagType::NAME1))
         wr_s16b(o_ptr->name1);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_NAME2))
+    if (any_bits(flags, SaveDataItemFlagType::NAME2))
         wr_byte((byte)o_ptr->name2);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_TIMEOUT))
+    if (any_bits(flags, SaveDataItemFlagType::TIMEOUT))
         wr_s16b(o_ptr->timeout);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_TO_H))
+    if (any_bits(flags, SaveDataItemFlagType::TO_H))
         wr_s16b(o_ptr->to_h);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_TO_D))
+    if (any_bits(flags, SaveDataItemFlagType::TO_D))
         wr_s16b((int16_t)o_ptr->to_d);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_TO_A))
+    if (any_bits(flags, SaveDataItemFlagType::TO_A))
         wr_s16b(o_ptr->to_a);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_AC))
+    if (any_bits(flags, SaveDataItemFlagType::AC))
         wr_s16b(o_ptr->ac);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_DD))
+    if (any_bits(flags, SaveDataItemFlagType::DD))
         wr_byte((byte)o_ptr->dd);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_DS))
+    if (any_bits(flags, SaveDataItemFlagType::DS))
         wr_byte((byte)o_ptr->ds);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_IDENT))
+    if (any_bits(flags, SaveDataItemFlagType::IDENT))
         wr_byte(o_ptr->ident);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_MARKED))
+    if (any_bits(flags, SaveDataItemFlagType::MARKED))
         wr_byte(o_ptr->marked);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_ART_FLAGS))
+    if (any_bits(flags, SaveDataItemFlagType::ART_FLAGS))
         wr_FlagGroup(o_ptr->art_flags, wr_byte);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_CURSE_FLAGS))
+    if (any_bits(flags, SaveDataItemFlagType::CURSE_FLAGS))
         wr_FlagGroup(o_ptr->curse_flags, wr_byte);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_HELD_M_IDX))
+    if (any_bits(flags, SaveDataItemFlagType::HELD_M_IDX))
         wr_s16b(o_ptr->held_m_idx);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_XTRA1))
+    if (any_bits(flags, SaveDataItemFlagType::XTRA1))
         wr_byte(o_ptr->xtra1);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_ACTIVATION_ID))
+    if (any_bits(flags, SaveDataItemFlagType::ACTIVATION_ID))
         wr_s16b(enum2i(o_ptr->activation_id));
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_XTRA3))
+    if (any_bits(flags, SaveDataItemFlagType::XTRA3))
         wr_byte(o_ptr->xtra3);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_XTRA4))
+    if (any_bits(flags, SaveDataItemFlagType::XTRA4))
         wr_s16b(o_ptr->xtra4);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_XTRA5))
+    if (any_bits(flags, SaveDataItemFlagType::XTRA5))
         wr_s16b(o_ptr->xtra5);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_FEELING))
+    if (any_bits(flags, SaveDataItemFlagType::FEELING))
         wr_byte(o_ptr->feeling);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_STACK_IDX))
+    if (any_bits(flags, SaveDataItemFlagType::STACK_IDX))
         wr_s16b(o_ptr->stack_idx);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_SMITH)) {
+    if (any_bits(flags, SaveDataItemFlagType::SMITH)) {
         if (o_ptr->smith_effect.has_value()){
             wr_s16b(enum2i(o_ptr->smith_effect.value()));
         } else {
@@ -188,20 +188,20 @@ void wr_item(object_type *o_ptr)
     wr_s16b(o_ptr->k_idx);
     wr_byte((byte)o_ptr->iy);
     wr_byte((byte)o_ptr->ix);
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_PVAL))
+    if (any_bits(flags, SaveDataItemFlagType::PVAL))
         wr_s16b(o_ptr->pval);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_DISCOUNT))
+    if (any_bits(flags, SaveDataItemFlagType::DISCOUNT))
         wr_byte(o_ptr->discount);
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_NUMBER))
+    if (any_bits(flags, SaveDataItemFlagType::NUMBER))
         wr_byte((byte)o_ptr->number);
 
     write_item_info(o_ptr, flags);
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_INSCRIPTION))
+    if (any_bits(flags, SaveDataItemFlagType::INSCRIPTION))
         wr_string(quark_str(o_ptr->inscription));
 
-    if (any_bits(flags, SaveDataItemFlagType::SAVE_ITEM_ART_NAME))
+    if (any_bits(flags, SaveDataItemFlagType::ART_NAME))
         wr_string(quark_str(o_ptr->art_name));
 }
 
