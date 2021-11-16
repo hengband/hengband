5798fddb839b45cccc010431918e60c115226464
Deskull <61610939+sikabane-works@users.noreply.github.com>
2021-11-16 20:41:38 +0900

    [Fix] ＠さんのタイトル表示が勝利者になる条件に「レベル50を超える」という意味不明なor条件が混じっていたので削除。

--- src/window/main-window-left-frame.cpp
+++ src/window/main-window-left-frame.cpp
@@ -26,7 +26,7 @@ void print_title(PlayerType *player_ptr)
 {
     GAME_TEXT str[14];
     concptr p = "";
-    if (w_ptr->total_winner || (player_ptr->lev > PY_MAX_LEVEL)) {
+    if (w_ptr->total_winner) {
         if (player_ptr->arena_number > MAX_ARENA_MONS + 2) {
             p = _("*真・勝利者*", "*TRUEWINNER*");
         } else {
