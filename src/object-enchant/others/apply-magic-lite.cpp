1625ee881a4bfcbec06edc62f3249aba539b5b0e
Hourier <66951241+Hourier@users.noreply.github.com>
2022-02-12 12:20:36 +0900

    [Refactor] #2118 Moved processes from apply_magic() to give_ego_index() and give_cursed()

--- src/object-enchant/others/apply-magic-lite.cpp
+++ src/object-enchant/others/apply-magic-lite.cpp
@@ -29,38 +29,19 @@ void LiteEnchanter::apply_magic()
 {
     if (this->power > 2) {
         become_random_artifact(this->player_ptr, this->o_ptr, false);
-    } else if ((this->power == 2) || ((this->power == 1) && one_in_(3))) {
-        while (!this->o_ptr->name2) {
-            while (true) {
-                bool okay_flag = true;
-
-                this->o_ptr->name2 = get_random_ego(INVEN_LITE, true);
-
-                switch (this->o_ptr->name2) {
-                case EGO_LITE_LONG:
-                    if (this->o_ptr->sval == SV_LITE_FEANOR)
-                        okay_flag = false;
-                }
-
-                if (okay_flag)
-                    break;
-            }
+        return;
+    }
+    
+    if ((this->power == 2) || ((this->power == 1) && one_in_(3))) {
+        while (this->o_ptr->name2 == 0) {
+            this->give_ego_index();
         }
-    } else if (this->power == -2) {
-        this->o_ptr->name2 = get_random_ego(INVEN_LITE, false);
-        switch (this->o_ptr->name2) {
-        case EGO_LITE_DARKNESS:
-            this->o_ptr->xtra4 = 0;
 
-            if (this->o_ptr->sval == SV_LITE_TORCH) {
-                this->o_ptr->art_flags.set(TR_LITE_M1);
-            } else if (this->o_ptr->sval == SV_LITE_LANTERN) {
-                this->o_ptr->art_flags.set(TR_LITE_M2);
-            } else if (this->o_ptr->sval == SV_LITE_FEANOR) {
-                this->o_ptr->art_flags.set(TR_LITE_M3);
-            }
-            break;
-        }
+        return;
+    }
+    
+    if (this->power == -2) {
+        this->give_cursed();
     }
 }
 
@@ -70,6 +51,24 @@ void LiteEnchanter::enchant()
 
 void LiteEnchanter::give_ego_index()
 {
+    while (true) {
+        auto okay_flag = true;
+        this->o_ptr->name2 = get_random_ego(INVEN_LITE, true);
+        switch (this->o_ptr->name2) {
+        case EGO_LITE_LONG:
+            if (this->o_ptr->sval == SV_LITE_FEANOR) {
+                okay_flag = false;
+            }
+
+            break;
+        default:
+            break;
+        }
+
+        if (okay_flag) {
+            return;
+        }
+    }
 }
 
 void LiteEnchanter::give_high_ego_index()
@@ -78,4 +77,24 @@ void LiteEnchanter::give_high_ego_index()
 
 void LiteEnchanter::give_cursed()
 {
+    this->o_ptr->name2 = get_random_ego(INVEN_LITE, false);
+    switch (this->o_ptr->name2) {
+    case EGO_LITE_DARKNESS:
+        this->o_ptr->xtra4 = 0;
+        switch (this->o_ptr->sval) {
+            case SV_LITE_TORCH:
+                this->o_ptr->art_flags.set(TR_LITE_M1);
+                return;
+            case SV_LITE_LANTERN:
+                this->o_ptr->art_flags.set(TR_LITE_M2);
+                return;
+            case SV_LITE_FEANOR:
+                this->o_ptr->art_flags.set(TR_LITE_M3);
+                return;
+            default:
+                return;
+        }
+
+        return;
+    }
 }
