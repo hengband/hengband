8fbe780850b49bb852b2256e10f3f936bf9947dd
Deskull <61610939+sikabane-works@users.noreply.github.com>
2022-02-15 22:28:03 +0900

    [Fix] wiz_kill_enemy()の数値入力をget_value()で実装しなおし。

--- src/wizard/wizard-spells.cpp
+++ src/wizard/wizard-spells.cpp
@@ -231,36 +231,27 @@ void wiz_summon_pet(PlayerType *player_ptr, MONRACE_IDX r_idx)
 void wiz_kill_enemy(PlayerType *player_ptr, HIT_POINT dam, AttributeType effect_idx)
 {
     if (dam <= 0) {
-        char tmp[80] = "";
-        sprintf(tmp, "Damage (1-999999): ");
-        char tmp_val[10] = "1000";
-        if (!get_string(tmp, tmp_val, 6))
+        dam = 1000000;
+        if (!get_value("Damage", 1, 1000000, &dam)) {
             return;
-
-        dam = (HIT_POINT)atoi(tmp_val);
+        }
     }
-    int max = (int)AttributeType::MAX;
-    int idx = (int)effect_idx;
+
+    constexpr auto max = enum2i(AttributeType::MAX);
+    auto idx = enum2i(effect_idx);
 
     if (idx <= 0) {
-        char tmp[80] = "";
-        sprintf(tmp, "Effect ID (1-%d): ", max - 1);
-        char tmp_val[10] = "";
-        if (!get_string(tmp, tmp_val, 3))
+        if (!get_value("EffectID", 1, max - 1, &idx)) {
             return;
-
-        effect_idx = (AttributeType)atoi(tmp_val);
+        }
     }
 
-    if (idx <= 0 || idx >= max) {
-        msg_format(_("番号は1から%dの間で指定して下さい。", "ID must be between 1 to %d."), max - 1);
-        return;
-    }
-
+    effect_idx = i2enum<AttributeType>(idx);
     DIRECTION dir;
 
-    if (!get_aim_dir(player_ptr, &dir))
-        return;
+    if (!get_aim_dir(player_ptr, &dir)) {
+        return;    
+    }
 
     fire_ball(player_ptr, effect_idx, dir, dam, 0);
 }
