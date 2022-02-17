7331803b207ad1b145d00b9bb05bdbb335ac1180
Hourier <66951241+Hourier@users.noreply.github.com>
2022-02-17 19:12:50 +0900

    [Refactor] #2120 cap_* の変数型を実態に合わせた

--- src/specific-object/monster-ball.cpp
+++ src/specific-object/monster-ball.cpp
@@ -75,10 +75,10 @@ static bool set_activation_target(PlayerType *player_ptr, ae_type *ae_ptr)
     if (!fire_ball(player_ptr, AttributeType::CAPTURE, ae_ptr->dir, 0, 0))
         return true;
 
-    ae_ptr->o_ptr->pval = (PARAMETER_VALUE)cap_mon;
-    ae_ptr->o_ptr->captured_monster_speed = static_cast<int8_t>(cap_mspeed);
-    ae_ptr->o_ptr->captured_monster_current_hp = static_cast<short>(cap_hp);
-    ae_ptr->o_ptr->captured_monster_max_hp = static_cast<short>(cap_maxhp);
+    ae_ptr->o_ptr->pval = cap_mon;
+    ae_ptr->o_ptr->captured_monster_speed = cap_mspeed;
+    ae_ptr->o_ptr->captured_monster_current_hp = cap_hp;
+    ae_ptr->o_ptr->captured_monster_max_hp = cap_maxhp;
     inscribe_nickname(ae_ptr);
     return true;
 }
