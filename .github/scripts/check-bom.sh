69ea83929a32d3e087b99bb81c7d5fb8ef6c332b
Habu <habu1010+github@gmail.com>
2022-02-20 11:21:59 +0900

    [Feature] ビルドテストの Workflow に BOM チェックを追加
    新しく追加したソースファイルへの BOM 付け忘れや修正したソースファイルからの BOM 消失
    防止のため、GitHub Actions のビルドテストでビルド実行前にすべてのソースフィアルに
    BOM が付いているかどうかのチェックを行うようにする。

---
+++ .github/scripts/check-bom.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SRC_FILES=$(find src/ -name \*.cpp -or -name \*.h)
+
+STATUS=0
+
+for file in $SRC_FILES; do
+    file $file | grep "BOM" >/dev/null
+    if [ $? != 0 ]; then
+        echo "$file: BOM does not exists."
+        STATUS=1
+    fi
+done
+
+exit $STATUS
