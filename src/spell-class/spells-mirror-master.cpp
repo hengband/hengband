2458e098ed1afd1eb1492c8db67e29d15eddbb99
Hourier <66951241+Hourier@users.noreply.github.com>
2022-03-02 21:32:05 +0900

    [Refactor] #2355 spells-mirror-master.cpp/h を作成した

---
+++ src/spell-class/spells-mirror-master.cpp
@@ -0,0 +1,7 @@
+﻿#include "spell-class/spells-mirror-master.h"
+#include "system/player-type-definition.h"
+
+SpellsMirrorMaster::SpellsMirrorMaster(PlayerType *player_ptr)
+    : player_ptr(player_ptr)
+{
+}
---
+++ src/spell-class/spells-mirror-master.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+class PlayerType;
+class SpellsMirrorMaster {
+public:
+    SpellsMirrorMaster(PlayerType *player_ptr);
+
+private:
+    PlayerType *player_ptr;
+};
