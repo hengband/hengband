4ca06da6526d96fa3c768aa13f4e46b9a7c8c652
Hourier <66951241+Hourier@users.noreply.github.com>
2022-03-09 21:06:43 +0900

    [Refactor] #1642 Defined QuaffEffects class

---
+++ src/object-use/quaff/quaff-effects.cpp
@@ -0,0 +1,9 @@
+﻿#include "object-use/quaff/quaff-effects.h"
+#include "object/object-info.h"
+#include "system/object-type-definition.h"
+#include "system/player-type-definition.h"
+
+QuaffEffects::QuaffEffects(PlayerType *player_ptr)
+    : player_ptr(player_ptr)
+{
+}
---
+++ src/object-use/quaff/quaff-effects.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+class PlayerType;
+class QuaffEffects {
+public:
+    QuaffEffects(PlayerType *player_ptr);
+
+private:
+    PlayerType *player_ptr;
+};
