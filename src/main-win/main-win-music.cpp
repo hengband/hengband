adffae8096ad3ad50417aaf21644650c43893934
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2022-03-11 01:36:54 +0900

    [Refactor] セーブデータ、misc.txt 関連以外を max_q_idx に依存しないようにした
    セーブデータは別コミットで、 misc.txt は max_q_idx の削除と共に処理する

--- src/main-win/main-win-music.cpp
+++ src/main-win/main-win-music.cpp
@@ -80,7 +80,7 @@ static concptr dungeon_key_at(int index, char *buf)
  */
 static concptr quest_key_at(int index, char *buf)
 {
-    if (index >= static_cast<int>(max_q_idx)) {
+    if (index > enum2i(quest_map.rbegin()->first)) {
         return nullptr;
     }
 
