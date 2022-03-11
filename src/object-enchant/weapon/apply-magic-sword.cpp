84d944c418e98b38ef79d109b61266f280dfcd00
Hourier <66951241+Hourier@users.noreply.github.com>
2022-03-11 21:42:20 +0900

    [Refactor] #2403 Defined SwordEnchanter class

---
+++ src/object-enchant/weapon/apply-magic-sword.cpp
@@ -0,0 +1,20 @@
+﻿/*!
+ * @brief 剣・鈍器・戦斧に耐性等の追加効果を付与する処理
+ * @date 2022/03/11
+ * @author Hourier
+ */
+
+#include "object-enchant/weapon/apply-magic-sword.h"
+
+/*!
+ * @brief 剣・鈍器・戦斧強化クラスのコンストラクタ
+ * @param player_ptr プレイヤーへの参照ポインタ
+ * @param o_ptr 強化を与えたいオブジェクトの構造体参照ポインタ
+ * @param level 生成基準階
+ * @param power 生成ランク
+ */
+SwordEnchanter::SwordEnchanter(PlayerType *player_ptr, ObjectType *o_ptr, DEPTH level, int power)
+    : AbstractWeaponEnchanter(o_ptr, level, power)
+    , player_ptr(player_ptr)
+{
+}
