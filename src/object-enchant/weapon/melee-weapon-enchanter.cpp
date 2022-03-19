9aad0d33f30452a4de7dc0dd11412897a882c7e2
Hourier <66951241+Hourier@users.noreply.github.com>
2022-03-19 09:22:14 +0900

    [Refactor] #2425 Deifined AbstractMeleeWeaponEnchanter

---
+++ src/object-enchant/weapon/melee-weapon-enchanter.cpp
@@ -0,0 +1,13 @@
+﻿/*!
+ * @brief 剣・鈍器・戦斧武器に耐性等の追加効果を付与する処理
+ * @date 2022/03/22
+ * @author Hourier
+ */
+
+#include "object-enchant/weapon/melee-weapon-enchanter.h"
+
+MeleeWeaponEnchanter::MeleeWeaponEnchanter(PlayerType *player_ptr, ObjectType *o_ptr, DEPTH level, int power)
+    : AbstractWeaponEnchanter(o_ptr, level, power)
+    , player_ptr(player_ptr)
+{
+}
---
+++ src/object-enchant/weapon/melee-weapon-enchanter.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "object-enchant/weapon/abstract-weapon-enchanter.h"
+#include "system/angband.h"
+
+class ObjectType;
+class PlayerType;
+class MeleeWeaponEnchanter : AbstractWeaponEnchanter {
+public:
+    virtual ~MeleeWeaponEnchanter() = default;
+
+    void apply_magic() override{};
+
+protected:
+    MeleeWeaponEnchanter(PlayerType *player_ptr, ObjectType *o_ptr, DEPTH level, int power);
+
+    void sval_enchant() override{};
+    void give_ego_index() override{};
+    void give_high_ego_index() override{};
+    void give_cursed() override{};
+    void strengthen(){};
+
+private:
+    PlayerType *player_ptr;
+};
