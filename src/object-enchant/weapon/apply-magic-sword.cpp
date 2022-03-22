0017944738e10bbfa5a0110196be19d3ca676caa
Hourier <66951241+Hourier@users.noreply.github.com>
2022-03-22 11:31:15 +0900

    [Refactor] #2425 SwordEnchanterに残っていた不要な条件分岐を削除した

--- src/object-enchant/weapon/apply-magic-sword.cpp
+++ src/object-enchant/weapon/apply-magic-sword.cpp
@@ -1,23 +1,19 @@
 ﻿/*!
- * @brief 剣・鈍器・戦斧に耐性等の追加効果を付与する処理
- * @date 2022/03/11
+ * @brief 剣に耐性等の追加効果を付与する処理
+ * @date 2022/03/22
  * @author Hourier
- * @details give_ego_index() を中心に、剣と鈍器はまだ分ける余地はあるが、これ以上分割しても煩雑なだけになりそうなので保留.
  */
 
 #include "object-enchant/weapon/apply-magic-sword.h"
-#include "artifact/random-art-generator.h"
 #include "floor/floor-base-definitions.h"
 #include "inventory/inventory-slot-types.h"
 #include "object-enchant/object-boost.h"
-#include "object/tval-types.h"
 #include "sv-definition/sv-weapon-types.h"
 #include "system/object-type-definition.h"
-#include "system/player-type-definition.h"
 #include "view/display-messages.h"
 
 /*!
- * @brief 剣・鈍器・戦斧強化クラスのコンストラクタ
+ * @brief 剣強化クラスのコンストラクタ
  * @param player_ptr プレイヤーへの参照ポインタ
  * @param o_ptr 強化を与えたいオブジェクトの構造体参照ポインタ
  * @param level 生成基準階
@@ -32,11 +28,7 @@ void SwordEnchanter::give_ego_index()
 {
     while (true) {
         this->o_ptr->ego_idx = get_random_ego(INVEN_MAIN_HAND, true);
-        if (this->o_ptr->ego_idx == EgoType::SHARPNESS && this->o_ptr->tval != ItemKindType::SWORD) {
-            continue;
-        }
-
-        if (this->o_ptr->ego_idx == EgoType::EARTHQUAKES && this->o_ptr->tval != ItemKindType::HAFTED) {
+        if (this->o_ptr->ego_idx == EgoType::EARTHQUAKES) {
             continue;
         }
 
@@ -46,14 +38,6 @@ void SwordEnchanter::give_ego_index()
     switch (this->o_ptr->ego_idx) {
     case EgoType::SHARPNESS:
         this->o_ptr->pval = static_cast<short>(m_bonus(5, this->level) + 1);
-        break;
-    case EgoType::EARTHQUAKES:
-        if (one_in_(3) && (this->level > 60)) {
-            this->o_ptr->art_flags.set(TR_BLOWS);
-        } else {
-            this->o_ptr->pval = static_cast<short>(m_bonus(3, this->level));
-        }
-
         break;
     default:
         break;
@@ -69,12 +53,8 @@ void SwordEnchanter::give_cursed()
     auto n = 0;
     while (true) {
         this->o_ptr->ego_idx = get_random_ego(INVEN_MAIN_HAND, false);
-        if ((this->o_ptr->ego_idx == EgoType::WEIRD) && (this->o_ptr->tval != ItemKindType::SWORD)) {
-            continue;
-        }
-
         const auto *e_ptr = &e_info[this->o_ptr->ego_idx];
-        if ((this->o_ptr->tval != ItemKindType::SWORD) || (this->o_ptr->sval != SV_HAYABUSA) || (e_ptr->max_pval >= 0)) {
+        if ((this->o_ptr->sval != SV_HAYABUSA) || (e_ptr->max_pval >= 0)) {
             return;
         }
 
