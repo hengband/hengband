3d38e27c3b92e9b142769f8148b617928c0d52ec
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2022-03-24 23:31:56 +0900

    [Refactor] 敵魔法から属性を取得するための基底クラスを追加

---
+++ src/mspell/mspell-data.cpp
@@ -0,0 +1,75 @@
+﻿#include "mspell/mspell-data.h"
+#include "effect/attribute-types.h"
+#include "monster/monster-update.h"
+#include "mspell/mspell-util.h"
+#include "system/player-type-definition.h"
+
+MSpellData::MSpellData()
+    : msg()
+    , type()
+    , drs()
+    , contain(false)
+{
+}
+
+MSpellData::MSpellData(const MSpellMessageData &msg)
+    : msg(msg)
+    , type()
+    , drs()
+    , contain(true)
+{
+}
+
+MSpellData::MSpellData(const MSpellMessageData &msg, const AttributeType &type)
+    : msg(msg)
+    , type(type)
+    , drs()
+    , contain(true)
+{
+}
+
+MSpellData::MSpellData(const MSpellMessageData &msg, const AttributeType &type, const MSpellDrsData &drs)
+    : msg(msg)
+    , type(type)
+    , drs(drs)
+    , contain(true)
+{
+}
+
+MSpellMessageData::MSpellMessageData()
+    : output([](PlayerType *, MONSTER_IDX, MONSTER_IDX, int) { return false; })
+{
+}
+
+MSpellMessageData::MSpellMessageData(const mspell_cast_msg_blind &msg_string)
+    : output([msg_string](auto player_ptr, auto m_idx, auto t_idx, int target_type) {
+        return monspell_message(player_ptr, m_idx, t_idx, msg_string, target_type);
+    })
+{
+}
+
+MSpellMessageData::MSpellMessageData(const std::string_view &blind, const std::string_view &to_player, const std::string_view &to_monster)
+    : output([blind, to_player, to_monster](auto player_ptr, auto m_idx, auto t_idx, int target_type) {
+        return monspell_message(player_ptr, m_idx, t_idx, { blind.data(), to_player.data(), to_monster.data() }, target_type);
+    })
+{
+}
+
+MSpellDrsData::MSpellDrsData()
+    : execute([](PlayerType *, MONSTER_IDX) {})
+{
+}
+
+MSpellDrsData::MSpellDrsData(std::initializer_list<drs_type> drs)
+    : execute([drs](auto player_ptr, auto m_idx) {
+        for (auto &d : drs) {
+            update_smart_learn(player_ptr, m_idx, d);
+        }
+    })
+{
+}
+
+MSpellDrsData::MSpellDrsData(const drs_type &drs)
+    : execute([drs](auto player_ptr, auto m_idx) { update_smart_learn(player_ptr, m_idx, drs); })
+{
+}
---
+++ src/mspell/mspell-data.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include "mind/drs-types.h"
+#include "system/angband.h"
+#include <functional>
+#include <type_traits>
+
+enum class AttributeType;
+struct mspell_cast_msg_blind;
+class PlayerType;
+
+class MSpellMessageData {
+public:
+    MSpellMessageData();
+    template <typename FUNC>
+    MSpellMessageData(const FUNC &output)
+        : output(output)
+    {
+        static_assert(std::is_invocable_r<bool, decltype(output), PlayerType *, MONSTER_IDX, MONSTER_IDX, int>::value);
+    }
+    MSpellMessageData(const mspell_cast_msg_blind &msg_string);
+    MSpellMessageData(const std::string_view &blind, const std::string_view &to_player, const std::string_view &to_monster);
+    std::function<bool(PlayerType *, MONSTER_IDX, MONSTER_IDX, int)> output;
+};
+
+class MSpellDrsData {
+public:
+    MSpellDrsData();
+    MSpellDrsData(std::initializer_list<drs_type> drs);
+    MSpellDrsData(const drs_type &drs);
+    std::function<void(PlayerType *, MONSTER_IDX)> execute;
+};
+
+class MSpellData {
+public:
+    MSpellData();
+    MSpellData(const MSpellMessageData &msg);
+    MSpellData(const MSpellMessageData &msg, const AttributeType &type);
+    MSpellData(const MSpellMessageData &msg, const AttributeType &type, const MSpellDrsData &drs);
+    MSpellData(const MSpellData &) = default;
+    MSpellData(MSpellData &&) = default;
+    MSpellData &operator=(const MSpellData &) = default;
+    MSpellData &operator=(MSpellData &&) = default;
+    virtual ~MSpellData() = default;
+
+    MSpellMessageData msg;
+    AttributeType type;
+    MSpellDrsData drs;
+    bool contain;
+};
+
+template <typename T>
+MSpellData get_mspell_data(std::unordered_map<T, MSpellData> map, T key)
+{
+    if (auto it = map.find(key); it != map.end()) {
+        return it->second;
+    }
+    return MSpellData();
+}
