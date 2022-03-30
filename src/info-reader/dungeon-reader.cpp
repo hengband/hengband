8284ce2887e24040eeaf7aae8c1ff8639f9169bd
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2022-03-30 22:08:20 +0900

    [Refactor] トークン定義をセリフフラグの再定義に合わせる
    r_info.txt は機械的置き換えです

--- src/info-reader/dungeon-reader.cpp
+++ src/info-reader/dungeon-reader.cpp
@@ -89,6 +89,10 @@ static bool grab_one_basic_monster_flag(dungeon_type *d_ptr, std::string_view wh
         return true;
     }
 
+    if (EnumClassFlagGroup<MonsterSpeakType>::grab_one_flag(d_ptr->mon_speak_flags, r_info_speak_flags, what)) {
+        return true;
+    }
+
     msg_format(_("未知のモンスター・フラグ '%s'。", "Unknown monster flag '%s'."), what.data());
     return false;
 }
