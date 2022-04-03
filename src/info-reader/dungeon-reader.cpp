c2e060f1b9f649df6bcad8e88186e39d512cd843
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2022-04-03 21:00:31 +0900

    [Refactor] トークン定義を地形関連フラグに合わせる

--- src/info-reader/dungeon-reader.cpp
+++ src/info-reader/dungeon-reader.cpp
@@ -81,6 +81,10 @@ static bool grab_one_basic_monster_flag(dungeon_type *d_ptr, std::string_view wh
         return true;
     }
 
+    if (EnumClassFlagGroup<MonsterFeatureType>::grab_one_flag(d_ptr->mon_feature_flags, r_info_feature_flags, what)) {
+        return true;
+    }
+
     msg_format(_("未知のモンスター・フラグ '%s'。", "Unknown monster flag '%s'."), what.data());
     return false;
 }
