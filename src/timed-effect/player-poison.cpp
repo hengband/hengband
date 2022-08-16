dd17848a8b2f5705b9fe9ac4f681d4cba0c93584
Hourier <66951241+Hourier@users.noreply.github.com>
2022-08-16 18:14:39 +0900

    [Refactor] #2523 player-poison.cpp/h ファイルを定義した (まだPlayerTypeからは呼び出されない)

---
+++ src/timed-effect/player-poison.cpp
@@ -0,0 +1,32 @@
+﻿/*!
+ * @brief プレイヤーの一時減速ステータス変更と判定
+ * @date 2022/08/16
+ * @author Hourier
+ */
+
+#include "timed-effect/player-poison.h"
+#include <stdexcept>
+
+short PlayerPoison::current() const
+{
+    return this->poison;
+}
+
+bool PlayerPoison::is_poisoned() const
+{
+    return this->poison > 0;
+}
+
+void PlayerPoison::set(short value)
+{
+    if (value < 0) {
+        throw std::invalid_argument("Negative value can't be set in the player's poison parameter!");
+    }
+
+    this->poison = value;
+}
+
+void PlayerPoison::reset()
+{
+    this->poison = 0;
+}
