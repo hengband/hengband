4ce78aaaf88b2efafc912fe6d495e50cc0f5e205
Hourier <66951241+Hourier@users.noreply.github.com>
2022-08-30 19:37:08 +0900

    [Refactor] #2522 player-blindness.cpp/h ファイルを定義した (まだPlayerTypeからは呼び出されない)

---
+++ src/timed-effect/player-blindness.cpp
@@ -0,0 +1,32 @@
+﻿/*!
+ * @brief プレイヤーの盲目ステータス変更と判定
+ * @date 2022/08/30
+ * @author Hourier
+ */
+
+#include "timed-effect/player-blindness.h"
+#include <stdexcept>
+
+short PlayerBlindness::current() const
+{
+    return this->blindness;
+}
+
+bool PlayerBlindness::is_blind() const
+{
+    return this->blindness > 0;
+}
+
+void PlayerBlindness::set(short value)
+{
+    if (value < 0) {
+        throw std::invalid_argument("Negative value can't be set in the player's blindness parameter!");
+    }
+
+    this->blindness = value;
+}
+
+void PlayerBlindness::reset()
+{
+    this->set(0);
+}
