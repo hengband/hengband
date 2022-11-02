8dc7490ce44f771bb9943445e65eb0c128ffa001
Hourier <66951241+Hourier@users.noreply.github.com>
2022-11-02 23:08:11 +0900

    [Refactor] #2680 その他雑多な、真にgamevalue.h にいるべき定数群をconstexpr に置き換えた

--- src/system/gamevalue.h
+++ src/system/gamevalue.h
@@ -1,20 +1,20 @@
 ﻿#pragma once
 
-#define PENETRATE_INVULNERABILITY 13 /*!< 無敵化が破られる確率(1/x) / 1/x chance of hurting even if invulnerable! */
-#define FUEL_TORCH 5000 /*!< 松明の基本寿命値 / Maximum amount of fuel in a torch */
-#define FUEL_LAMP 15000 /*!< ランタンの基本寿命値 / Maximum amount of fuel in a lantern */
-#define TOWN_DAWN 10000 /*!< 1日分のターン / Number of ticks from dawn to dawn XXX */
-#define TURNS_PER_TICK 10L /*!< 時間経過処理を行うターン数の刻み / Number of energy-gain-turns per ticks */
-#define MAX_DAYS 20000 /*!< 内部処理中で保持される最大日数 / Maximum days */
-#define BTH_PLUS_ADJ 3 /*!< 武器経験値及びプレイヤーの打撃/射撃能力に応じた修正値倍率 / Adjust BTH per plus-to-hit */
-#define MON_DRAIN_LIFE 2 /*!< モンスターの打撃によるプレイヤーの経験値吸収基本倍率(%) / Percent of player exp drained per hit */
-#define USE_DEVICE 3 /*!< 魔道具の最低失敗基準値 x> Harder devices x< Easier devices     */
-
-/*!< ベースアイテム生成階層が加算される確率 */
-constexpr auto CHANCE_BASEITEM_LEVEL_BOOST = 10;
-
+constexpr auto PENETRATE_INVULNERABILITY = 13; /*!< 無敵化が破られる確率(1/n) */
+constexpr auto FUEL_TORCH = 5000; /*!< 松明の基本寿命値 */
+constexpr auto FUEL_LAMP = 15000; /*!< ランタンの基本寿命値 */
+constexpr auto TOWN_DAWN = 10000; /*!< 1日分のターン */
+constexpr auto TURNS_PER_TICK = 10L; /*!< 時間経過処理を行うターン数の刻み */
+constexpr auto MAX_DAYS = 20000; /*!< 内部処理中で保持される最大日数 */
+constexpr auto BTH_PLUS_ADJ = 3; /*!< 武器経験値及びプレイヤーの打撃/射撃能力に応じた修正値倍率 */
+constexpr auto MON_DRAIN_LIFE = 2; /*!< モンスターの打撃によるプレイヤーの経験値吸収基本倍率(%) */
+constexpr auto USE_DEVICE = 3; /*!< 魔道具の最低失敗基準値 */
+constexpr auto CHANCE_BASEITEM_LEVEL_BOOST = 10; /*!< ベースアイテム生成階層が加算される確率 */
 constexpr auto MAX_PLAYER_SIGHT = 20; /*!< プレイヤーの最大視界グリッド数 */
 constexpr auto MAX_MONSTER_SENSING = 100; /*!< モンスターの最大感知グリッド数 */
+constexpr auto CHANCE_ABILITY_SCORE_DECREASE = 16; /*!< 属性攻撃を受けた際に能力値低下を起こす確率(1/n) */
+constexpr auto CHANCE_STRENGTHENING = 12; /*!< ランダムアーティファクトにバイアス外の耐性がつき、4を超えるpvalを許可する確率 */
+constexpr auto STANDARD_SPEED = 110; /* プレイヤー/モンスターの標準速度 (加速0) */
 
 /*!
  * @brief 1フロアに存在可能な、増殖フラグ付きモンスター実体の最大数
@@ -22,15 +22,6 @@ constexpr auto MAX_MONSTER_SENSING = 100; /*!< モンスターの最大感知グ
  * フロア構造体の「増殖フラグ付きモンスター実体の現在数」が強制的に最大値まで引き上げられる.
  */
 constexpr auto MAX_REPRODUCTION = 100;
-
-/*!< 属性攻撃を受けた際に能力値低下を起こす確率(1/n) */
-constexpr auto CHANCE_ABILITY_SCORE_DECREASE = 16;
-
-/*!< ランダムアーティファクトにバイアス外の耐性がつき、4を超えるpvalを許可する確率 */
-constexpr auto CHANCE_STRENGTHENING = 12;
-
-/* プレイヤー/モンスターの標準速度 (加速0) */
-constexpr auto STANDARD_SPEED = 110;
 
 constexpr auto QUEST_DEFINITION_LIST = "QuestDefinitionList.txt";
 constexpr auto TOWN_DEFINITION_LIST = "TownDefinitionList.txt";
