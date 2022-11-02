bf68f0d1f1b922231a175dc385896f9f8ab8fb39
Hourier <66951241+Hourier@users.noreply.github.com>
2022-11-02 23:00:03 +0900

    [Refactor] #2680 MON_MULT_ADJ が1箇所からしか参照されていないのでmonster-processor.cpp に移した

--- src/monster/monster-processor.cpp
+++ src/monster/monster-processor.cpp
@@ -446,7 +446,8 @@ bool decide_monster_multiplication(PlayerType *player_ptr, MONSTER_IDX m_idx, PO
         k = 8;
     }
 
-    if ((k < 4) && (!k || !randint0(k * MON_MULT_ADJ))) {
+    constexpr auto chance_reproduction = 8;
+    if ((k < 4) && (!k || !randint0(k * chance_reproduction))) {
         if (multiply_monster(player_ptr, m_idx, false, (m_ptr->is_pet() ? PM_FORCE_PET : 0))) {
             if (player_ptr->current_floor_ptr->m_list[hack_m_idx_ii].ml && is_original_ap_and_seen(player_ptr, m_ptr)) {
                 r_ptr->r_flags2 |= RF2_MULTIPLY;
