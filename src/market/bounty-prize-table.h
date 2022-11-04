9ca0828dcfa604150296ac08b8b3a0d2ac0e54e1
Hourier <66951241+Hourier@users.noreply.github.com>
2022-11-04 21:38:33 +0900

    [Refactor] #2745 lookup_kind() の呼び出しを、(tval, sval)から(BaseitemKey)に全て差し替えた (その過程で見つけたtval/svalペアの内、影響範囲の小さいものはBaseitemKeyに置換した)

--- src/market/bounty-prize-table.h
+++ src/market/bounty-prize-table.h
@@ -1,15 +1,12 @@
-﻿#pragma once
-
-#include "system/angband.h"
-#include "world/world.h"
-
-/*!
- * 賞金首の報酬テーブル / List of prize object
+﻿/*!
+ * @brief 賞金首の報酬テーブル
+ * @author Hourier
+ * @date 2022/11/04
  */
-enum class ItemKindType : short;
-struct bounty_prize_type {
-    ItemKindType tval; /*!< ベースアイテムのメイン種別ID */
-    OBJECT_SUBTYPE_VALUE sval; /*!< ベースアイテムのサブ種別ID */
-};
 
-extern bounty_prize_type prize_list[MAX_BOUNTY];
+#pragma once
+
+#include <vector>
+
+class BaseitemKey;
+extern const std::vector<BaseitemKey> prize_list;
