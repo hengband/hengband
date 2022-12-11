50e0d6a1fa344d8abf101fb5f6e483ede4e74314
Habu <habu1010+github@gmail.com>
2022-12-11 14:59:14 +0900

    [Refactor] describe_flavor からしか呼ばれていない関数を同ファイルに移動
    describe_flavor からしか呼ばれていない以下の関数を describe_flavor と同ファイルに
    移動し、これらの関数しか定義されていない display-sub-window-items.cpp/h を削除する。
    
    - display_short_flavors
    - display_item_discount
    - display_item_fake_inscription
    
    また、関数名として display ではなく descirbe が適切なので変更する。

--- src/flavor/flavor-describer.cpp
+++ src/flavor/flavor-describer.cpp
@@ -35,8 +35,8 @@
 #include "system/item-entity.h"
 #include "system/player-type-definition.h"
 #include "util/bit-flags-calculator.h"
+#include "util/quarks.h"
 #include "util/string-processor.h"
-#include "window/display-sub-window-items.h"
 #include <sstream>
 
 static void check_object_known_aware(flavor_type *flavor_ptr)
@@ -499,6 +499,69 @@ static void decide_item_feeling(flavor_type *flavor_ptr)
     }
 }
 
+static void describe_short_flavors(flavor_type *flavor_ptr)
+{
+    flavor_ptr->tmp_val2[0] = '\0';
+    if ((abbrev_extra || abbrev_all) && flavor_ptr->o_ptr->is_fully_known()) {
+        if (!flavor_ptr->o_ptr->inscription || !angband_strchr(quark_str(flavor_ptr->o_ptr->inscription), '%')) {
+            bool kanji = _(true, false);
+            get_ability_abbreviation(flavor_ptr->tmp_val2, flavor_ptr->o_ptr, kanji, abbrev_all);
+        }
+    }
+
+    if (flavor_ptr->o_ptr->inscription == 0) {
+        return;
+    }
+
+    char buff[1024] = "";
+    if (flavor_ptr->tmp_val2[0]) {
+        strcat(flavor_ptr->tmp_val2, ", ");
+    }
+
+    get_inscription(buff, flavor_ptr->o_ptr);
+    angband_strcat(flavor_ptr->tmp_val2, buff, sizeof(flavor_ptr->tmp_val2));
+}
+
+static void describe_item_discount(flavor_type *flavor_ptr)
+{
+    if ((flavor_ptr->o_ptr->discount == 0) || (flavor_ptr->tmp_val2[0] && ((flavor_ptr->o_ptr->ident & IDENT_STORE) == 0))) {
+        return;
+    }
+
+    char discount_num_buf[4];
+    if (flavor_ptr->fake_insc_buf[0]) {
+        strcat(flavor_ptr->fake_insc_buf, ", ");
+    }
+
+    (void)object_desc_num(discount_num_buf, flavor_ptr->o_ptr->discount);
+    strcat(flavor_ptr->fake_insc_buf, discount_num_buf);
+    strcat(flavor_ptr->fake_insc_buf, _("%引き", "% off"));
+}
+
+static void describe_item_fake_inscription(flavor_type *flavor_ptr)
+{
+    if ((flavor_ptr->fake_insc_buf[0] == '\0') && (flavor_ptr->tmp_val2[0] == '\0')) {
+        return;
+    }
+
+    flavor_ptr->t = object_desc_chr(flavor_ptr->t, ' ');
+    flavor_ptr->t = object_desc_chr(flavor_ptr->t, flavor_ptr->c1);
+    if (flavor_ptr->fake_insc_buf[0]) {
+        flavor_ptr->t = object_desc_str(flavor_ptr->t, flavor_ptr->fake_insc_buf);
+    }
+
+    if ((flavor_ptr->fake_insc_buf[0] != '\0') && (flavor_ptr->tmp_val2[0] != '\0')) {
+        flavor_ptr->t = object_desc_chr(flavor_ptr->t, ',');
+        flavor_ptr->t = object_desc_chr(flavor_ptr->t, ' ');
+    }
+
+    if (flavor_ptr->tmp_val2[0]) {
+        flavor_ptr->t = object_desc_str(flavor_ptr->t, flavor_ptr->tmp_val2);
+    }
+
+    flavor_ptr->t = object_desc_chr(flavor_ptr->t, flavor_ptr->c2);
+}
+
 static describe_option_type decide_describe_option(const ItemEntity &item, BIT_FLAGS mode)
 {
     describe_option_type opt{};
@@ -586,9 +649,9 @@ void describe_flavor(PlayerType *player_ptr, char *buf, ItemEntity *o_ptr, BIT_F
     angband_strcpy(flavor_ptr->tmp_val, desc_ss.str().data(), sizeof(flavor_ptr->tmp_val));
     flavor_ptr->t = flavor_ptr->tmp_val + strlen(flavor_ptr->tmp_val);
 
-    display_short_flavors(flavor_ptr);
+    describe_short_flavors(flavor_ptr);
     decide_item_feeling(flavor_ptr);
-    display_item_discount(flavor_ptr);
-    display_item_fake_inscription(flavor_ptr);
+    describe_item_discount(flavor_ptr);
+    describe_item_fake_inscription(flavor_ptr);
     angband_strcpy(flavor_ptr->buf, flavor_ptr->tmp_val, MAX_NLEN);
 }
