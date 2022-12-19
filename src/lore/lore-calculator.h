ee6c15f1acd1bd7e7d2309993e212f90e80cc1ce
Eric Branlund <ebranlund@fastmail.com>
2022-12-18 22:45:14 -0700

    Refactor dice_to_string() to avoid sprintf().  Does part of the work of resolving https://github.com/hengband/hengband/issues/2858 and https://github.com/hengband/hengband/issues/2859 .

--- src/lore/lore-calculator.h
+++ src/lore/lore-calculator.h
@@ -1,13 +1,14 @@
 ﻿#pragma once
 
 #include "system/angband.h"
+#include <string>
 
 enum class MonsterAbilityType;
 enum class MonsterRaceId : int16_t;
 
 struct lore_type;
 class PlayerType;
-void dice_to_string(int base_damage, int dice_num, int dice_side, int dice_mult, int dice_div, char *msg);
+std::string dice_to_string(int base_damage, int dice_num, int dice_side, int dice_mult, int dice_div);
 bool know_armour(MonsterRaceId r_idx, const bool know_everything);
 bool know_damage(MonsterRaceId r_idx, int i);
 void set_damage(PlayerType *player_ptr, lore_type *lore_ptr, MonsterAbilityType ms_type, concptr msg);
