c368ca7f6c91f96d81752b728decc416ca379144
Hourier <66951241+Hourier@users.noreply.github.com>
2023-04-28 22:56:42 +0900

    [Refactor] #2886 redrawing-flags-updater.cpp/h を定義した

---
+++ src/system/redrawing-flags-updater.cpp
@@ -0,0 +1,121 @@
+﻿#include "system/redrawing-flags-updater.h"
+#include "util/enum-range.h"
+
+RedrawingFlagsUpdater RedrawingFlagsUpdater::instance{};
+
+RedrawingFlagsUpdater &RedrawingFlagsUpdater::get_instance()
+{
+    return instance;
+}
+
+bool RedrawingFlagsUpdater::any_main() const
+{
+    return this->main_window_flags.any();
+}
+
+bool RedrawingFlagsUpdater::any_sub() const
+{
+    return this->sub_window_flags.any();
+}
+
+bool RedrawingFlagsUpdater::any_stats() const
+{
+    return this->status_flags.any();
+}
+
+bool RedrawingFlagsUpdater::has(MainWindowRedrawingFlag flag) const
+{
+    return this->main_window_flags.has(flag);
+}
+
+bool RedrawingFlagsUpdater::has(SubWindowRedrawingFlag flag) const
+{
+    return this->sub_window_flags.has(flag);
+}
+
+bool RedrawingFlagsUpdater::has(StatusRedrawingFlag flag) const
+{
+    return this->status_flags.has(flag);
+}
+
+bool RedrawingFlagsUpdater::has_any_of(const EnumClassFlagGroup<MainWindowRedrawingFlag> &flags) const
+{
+    return this->main_window_flags.has_any_of(flags);
+}
+
+bool RedrawingFlagsUpdater::has_any_of(const EnumClassFlagGroup<SubWindowRedrawingFlag> &flags) const
+{
+    return this->sub_window_flags.has_any_of(flags);
+}
+
+bool RedrawingFlagsUpdater::has_any_of(const EnumClassFlagGroup<StatusRedrawingFlag> &flags) const
+{
+    return this->status_flags.has_any_of(flags);
+}
+
+void RedrawingFlagsUpdater::set_flag(MainWindowRedrawingFlag flag)
+{
+    this->main_window_flags.set(flag);
+}
+
+void RedrawingFlagsUpdater::set_flag(SubWindowRedrawingFlag flag)
+{
+    this->sub_window_flags.set(flag);
+}
+
+void RedrawingFlagsUpdater::set_flag(StatusRedrawingFlag flag)
+{
+    this->status_flags.set(flag);
+}
+
+void RedrawingFlagsUpdater::set_flags(const EnumClassFlagGroup<MainWindowRedrawingFlag> &flags)
+{
+    this->main_window_flags.set(flags);
+}
+
+void RedrawingFlagsUpdater::set_flags(const EnumClassFlagGroup<SubWindowRedrawingFlag> &flags)
+{
+    this->sub_window_flags.set(flags);
+}
+
+void RedrawingFlagsUpdater::set_flags(const EnumClassFlagGroup<StatusRedrawingFlag> &flags)
+{
+    this->status_flags.set(flags);
+}
+
+void RedrawingFlagsUpdater::reset_flag(MainWindowRedrawingFlag flag)
+{
+    this->main_window_flags.reset(flag);
+}
+
+void RedrawingFlagsUpdater::reset_flag(SubWindowRedrawingFlag flag)
+{
+    this->sub_window_flags.reset(flag);
+}
+
+void RedrawingFlagsUpdater::reset_flag(StatusRedrawingFlag flag)
+{
+    this->status_flags.reset(flag);
+}
+
+void RedrawingFlagsUpdater::reset_flags(const EnumClassFlagGroup<MainWindowRedrawingFlag> &flags)
+{
+    this->main_window_flags.reset(flags);
+}
+
+void RedrawingFlagsUpdater::reset_flags(const EnumClassFlagGroup<SubWindowRedrawingFlag> &flags)
+{
+    this->sub_window_flags.reset(flags);
+}
+
+void RedrawingFlagsUpdater::reset_flags(const EnumClassFlagGroup<StatusRedrawingFlag> &flags)
+{
+    this->status_flags.reset(flags);
+}
+
+void RedrawingFlagsUpdater::fill_up_sub_flags()
+{
+    for (const auto flag : EnumRange(SubWindowRedrawingFlag::INVENTORY, SubWindowRedrawingFlag::FOUND_ITEMS)) {
+        this->sub_window_flags.set(flag);
+    }
+}
---
+++ src/system/redrawing-flags-updater.h
@@ -0,0 +1,120 @@
+﻿#pragma once
+
+#include "util/flag-group.h"
+
+enum class MainWindowRedrawingFlag {
+    MISC, /*!< 種族と職業 */
+    TITLE, /*!< 称号 */
+    LEVEL,
+    EXP, /*!< 経験値 */
+    ABILITY_SCORE,
+    AC,
+    HP,
+    MP,
+    GOLD,
+    DEPTH,
+    EQUIPPY, /*!< 装備シンボル */
+    HEALTH, /*!< モンスターのステータス */
+    CUT, /*!< 負傷度 */
+    STUN, /*!< 朦朧度 */
+    HUNGER, /*!< 空腹度 */
+    TIMED_EFFECT,
+    UHEALTH, /*!< ペットのステータス */
+    ACTION, /*!< プレイヤーの行動状態 */
+    SPEED, /*!< 加速 */
+    STUDY, /*!< 学習 */
+    IMITATION, /*!< ものまね */
+    EXTRA, /*!< 拡張ステータス全体 */
+    BASIC, /*!< 基本ステータス全体 */
+    MAP, /*!< ゲームマップ */
+    WIPE, /*!< 画面消去 */
+    MAX,
+};
+
+enum class SubWindowRedrawingFlag {
+    INVENTORY, /*!< 所持品-装備品 */
+    EQUIPMENT, /*!< 装備品-所持品 */
+    SPELL, /*!< 魔法一覧 */
+    PLAYER, /*!< プレイヤーのステータス */
+    SIGHT_MONSTERS, /*!< 視界内モンスターの一覧 */
+    MESSAGE, /*!< メッセージログ */
+    OVERHEAD, /*!< 周辺の光景 */
+    MONSTER_LORE, /*!< モンスターの思い出 */
+    ITEM_KNOWLEDGTE, /*!< アイテムの知識 */
+    DUNGEON, /*!< ダンジョンの地形 */
+    SNAPSHOT, /*!< 記念写真 */
+    FLOOR_ITEMS, /*!< 床上のアイテム一覧 */
+    FOUND_ITEMS, /*!< 発見済みのアイテム一覧 */
+    MAX,
+};
+
+enum class StatusRedrawingFlag {
+    BONUS, /*!< 能力値修正 */
+    TORCH, /*!< 光源半径 */
+    HP,
+    MP,
+    SPELLS, /*!< 魔法学習数 */
+    COMBINATION, /*!< アイテム処理フラグ: アイテムの結合を要する */
+    REORDER, /*!< アイテム処理フラグ: アイテムの並び替えを要する */
+    AUTO_DESTRUCTION, /*!< アイテム処理フラグ: アイテムの自動破壊を要する */
+    UN_VIEW, /*!< 地形の視界外化 */
+    UN_LITE, /*!< 明暗範囲の視界外化 */
+    VIEW, /*!< 視界 */
+    LITE, /*!< 明暗範囲 */
+    MONSTER_LITE, /*!< モンスターの光源範囲 */
+    DELAY_VISIBILITY, /*!< 視界の追加更新 */
+    MONSTER_STATUSES, /*!< モンスターのステータス */
+    DISTANCE, /*!< プレイヤーとモンスターの距離 */
+    FLOW, /*!< プレイヤーから各マスへの到達距離 */
+    MAX,
+};
+
+class RedrawingFlagsUpdater {
+public:
+    RedrawingFlagsUpdater(const RedrawingFlagsUpdater &) = delete;
+    RedrawingFlagsUpdater(RedrawingFlagsUpdater &&) = delete;
+    RedrawingFlagsUpdater &operator=(const RedrawingFlagsUpdater &) = delete;
+    RedrawingFlagsUpdater &operator=(RedrawingFlagsUpdater &&) = delete;
+    ~RedrawingFlagsUpdater() = default;
+
+    static RedrawingFlagsUpdater &get_instance();
+
+    bool any_main() const;
+    bool any_sub() const;
+    bool any_stats() const;
+
+    bool has(MainWindowRedrawingFlag flag) const;
+    bool has(SubWindowRedrawingFlag flag) const;
+    bool has(StatusRedrawingFlag flag) const;
+
+    bool has_any_of(const EnumClassFlagGroup<MainWindowRedrawingFlag> &flags) const;
+    bool has_any_of(const EnumClassFlagGroup<SubWindowRedrawingFlag> &flags) const;
+    bool has_any_of(const EnumClassFlagGroup<StatusRedrawingFlag> &flags) const;
+
+    void set_flag(MainWindowRedrawingFlag flag);
+    void set_flag(SubWindowRedrawingFlag flag);
+    void set_flag(StatusRedrawingFlag flag);
+
+    void set_flags(const EnumClassFlagGroup<MainWindowRedrawingFlag> &flags);
+    void set_flags(const EnumClassFlagGroup<SubWindowRedrawingFlag> &flags);
+    void set_flags(const EnumClassFlagGroup<StatusRedrawingFlag> &flags);
+
+    void reset_flag(MainWindowRedrawingFlag flag);
+    void reset_flag(SubWindowRedrawingFlag flag);
+    void reset_flag(StatusRedrawingFlag flag);
+
+    void reset_flags(const EnumClassFlagGroup<MainWindowRedrawingFlag> &flags);
+    void reset_flags(const EnumClassFlagGroup<SubWindowRedrawingFlag> &flags);
+    void reset_flags(const EnumClassFlagGroup<StatusRedrawingFlag> &flags);
+
+    void fill_up_sub_flags();
+
+private:
+    RedrawingFlagsUpdater() = default;
+
+    static RedrawingFlagsUpdater instance;
+
+    EnumClassFlagGroup<MainWindowRedrawingFlag> main_window_flags{};
+    EnumClassFlagGroup<SubWindowRedrawingFlag> sub_window_flags{};
+    EnumClassFlagGroup<StatusRedrawingFlag> status_flags{};
+};
