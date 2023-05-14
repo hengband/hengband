e87db59152d655966488e3ead5021db4f79fd023
Hourier <66951241+Hourier@users.noreply.github.com>
2023-05-14 13:51:14 +0900

    [Refactor] #2769 town_type の使われていないフィールドを消した

--- src/floor/floor-town.h
+++ src/floor/floor-town.h
@@ -10,9 +10,7 @@
  */
 struct town_type {
     GAME_TEXT name[32];
-    uint32_t seed; /* Seed for RNG */
     std::vector<store_type> store; /* The stores [MAX_STORES] */
-    byte numstores;
 };
 
 constexpr short VALID_TOWNS = 6; // @details 旧海底都市クエストのマップを除外する. 有効な町に差し替え完了したら不要になるので注意.
