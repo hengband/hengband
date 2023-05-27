7627d51a9130e955bc75ebb80aa42634c9a1e92c
Hourier <66951241+Hourier@users.noreply.github.com>
2023-05-27 12:39:17 +0900

    [Refactor] #3346 UnixUserIds クラスを定義した

---
+++ src/main-unix/unix-user-ids.cpp
@@ -0,0 +1,49 @@
+﻿/*!
+ * @brief UNIX用ユーザID定義
+ * @author Hourier
+ * @date 2023/05/27
+ */
+
+#include "main-unix/unix-user-ids.h"
+
+UnixUserIds UnixUserIds::instance{};
+
+UnixUserIds &UnixUserIds::get_instance()
+{
+    return instance;
+}
+
+int UnixUserIds::get_user_id() const
+{
+    return this->user_id;
+}
+
+void UnixUserIds::set_user_id(const int id)
+{
+    this->user_id = id;
+}
+
+void UnixUserIds::mod_user_id(const int increment)
+{
+    this->user_id += increment;
+}
+
+int UnixUserIds::get_effective_user_id() const
+{
+    return this->effective_user_id;
+}
+
+void UnixUserIds::set_effective_user_id(const int id)
+{
+    this->effective_user_id = id;
+}
+
+int UnixUserIds::get_effective_group_id() const
+{
+    return this->effective_group_id;
+}
+
+void UnixUserIds::set_effective_group_id(const int id)
+{
+    this->effective_group_id = id;
+}
---
+++ src/main-unix/unix-user-ids.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+class UnixUserIds {
+public:
+    UnixUserIds(const UnixUserIds &) = delete;
+    UnixUserIds(UnixUserIds &&) = delete;
+    UnixUserIds &operator=(const UnixUserIds &) = delete;
+    UnixUserIds &operator=(UnixUserIds &&) = delete;
+    ~UnixUserIds() = default;
+
+    static UnixUserIds &get_instance();
+    int get_user_id() const;
+    void set_user_id(const int id);
+    void mod_user_id(const int increment);
+    int get_effective_user_id() const;
+    void set_effective_user_id(const int id);
+    int get_effective_group_id() const;
+    void set_effective_group_id(const int id);
+
+private:
+    UnixUserIds() = default;
+
+    static UnixUserIds instance;
+    int user_id = 0;
+    int effective_user_id = 0;
+    int effective_group_id = 0;
+};
