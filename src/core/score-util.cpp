343ab91953a6e0a9bcc87f0f47fc61a32947d015
Hourier <66951241+Hourier@users.noreply.github.com>
2023-06-02 19:40:19 +0900

    [Refactor] #3336 copy_info() で使われていたsnprintf() とmemcpy() をそれぞれ廃止した

--- src/core/score-util.cpp
+++ src/core/score-util.cpp
@@ -3,6 +3,8 @@
 #include "system/floor-type-definition.h"
 #include "system/player-type-definition.h"
 #include "util/angband-files.h"
+#include "util/string-processor.h"
+#include <algorithm>
 #ifdef SET_UID
 #include "main-unix/unix-user-ids.h"
 #endif
@@ -34,26 +36,30 @@ errr highscore_read(high_score *score)
 
 void high_score::copy_info(const PlayerType &player)
 {
-    snprintf(this->who, sizeof(this->who), "%-.15s", player.name);
+    const auto name = format("%-.15s", player.name);
+    std::copy_n(name.begin(), name.length(), this->who);
 
 #ifdef SET_UID
     const auto tmp_uid = UnixUserIds::get_instance().get_user_id();
 #else
     const auto tmp_uid = 0;
 #endif
-    snprintf(this->uid, sizeof(this->uid), "%7u", tmp_uid);
-    snprintf(this->sex, sizeof(this->sex), "%c", (player.psex ? 'm' : 'f'));
-    char buf[32];
-    snprintf(buf, sizeof(buf), "%2d", std::min(enum2i(player.prace), MAX_RACES));
-    memcpy(this->p_r, buf, 3);
-    snprintf(buf, sizeof(buf), "%2d", enum2i(std::min(player.pclass, PlayerClassType::MAX)));
-    memcpy(this->p_c, buf, 3);
-    snprintf(buf, sizeof(buf), "%2d", std::min(player.ppersonality, MAX_PERSONALITIES));
-    memcpy(this->p_a, buf, 3);
-
-    snprintf(this->cur_lev, sizeof(this->cur_lev), "%3d", std::min<ushort>(player.lev, 999));
+    const auto uid_str = format("%7u", tmp_uid);
+    std::copy_n(uid_str.begin(), uid_str.length(), this->uid);
+    this->sex[0] = player.psex ? 'm' : 'f';
+    const auto prace = format("%2d", std::min(enum2i(player.prace), MAX_RACES));
+    std::copy_n(prace.begin(), prace.length(), this->p_r);
+    const auto pclass = format("%2d", enum2i(std::min(player.pclass, PlayerClassType::MAX)));
+    std::copy_n(pclass.begin(), pclass.length(), this->p_c);
+    const auto ppersonality = format("%2d", std::min(player.ppersonality, MAX_PERSONALITIES));
+    std::copy_n(ppersonality.begin(), ppersonality.length(), this->p_a);
+    const auto current_level = format("%3d", std::min<ushort>(player.lev, 999));
+    std::copy_n(current_level.begin(), current_level.length(), this->cur_lev);
     const auto &floor = *player.current_floor_ptr;
-    snprintf(this->cur_dun, sizeof(this->cur_dun), "%3d", floor.dun_level);
-    snprintf(this->max_lev, sizeof(this->max_lev), "%3d", std::min<ushort>(player.max_plv, 999));
-    snprintf(this->max_dun, sizeof(this->max_dun), "%3d", max_dlv[floor.dungeon_idx]);
+    const auto current_dungeon = format("%3d", floor.dun_level);
+    std::copy_n(current_dungeon.begin(), current_dungeon.length(), this->cur_dun);
+    const auto max_level = format("%3d", std::min<ushort>(player.max_plv, 999));
+    std::copy_n(max_level.begin(), max_level.length(), this->max_lev);
+    const auto max_dungeon = format("%3d", max_dlv[floor.dungeon_idx]);
+    std::copy_n(max_dungeon.begin(), max_dungeon.length(), this->max_dun);
 }
