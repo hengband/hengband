6b86c3a7f61ab297c3a40cb5d3d101fce21b3653
Hourier <66951241+Hourier@users.noreply.github.com>
2023-06-25 01:11:25 +0900

    [Refactor] #3461 Reshaped do_name_pet()

--- src/cmd-action/cmd-pet.cpp
+++ src/cmd-action/cmd-pet.cpp
@@ -322,10 +322,7 @@ bool do_cmd_riding(PlayerType *player_ptr, bool force)
  */
 static void do_name_pet(PlayerType *player_ptr)
 {
-    MonsterEntity *m_ptr;
-    bool old_name = false;
-    bool old_target_pet = target_pet;
-
+    auto old_target_pet = target_pet;
     target_pet = true;
     if (!target_set(player_ptr, TARGET_KILL)) {
         target_pet = old_target_pet;
@@ -333,48 +330,57 @@ static void do_name_pet(PlayerType *player_ptr)
     }
 
     target_pet = old_target_pet;
-
-    if (player_ptr->current_floor_ptr->grid_array[target_row][target_col].m_idx) {
-        m_ptr = &player_ptr->current_floor_ptr->m_list[player_ptr->current_floor_ptr->grid_array[target_row][target_col].m_idx];
-
-        if (!m_ptr->is_pet()) {
-            msg_print(_("そのモンスターはペットではない。", "This monster is not a pet."));
-            return;
-        }
-        if (monraces_info[m_ptr->r_idx].kind_flags.has(MonsterKindType::UNIQUE)) {
-            msg_print(_("そのモンスターの名前は変えられない！", "You cannot change the name of this monster!"));
-            return;
-        }
-
-        msg_format(_("%sに名前をつける。", "Name %s."), monster_desc(player_ptr, m_ptr, 0).data());
-        msg_print(nullptr);
-
-        /* Start with nothing */
-        char out_val[20]{};
-
-        /* Use old inscription */
-        if (m_ptr->is_named()) {
-            /* Start with the old inscription */
-            angband_strcpy(out_val, m_ptr->nickname, sizeof(out_val));
-            old_name = true;
-        }
-
-        /* Get a new inscription (possibly empty) */
-        if (input_string(_("名前: ", "Name: "), out_val, 15)) {
-            if (out_val[0]) {
-                /* Save the inscription */
-                m_ptr->nickname = out_val;
-                if (record_named_pet) {
-                    exe_write_diary(player_ptr, DiaryKind::NAMED_PET, RECORD_NAMED_PET_NAME, monster_desc(player_ptr, m_ptr, MD_INDEF_VISIBLE));
-                }
-            } else {
-                if (record_named_pet && old_name) {
-                    exe_write_diary(player_ptr, DiaryKind::NAMED_PET, RECORD_NAMED_PET_UNNAME, monster_desc(player_ptr, m_ptr, MD_INDEF_VISIBLE));
-                }
-                m_ptr->nickname.clear();
-            }
-        }
+    auto &floor = *player_ptr->current_floor_ptr;
+    const auto &grid = floor.grid_array[target_row][target_col];
+    if (grid.m_idx == 0) {
+        return;
     }
+
+    auto *m_ptr = &floor.m_list[grid.m_idx];
+    if (!m_ptr->is_pet()) {
+        msg_print(_("そのモンスターはペットではない。", "This monster is not a pet."));
+        return;
+    }
+
+    if (monraces_info[m_ptr->r_idx].kind_flags.has(MonsterKindType::UNIQUE)) {
+        msg_print(_("そのモンスターの名前は変えられない！", "You cannot change the name of this monster!"));
+        return;
+    }
+
+    msg_format(_("%sに名前をつける。", "Name %s."), monster_desc(player_ptr, m_ptr, 0).data());
+    msg_print(nullptr);
+
+    /* Start with nothing */
+    char out_val[20]{};
+
+    /* Use old inscription */
+    auto old_name = false;
+    if (m_ptr->is_named()) {
+        /* Start with the old inscription */
+        angband_strcpy(out_val, m_ptr->nickname, sizeof(out_val));
+        old_name = true;
+    }
+
+    /* Get a new inscription (possibly empty) */
+    if (!input_string(_("名前: ", "Name: "), out_val, 15)) {
+        return;
+    }
+
+    if (out_val[0]) {
+        /* Save the inscription */
+        m_ptr->nickname = out_val;
+        if (record_named_pet) {
+            exe_write_diary(player_ptr, DiaryKind::NAMED_PET, RECORD_NAMED_PET_NAME, monster_desc(player_ptr, m_ptr, MD_INDEF_VISIBLE));
+        }
+
+        return;
+    }
+
+    if (record_named_pet && old_name) {
+        exe_write_diary(player_ptr, DiaryKind::NAMED_PET, RECORD_NAMED_PET_UNNAME, monster_desc(player_ptr, m_ptr, MD_INDEF_VISIBLE));
+    }
+
+    m_ptr->nickname.clear();
 }
 
 /*!
