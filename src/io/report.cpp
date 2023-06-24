8f110d72e5944f482a1e994326affe23e9848fdd
Habu <habu1010+github@gmail.com>
2023-06-24 13:50:39 +0900

    [Refactor] スコア送信機能のHTTP通信処理
    スコア送信機能のHTTP通信処理をhttp::Clientクラスで書き換える。

--- src/io/report.cpp
+++ src/io/report.cpp
@@ -14,6 +14,7 @@
 #include "io-dump/character-dump.h"
 #include "io/input-key-acceptor.h"
 #include "mind/mind-elementalist.h"
+#include "net/http-client.h"
 #include "player-base/player-class.h"
 #include "player-info/class-info.h"
 #include "player-info/race-info.h"
@@ -41,100 +42,55 @@
 #include <vector>
 
 #ifdef WORLD_SCORE
-#ifdef WINDOWS
-#define CURL_STATICLIB
-#endif
-#include <curl/curl.h>
 
 concptr screen_dump = nullptr;
 
-/*
- * internet resource value
- */
-#define HTTP_TIMEOUT 30 /*!< デフォルトのタイムアウト時間(秒) / Timeout length (second) */
-
 #ifdef JP
-#define SCORE_PATH "http://mars.kmc.gr.jp/~dis/heng_score/register_score.php" /*!< スコア開示URL */
-#else
-#define SCORE_PATH "http://moon.kmc.gr.jp/hengband/hengscore-en/score.cgi" /*!< スコア開示URL */
+constexpr auto SCORE_POST_URL = "http://mars.kmc.gr.jp/~dis/heng_score/register_score.php"; /*!< スコア開示URL */
 #endif
 
-size_t read_callback(char *buffer, size_t size, size_t nitems, void *userdata)
+static constexpr auto get_score_content_type()
 {
-    auto &data = *static_cast<std::span<const char> *>(userdata);
-    const auto copy_size = std::min<size_t>(size * nitems, data.size());
-
-    std::copy_n(data.begin(), copy_size, buffer);
-    data = data.subspan(copy_size);
-
-    return copy_size;
+#ifdef JP
+#ifdef SJIS
+    return "text/plain; charset=SHIFT_JIS";
+#endif
+#ifdef EUC
+    return "text/plain; charset=EUC-JP";
+#endif
+#else
+    return "text/plain; charset=ASCII";
+#endif
 }
 
 /*!
- * @brief HTTPによるダンプ内容伝送
- * @param url 伝送先URL
- * @param buf 伝送内容バッファ
- * @return 送信に成功した場合TRUE、失敗した場合FALSE
+ * @brief スコアサーバにスコアを送信する
+ * @param score_data 送信するスコアデータ
+ * @return 送信に成功した場合true、失敗した場合false
  */
-static bool http_post(concptr url, const std::vector<char> &buf)
+static bool post_score_to_score_server(PlayerType *player_ptr, const std::string &score_data)
 {
-    bool succeeded = false;
-    CURL *curl = curl_easy_init();
-    if (curl == nullptr) {
-        return false;
-    }
+    http::Client client;
+    client.user_agent = format("Hengband %d.%d.%d", H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH);
 
-    struct curl_slist *slist = nullptr;
-    slist = curl_slist_append(slist,
-#ifdef JP
-#ifdef SJIS
-        "Content-Type: text/plain; charset=SHIFT_JIS"
-#endif
-#ifdef EUC
-        "Content-Type: text/plain; charset=EUC-JP"
-#endif
-#else
-        "Content-Type: text/plain; charset=ASCII"
-#endif
-    );
+    term_clear();
 
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist);
+    while (true) {
+        term_fresh();
+        prt(_("スコア送信中...", "Sending the score..."), 0, 0);
+        term_fresh();
 
-    char user_agent[64];
-    snprintf(user_agent, sizeof(user_agent), "Hengband %d.%d.%d", H_VER_MAJOR, H_VER_MINOR, H_VER_PATCH);
-    curl_easy_setopt(curl, CURLOPT_USERAGENT, user_agent);
+        const auto res = client.post(SCORE_POST_URL, score_data, get_score_content_type());
+        if (res.has_value() && (res->status == 200)) {
+            return true;
+        }
 
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-
-    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
-    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, HTTP_TIMEOUT);
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, HTTP_TIMEOUT);
-
-    curl_easy_setopt(curl, CURLOPT_POST, 1);
-
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 10);
-    curl_easy_setopt(curl, CURLOPT_POSTREDIR, CURL_REDIR_POST_ALL);
-
-    std::span<const char> data(buf);
-    curl_easy_setopt(curl, CURLOPT_READDATA, &data);
-    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, data.size());
-
-    if (curl_easy_perform(curl) == CURLE_OK) {
-        long response_code;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-        if (response_code == 200) {
-            succeeded = true;
+        prt(_("スコア・サーバへの送信に失敗しました。", "Failed to send to the score server."), 0, 0);
+        (void)inkey();
+        if (!get_check_strict(player_ptr, _("もう一度接続を試みますか? ", "Try again? "), CHECK_NO_HISTORY)) {
+            return false;
         }
     }
-
-    curl_slist_free_all(slist);
-    curl_easy_cleanup(curl);
-
-    return succeeded;
 }
 
 /*!
@@ -341,26 +297,7 @@ bool report_score(PlayerType *player_ptr)
                  << screen_dump;
     }
 
-    const auto score = score_ss.str();
-    const std::vector<char> score_data(score.begin(), score.end());
-
-    term_clear();
-    while (true) {
-        term_fresh();
-        prt(_("スコア送信中...", "Sending the score..."), 0, 0);
-        term_fresh();
-        if (http_post(SCORE_PATH, score_data)) {
-            return true;
-        }
-
-        prt(_("スコア・サーバへの送信に失敗しました。", "Failed to send to the score server."), 0, 0);
-        (void)inkey();
-        if (get_check_strict(player_ptr, _("もう一度接続を試みますか? ", "Try again? "), CHECK_NO_HISTORY)) {
-            continue;
-        }
-
-        return false;
-    }
+    return post_score_to_score_server(player_ptr, score_ss.str());
 }
 #else
 concptr screen_dump = nullptr;
