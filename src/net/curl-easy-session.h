df959867f074e5ddbb77c014591f95a432cee7d8
Habu <habu1010+github@gmail.com>
2023-06-24 13:50:39 +0900

    [Feature] HTTP通信機能
    HTTP通信機能を実現するクラス群を追加する。

---
+++ src/net/curl-easy-session.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+#include "system/angband.h"
+#include <functional>
+#include <memory>
+#include <string>
+
+#if defined(WORLD_SCORE)
+
+#ifdef WINDOWS
+#define CURL_STATICLIB
+#endif
+#include <curl/curl.h>
+
+namespace libcurl {
+
+/*!
+ * @brief libcurl easy session の薄いラッパークラス
+ */
+class EasySession {
+public:
+    using SendHandler = std::function<size_t(char *, size_t)>;
+    using ReceiveHandler = std::function<size_t(char *, size_t)>;
+
+    EasySession();
+    ~EasySession();
+    EasySession(const EasySession &) = delete;
+    EasySession &operator=(const EasySession &) = delete;
+    EasySession(EasySession &&);
+    EasySession &operator=(EasySession &&);
+
+    bool is_valid() const;
+    void common_setup(const std::string &url, int timeout_sec = 0);
+    void sender_setup(SendHandler handler, long post_field_size);
+    void receiver_setup(ReceiveHandler handler);
+    bool perform();
+
+    template <typename T>
+    void setopt(CURLoption opt, T param)
+    {
+        curl_easy_setopt(this->get_curl_handle(), opt, param);
+    }
+
+    template <typename T>
+    bool getinfo(CURLINFO info, T arg)
+    {
+        return curl_easy_getinfo(this->get_curl_handle(), info, arg) == CURLE_OK;
+    }
+
+private:
+    CURL *get_curl_handle();
+
+    class Impl;
+    std::unique_ptr<Impl> pimpl_;
+};
+
+}
+
+#endif
---
+++ src/net/curl-slist.cpp
@@ -0,0 +1,65 @@
+﻿#include "net/curl-slist.h"
+
+#if defined(WORLD_SCORE)
+
+namespace libcurl {
+
+class SList::Impl {
+public:
+    Impl()
+        : slist(nullptr, curl_slist_free_all)
+    {
+    }
+    ~Impl() = default;
+    Impl(const Impl &) = delete;
+    Impl &operator=(const Impl &) = delete;
+    Impl(Impl &&) = default;
+    Impl &operator=(Impl &&) = default;
+
+    std::unique_ptr<curl_slist, decltype(&curl_slist_free_all)> slist;
+    bool valid = true;
+};
+
+SList::SList()
+    : pimpl_(std::make_unique<Impl>())
+{
+}
+
+SList::~SList() = default;
+SList::SList(SList &&) = default;
+SList &SList::operator=(SList &&) = default;
+
+bool SList::is_valid() const
+{
+    return pimpl_->valid;
+}
+
+void SList::append(const char *str)
+{
+    if (!pimpl_->valid) {
+        return;
+    }
+
+    auto result = curl_slist_append(pimpl_->slist.get(), str);
+    if (result == nullptr) {
+        pimpl_->valid = false;
+        return;
+    }
+
+    pimpl_->slist.release();
+    pimpl_->slist.reset(result);
+}
+
+void SList::append(const std::string &str)
+{
+    this->append(str.data());
+}
+
+curl_slist *SList::get()
+{
+    return pimpl_->slist.get();
+}
+
+}
+
+#endif
