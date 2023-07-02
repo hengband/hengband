5866548fe3ff7a311667fc44e9c63884c751a068
Hourier <66951241+Hourier@users.noreply.github.com>
2023-07-02 21:07:52 +0900

    [Refactor] #3503 cmd-macro.cpp/cmd-dump.cpp における長大なif-elseif 文の塊をswitch/case 文に改めた

--- src/cmd-io/cmd-dump.cpp
+++ src/cmd-io/cmd-dump.cpp
@@ -64,7 +64,6 @@ void do_cmd_pref(PlayerType *player_ptr)
  */
 void do_cmd_colors(PlayerType *player_ptr)
 {
-    int i;
     char tmp[160];
     FILE *auto_dump_stream;
     screen_save();
@@ -75,12 +74,13 @@ void do_cmd_colors(PlayerType *player_ptr)
         prt(_("(2) カラーの設定をファイルに書き出す", "(2) Dump colors"), 5, 5);
         prt(_("(3) カラーの設定を変更する", "(3) Modify colors"), 6, 5);
         prt(_("コマンド: ", "Command: "), 8, 0);
-        i = inkey();
-        if (i == ESCAPE) {
+        const auto key = inkey();
+        if (key == ESCAPE) {
             break;
         }
 
-        if (i == '1') {
+        switch (key) {
+        case '1': {
             prt(_("コマンド: ユーザー設定ファイルをロードします", "Command: Load a user pref file"), 8, 0);
             prt(_("ファイル: ", "File: "), 10, 0);
             strnfmt(tmp, sizeof(tmp), "%s.prf", player_ptr->base_name);
@@ -91,7 +91,9 @@ void do_cmd_colors(PlayerType *player_ptr)
             (void)process_pref_file(player_ptr, tmp, true);
             term_xtra(TERM_XTRA_REACT, 0);
             term_redraw();
-        } else if (i == '2') {
+            break;
+        }
+        case '2': {
             constexpr auto mark = "Colors";
             prt(_("コマンド: カラーの設定をファイルに書き出します", "Command: Dump colors"), 8, 0);
             prt(_("ファイル: ", "File: "), 10, 0);
@@ -106,7 +108,7 @@ void do_cmd_colors(PlayerType *player_ptr)
             }
 
             auto_dump_printf(auto_dump_stream, _("\n# カラーの設定\n\n", "\n# Color redefinitions\n\n"));
-            for (i = 0; i < 256; i++) {
+            for (auto i = 0; i < 256; i++) {
                 int kv = angband_color_table[i][0];
                 int rv = angband_color_table[i][1];
                 int gv = angband_color_table[i][2];
@@ -127,15 +129,17 @@ void do_cmd_colors(PlayerType *player_ptr)
 
             close_auto_dump(&auto_dump_stream, mark);
             msg_print(_("カラーの設定をファイルに書き出しました。", "Dumped color redefinitions."));
-        } else if (i == '3') {
+            break;
+        }
+        case '3': {
             static byte a = 0;
             prt(_("コマンド: カラーの設定を変更します", "Command: Modify colors"), 8, 0);
             while (true) {
                 concptr name;
                 clear_from(10);
-                for (byte j = 0; j < 16; j++) {
-                    term_putstr(j * 4, 20, -1, a, "###");
-                    term_putstr(j * 4, 22, -1, j, format("%3d", j));
+                for (byte i = 0; i < 16; i++) {
+                    term_putstr(i * 4, 20, -1, a, "###");
+                    term_putstr(i * 4, 22, -1, i, format("%3d", i));
                 }
 
                 name = ((a < 16) ? color_names[a] : _("未定義", "undefined"));
@@ -144,47 +148,55 @@ void do_cmd_colors(PlayerType *player_ptr)
                     format("K = 0x%02x / R,G,B = 0x%02x,0x%02x,0x%02x", angband_color_table[a][0], angband_color_table[a][1], angband_color_table[a][2],
                         angband_color_table[a][3]));
                 term_putstr(0, 14, -1, TERM_WHITE, _("コマンド (n/N/k/K/r/R/g/G/b/B): ", "Command (n/N/k/K/r/R/g/G/b/B): "));
-                i = inkey();
-                if (i == ESCAPE) {
+                auto ch = inkey();
+                if (ch == ESCAPE) {
                     break;
                 }
 
-                if (i == 'n') {
-                    a = (byte)(a + 1);
-                }
-                if (i == 'N') {
-                    a = (byte)(a - 1);
-                }
-                if (i == 'k') {
-                    angband_color_table[a][0] = (byte)(angband_color_table[a][0] + 1);
-                }
-                if (i == 'K') {
-                    angband_color_table[a][0] = (byte)(angband_color_table[a][0] - 1);
-                }
-                if (i == 'r') {
-                    angband_color_table[a][1] = (byte)(angband_color_table[a][1] + 1);
-                }
-                if (i == 'R') {
-                    angband_color_table[a][1] = (byte)(angband_color_table[a][1] - 1);
-                }
-                if (i == 'g') {
-                    angband_color_table[a][2] = (byte)(angband_color_table[a][2] + 1);
-                }
-                if (i == 'G') {
-                    angband_color_table[a][2] = (byte)(angband_color_table[a][2] - 1);
-                }
-                if (i == 'b') {
+                switch (ch) {
+                case 'b':
                     angband_color_table[a][3] = (byte)(angband_color_table[a][3] + 1);
-                }
-                if (i == 'B') {
+                    break;
+                case 'B':
                     angband_color_table[a][3] = (byte)(angband_color_table[a][3] - 1);
+                    break;
+                case 'g':
+                    angband_color_table[a][2] = (byte)(angband_color_table[a][2] + 1);
+                    break;
+                case 'G':
+                    angband_color_table[a][2] = (byte)(angband_color_table[a][2] - 1);
+                    break;
+                case 'k':
+                    angband_color_table[a][0] = (byte)(angband_color_table[a][0] + 1);
+                    break;
+                case 'K':
+                    angband_color_table[a][0] = (byte)(angband_color_table[a][0] - 1);
+                    break;
+                case 'n':
+                    a++;
+                    break;
+                case 'N':
+                    a--;
+                    break;
+                case 'r':
+                    angband_color_table[a][1] = (byte)(angband_color_table[a][1] + 1);
+                    break;
+                case 'R':
+                    angband_color_table[a][1] = (byte)(angband_color_table[a][1] - 1);
+                    break;
+                default:
+                    break;
                 }
 
                 term_xtra(TERM_XTRA_REACT, 0);
                 term_redraw();
             }
-        } else {
+
+            break;
+        }
+        default:
             bell();
+            break;
         }
 
         msg_erase();
