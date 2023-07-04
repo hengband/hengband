bbe8f63837fb53fd1f970283a7ab1c2ddec6c15b
Habu <habu1010+github@gmail.com>
2023-07-04 21:44:13 +0900

    [Add] JSON for Modern C++ の導入
    ネットワーキング機能で使用するため、MITライセンスでヘッダオンリーな使い
    勝手の良いJSONライブラリである、JSON for Modern C++ を導入する。

---
+++ src/external-lib/include-json.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+// MSVCの警告レベルを最大に設定してあるためjson.hppが大量の警告を
+// 出力してしまうのでヘッダのインクルード時のみ警告を抑制しておく
+#if defined(_MSC_VER)
+#pragma warning(push, 0)
+#endif
+
+#include "external-lib/json.hpp"
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
