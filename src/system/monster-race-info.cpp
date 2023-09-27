fd282a4723b0fd1079bb78840fbc0c9fcf163013
Hourier <66951241+Hourier@users.noreply.github.com>
2023-09-27 09:55:34 +0900

    [Refactor] #3627 Controller的な処理 MonsterDamageProcessor::death_combined_uniques() をModel的な処理 MonraceList::kill_unified_unique() に置き換えた

--- src/system/monster-race-info.cpp
+++ src/system/monster-race-info.cpp
@@ -1,4 +1,5 @@
 #include "system/monster-race-info.h"
+#include "monster-race/monster-race.h"
 #include "monster-race/race-indice-types.h"
 #include "monster/horror-descriptions.h"
 #include <algorithm>
@@ -75,4 +76,43 @@ bool MonraceList::can_unify_separate(const MonsterRaceId r_idx) const
     }
 
     return std::any_of(unified_uniques.begin(), unified_uniques.end(), [&r_idx](const auto &x) { return x.second.contains(r_idx); });
+}
+
+/*!
+ * @brief 合体ユニークの死亡処理
+ * @details 分離/合体が A = B + C + D という図式の時、Aが死亡した場合BとCとDも死亡処理を行う。
+ * B・C・Dのいずれかが死亡した場合、その死亡したユニークに加えてAの死亡処理も行う。
+ * v3.0.0 α89現在は、分離後のユニーク数は2のみ。3以上は将来の拡張。
+ * @param r_idx 実際に死亡したモンスターの種族ID
+ */
+void MonraceList::kill_unified_unique(const MonsterRaceId r_idx)
+{
+    auto kill_monrace = [](MonsterRaceId r_idx) {
+        auto &monrace = monraces_info[r_idx];
+        monrace.max_num = 0;
+        monrace.r_pkills++;
+        monrace.r_akills++;
+        if (monrace.r_tkills < MAX_SHORT) {
+            monrace.r_tkills++;
+        }
+    };
+
+    const auto it_unique = unified_uniques.find(r_idx);
+    if (it_unique != unified_uniques.end()) {
+        kill_monrace(it_unique->first);
+        for (const auto separate : it_unique->second) {
+            kill_monrace(separate);
+        }
+
+        return;
+    }
+
+    for (const auto &[unified_unique, separates] : unified_uniques) {
+        const auto it_separate = separates.find(r_idx);
+        if (it_separate != separates.end()) {
+            kill_monrace(*it_separate);
+            kill_monrace(unified_unique);
+            return;
+        }
+    }
 }
--- src/system/monster-race-info.h
+++ src/system/monster-race-info.h
@@ -153,6 +153,7 @@ public:
 
     static MonraceList &get_instance();
     bool can_unify_separate(const MonsterRaceId r_idx) const;
+    void kill_unified_unique(const MonsterRaceId r_idx);
 
 private:
     MonraceList() = default;
