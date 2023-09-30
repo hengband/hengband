b67894ef18f53c662103537faaa6bf8e56531d9a
Hourier <66951241+Hourier@users.noreply.github.com>
2023-10-01 00:15:13 +0900

    [Refactor] #2666 フェーズアウト (段階的廃止)では意味が分からないので、観戦中という意味のis_watching 変数を新しく作り、AngbandSystem クラスに導入した

--- src/system/angband-system.cpp
+++ src/system/angband-system.cpp
@@ -6,3 +6,13 @@ AngbandSystem &AngbandSystem::get_instance()
 {
     return instance;
 }
+
+void AngbandSystem::set_watch(bool new_status)
+{
+    this->watch_stat = new_status;
+}
+
+bool AngbandSystem::is_watching() const
+{
+    return this->watch_stat;
+}
--- src/system/angband-system.h
+++ src/system/angband-system.h
@@ -8,9 +8,12 @@ public:
     AngbandSystem &operator=(AngbandSystem &&) = delete;
 
     static AngbandSystem &get_instance();
+    void set_watch(bool new_status);
+    bool is_watching() const;
 
 private:
     AngbandSystem() = default;
 
     static AngbandSystem instance;
+    bool watch_stat = false; // カジノ闘技場の観戦状態等に利用。NPCの処理の対象にならず自身もほとんどの行動ができない.
 };
