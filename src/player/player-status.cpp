ca48a00c94d9b7a1bed75ba70b6e5d1f85d7fea6
rurero2 <0121mrei@gmail.com>
2023-12-16 21:17:53 +0900

    [Fix]両手がふさがっていてもふらつかないバグを修正

--- src/player/player-status.cpp
+++ src/player/player-status.cpp
@@ -1968,21 +1968,23 @@ int16_t calc_double_weapon_penalty(PlayerType *player_ptr, INVENTORY_IDX slot)
 
 static bool is_riding_two_hands(PlayerType *player_ptr)
 {
-    if (!player_ptr->riding || none_bits(player_ptr->pet_extra_flags, PF_TWO_HANDS)) {
+    if (!player_ptr->riding) {
         return false;
     }
 
     if (has_two_handed_weapons(player_ptr) || (empty_hands(player_ptr, false) == EMPTY_HAND_NONE)) {
         return true;
     }
-
-    switch (player_ptr->pclass) {
-    case PlayerClassType::MONK:
-    case PlayerClassType::FORCETRAINER:
-    case PlayerClassType::BERSERKER:
-        return (empty_hands(player_ptr, false) != EMPTY_HAND_NONE) && !has_melee_weapon(player_ptr, INVEN_MAIN_HAND) && !has_melee_weapon(player_ptr, INVEN_SUB_HAND);
-    default:
-        return false;
+ 
+    if (any_bits(player_ptr->pet_extra_flags, PF_TWO_HANDS)) { 
+        switch (player_ptr->pclass) {
+        case PlayerClassType::MONK:
+        case PlayerClassType::FORCETRAINER:
+        case PlayerClassType::BERSERKER:
+            return (empty_hands(player_ptr, false) != EMPTY_HAND_NONE) && !has_melee_weapon(player_ptr, INVEN_MAIN_HAND) && !has_melee_weapon(player_ptr, INVEN_SUB_HAND);
+        default:
+            return false;
+        }
     }
 }
 
