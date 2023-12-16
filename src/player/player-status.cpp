58dc4dfecdd2d0060843673f1b13538cfdbc99ec
rurero2 <0121mrei@gmail.com>
2023-12-16 21:17:53 +0900

    [Fix]両手がふさがっていてもふらつかないバグを修正

--- src/player/player-status.cpp
+++ src/player/player-status.cpp
@@ -1968,7 +1968,7 @@ int16_t calc_double_weapon_penalty(PlayerType *player_ptr, INVENTORY_IDX slot)
 
 static bool is_riding_two_hands(PlayerType *player_ptr)
 {
-    if (!player_ptr->riding || none_bits(player_ptr->pet_extra_flags, PF_TWO_HANDS)) {
+    if (!player_ptr->riding) {
         return false;
     }
 
