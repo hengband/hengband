9ac6b930c3752b65ed9db3ca7416922612797ec8
Hourier <66951241+Hourier@users.noreply.github.com>
2024-04-27 19:50:14 +0900

    [Refactor] #3952 rooms-special.cpp において、変数宣言と代入を同時にするよう差し替えた

--- src/room/rooms-special.cpp
+++ src/room/rooms-special.cpp
@@ -30,47 +30,43 @@
  */
 bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
 {
-    POSITION y, x, y2, x2, yval, xval;
-    POSITION y1, x1, xsize, ysize;
-    bool light;
-
-    Grid *g_ptr;
-
     /* Pick a room size */
-    xsize = rand_range(9, 13);
-    ysize = rand_range(9, 13);
+    const auto xsize = rand_range(9, 13);
+    const auto ysize = rand_range(9, 13);
 
     /* Find and reserve some space in the dungeon.  Get center of room. */
     auto *floor_ptr = player_ptr->current_floor_ptr;
+    int yval;
+    int xval;
     if (!find_space(player_ptr, dd_ptr, &yval, &xval, ysize + 2, xsize + 2)) {
         return false;
     }
 
     /* Choose lite or dark */
-    light = ((floor_ptr->dun_level <= randint1(25)) && floor_ptr->get_dungeon_definition().flags.has_not(DungeonFeatureType::DARKNESS));
+    const auto should_brighten = ((floor_ptr->dun_level <= randint1(25)) && floor_ptr->get_dungeon_definition().flags.has_not(DungeonFeatureType::DARKNESS));
 
     /* Get corner values */
-    y1 = yval - ysize / 2;
-    x1 = xval - xsize / 2;
-    y2 = yval + (ysize - 1) / 2;
-    x2 = xval + (xsize - 1) / 2;
+    const auto y1 = yval - ysize / 2;
+    const auto x1 = xval - xsize / 2;
+    const auto y2 = yval + (ysize - 1) / 2;
+    const auto x2 = xval + (xsize - 1) / 2;
 
     /* Place a full floor under the room */
-    for (y = y1 - 1; y <= y2 + 1; y++) {
-        for (x = x1 - 1; x <= x2 + 1; x++) {
-            g_ptr = &floor_ptr->grid_array[y][x];
+    for (auto y = y1 - 1; y <= y2 + 1; y++) {
+        for (auto x = x1 - 1; x <= x2 + 1; x++) {
+            auto *g_ptr = &floor_ptr->grid_array[y][x];
             place_grid(player_ptr, g_ptr, GB_FLOOR);
             g_ptr->feat = feat_glass_floor;
             g_ptr->info |= (CAVE_ROOM);
-            if (light) {
+            if (should_brighten) {
                 g_ptr->info |= (CAVE_GLOW);
             }
         }
     }
 
     /* Walls around the room */
-    for (y = y1 - 1; y <= y2 + 1; y++) {
-        g_ptr = &floor_ptr->grid_array[y][x1 - 1];
+    for (auto y = y1 - 1; y <= y2 + 1; y++) {
+        auto *g_ptr = &floor_ptr->grid_array[y][x1 - 1];
         place_grid(player_ptr, g_ptr, GB_OUTER);
         g_ptr->feat = feat_glass_wall;
         g_ptr = &floor_ptr->grid_array[y][x2 + 1];
@@ -78,8 +74,8 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
         g_ptr->feat = feat_glass_wall;
     }
 
-    for (x = x1 - 1; x <= x2 + 1; x++) {
-        g_ptr = &floor_ptr->grid_array[y1 - 1][x];
+    for (auto x = x1 - 1; x <= x2 + 1; x++) {
+        auto *g_ptr = &floor_ptr->grid_array[y1 - 1][x];
         place_grid(player_ptr, g_ptr, GB_OUTER);
         g_ptr->feat = feat_glass_wall;
         g_ptr = &floor_ptr->grid_array[y2 + 1][x];
@@ -90,43 +86,41 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
     switch (randint1(3)) {
     case 1: /* 4 lite breathers + potion */
     {
-        DIRECTION dir1, dir2;
         get_mon_num_prep(player_ptr, vault_aux_lite, nullptr);
 
         /* Place fixed lite berathers */
-        for (dir1 = 4; dir1 < 8; dir1++) {
-            MonsterRaceId r_idx = get_mon_num(player_ptr, 0, floor_ptr->dun_level, 0);
-
-            y = yval + 2 * ddy_ddd[dir1];
-            x = xval + 2 * ddx_ddd[dir1];
-            if (MonsterRace(r_idx).is_valid()) {
-                place_specific_monster(player_ptr, 0, y, x, r_idx, PM_ALLOW_SLEEP);
+        for (auto dir1 = 4; dir1 < 8; dir1++) {
+            const auto monrace_id = get_mon_num(player_ptr, 0, floor_ptr->dun_level, 0);
+            auto y = yval + 2 * ddy_ddd[dir1];
+            auto x = xval + 2 * ddx_ddd[dir1];
+            if (MonsterRace(monrace_id).is_valid()) {
+                place_specific_monster(player_ptr, 0, y, x, monrace_id, PM_ALLOW_SLEEP);
             }
 
             /* Walls around the breather */
-            for (dir2 = 0; dir2 < 8; dir2++) {
-                g_ptr = &floor_ptr->grid_array[y + ddy_ddd[dir2]][x + ddx_ddd[dir2]];
+            for (auto dir2 = 0; dir2 < 8; dir2++) {
+                auto *g_ptr = &floor_ptr->grid_array[y + ddy_ddd[dir2]][x + ddx_ddd[dir2]];
                 place_grid(player_ptr, g_ptr, GB_INNER);
                 g_ptr->feat = feat_glass_wall;
             }
         }
 
         /* Walls around the potion */
-        for (dir1 = 0; dir1 < 4; dir1++) {
-            y = yval + 2 * ddy_ddd[dir1];
-            x = xval + 2 * ddx_ddd[dir1];
-            g_ptr = &floor_ptr->grid_array[y][x];
+        for (auto dir1 = 0; dir1 < 4; dir1++) {
+            auto y = yval + 2 * ddy_ddd[dir1];
+            auto x = xval + 2 * ddx_ddd[dir1];
+            auto *g_ptr = &floor_ptr->grid_array[y][x];
             place_grid(player_ptr, g_ptr, GB_INNER_PERM);
             g_ptr->feat = feat_permanent_glass_wall;
             floor_ptr->grid_array[yval + ddy_ddd[dir1]][xval + ddx_ddd[dir1]].info |= (CAVE_ICKY);
         }
 
         /* Glass door */
-        dir1 = randint0(4);
-        y = yval + 2 * ddy_ddd[dir1];
-        x = xval + 2 * ddx_ddd[dir1];
+        auto dir1 = randint0(4);
+        auto y = yval + 2 * ddy_ddd[dir1];
+        auto x = xval + 2 * ddx_ddd[dir1];
         place_secret_door(player_ptr, y, x, DOOR_GLASS_DOOR);
-        g_ptr = &floor_ptr->grid_array[y][x];
+        auto *g_ptr = &floor_ptr->grid_array[y][x];
         if (is_closed_door(player_ptr, g_ptr->feat)) {
             g_ptr->mimic = feat_glass_wall;
         }
@@ -139,11 +133,8 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
 
     case 2: /* 1 lite breather + random object */
     {
-        MonsterRaceId r_idx;
-        DIRECTION dir1;
-
         /* Pillars */
-        g_ptr = &floor_ptr->grid_array[y1 + 1][x1 + 1];
+        auto *g_ptr = &floor_ptr->grid_array[y1 + 1][x1 + 1];
         place_grid(player_ptr, g_ptr, GB_INNER);
         g_ptr->feat = feat_glass_wall;
 
@@ -160,25 +151,25 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
         g_ptr->feat = feat_glass_wall;
         get_mon_num_prep(player_ptr, vault_aux_lite, nullptr);
 
-        r_idx = get_mon_num(player_ptr, 0, floor_ptr->dun_level, 0);
-        if (MonsterRace(r_idx).is_valid()) {
-            place_specific_monster(player_ptr, 0, yval, xval, r_idx, 0L);
+        const auto monrace_id = get_mon_num(player_ptr, 0, floor_ptr->dun_level, 0);
+        if (MonsterRace(monrace_id).is_valid()) {
+            place_specific_monster(player_ptr, 0, yval, xval, monrace_id, 0L);
         }
 
         /* Walls around the breather */
-        for (dir1 = 0; dir1 < 8; dir1++) {
+        for (auto dir1 = 0; dir1 < 8; dir1++) {
             g_ptr = &floor_ptr->grid_array[yval + ddy_ddd[dir1]][xval + ddx_ddd[dir1]];
             place_grid(player_ptr, g_ptr, GB_INNER);
             g_ptr->feat = feat_glass_wall;
         }
 
         /* Curtains around the breather */
-        for (y = yval - 1; y <= yval + 1; y++) {
+        for (auto y = yval - 1; y <= yval + 1; y++) {
             place_secret_door(player_ptr, y, xval - 2, DOOR_CURTAIN);
             place_secret_door(player_ptr, y, xval + 2, DOOR_CURTAIN);
         }
 
-        for (x = xval - 1; x <= xval + 1; x++) {
+        for (auto x = xval - 1; x <= xval + 1; x++) {
             place_secret_door(player_ptr, yval - 2, x, DOOR_CURTAIN);
             place_secret_door(player_ptr, yval + 2, x, DOOR_CURTAIN);
         }
@@ -190,11 +181,9 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
 
     case 3: /* 4 shards breathers + 2 potions */
     {
-        DIRECTION dir1;
-
         /* Walls around the potion */
-        for (y = yval - 2; y <= yval + 2; y++) {
-            g_ptr = &floor_ptr->grid_array[y][xval - 3];
+        for (auto y = yval - 2; y <= yval + 2; y++) {
+            auto *g_ptr = &floor_ptr->grid_array[y][xval - 3];
             place_grid(player_ptr, g_ptr, GB_INNER);
             g_ptr->feat = feat_glass_wall;
             g_ptr = &floor_ptr->grid_array[y][xval + 3];
@@ -202,8 +191,8 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
             g_ptr->feat = feat_glass_wall;
         }
 
-        for (x = xval - 2; x <= xval + 2; x++) {
-            g_ptr = &floor_ptr->grid_array[yval - 3][x];
+        for (auto x = xval - 2; x <= xval + 2; x++) {
+            auto *g_ptr = &floor_ptr->grid_array[yval - 3][x];
             place_grid(player_ptr, g_ptr, GB_INNER);
             g_ptr->feat = feat_glass_wall;
             g_ptr = &floor_ptr->grid_array[yval + 3][x];
@@ -211,8 +200,8 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
             g_ptr->feat = feat_glass_wall;
         }
 
-        for (dir1 = 4; dir1 < 8; dir1++) {
-            g_ptr = &floor_ptr->grid_array[yval + 2 * ddy_ddd[dir1]][xval + 2 * ddx_ddd[dir1]];
+        for (auto dir1 = 4; dir1 < 8; dir1++) {
+            auto *g_ptr = &floor_ptr->grid_array[yval + 2 * ddy_ddd[dir1]][xval + 2 * ddx_ddd[dir1]];
             place_grid(player_ptr, g_ptr, GB_INNER);
             g_ptr->feat = feat_glass_wall;
         }
@@ -220,13 +209,12 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
         get_mon_num_prep(player_ptr, vault_aux_shards, nullptr);
 
         /* Place shard berathers */
-        for (dir1 = 4; dir1 < 8; dir1++) {
-            MonsterRaceId r_idx = get_mon_num(player_ptr, 0, floor_ptr->dun_level, 0);
-
-            y = yval + ddy_ddd[dir1];
-            x = xval + ddx_ddd[dir1];
-            if (MonsterRace(r_idx).is_valid()) {
-                place_specific_monster(player_ptr, 0, y, x, r_idx, 0L);
+        for (auto dir1 = 4; dir1 < 8; dir1++) {
+            const auto monrace_id = get_mon_num(player_ptr, 0, floor_ptr->dun_level, 0);
+            auto y = yval + ddy_ddd[dir1];
+            auto x = xval + ddx_ddd[dir1];
+            if (MonsterRace(monrace_id).is_valid()) {
+                place_specific_monster(player_ptr, 0, y, x, monrace_id, 0L);
             }
         }
 
@@ -243,8 +231,8 @@ bool build_type15(PlayerType *player_ptr, dun_data_type *dd_ptr)
             place_object(player_ptr, yval + 1, xval, AM_NO_FIXED_ART);
         }
 
-        for (y = yval - 2; y <= yval + 2; y++) {
-            for (x = xval - 2; x <= xval + 2; x++) {
+        for (auto y = yval - 2; y <= yval + 2; y++) {
+            for (auto x = xval - 2; x <= xval + 2; x++) {
                 floor_ptr->grid_array[y][x].info |= (CAVE_ICKY);
             }
         }
