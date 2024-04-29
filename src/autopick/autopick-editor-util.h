ad9894e00011259b3595b9446082a4d6ba77a3a7
Hourier <66951241+Hourier@users.noreply.github.com>
2024-04-29 22:09:24 +0900

    [Refactor] #3965 add_str_to_yank() の引数をconcptr からstring_view に差し替えた
    これによりstring を引数にすることもできるようになり、もってchar[] の未初期化警告を排除した

--- src/autopick/autopick-editor-util.h
+++ src/autopick/autopick-editor-util.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "system/angband.h"
+#include <string_view>
 
 struct text_body_type;
 void toggle_keyword(text_body_type *tb, BIT_FLAGS flg);
@@ -8,5 +9,5 @@ void toggle_command_letter(text_body_type *tb, byte flg);
 void add_keyword(text_body_type *tb, BIT_FLAGS flg);
 bool add_empty_line(text_body_type *tb);
 void kill_yank_chain(text_body_type *tb);
-void add_str_to_yank(text_body_type *tb, concptr str);
+void add_str_to_yank(text_body_type *tb, std::string_view str);
 void copy_text_to_yank(text_body_type *tb);
