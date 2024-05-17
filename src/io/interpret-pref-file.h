8a8e9990e152d763f8e9c89c3f486631b79c59b3
Hourier <66951241+Hourier@users.noreply.github.com>
2024-05-17 23:26:26 +0900

    [Refactor] interpret_pref_file() の戻り値をint に変え、その他のstatic な関数群で0/1しか返していないものの戻り値をboolに変えた

--- src/io/interpret-pref-file.h
+++ src/io/interpret-pref-file.h
@@ -7,5 +7,5 @@
 extern char *histpref_buf;
 
 class PlayerType;
-errr interpret_pref_file(PlayerType *player_ptr, char *buf);
+int interpret_pref_file(PlayerType *player_ptr, char *buf);
 void add_history_from_pref_line(concptr t);
