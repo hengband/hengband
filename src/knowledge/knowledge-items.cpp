0e76561e9188c29ce51ca85aafbb3fa92e2bb11f
Hourier <66951241+Hourier@users.noreply.github.com>
2024-05-18 23:29:54 +0900

    [Refactor] #4104 使わなくなったsort.h のインクルードを消した

--- src/knowledge/knowledge-items.cpp
+++ src/knowledge/knowledge-items.cpp
@@ -28,7 +28,6 @@
 #include "term/term-color-types.h"
 #include "util/angband-files.h"
 #include "util/int-char-converter.h"
-#include "util/sort.h"
 #include "view/display-messages.h"
 #include "world/world.h"
 #include <numeric>
