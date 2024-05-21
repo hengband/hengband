0a5d491e1c46bbb333982f22a6b4f14e0f4e69b5
Hourier <66951241+Hourier@users.noreply.github.com>
2024-05-21 22:18:16 +0900

    [Refactor] #4128 MonsraceList::order() を実装した

--- src/system/monster-race-info.cpp
+++ src/system/monster-race-info.cpp
@@ -295,6 +295,55 @@ int MonraceList::calc_capture_value(const MonsterRaceId r_idx) const
     return (*this)[r_idx].level * 50 + 1000;
 }
 
+bool MonraceList::order(MonsterRaceId id1, MonsterRaceId id2, bool is_detailed) const
+{
+    const auto &monrace1 = monraces_info[id1];
+    const auto &monrace2 = monraces_info[id2];
+    if (is_detailed) {
+        const auto pkills1 = monrace1.r_pkills;
+        const auto pkills2 = monrace2.r_pkills;
+        if (pkills1 < pkills2) {
+            return true;
+        }
+
+        if (pkills1 > pkills2) {
+            return false;
+        }
+
+        const auto tkills1 = monrace1.r_tkills;
+        const auto tkills2 = monrace2.r_tkills;
+        if (tkills1 < tkills2) {
+            return true;
+        }
+
+        if (tkills1 > tkills2) {
+            return false;
+        }
+    }
+
+    const auto level1 = monrace1.level;
+    const auto level2 = monrace2.level;
+    if (level1 < level2) {
+        return true;
+    }
+
+    if (level1 > level2) {
+        return false;
+    }
+
+    const auto exp1 = monrace1.mexp;
+    const auto exp2 = monrace2.mexp;
+    if (exp1 < exp2) {
+        return true;
+    }
+
+    if (exp1 > exp2) {
+        return false;
+    }
+
+    return id1 <= id2;
+}
+
 void MonraceList::reset_all_visuals()
 {
     for (auto &[_, monrace] : monraces_info) {
--- src/system/monster-race-info.h
+++ src/system/monster-race-info.h
@@ -168,6 +168,7 @@ public:
     bool can_select_separate(const MonsterRaceId r_idx, const int hp, const int maxhp) const;
     int calc_figurine_value(const MonsterRaceId r_idx) const;
     int calc_capture_value(const MonsterRaceId r_idx) const;
+    bool order(MonsterRaceId id1, MonsterRaceId id2, bool is_detailed = false) const;
 
     void reset_all_visuals();
 
