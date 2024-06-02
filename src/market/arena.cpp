d04e571952c260fa43dc2c22df6815d9deeeb40d
Hourier <66951241+Hourier@users.noreply.github.com>
2024-06-02 14:30:00 +0900

    [Refactor] #4193 MonsterRace::calc_power() をMonsterRaceInfo に移した

--- src/market/arena.cpp
+++ src/market/arena.cpp
@@ -252,7 +252,7 @@ void update_gambling_monsters(PlayerType *player_ptr)
         }
 
         std::transform(std::begin(battle_mon_list), std::end(battle_mon_list), std::begin(power),
-            [](MonsterRace r_idx) { return MonsterRace(r_idx).calc_power(); });
+            [&monraces](auto monrace_id) { return monraces.get_monrace(monrace_id).calc_power(); });
         total += std::reduce(std::begin(power), std::end(power));
 
         for (i = 0; i < 4; i++) {
--- src/monster-race/monster-race.cpp
+++ src/monster-race/monster-race.cpp
@@ -1,7 +1,6 @@
 #include "monster-race/monster-race.h"
 #include "monster-race/race-flags-resistance.h"
 #include "monster-race/race-indice-types.h"
-#include "monster-race/race-resistance-mask.h"
 #include "system/monster-race-info.h"
 #include "world/world.h"
 #include <algorithm>
@@ -13,51 +12,4 @@ std::map<MonsterRaceId, MonsterRaceInfo> monraces_info;
 MonsterRace::MonsterRace(MonsterRaceId r_idx)
     : r_idx(r_idx)
 {
-}
-
-/*!
- * @brief モンスター種族の総合的な強さを計算する。
- * @details 現在はモンスター闘技場でのモンスターの強さの総合的な評価にのみ使用されている。
- * @return 計算した結果のモンスター種族の総合的な強さの値を返す。
- */
-int MonsterRace::calc_power() const
-{
-    int ret = 0;
-    const auto *r_ptr = &monraces_info[this->r_idx];
-    auto num_resistances = EnumClassFlagGroup<MonsterResistanceType>(r_ptr->resistance_flags & RFR_EFF_IMMUNE_ELEMENT_MASK).count();
-
-    if (r_ptr->misc_flags.has(MonsterMiscType::FORCE_MAXHP)) {
-        ret = r_ptr->hdice * r_ptr->hside * 2;
-    } else {
-        ret = r_ptr->hdice * (r_ptr->hside + 1);
-    }
-    ret = ret * (100 + r_ptr->level) / 100;
-    if (r_ptr->speed > STANDARD_SPEED) {
-        ret = ret * (r_ptr->speed * 2 - 110) / 100;
-    }
-    if (r_ptr->speed < STANDARD_SPEED) {
-        ret = ret * (r_ptr->speed - 20) / 100;
-    }
-    if (num_resistances > 2) {
-        ret = ret * (num_resistances * 2 + 5) / 10;
-    } else if (r_ptr->ability_flags.has(MonsterAbilityType::INVULNER)) {
-        ret = ret * 4 / 3;
-    } else if (r_ptr->ability_flags.has(MonsterAbilityType::HEAL)) {
-        ret = ret * 4 / 3;
-    } else if (r_ptr->ability_flags.has(MonsterAbilityType::DRAIN_MANA)) {
-        ret = ret * 11 / 10;
-    }
-    if (r_ptr->behavior_flags.has(MonsterBehaviorType::RAND_MOVE_25)) {
-        ret = ret * 9 / 10;
-    }
-    if (r_ptr->behavior_flags.has(MonsterBehaviorType::RAND_MOVE_50)) {
-        ret = ret * 9 / 10;
-    }
-    if (r_ptr->resistance_flags.has(MonsterResistanceType::RESIST_ALL)) {
-        ret *= 100000;
-    }
-    if (r_ptr->arena_ratio) {
-        ret = ret * r_ptr->arena_ratio / 100;
-    }
-    return ret;
 }
