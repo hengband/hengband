895ee3f3039e02f0a4386670135881cb174febc6
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2024-06-09 16:50:58 +0900

    [Refactor] カメレオン捕獲時の変身解除処理を分離した

--- src/effect/effect-monster-charm.cpp
+++ src/effect/effect-monster-charm.cpp
@@ -411,7 +411,7 @@ static int calcutate_capturable_hp(PlayerType *player_ptr, MonsterEntity *m_ptr,
 static void effect_monster_captured(PlayerType *player_ptr, EffectMonster *em_ptr, std::optional<CapturedMonsterType *> tmp_cap_mon_ptr)
 {
     if (em_ptr->m_ptr->mflag2.has(MonsterConstantFlagType::CHAMELEON)) {
-        choose_new_monster(player_ptr, em_ptr->g_ptr->m_idx, false, MonsterRaceId::CHAMELEON);
+        em_ptr->m_ptr->reset_chameleon_polymorph();
     }
 
     msg_format(_("%sを捕えた！", "You capture %s^!"), em_ptr->m_name);
