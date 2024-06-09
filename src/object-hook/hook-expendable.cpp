hengband/hengband

commit 837179e4d1a735595e246830084e1bca9db2527b

[Refactor] #4222 pval にモンスター種族IDを入れている箇所のモンスター種族定義取得処理をget_monrace() でカプセル化した
この過程でMonraceList::size() が必要になったので実装した

src/object-hook/hook-expendable.cpp
@@ -35,8 +35,8 @@ bool item_tester_hook_eatable(PlayerType *player_ptr, const ItemEntity *o_ptr)
             return true;
         }
     } else if (food_type == PlayerRaceFoodType::CORPSE) {
-        auto corpse_r_idx = i2enum<MonsterRaceId>(o_ptr->pval);
-        if (o_ptr->is_corpse() && angband_strchr("pht", monraces_info[corpse_r_idx].symbol_definition.character)) {
+        const auto &monrace = o_ptr->get_monrace();
+        if (o_ptr->is_corpse() && angband_strchr("pht", monrace.symbol_definition.character)) {
             return true;
         }
     }

src/autopick/autopick-destroyer.cpp
@@ -48,8 +48,7 @@ static bool is_leave_special_item(PlayerType *player_ptr, ItemEntity *o_ptr)
     const auto &bi_key = o_ptr->bi_key;
     const auto tval = bi_key.tval();
     if (PlayerRace(player_ptr).equals(PlayerRaceType::BALROG)) {
-        const auto r_idx = i2enum<MonsterRaceId>(o_ptr->pval);
-        if (o_ptr->is_corpse() && angband_strchr("pht", monraces_info[r_idx].symbol_definition.character)) {
+        if (o_ptr->is_corpse() && angband_strchr("pht", o_ptr->get_monrace().symbol_definition.character)) {
             return false;
         }
     } else if (pc.equals(PlayerClassType::ARCHER)) {

src/autopick/autopick-entry.cpp
@@ -433,15 +433,18 @@ void autopick_entry_from_object(PlayerType *player_ptr, autopick_type *entry, co
         entry->add(FLG_WANTED);
     }
 
-    const auto r_idx = i2enum<MonsterRaceId>(o_ptr->pval);
     const auto &bi_key = o_ptr->bi_key;
     const auto tval = bi_key.tval();
-    if ((tval == ItemKindType::CORPSE || tval == ItemKindType::STATUE) && monraces_info[r_idx].kind_flags.has(MonsterKindType::UNIQUE)) {
-        entry->add(FLG_UNIQUE);
+    if ((tval == ItemKindType::CORPSE) || (tval == ItemKindType::STATUE)) {
+        if (o_ptr->get_monrace().kind_flags.has(MonsterKindType::UNIQUE)) {
+            entry->add(FLG_UNIQUE);
+        }
     }
 
-    if (tval == ItemKindType::CORPSE && angband_strchr("pht", monraces_info[r_idx].symbol_definition.character)) {
-        entry->add(FLG_HUMAN);
+    if (tval == ItemKindType::CORPSE) {
+        if (angband_strchr("pht", o_ptr->get_monrace().symbol_definition.character)) {
+            entry->add(FLG_HUMAN);
+        }
     }
 
     if (o_ptr->is_spell_book() && !check_book_realm(player_ptr, bi_key)) {

src/autopick/autopick-matcher.cpp
@@ -296,13 +296,18 @@ bool is_autopick_match(PlayerType *player_ptr, const ItemEntity *o_ptr, const au
     const auto &bi_key = o_ptr->bi_key;
     const auto tval = bi_key.tval();
     const auto sval = *bi_key.sval();
-    const auto r_idx = i2enum<MonsterRaceId>(o_ptr->pval);
-    if (entry.has(FLG_UNIQUE) && ((tval != ItemKindType::CORPSE && tval != ItemKindType::STATUE) || monraces_info[r_idx].kind_flags.has_not(MonsterKindType::UNIQUE))) {
-        return false;
+    if (entry.has(FLG_UNIQUE) && o_ptr->has_monrace()) {
+        const auto &monrace = o_ptr->get_monrace();
+        if (((tval != ItemKindType::CORPSE && tval != ItemKindType::STATUE) || monrace.kind_flags.has_not(MonsterKindType::UNIQUE))) {
+            return false;
+        }
     }
 
-    if (entry.has(FLG_HUMAN) && (tval != ItemKindType::CORPSE || !angband_strchr("pht", monraces_info[r_idx].symbol_definition.character))) {
-        return false;
+    if (entry.has(FLG_HUMAN) && o_ptr->has_monrace()) {
+        const auto &monrace = o_ptr->get_monrace();
+        if ((tval != ItemKindType::CORPSE || !angband_strchr("pht", monrace.symbol_definition.character))) {
+            return false;
+        }
     }
 
     if (entry.has(FLG_UNREADABLE) && check_book_realm(player_ptr, bi_key)) {

src/cmd-item/cmd-eat.cpp
@@ -281,16 +281,16 @@ void exe_eat_food(PlayerType *player_ptr, INVENTORY_IDX i_idx)
     auto food_type = PlayerRace(player_ptr).food();
 
     /* Balrogs change humanoid corpses to energy */
-    const auto corpse_r_idx = i2enum<MonsterRaceId>(o_ptr->pval);
-    const auto search = angband_strchr("pht", monraces_info[corpse_r_idx].symbol_definition.character);
-    if (food_type == PlayerRaceFoodType::CORPSE && o_ptr->is_corpse() && (search != nullptr)) {
-        const auto item_name = describe_flavor(player_ptr, o_ptr, (OD_OMIT_PREFIX | OD_NAME_ONLY));
-        msg_format(_("%sは燃え上り灰になった。精力を吸収した気がする。", "%s^ is burnt to ashes.  You absorb its vitality!"), item_name.data());
-        (void)set_food(player_ptr, PY_FOOD_MAX - 1);
+    if (food_type == PlayerRaceFoodType::CORPSE && o_ptr->is_corpse()) {
+        if (angband_strchr("pht", o_ptr->get_monrace().symbol_definition.character) != nullptr) {
+            const auto item_name = describe_flavor(player_ptr, o_ptr, (OD_OMIT_PREFIX | OD_NAME_ONLY));
+            msg_format(_("%sは燃え上り灰になった。精力を吸収した気がする。", "%s^ is burnt to ashes.  You absorb its vitality!"), item_name.data());
+            (void)set_food(player_ptr, PY_FOOD_MAX - 1);
 
-        rfu.set_flags(flags_srf);
-        vary_item(player_ptr, i_idx, -1);
-        return;
+            rfu.set_flags(flags_srf);
+            vary_item(player_ptr, i_idx, -1);
+            return;
+        }
     }
 
     if (PlayerRace(player_ptr).equals(PlayerRaceType::SKELETON)) {

src/effect/effect-item.cpp
@@ -19,6 +19,7 @@
 #include "system/grid-type-definition.h"
 #include "system/item-entity.h"
 #include "system/monster-entity.h"
+#include "system/monster-race-info.h"
 #include "system/player-type-definition.h"
 #include "util/bit-flags-calculator.h"
 #include "view/display-messages.h"
@@ -233,7 +234,7 @@ bool affect_item(PlayerType *player_ptr, MONSTER_IDX src_idx, POSITION r, POSITI
             }
 
             for (int i = 0; i < o_ptr->number; i++) {
-                auto corpse_r_idx = i2enum<MonsterRaceId>(o_ptr->pval);
+                const auto &monrace = o_ptr->get_monrace();
                 const auto sval = *o_ptr->bi_key.sval();
                 if (((sval == SV_CORPSE) && (randint1(100) > 80)) || ((sval == SV_SKELETON) && (randint1(100) > 60))) {
                     if (!note_kill) {
@@ -241,7 +242,7 @@ bool affect_item(PlayerType *player_ptr, MONSTER_IDX src_idx, POSITION r, POSITI
                     }
 
                     continue;
-                } else if (summon_named_creature(player_ptr, src_idx, y, x, corpse_r_idx, mode)) {
+                } else if (summon_named_creature(player_ptr, src_idx, y, x, monrace.idx, mode)) {
                     note_kill = _("生き返った。", " revived.");
                 } else if (!note_kill) {
                     note_kill = _("灰になった。", (plural ? " become dust." : " becomes dust."));

src/flavor/named-item-describer.cpp
@@ -248,10 +248,9 @@ static std::string describe_item_count_or_article_en(const ItemEntity &item, con
         return prefix;
     }
 
-    const auto corpse_r_idx = i2enum<MonsterRaceId>(item.pval);
-    auto is_unique_corpse = item.bi_key.tval() == ItemKindType::CORPSE;
-    is_unique_corpse &= monraces_info[corpse_r_idx].kind_flags.has(MonsterKindType::UNIQUE);
-    if ((opt.known && item.is_fixed_or_random_artifact()) || is_unique_corpse) {
+    auto is_unique_item = opt.known && item.is_fixed_or_random_artifact();
+    is_unique_item |= (item.bi_key.tval() == ItemKindType::CORPSE) && item.get_monrace().kind_flags.has(MonsterKindType::UNIQUE);
+    if (is_unique_item) {
         return "The ";
     }
 

src/flavor/tval-description-switcher.cpp
@@ -23,8 +23,7 @@
 static std::pair<std::string, std::string> describe_monster_ball(const ItemEntity &item, const describe_option_type &opt)
 {
     const auto &basename = item.get_baseitem().name;
-    const auto monrace_id = i2enum<MonsterRaceId>(item.pval);
-    const auto &monrace = monraces_info[monrace_id];
+    const auto &monrace = item.get_monrace();
     if (!opt.known) {
         return { basename, "" };
     }
@@ -49,8 +48,7 @@ static std::pair<std::string, std::string> describe_monster_ball(const ItemEntit
 static std::pair<std::string, std::string> describe_statue(const ItemEntity &item)
 {
     const auto &basename = item.get_baseitem().name;
-    const auto monrace_id = i2enum<MonsterRaceId>(item.pval);
-    const auto &monrace = monraces_info[monrace_id];
+    const auto &monrace = item.get_monrace();
 #ifdef JP
     const auto &modstr = monrace.name;
 #else
@@ -66,8 +64,7 @@ static std::pair<std::string, std::string> describe_statue(const ItemEntity &ite
 
 static std::pair<std::string, std::string> describe_corpse(const ItemEntity &item)
 {
-    const auto monrace_id = i2enum<MonsterRaceId>(item.pval);
-    const auto &monrace = monraces_info[monrace_id];
+    const auto &monrace = item.get_monrace();
 #ifdef JP
     const auto basename = "#%";
 #else

commit a41274dbe39b299e65bf831bcfd431e864ff9a00

Prevent unhandled exception when corpse-eating player tries to eat
That is a regression introduced by 837179e4d1a735595e246830084e1bca9db2527b.  Resolves https://github.com/hengband/hengband/issues/4237 .

src/object-hook/hook-expendable.cpp
@@ -35,9 +35,11 @@ bool item_tester_hook_eatable(PlayerType *player_ptr, const ItemEntity *o_ptr)
             return true;
         }
     } else if (food_type == PlayerRaceFoodType::CORPSE) {
-        const auto &monrace = o_ptr->get_monrace();
-        if (o_ptr->is_corpse() && angband_strchr("pht", monrace.symbol_definition.character)) {
-            return true;
+        if (o_ptr->is_corpse()) {
+            const auto &monrace = o_ptr->get_monrace();
+            if (angband_strchr("pht", monrace.symbol_definition.character)) {
+                return true;
+            }
         }
     }
 