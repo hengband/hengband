ed1fe3f38c5e3551b7dc72851c4c3782b6fb470d
Habu <habu1010+github@gmail.com>
2024-06-12 19:33:12 +0900

    [Fix] check-bom.sh が機能していない
    文法ミスにより正しくBOMチェックができていなかった。修正するとともに、
    チェックをすり抜けていたBOMありファイルからBOMを削除する。

--- .github/scripts/check-bom.sh
+++ .github/scripts/check-bom.sh
@@ -6,7 +6,7 @@ STATUS=0
 
 for file in $SRC_FILES; do
     file $file | grep "BOM" >/dev/null
-    if [ $? == 0 ]; then
+    if [ $? -eq 0 ]; then
         echo "$file: BOM exists."
         STATUS=1
     fi
