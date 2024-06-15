458e66a2717b5301b388df52f4da4c780e40070d
Slimebreath6078 <slimebreath6078@yahoo.co.jp>
2024-06-15 18:17:55 +0900

    [Refactor] get_mspeed() をset_individual_speed() のセッターに変えてMonsterEntity のメソッドとして繰り込んだ

--- src/monster/monster-list.h
+++ src/monster/monster-list.h
@@ -11,5 +11,4 @@ MONSTER_IDX m_pop(FloorType *floor_ptr);
 
 MonsterRaceId get_mon_num(PlayerType *player_ptr, DEPTH min_level, DEPTH max_level, BIT_FLAGS mode);
 void choose_new_monster(PlayerType *player_ptr, MONSTER_IDX m_idx, bool born, MonsterRaceId r_idx, std::optional<MONSTER_IDX> summoner_m_idx = std::nullopt);
-byte get_mspeed(FloorType *player_ptr, MonsterRaceInfo *r_ptr);
 int get_monster_crowd_number(FloorType *floor_ptr, MONSTER_IDX m_idx);
