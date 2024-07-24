0aeade246e9643d3b39e3832e9da0503ec95c000
dis <dis.rogue@gmail.com>
2024-07-24 18:25:18 +0900

    [Feature] 悪魔領域に深淵属性の攻撃魔法を追加
    最終攻撃魔法の合わせて深淵属性の攻撃魔法を一つ追加する。
    役割の薄かった悪魔火と差替。
    
    堕落:深淵属性のボルト。威力はplev*3。消費はファイアボールの2倍弱。

--- src/realm/realm-demon.cpp
+++ src/realm/realm-demon.cpp
@@ -224,7 +224,7 @@ std::optional<std::string> do_daemon_spell(PlayerType *player_ptr, SPELL_IDX spe
     } break;
 
     case 13: {
-        int dam = plev * 5;
+        int dam = plev * 3;
 
         if (info) {
             return info_damage(dam);
@@ -234,7 +234,7 @@ std::optional<std::string> do_daemon_spell(PlayerType *player_ptr, SPELL_IDX spe
             if (!get_aim_dir(player_ptr, &dir)) {
                 return std::nullopt;
             }
-            fire_bolt(player_ptr, AttributeType::FIRE, dir, dam);
+            fire_bolt(player_ptr, AttributeType::ABYSS, dir, dam);
         }
     } break;
 
