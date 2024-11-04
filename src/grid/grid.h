be98ea731f260a4ef558e439bbaa5c68897f01c6
Hourier <66951241+Hourier@users.noreply.github.com>
2024-11-04 20:05:57 +0900

    [Refactor] #4572 is_open() をdungeon_type::is_open() に差し替えた

--- src/grid/grid.h
+++ src/grid/grid.h
@@ -84,7 +84,6 @@ void note_spot(PlayerType *player_ptr, POSITION y, POSITION x);
 void lite_spot(PlayerType *player_ptr, POSITION y, POSITION x);
 void update_flow(PlayerType *player_ptr);
 void cave_alter_feat(PlayerType *player_ptr, POSITION y, POSITION x, TerrainCharacteristics action);
-bool is_open(PlayerType *player_ptr, FEAT_IDX feat);
 bool check_local_illumination(PlayerType *player_ptr, POSITION y, POSITION x);
 bool cave_monster_teleportable_bold(PlayerType *player_ptr, MONSTER_IDX m_idx, POSITION y, POSITION x, teleport_flags mode);
 bool cave_player_teleportable_bold(PlayerType *player_ptr, POSITION y, POSITION x, teleport_flags mode);
