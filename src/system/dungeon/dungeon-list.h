11b3773d7fb29e8a3ea432909a134f6fee56de72
Hourier <66951241+Hourier@users.noreply.github.com>
2024-12-12 20:09:45 +0900

    [Refactor] DungeonList クラスを作った

--- src/system/dungeon/dungeon-list.h
+++ src/system/dungeon/dungeon-list.h
@@ -6,7 +6,38 @@
 
 #pragma once
 
+#include <map>
 #include <vector>
 
 class DungeonDefinition;
 extern std::vector<DungeonDefinition> dungeons_info;
+
+class DungeonList {
+public:
+    DungeonList(DungeonList &&) = delete;
+    DungeonList(const DungeonList &) = delete;
+    DungeonList &operator=(const DungeonList &) = delete;
+    DungeonList &operator=(DungeonList &&) = delete;
+    ~DungeonList() = default;
+
+    static DungeonList &get_instance();
+    DungeonDefinition &get_dungeon(int dungeon_id);
+    const DungeonDefinition &get_dungeon(int dungeon_id) const;
+    std::map<int, DungeonDefinition>::iterator begin();
+    std::map<int, DungeonDefinition>::const_iterator begin() const;
+    std::map<int, DungeonDefinition>::iterator end();
+    std::map<int, DungeonDefinition>::const_iterator end() const;
+    std::map<int, DungeonDefinition>::reverse_iterator rbegin();
+    std::map<int, DungeonDefinition>::const_reverse_iterator rbegin() const;
+    std::map<int, DungeonDefinition>::reverse_iterator rend();
+    std::map<int, DungeonDefinition>::const_reverse_iterator rend() const;
+    size_t size() const;
+    bool empty() const;
+    void emplace(int dungeon_id, DungeonDefinition &&definition);
+
+private:
+    DungeonList() = default;
+    static DungeonList instance;
+
+    std::map<int, DungeonDefinition> dungeons;
+};
