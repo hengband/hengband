59c21c10556c62da6434279da384dedf15818db4
Hourier <66951241+Hourier@users.noreply.github.com>
2025-01-24 23:35:45 +0900

    [Refactor] 1箇所しか使われていない定数宣言を使用箇所へ移し、不要になったコメントなどを消した

--- src/room/door-definition.h
+++ src/room/door-definition.h
@@ -3,20 +3,12 @@
 #include <map>
 #include <vector>
 
-#define DUN_ROOMS_MAX 40 /*!< 部屋生成処理の基本比率(ダンジョンのサイズに比例する) / Max number rate of rooms */
-
 enum class DoorKind {
     DOOR = 0,
     GLASS_DOOR = 1,
     CURTAIN = 2,
 };
 
-enum class LockJam {
-    LOCKED,
-    JAMMED,
-};
-
-/* A structure type for doors */
 enum class TerrainTag;
 class Door {
 public:
