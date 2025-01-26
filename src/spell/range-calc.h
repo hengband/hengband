f47c29e608b13f67e313c0c789da3d54d4246577
Habu <habu1010+github@gmail.com>
2025-01-26 13:00:35 +0900

    [Refactor] dist_to_line()のシグネチャ
    dist_to_line()のシグネチャをPos2D型で書き替える。

--- src/spell/range-calc.h
+++ src/spell/range-calc.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "effect/attribute-types.h"
-#include "system/angband.h"
 #include "util/point-2d.h"
 #include <span>
 #include <utility>
@@ -10,7 +8,8 @@
 class FloorType;
 class PlayerType;
 class ProjectionPath;
+enum class AttributeType;
 bool in_disintegration_range(const FloorType &floor, const Pos2D &pos1, const Pos2D &pos2);
 std::vector<std::pair<int, Pos2D>> breath_shape(PlayerType *player_ptr, const ProjectionPath &path, int dist, int rad, const Pos2D &pos_source, const Pos2D &pos_target, AttributeType typ);
 std::vector<std::pair<int, Pos2D>> ball_shape(PlayerType *player_ptr, const Pos2D &center, int rad, AttributeType typ);
-POSITION dist_to_line(POSITION y, POSITION x, POSITION y1, POSITION x1, POSITION y2, POSITION x2);
+int dist_to_line(const Pos2D &pos, const Pos2D &pos_line1, const Pos2D &pos_line2);
