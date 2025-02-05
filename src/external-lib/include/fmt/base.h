451438bc43c83ceafb9e28dc16157564e95d33be
Habu <habu1010+github@gmail.com>
2025-02-05 20:21:24 +0900

    [Fix] utf-8に関するstatic_assert対策
    ソースコードがUTF-8の場合場合、コマンドラインオプションに /utf-8 を
    設定するようstatic_assertが出るが、変愚蛮怒では /source-charset:utf-8 と
    /execution-charset:shift-jis を指定しており、このオプションとは両立
    できない。
    このstatic_assertを無視して実際にfmtライブラリを使用しても特に問題なく
    日本語を処理できているので、コメントアウトしstatic_assertにひっかからない
    ようにしておく。

--- src/external-lib/include/fmt/base.h
+++ src/external-lib/include/fmt/base.h
@@ -464,8 +464,10 @@ enum { use_utf8 = !FMT_WIN32 || is_utf8_enabled };
 #  define FMT_UNICODE 1
 #endif
 
+#if !defined(_MSC_VER)
 static_assert(!FMT_UNICODE || use_utf8,
               "Unicode support requires compiling with /utf-8");
+#endif
 
 template <typename T> constexpr const char* narrow(const T*) { return nullptr; }
 constexpr FMT_ALWAYS_INLINE const char* narrow(const char* s) { return s; }
