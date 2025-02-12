d19b2b3cd5b80e6663367ecf088b5da373e86f92
Hourier <66951241+Hourier@users.noreply.github.com>
2025-02-12 22:38:41 +0900

    [Refactor] object-allocator.cpp でin_bounds() の周辺をPos2D に変えた

--- src/floor/object-allocator.cpp
+++ src/floor/object-allocator.cpp
@@ -20,48 +20,35 @@
 #include "wizard/wizard-messages.h"
 
 /*!
- * @brief 上下左右の外壁数をカウントする / Count the number of walls adjacent to the given grid.
- * @param y 基準のy座標
- * @param x 基準のx座標
+ * @brief 上下左右の外壁数をカウントする
+ * @param pos 基準座標
  * @return 隣接する外壁の数
- * @note Assumes "in_bounds()"
- * @details We count only granite walls and permanent walls.
  */
-static int next_to_walls(const FloorType &floor, POSITION y, POSITION x)
+static int next_to_walls(const FloorType &floor, const Pos2D &pos)
 {
-    int k = 0;
-    if (in_bounds(floor, y + 1, x) && floor.grid_array[y + 1][x].is_extra()) {
-        k++;
-    }
-
-    if (in_bounds(floor, y - 1, x) && floor.grid_array[y - 1][x].is_extra()) {
-        k++;
-    }
-
-    if (in_bounds(floor, y, x + 1) && floor.grid_array[y][x + 1].is_extra()) {
-        k++;
-    }
-
-    if (in_bounds(floor, y, x - 1) && floor.grid_array[y][x - 1].is_extra()) {
-        k++;
+    auto k = 0;
+    for (const auto &d : Direction::directions_4()) {
+        const auto pos_neighbor = pos + d.vec();
+        if (in_bounds(floor, pos_neighbor.y, pos_neighbor.x) && floor.get_grid(pos_neighbor).is_extra()) {
+            k++;
+        }
     }
 
     return k;
 }
 
 /*!
- * @brief alloc_stairs()の補助として指定の位置に階段を生成できるかの判定を行う / Helper function for alloc_stairs(). Is this a good location for stairs?
+ * @brief alloc_stairs()の補助として指定の位置に階段を生成できるかの判定を行う
  * @param player_ptr プレイヤーへの参照ポインタ
- * @param y 基準のy座標
- * @param x 基準のx座標
+ * @param pos 基準座標
  * @param walls 最低減隣接させたい外壁の数
  * @return 階段を生成して問題がないならばTRUEを返す。
  */
-static bool alloc_stairs_aux(PlayerType *player_ptr, POSITION y, POSITION x, int walls)
+static bool alloc_stairs_aux(PlayerType *player_ptr, const Pos2D &pos, int walls)
 {
     const auto &floor = *player_ptr->current_floor_ptr;
-    const auto &grid = floor.grid_array[y][x];
-    if (!grid.is_floor() || pattern_tile(floor, y, x) || !grid.o_idx_list.empty() || grid.has_monster() || next_to_walls(floor, y, x) < walls) {
+    const auto &grid = floor.get_grid(pos);
+    if (!grid.is_floor() || pattern_tile(floor, pos.y, pos.x) || !grid.o_idx_list.empty() || grid.has_monster() || next_to_walls(floor, pos) < walls) {
         return false;
     }
 
@@ -111,13 +98,13 @@ bool alloc_stairs(PlayerType *player_ptr, FEAT_IDX feat, int num, int walls)
         return false;
     }
 
-    for (int i = 0; i < num; i++) {
+    for (auto i = 0; i < num; i++) {
         while (true) {
-            int candidates = 0;
-            const POSITION max_x = floor.width - 1;
-            for (POSITION y = 1; y < floor.height - 1; y++) {
-                for (POSITION x = 1; x < max_x; x++) {
-                    if (alloc_stairs_aux(player_ptr, y, x, walls)) {
+            auto candidates = 0;
+            const auto max_x = floor.width - 1;
+            for (auto y = 1; y < floor.height - 1; y++) {
+                for (auto x = 1; x < max_x; x++) {
+                    if (alloc_stairs_aux(player_ptr, { y, x }, walls)) {
                         candidates++;
                     }
                 }
@@ -132,12 +119,12 @@ bool alloc_stairs(PlayerType *player_ptr, FEAT_IDX feat, int num, int walls)
                 continue;
             }
 
-            int pick = randint1(candidates);
-            POSITION y;
-            POSITION x = max_x;
+            auto pick = randint1(candidates);
+            int y;
+            auto x = max_x;
             for (y = 1; y < floor.height - 1; y++) {
                 for (x = 1; x < floor.width - 1; x++) {
-                    if (alloc_stairs_aux(player_ptr, y, x, walls)) {
+                    if (alloc_stairs_aux(player_ptr, { y, x }, walls)) {
                         pick--;
                         if (pick == 0) {
                             break;
