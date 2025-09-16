c83683c6d774623522c27a5bc38cac3d922954b0
Hourier <66951241+Hourier@users.noreply.github.com>
2025-09-16 23:34:42 +0900

    [Refactor] #5228 scan_floor_items()の引数をconst参照に変えた

--- src/floor/object-scanner.h
+++ src/floor/object-scanner.h
@@ -19,5 +19,5 @@ enum class ScanFloorMode {
 class FloorType;
 class PlayerType;
 class ItemTester;
-std::vector<short> scan_floor_items(const FloorType &floor, const Pos2D &pos, EnumClassFlagGroup<ScanFloorMode> mode, const ItemTester &item_tester);
+std::vector<short> scan_floor_items(const FloorType &floor, const Pos2D &pos, const EnumClassFlagGroup<ScanFloorMode> &mode, const ItemTester &item_tester);
 COMMAND_CODE show_floor_items(PlayerType *player_ptr, int target_item, POSITION y, POSITION x, TERM_LEN *min_width, const ItemTester &item_tester);
