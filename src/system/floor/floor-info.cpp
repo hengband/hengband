72fc1757007c6e9500949f7783fd4727bc8ce95f
Hourier <66951241+Hourier@users.noreply.github.com>
2025-09-26 23:03:27 +0900

    [Refactor] 外から与えられた照明/視界のメソッド入力に範囲チェックを追加した

--- src/system/floor/floor-info.cpp
+++ src/system/floor/floor-info.cpp
@@ -703,6 +703,10 @@ void FloorType::reset_lite_area()
 
 void FloorType::set_lite_at(const Pos2D &pos)
 {
+    if (!this->contains(pos)) {
+        return;
+    }
+
     auto &grid = this->get_grid(pos);
     if (grid.is_lite() || (this->lite_n >= LITE_MAX)) {
         return;
@@ -715,6 +719,10 @@ void FloorType::set_lite_at(const Pos2D &pos)
 
 void FloorType::set_redraw_at(const Pos2D &pos)
 {
+    if (!this->contains(pos)) {
+        return;
+    }
+
     auto &grid = this->get_grid(pos);
     if (grid.is_redraw() || (this->redraw_n >= REDRAW_MAX)) {
         return;
@@ -727,6 +735,10 @@ void FloorType::set_redraw_at(const Pos2D &pos)
 
 void FloorType::set_view_at(const Pos2D &pos)
 {
+    if (!this->contains(pos)) {
+        return;
+    }
+
     auto &grid = this->get_grid(pos);
     if (grid.is_view() || (this->view_n >= VIEW_MAX)) {
         return;
@@ -752,12 +764,6 @@ void FloorType::set_view()
 
         this->set_note_and_redraw_at(pos);
     }
-}
-
-void FloorType::set_note_and_redraw_at(const Pos2D &pos)
-{
-    this->get_grid(pos).info |= CAVE_NOTE;
-    this->set_redraw_at(pos);
 }
 
 void FloorType::set_note_and_redraw()
@@ -978,4 +984,10 @@ int FloorType::decide_selection_count()
     }
 
     return count;
+}
+
+void FloorType::set_note_and_redraw_at(const Pos2D &pos)
+{
+    this->get_grid(pos).info |= CAVE_NOTE;
+    this->set_redraw_at(pos);
 }
--- src/system/floor/floor-info.h
+++ src/system/floor/floor-info.h
@@ -145,7 +145,6 @@ public:
     void set_redraw_at(const Pos2D &pos);
     void set_view_at(const Pos2D &pos);
     void set_view();
-    void set_note_and_redraw_at(const Pos2D &pos);
     void set_note_and_redraw();
     std::vector<Pos2D> reset_lite();
     std::vector<Pos2D> reset_view();
@@ -159,8 +158,6 @@ public:
     void place_trap_at(const Pos2D &pos);
 
 private:
-    static int decide_selection_count();
-
     bool entering_dungeon = false;
     bool leaving_dungeon = false;
 
@@ -179,4 +176,8 @@ private:
     short redraw_n = 0; //!< Array of grids for delayed visual updating
     std::array<int, REDRAW_MAX> redraw_y{};
     std::array<int, REDRAW_MAX> redraw_x{};
+
+    static int decide_selection_count();
+
+    void set_note_and_redraw_at(const Pos2D &pos);
 };
