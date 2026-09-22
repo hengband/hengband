d11d323188c9ba29a3c51e12eba043717a1a9165
Habu <habu1010+github@gmail.com>
2026-09-22 19:24:14 +0900

    [Refactor] 青魔法のブレス・ボール・ボルトの詠唱を1つの関数にまとめる
    cast_blue_magic_breath()・cast_blue_magic_ball()・cast_blue_magic_bolt() は、
    共通の表を引いた後で呼ぶ fire_breath()・fire_ball()・fire_bolt() が
    違うだけだった。これを cast_blue_magic_projection() にまとめ、放つ処理は
    ものまねと同じ fire_mspell_projection() を使う。switch_cast_blue_magic() は
    switch の前で共通の表を引いて呼ぶようにし、switch から 53 個の case を
    削除する。
    
    空になった blue-magic-breath.{h,cpp} と blue-magic-ball-bolt.{h,cpp} を
    削除する。

--- src/blue-magic/blue-magic-breath.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-/*!
- * @file blue-magic-breath.h
- * @brief 青魔法のブレス系呪文ヘッダ
- */
-
-struct bmc_type;
-class PlayerType;
-bool cast_blue_magic_breath(PlayerType *player_ptr, bmc_type *bmc_ptr);
